5aa9d1ef1c2b87e1f53a3b45b3a83a806006d310
子風 <zpsyhapcst@gmail.com>
2021-07-28 21:57:42 +0800

    Time: 4 ms (72.04%), Space: 2.2 MB (61.97%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.rs
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.rs
@@ -5,21 +5,17 @@ impl Solution {
         if s.len() < 2{
             return s.len() as i32
         }
-        //let s = String::from("abc");
              
         let (mut start, mut maxlen) = (0, 0);
         let mut record:HashMap<char, usize> = HashMap::new();
         
         for (i,c) in s.chars().enumerate(){
             if let Some(&loc) = record.get(&c){
-                println!("loc:{}",loc);
                 if loc >= start{
-                    println!("start:{}",start);
                     let len = i - start;
                                 
                     if len > maxlen{
                         maxlen = len;
-                        println!("{},{},{}", start, i, &s[start..i]);
                     }
                     
                     start = loc + 1
@@ -29,12 +25,10 @@ impl Solution {
 
             if len > maxlen{
                 maxlen = len;
-                println!("{},{},{}", start, i, &s[start..=i]);
             }
             
             record.insert(c,i);
         }
-        println!("==========================");
         
         return maxlen as i32
     }
