ec0e52bd04f1b6aab584ad4a3d90b4ce440a3f43
子風 <zpsyhapcst@gmail.com>
2021-08-02 20:58:47 +0800

    Time: 268 ms (5.08%), Space: 2 MB (55.93%) - LeetHub

--- regular-expression-matching/regular-expression-matching.rs
+++ regular-expression-matching/regular-expression-matching.rs
@@ -1,16 +1,35 @@
+use std::collections::HashMap;
+
 fn is_match(mut s:&[u8], mut p: &[u8]) -> bool{
+    let mut mem:HashMap<(usize,usize), bool>  = HashMap::new();
+    
     //println!("s:{:?}\np:{:?}",s,p);
     if p.len() == 0{
         return s.len() == 0;
     }
     
-    let first_match = s.len() > 0 && (s[0] == p[0] || p[0] == b'.');
-    if p.len() >= 2 && p[1] == b'*'{
-        is_match(s, &p[2..]) ||
-        first_match && is_match(&s[1..], &p[..])
-    } else {
-        first_match && is_match(&s[1..], &p[1..])
-    }
+    let mut dp = |i:usize,j:usize| -> bool {
+        if let Some(&ans) = mem.get(&(i,j)){
+            return ans;
+        }
+        
+        let ans:bool;
+        let s = &s[i..];
+        let p = &p[j..];
+        
+        let first_match = s.len() > 0 && (s[0] == p[0] || p[0] == b'.');
+        if p.len() >= 2 && p[1] == b'*'{
+            ans = is_match(s, &p[2..]) ||
+            first_match && is_match(&s[1..], &p[..]);
+        } else {
+            ans = first_match && is_match(&s[1..], &p[1..]);
+        }
+        
+        mem.insert((i,j), ans);
+        ans
+    };
+    
+    dp(0,0)
 }
 
 impl Solution {
