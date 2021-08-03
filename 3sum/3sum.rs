9098d058b72403ecaddf6cd2f10e7ace5ea9a3ad
子風 <zpsyhapcst@gmail.com>
2021-08-03 10:32:21 +0800

    Time: 20 ms (91.44%), Space: 3.3 MB (93.00%) - LeetHub

--- 3sum/3sum.rs
+++ 3sum/3sum.rs
@@ -2,25 +2,48 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 impl Solution {
-    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut ans = HashSet::new();
-        let mut hashmap = HashMap::new();
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut ans:Vec<Vec<i32>> = vec![];
         
-        for i in (0..nums.len()){
-            for j in (i+1..nums.len()){
-                let complement = - (nums[i] + nums[j]);
-                if let Some(&k) = hashmap.get(&complement){
-                    if i != j && i != k && j != k{
-                        let mut v = vec![nums[i],nums[j],nums[k]];
-                        v.sort();
-                        ans.insert(v);
-                    }
+        if nums.len() < 3 {
+            return ans;
+        }
+        nums.sort();
+        
+        let mut i=0;
+        for i in (0..nums.len()-2){
+            if i>0 && nums[i-1] == nums[i]{
+                continue;
+            }
+            
+            let mut j=i+1;
+            let mut k=nums.len()-1;
+            
+            while j < k{
+                let sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum < 0) {
+                    j+=1;
+                    continue;
+                } else if (sum > 0) {
+                    k-=1;
+                    continue;
                 }
-                hashmap.insert(nums[j],j);
+                
+                ans.push(vec![nums[i], nums[j], nums[k]]);
+                j+=1;
+                k-=1;
+                
+                while j < k && nums[j-1] == nums[j]{
+                    j+=1;
+                }
+                while j < k && nums[k+1] == nums[k]{
+                    k-=1;
+                }
             }
         }
         
-        ans.into_iter().collect()
+        ans
     }
 }
 
