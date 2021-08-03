dc3216afc7d383e44d221fa7face8bc5ec869d27
子風 <zpsyhapcst@gmail.com>
2021-08-03 16:05:31 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (26.79%) - LeetHub

--- 4sum/4sum.rs
+++ 4sum/4sum.rs
@@ -2,58 +2,60 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn four_sum(mut nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        if nums.len() < 4 {
-            return vec![];
-        }
         nums.sort();
-        
-        let mut ans = HashSet::new();
-        
-        for i in (0..nums.len()-3){
-            let mut temp = three_sum(&nums[i+1..], target-nums[i]);
-            for mut v in temp.iter_mut(){
-                v.push(nums[i]);
-                ans.insert(v.to_vec());
-            }
-        }
-        
-        ans.into_iter().collect()
+        k_sum(&nums[..], target, 4)
     }
 }
 
-fn three_sum(mut nums: &[i32], target: i32) -> Vec<Vec<i32>> {
+fn k_sum(mut nums: &[i32], target: i32, k:usize) -> Vec<Vec<i32>> {
+    if nums.len() < k || (nums[0] * k as i32) > target || (nums.last().unwrap() * k as i32) < target {
+        return vec![];
+    }
+    if k == 2{
+        return two_sum(&nums[..], target)
+    }
+    
     let mut ans:Vec<Vec<i32>> = vec![];
 
-    let mut i=0;
-    for i in (0..nums.len()-2){
-        if i>0 && nums[i-1] == nums[i]{
+    for i in (0..nums.len()-k+1){
+        if i != 0 && nums[i-1] == nums[i]{
+            continue;
+        }
+        for mut v in k_sum(&nums[i+1..], target-nums[i], k-1){
+            v.extend_from_slice(&nums[i..i+1]);
+            ans.push(v);
+        }
+    }
+
+    ans
+}
+
+fn two_sum(mut nums: &[i32], target: i32) -> Vec<Vec<i32>> {
+    let mut ans:Vec<Vec<i32>> = vec![];
+
+    let mut left=0;
+    let mut right=nums.len()-1;
+
+    while left < right{
+        let sum = nums[left] + nums[right];
+
+        if (sum < target) {
+            left+=1;
+            continue;
+        } else if (sum > target) {
+            right-=1;
             continue;
         }
 
-        let mut j=i+1;
-        let mut k=nums.len()-1;
+        ans.push(vec![nums[left], nums[right]]);
+        left+=1;
+        right-=1;
 
-        while j < k{
-            let sum = nums[i] + nums[j] + nums[k];
-
-            if (sum < target) {
-                j+=1;
-                continue;
-            } else if (sum > target) {
-                k-=1;
-                continue;
-            }
-
-            ans.push(vec![nums[i], nums[j], nums[k]]);
-            j+=1;
-            k-=1;
-
-            while j < k && nums[j-1] == nums[j]{
-                j+=1;
-            }
-            while j < k && nums[k+1] == nums[k]{
-                k-=1;
-            }
+        while left < right && nums[left-1] == nums[left]{
+            left+=1;
+        }
+        while left < right && nums[right+1] == nums[right]{
+            right-=1;
         }
     }
 
