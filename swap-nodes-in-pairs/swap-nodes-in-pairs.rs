442178ed4ebf6e4b8aa55d08f8891f823c49050b
子風 <zpsyhapcst@gmail.com>
2021-08-05 16:12:27 +0800

    Time: 0 ms (100.00%), Space: 2 MB (84.00%) - LeetHub

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.rs
@@ -0,0 +1,40 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn swap_pairs(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        swap_pairs(head)     
+    }
+}
+use std::mem;
+fn swap_pairs(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>>{
+    if head.is_none(){
+        return None;
+    }
+    
+    let next = head.as_mut().unwrap().next.take();
+    match (head, next){
+        (None, _) => return None,
+        (head,None) => return head,
+        (mut left,mut right) => {
+            let next = swap_pairs(right.as_mut().unwrap().next.take());
+            left.as_mut().unwrap().next = next;            
+            right.as_mut().unwrap().next = left;
+            
+            return right;
+        },
+    }
+}
