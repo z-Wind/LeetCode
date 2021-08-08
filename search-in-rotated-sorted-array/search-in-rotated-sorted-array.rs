e0001ab6aa7fb84a48ef045a8da294f4f2a7a5c7
子風 <zpsyhapcst@gmail.com>
2021-08-08 21:40:33 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (41.51%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.rs
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.rs
@@ -1,8 +1,30 @@
 impl Solution {
-    pub fn search(mut nums: Vec<i32>, target: i32) -> i32 {
-        match nums.iter().position(|&x| x == target) {
-            Some(i) => i as i32,
-            None => -1,
-        }
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        search(&nums, target, 0, nums.len()-1)
     }
+}
+
+pub fn search(nums: &Vec<i32>, target: i32, start: usize, end: usize) -> i32 {
+    let middle = (start + end)/2;
+    if nums[middle] == target{
+        return middle as i32;
+    } else if nums[start] == target{
+        return start as i32;
+    } else if nums[end] == target{
+        return end as i32;
+    } else if start >= end{
+        return -1;
+    }
+    
+    
+    //println!("{},{},{},{:?}",start,middle,end, &nums[start..=end]);
+    
+    let mut i = -1;
+    if nums[start] < target || nums[middle] > target{
+        i = search(&nums, target, start, middle);
+    }
+    if i == -1 && (nums[middle] < target || nums[end] > target){
+        i = search(&nums, target, middle+1, end);
+    }
+    return i;
 }
