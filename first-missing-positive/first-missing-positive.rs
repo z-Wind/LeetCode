c4b70f8b2501d3700e57a7b5f11dd1b6e7ac7d01
子風 <zpsyhapcst@gmail.com>
2021-08-13 16:18:16 +0800

    Time: 18 ms (20.34%), Space: 7.3 MB (45.76%) - LeetHub

--- first-missing-positive/first-missing-positive.rs
+++ first-missing-positive/first-missing-positive.rs
@@ -1,15 +1,26 @@
+const INF: i32 = i32::MAX;
+
 impl Solution {
-    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        let mut v:Vec<i32> = nums.into_iter().filter(|x|x.is_positive()).collect();
-        v.sort_unstable();
-        v.dedup();
-        //println!("{:?}",v);
-        for (i,&x) in v.iter().enumerate(){
-            let pos = i as i32 + 1;
-            if pos != x{
-                return pos;
+    pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
+        for i in 0..nums.len() {
+            if !(1 <= nums[i] && nums[i] <= nums.len() as i32) {
+                nums[i] = INF;
             }
         }
-        return v.len() as i32 +1;
+
+        for i in 0..nums.len() {
+            let index = nums[i].abs() - 1;
+            if index != INF - 1 {
+                nums[index as usize] = -nums[index as usize].abs();
+            }
+        }
+        //println!("{:?}",nums);
+        for i in 0..nums.len() {
+            if nums[i].is_positive() {
+                return i as i32 + 1;
+            }
+        }
+
+        nums.len() as i32 + 1
     }
 }
