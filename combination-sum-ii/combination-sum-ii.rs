9914320be5c999d35b76a1970384995f9b449a7f
子風 <zpsyhapcst@gmail.com>
2021-08-13 09:39:54 +0800

    Time: 3 ms (66.67%), Space: 2.1 MB (66.67%) - LeetHub

---
+++ combination-sum-ii/combination-sum-ii.rs
@@ -0,0 +1,44 @@
+impl Solution {
+    pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut csum = CSum::new(candidates, target);
+        csum.explore(vec![], 0, 0);
+        csum.ans
+    }
+}
+
+struct CSum{
+    candidates: Vec<i32>, 
+    target: i32,
+    ans: Vec<Vec<i32>>,
+}
+
+impl CSum{
+    fn new(mut candidates: Vec<i32>, target: i32) -> Self{
+        candidates.sort();
+        CSum{
+            candidates,
+            target,
+            ans:vec![],
+        }
+    }
+    fn explore (&mut self, mut v:Vec<i32>, i_start:usize, mut cur_sum:i32){
+        for i in (i_start..self.candidates.len()){
+            if i>i_start && self.candidates[i] == self.candidates[i-1]{
+                continue;
+            }
+            //println!("{}:{} {:?} cur_sum:{}",i,self.candidates[i],v,cur_sum);
+            let sum = cur_sum + self.candidates[i];
+            if sum == self.target{
+               let mut t = v.clone();
+               t.push(self.candidates[i]);
+               self.ans.push(t);
+            } else if sum < self.target{
+               let mut t = v.clone();
+               t.push(self.candidates[i]);
+               self.explore(t,i+1,sum);
+            } else{
+                break;
+            }
+        }            
+    }
+}
