943701ee47693b352e7d67690e938f54ff43fd9f
子風 <zpsyhapcst@gmail.com>
2021-08-13 09:48:02 +0800

    Time: 0 ms (100.00%), Space: 2 MB (66.67%) - LeetHub

--- combination-sum-ii/combination-sum-ii.rs
+++ combination-sum-ii/combination-sum-ii.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         let mut csum = CSum::new(candidates, target);
-        csum.explore(vec![], 0, 0);
+        csum.explore(&mut vec![], 0, 0);
         csum.ans
     }
 }
@@ -14,31 +14,27 @@ struct CSum{
 
 impl CSum{
     fn new(mut candidates: Vec<i32>, target: i32) -> Self{
-        candidates.sort();
+        candidates.sort_unstable();
         CSum{
             candidates,
             target,
             ans:vec![],
         }
     }
-    fn explore (&mut self, mut v:Vec<i32>, i_start:usize, mut cur_sum:i32){
+    fn explore (&mut self, v:&mut Vec<i32>, i_start:usize, mut cur_sum:i32){
+        if cur_sum > self.target{
+            return;
+        } else if cur_sum == self.target{
+            self.ans.push(v.clone());
+            return;
+        }
         for i in (i_start..self.candidates.len()){
             if i>i_start && self.candidates[i] == self.candidates[i-1]{
                 continue;
             }
-            //println!("{}:{} {:?} cur_sum:{}",i,self.candidates[i],v,cur_sum);
-            let sum = cur_sum + self.candidates[i];
-            if sum == self.target{
-               let mut t = v.clone();
-               t.push(self.candidates[i]);
-               self.ans.push(t);
-            } else if sum < self.target{
-               let mut t = v.clone();
-               t.push(self.candidates[i]);
-               self.explore(t,i+1,sum);
-            } else{
-                break;
-            }
+            v.push(self.candidates[i]);
+            self.explore(v,i+1,cur_sum + self.candidates[i]);
+            v.pop();
         }            
     }
 }
