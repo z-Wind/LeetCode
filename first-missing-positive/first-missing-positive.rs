9db742d10d2d0ddbcd98d143f87ad76e552aacfa
子風 <zpsyhapcst@gmail.com>
2021-08-13 14:27:22 +0800

    Time: 34 ms (10.17%), Space: 7.2 MB (45.76%) - LeetHub

--- first-missing-positive/first-missing-positive.rs
+++ first-missing-positive/first-missing-positive.rs
@@ -1,54 +1,15 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        let mut i:usize = 0;
-        let mut v:Vec<usize> = vec![];
-        let mut set:HashSet<usize> = HashSet::new();
-        for num in nums{
-            if num.is_positive(){
-                let num = num as usize;
-                if set.contains(&num){
-                    continue;
-                }
-                
-                //println!("len:{},num:{}",v.len(),num);
-                if v.len() == 0 || (num > v.len() && num > *v.last().unwrap()) {
-                    v.push(num);
-                } else if  num > v.len() && num < *v.last().unwrap(){
-                    let mut j=v.len()-1;
-                    while j>= 0 && v[j] > num{
-                        match j.overflowing_sub(1){
-                            (x,true) => {
-                                j=x;
-                                break;
-                            },
-                            (x,false) => j=x,
-                        }
-                    }
-                    v.insert(j+1,num);
-                } else if num < v[0]{
-                    v.insert(0,num);
-                } else if num > v[0]{
-                    let mut m=0;
-                    while m < v.len() && v[m] < num{
-                        m+=1;
-                    }
-                    v.insert(m,num);
-                }else {
-                    v.insert(num-1,num);
-                }
-                if num == i+1 {
-                    i = num;
-                    while i < v.len() && i+1 == v[i]{
-                        i+=1;
-                    }
-                }
-                //println!("{:?}",v);
-                set.insert(num);
+        let mut v:Vec<i32> = nums.into_iter().filter(|x|x.is_positive()).collect();
+        v.sort_unstable();
+        v.dedup();
+        //println!("{:?}",v);
+        for (i,&x) in v.iter().enumerate(){
+            let pos = i as i32 + 1;
+            if pos != x{
+                return pos;
             }
         }
-        //println!("{:?}",v);
-        (i+1) as i32
+        return v.len() as i32 +1;
     }
 }
