29b9fdf85b58a6c44510927223f142e648f00ed4
子風 <zpsyhapcst@gmail.com>
2021-08-14 11:58:06 +0800

    Time: 280 ms (5.52%), Space: 3.1 MB (6.21%) - LeetHub

--- trapping-rain-water/trapping-rain-water.rs
+++ trapping-rain-water/trapping-rain-water.rs
@@ -2,43 +2,11 @@ use std::cmp::{min,max};
 
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-        let mut waters=0;
-        let mut i=0;
-        let mut set:Vec<(usize,usize,i32)> = Vec::new();
-        while i < height.len(){
-            let (start, end) = find_trap(i, &height);
-            
-            if start == i || end == i{
-                i+=1;    
-                continue
-            } 
-            
-            let mut water = cal_water(start, end, &height);
-            let mut push = true;
-            let set_len = set.len();
-
-            for ii in (0..set_len).rev(){
-                let &(m,n,w) = &set[ii];
-                if m <= start && n>=end{
-                    water = 0;
-                    push = false;
-                } else if start <= m && end >= n{
-                    waters -= w;
-                    set.remove(ii);
-                }
-            }
-            if push{
-                set.push((start,end,water));
-            }
-            waters += water;
-            //println!("{} => {} to {} : {} get:{}",i, start, end, water, waters);
-            i=end+1;
-        }
-        waters
+        trap(&height[..])
     }
 }
 
-fn cal_water(start:usize, end:usize, height:&Vec<i32>) -> i32{
+fn cal_water(start:usize, end:usize, height:&[i32]) -> i32{
     let mut water = 0;
     let min_h = min(height[start], height[end]);
     for i in (start+1..end){
@@ -48,34 +16,46 @@ fn cal_water(start:usize, end:usize, height:&Vec<i32>) -> i32{
     water
 }
 
-fn find_trap(mid:usize, height:&Vec<i32>) -> (usize, usize){
-    let mut start = mid;
-    let mut end = mid;
-    
-     
-    let mut i = mid;
-    let mut j = mid;
-    let mut max_left = height[mid];
-    let mut max_right = height[mid];
-    while i as i32 >=0 && j < height.len(){
-        //println!("mid:{} => {}:{}, {}:{} => {}, {}",mid,i,max_left,j,max_right,start,end);
-        if i as i32 >=0 && height[i] > max_left{
-            max_left = height[i];
-            start = i;
-        }
-        if j < height.len() && height[j] > max_right{
-            max_right = height[j];
-            end = j;
-        }
-        
-        if j < height.len() && max_right >= max_left{
-            i-=1;
-        }
-        if i as i32 >=0 && max_left >= max_right{
-            j+=1;    
-        }
+fn trap(height: &[i32]) -> i32 {
+    //println!("{:?}",height);
+    if height.len() < 3{
+        return 0;
     }
     
-    //println!("get: {},{}",start, end);
-    (start, end)
+    let mut waters=0;
+    let (start, end) = max_area(&height);
+    //println!("{},{}",start,end);
+    if end-start < 2{
+        waters += trap(&height[..start+1]) + trap(&height[end..]);
+    } else {
+        let mut water = cal_water(start, end, &height);
+
+        //println!("{},{} => {}",start,end,water);
+        waters += water + trap(&height[..start+1]) + trap(&height[end..]);
+    }
+    
+    waters
+}
+
+fn max_area(height: &[i32]) -> (usize,usize) {
+    let mut l:usize = 0;
+    let mut r:usize = height.len() - 1;
+    let mut max_left:usize = l;
+    let mut max_right:usize = r;
+    while (l < r) {
+        if (height[max_left] < height[l]) {
+            max_left = l;
+        }
+        if (height[max_right] < height[r]) {
+            max_right = r;
+        }
+        
+        if (height[l] < height[r]) {
+            l+=1;
+        }
+        else{
+            r-=1;
+        }
+    }
+    return (max_left,max_right);
 }
