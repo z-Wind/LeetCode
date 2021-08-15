b661ce06b18f7dced50080083f845bcfc4ff052c
子風 <zpsyhapcst@gmail.com>
2021-08-15 09:45:44 +0800

    Time: 6 ms (61.97%), Space: 2.1 MB (83.10%) - LeetHub

--- jump-game-ii/jump-game-ii.rs
+++ jump-game-ii/jump-game-ii.rs
@@ -1,39 +1,26 @@
-use std::collections::LinkedList;
+use std::cmp;
 
 impl Solution {
-    pub fn jump(mut nums: Vec<i32>) -> i32 {
-        let mut queue: LinkedList<(usize,usize)> = LinkedList::new();
-        let mut i = 0;
-        let mut level = 0;
-        'outer: while i != nums.len()-1{
-            //println!("{:?}", nums);  
-            if nums[i].is_positive(){
-                for step in (1..=nums[i] as usize){
-                    if i+step == nums.len()-1{
-                        level += 1;
-                        break 'outer;
-                    } else if i+step < nums.len() && nums[i+step].is_positive(){
-                        //println!("push {}", i+step);
-                        queue.push_back((level+1,i+step));
-                    }
-                    let (index,overflow) = i.overflowing_sub(step);
-                    if !overflow && nums[index].is_positive(){
-                        //println!("push {}", index);
-                        queue.push_back((level+1,index));
-                    }
-                }
-                nums[i] = -nums[i];   
-            }
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        let size: usize = nums.len();
+        
+        let mut jumps: i32 = 0;
+        let mut cur_end: usize = 0;
+        let mut cur_furthest: usize = 0;
+        
+        for i in 0..size - 1{
+            cur_furthest = cmp::max(cur_furthest, i + nums[i] as usize);
             
-            match queue.pop_front(){
-                None => return -1,
-                Some((l,index)) => {
-                    level = l;
-                    i = index;
-                }
+            if i == cur_end{
+                jumps += 1;
+                cur_end = cur_furthest;
+				
+				if cur_end >= size - 1{
+					break;
+				}
             }
-            //println!("pop:{:?}", i);            
         }
-        level as i32
+        
+        jumps
     }
 }
