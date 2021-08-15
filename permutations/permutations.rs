e2ed9fa77c341d1b73bbf78bcd17200c7998f3de
子風 <zpsyhapcst@gmail.com>
2021-08-15 10:46:34 +0800

    Time: 2 ms (14.63%), Space: 2.1 MB (82.93%) - LeetHub

--- permutations/permutations.rs
+++ permutations/permutations.rs
@@ -4,22 +4,20 @@ impl Solution {
             return vec![nums];
         }
         let mut ans:Vec<Vec<i32>> = Vec::new();
-        push(&mut ans, vec![], nums);
+        push(&mut ans, nums, 0);
         ans
         
     }
 }
 
-fn push(ans: &mut Vec<Vec<i32>>, mut v:Vec<i32>, mut nums:Vec<i32>){
-    if nums.len() == 0{
-        ans.push(v);
+fn push(ans: &mut Vec<Vec<i32>>, nums:Vec<i32>, pos:usize){
+    if pos == nums.len(){
+        ans.push(nums);
     } else{
-        for _ in (0..nums.len()){
-            let mut tmp = v.clone();
-            tmp.push(nums[0]);
-            let (_, right) = nums.split_at(1);
-            push(ans, tmp, right.to_vec());
-            nums.rotate_left(1);
+        for i in (pos..nums.len()){
+            let mut nums = nums.clone();
+            nums.swap(pos,i);
+            push(ans, nums, pos+1);
         }
     }
 }
