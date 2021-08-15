1d622e99e928e7135c40bf6e21660e9a14b62dcf
子風 <zpsyhapcst@gmail.com>
2021-08-15 09:30:59 +0800

    Time: 801 ms (5.63%), Space: 26.5 MB (9.86%) - LeetHub

--- jump-game-ii/jump-game-ii.rs
+++ jump-game-ii/jump-game-ii.rs
@@ -5,27 +5,34 @@ impl Solution {
         let mut queue: LinkedList<(usize,usize)> = LinkedList::new();
         let mut i = 0;
         let mut level = 0;
-        while i != nums.len()-1{
-            //println!("{:?}", nums);
-            for step in (1..=nums[i] as usize){
-                if i+step < nums.len(){
-                    queue.push_back((level+1,i+step));
-                }
-                let (index,overflow) = i.overflowing_sub(step);
-                if !overflow{
-                    queue.push_back((level+1,index));
-                }
-            }
-            nums[i] = -nums[i];
-            while !nums[i].is_positive() && i != nums.len()-1{
-                match queue.pop_front(){
-                    None => return -1,
-                    Some((l,index)) => {
-                        level = l;
-                        i = index;
+        'outer: while i != nums.len()-1{
+            //println!("{:?}", nums);  
+            if nums[i].is_positive(){
+                for step in (1..=nums[i] as usize){
+                    if i+step == nums.len()-1{
+                        level += 1;
+                        break 'outer;
+                    } else if i+step < nums.len() && nums[i+step].is_positive(){
+                        //println!("push {}", i+step);
+                        queue.push_back((level+1,i+step));
+                    }
+                    let (index,overflow) = i.overflowing_sub(step);
+                    if !overflow && nums[index].is_positive(){
+                        //println!("push {}", index);
+                        queue.push_back((level+1,index));
                     }
                 }
+                nums[i] = -nums[i];   
             }
+            
+            match queue.pop_front(){
+                None => return -1,
+                Some((l,index)) => {
+                    level = l;
+                    i = index;
+                }
+            }
+            //println!("pop:{:?}", i);            
         }
         level as i32
     }
