c36ba7400d1f608198384fe04032e6e9f22bead9
子風 <zpsyhapcst@gmail.com>
2021-08-16 11:34:13 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (32.88%) - LeetHub

--- powx-n/powx-n.rs
+++ powx-n/powx-n.rs
@@ -1,5 +1,17 @@
 impl Solution {
     pub fn my_pow(x: f64, n: i32) -> f64 {
-        x.powi(n)
+        if n == 0 {
+            return 1.0;
+        }
+
+        if n % 2 == 0 {
+            Solution::my_pow(x, n / 2) * Solution::my_pow(x, n / 2)
+        } else {
+            if n > 0 {
+                x * Solution::my_pow(x, n / 2) * Solution::my_pow(x, n / 2)
+            } else {
+                (Solution::my_pow(x, n / 2) * Solution::my_pow(x, n / 2)) / x
+            }
+        }
     }
 }
