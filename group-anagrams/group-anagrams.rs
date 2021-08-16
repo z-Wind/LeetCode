bc2bf7dc14e5ed4b375abd90ff12e4039445537e
子風 <zpsyhapcst@gmail.com>
2021-08-16 10:44:00 +0800

    Time: 8 ms (94.87%), Space: 4.8 MB (67.18%) - LeetHub

--- group-anagrams/group-anagrams.rs
+++ group-anagrams/group-anagrams.rs
@@ -4,21 +4,25 @@ impl Solution {
         if strs.len() == 0{
             return vec![];
         }
-        let mut map:HashMap<[usize;26], Vec<String>> = HashMap::new();
+        let mut map:HashMap<usize, Vec<String>> = HashMap::with_capacity(strs.len()/4);
         
         for s in strs{
-            let mut key = [0;26];
-            for c in s.chars(){
-                let index = c as u32 - 'a' as u32;
-                key[index as usize] += 1;
-            }
-            (*map.entry(key).or_insert(vec![])).push(s);
+            let key = cal_key(&s);
+            map.entry(key).or_insert(vec![]).push(s);
         }
         
-        let mut ans: Vec<Vec<String>> = vec![];
-        for (_,v) in map.into_iter(){
-            ans.push(v);
-        }
-        ans
+        map.values().cloned().collect()
     }
+}
+
+fn cal_key(s: &str) -> usize{
+	let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 ];
+
+	let mut key = 1;
+	for c in s.chars() {
+        let index = c as u32 - 'a' as u32;
+		let prime = primes[index as usize];
+		key *= prime;
+	}
+	return key;
 }
