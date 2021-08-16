d63d1be095923e0add6ed0ab4bc0fb785001ba46
z-Wind <zpsyhapcst@gmail.com>
2021-08-16 19:12:37 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (62.40%) - LeetHub

--- maximum-subarray/maximum-subarray.rs
+++ maximum-subarray/maximum-subarray.rs
@@ -1,23 +1,19 @@
+// Kadane’s Algorithm
+// https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+use std::cmp::max;
+
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        if nums.len() == 1{
+        if nums.len() == 1 {
             return nums[0];
         }
-        
-        let mut max_sum = i32::MIN;
-        for i in (0..nums.len()){
-            let mut sum = nums[i];
-            if sum>max_sum{
-                max_sum = sum;
-            }
-            for j in (i+1..nums.len()){
-                sum+=nums[j];
-                if sum>max_sum{
-                    max_sum = sum;
-                }
-            }
+
+        let mut ans = nums[0];
+        let mut last = ans;
+        for &i in &nums[1..] {
+            last = max(i, last + i);
+            ans = max(ans, last);
         }
-        
-        max_sum
+        ans
     }
 }
