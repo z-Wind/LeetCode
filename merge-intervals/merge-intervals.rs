6b9e6db3a6e82063057659d68832155d24e66369
子風 <zpsyhapcst@gmail.com>
2021-08-17 12:11:06 +0800

    Time: 0 ms (100.00%), Space: 2.9 MB (22.03%) - LeetHub

--- merge-intervals/merge-intervals.rs
+++ merge-intervals/merge-intervals.rs
@@ -8,24 +8,13 @@ impl Solution {
         //println!("intervals: {:?}",intervals);
         
         let mut ans:Vec<Vec<i32>> = Vec::new();
-        'outer: for interval in intervals.into_iter(){
-            //println!("interval: {:?} ans: {:?}",interval, ans);
-            for rang in ans.iter_mut(){
-                if rang[0] <= interval[0] && interval[1] <= rang[1]{
-                    continue 'outer;
-                } else if interval[0] < rang[0] && rang[1] < interval[1]{
-                    rang[0] = interval[0];
-                    rang[1] = interval[1];
-                    continue 'outer;
-                } else if rang[0] <= interval[0] && interval[0] <= rang[1]{
-                    rang[1] = rang[1].max(interval[1]);
-                    continue 'outer;
-                } else if rang[0] <= interval[1] && interval[1] <= rang[1]{
-                    rang[0] = rang[0].min(interval[0]);
-                    continue 'outer;
-                }
-            }  
-            ans.push(interval);
+        for interval in intervals.into_iter(){
+            if ans.is_empty() || ans[ans.len()-1][1] < interval[0]{
+                ans.push(interval);    
+            } else {
+                let n = ans.len();
+                ans[n-1][1] = ans[n-1][1].max(interval[1]);
+            }
         }
         ans
     }
