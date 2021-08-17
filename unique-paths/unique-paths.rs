e81c1985bbe8017ac7da52892b1eb0ecb435c2fb
子風 <zpsyhapcst@gmail.com>
2021-08-17 19:43:31 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (59.62%) - LeetHub

--- unique-paths/unique-paths.rs
+++ unique-paths/unique-paths.rs
@@ -1,37 +1,14 @@
-use std::collections::HashMap;
+// https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let mut uni = UNI::new();
-        uni.unique_paths(m,n)
-    }
-}
-
-struct UNI{
-    dp:HashMap<(i32,i32), i32>,
-}
-
-impl UNI{
-    fn new() -> Self{
-        UNI{
-            dp: HashMap::new(),
+        let (m,n) = (m as usize, n as usize);
+        let mut cur = vec![1;n as usize];
+        for i in (1..m) {
+            for j in (1..n) {
+                cur[j] += cur[j - 1];
+            }
         }
-    }
-    fn unique_paths(&mut self, m: i32, n: i32) -> i32 {
-        if m == 1 && n == 1{
-            return 1;
-        } else if m == 0 || n == 0{
-            return 0;
-        }
-        
-        if self.dp.contains_key(&(m,n)){
-            return *self.dp.get(&(m,n)).unwrap();
-        }
-        
-        let step = self.unique_paths(m-1,n) + self.unique_paths(m,n-1);
-        self.dp.insert((m,n), step);
-        self.dp.insert((n,m), step);
-        
-        step
+        return cur[n - 1];
     }
 }
