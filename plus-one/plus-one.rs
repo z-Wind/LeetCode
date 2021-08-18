3c75c60043495b2f2581ea69be7906cf021342bf
子風 <zpsyhapcst@gmail.com>
2021-08-18 14:53:18 +0800

    Time: 0 ms (100.00%), Space: 2 MB (59.79%) - LeetHub

--- plus-one/plus-one.rs
+++ plus-one/plus-one.rs
@@ -1,5 +1,3 @@
-use std::char;
-
 impl Solution {
     pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
         let mut carry:i32 = 0;
@@ -7,19 +5,16 @@ impl Solution {
             match digit{
                 digit @ 0..=8 => {
                     *digit += 1;
-                    carry = 0;
-                    break;
+                    return digits;
                 }
                 digit @ 9 => {
                     *digit = 0;
-                    carry = 1;
                 },
                 _ => panic!(),
             }
         }        
-        if carry == 1{
-            digits.insert(0, 1);
-        }
+        
+        digits.insert(0, 1);
         digits
     }
 }
