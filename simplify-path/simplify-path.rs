de21b6311febf073206551a2dd76d6fbdd338caf
子風 <zpsyhapcst@gmail.com>
2021-08-19 13:53:30 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (5.00%) - LeetHub

--- simplify-path/simplify-path.rs
+++ simplify-path/simplify-path.rs
@@ -3,14 +3,14 @@ use std::path::PathBuf;
 
 impl Solution {
     pub fn simplify_path(path: String) -> String {
-        let mut dirs:Vec<String> = Vec::new();
+        let mut dirs:Vec<&str> = Vec::new();
         for dir in path.split_terminator("/"){
             match dir{
                 "." | "" => continue,
                 ".." => {
                     dirs.pop();
                 },
-                x => dirs.push(x.to_string()),
+                x => dirs.push(x),
             }
         }
         //println!("{:?}", dirs);
