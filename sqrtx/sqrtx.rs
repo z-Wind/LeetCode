19582a9b7451b9a210c46260086496b6a6685a2b
子風 <zpsyhapcst@gmail.com>
2021-08-19 09:58:51 +0800

    Time: 4 ms (25.97%), Space: 1.9 MB (87.01%) - LeetHub

--- sqrtx/sqrtx.rs
+++ sqrtx/sqrtx.rs
@@ -1,5 +1,27 @@
 impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
-        (x as f64).sqrt() as i32
+        if x <= 1{
+            return x;
+        }
+        let mut val = 1;
+        let mut shift = 0;
+        while val < (x>>2){
+            val <<= 2;
+            shift += 2;
+            //println!("shift:{}, val:{}",shift, val);
+        }
+        let mut root = (1 << shift/2);
+        //println!("root:{}, val:{}",root, val);
+        let mut square = root * root;
+        while square <= x{
+            root += 1;
+            square = match root.checked_mul(root){
+                Some(x) => x,
+                None => break,
+            };
+            //println!("root:{}, square:{}",root, square);
+        }
+        
+        return root-1;
     }
 }
