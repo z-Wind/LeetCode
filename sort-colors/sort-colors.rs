fc3fa80743c0293a9171231d2b5890fb0153031e
子風 <zpsyhapcst@gmail.com>
2021-08-20 14:47:49 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (22.39%) - LeetHub

--- sort-colors/sort-colors.rs
+++ sort-colors/sort-colors.rs
@@ -1,5 +1,25 @@
 impl Solution {
     pub fn sort_colors(nums: &mut Vec<i32>) {
-        nums.sort()
+        let mut red = 0;
+        let mut blue = nums.len()-1;
+        let mut white = 0;
+        while white <= blue{
+            match nums[white]{
+                0 => {
+                    nums.swap(red, white);
+                    red += 1;
+                    white += 1;
+                },
+                1 => white += 1,
+                2 => {
+                    nums.swap(blue, white);
+                    match blue.checked_sub(1){
+                        None => break,
+                        Some(x) => blue = x,
+                    }
+                },
+                _ => panic!(),
+            }
+        }
     }
 }
