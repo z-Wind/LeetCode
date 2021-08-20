f022a2887ec45c455e5a41da87dcdca31aebeea6
子風 <zpsyhapcst@gmail.com>
2021-08-20 13:55:37 +0800

    Time: 8 ms (46.79%), Space: 2.3 MB (93.58%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.rs
+++ set-matrix-zeroes/set-matrix-zeroes.rs
@@ -1,69 +1,40 @@
 use std::collections::HashSet;
 impl Solution {
     pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let mut zeros = Zeros::new(matrix);
-        zeros.bump();
-    }
-}
-
-struct Zeros<'a>{
-    matrix: &'a mut Vec<Vec<i32>>,
-    
-    zeros_i:HashSet<usize>,
-    zeros_j:HashSet<usize>,
-}
-
-impl <'a>Zeros<'a>{
-    fn new(matrix: &'a mut Vec<Vec<i32>>) -> Self{
-        Zeros{
-            matrix,
-            zeros_i: HashSet::new(),
-            zeros_j: HashSet::new(),
-        }
-    }
-    fn bump(&mut self){
-        let m = self.matrix.len();
-        let n = self.matrix[0].len();
-        'outer: for i in (0..m){
-            if self.zeros_i.contains(&i){
-                continue;
-            }
-            for j in (0..n){
-                if self.zeros_j.contains(&j){
-                    continue;
-                }
-                if self.matrix[i][j] == 0 {
-                    //println!("bump:{}, {}",i,j);
-                    self.zeros_i.insert(i);
-                    self.zeros_j.insert(j);
-                    self.bump_i(i);
-                    self.bump_j(j);
-                    continue 'outer;
+        let m = matrix.len();
+        let n = matrix[0].len();
+        let mut col_clear = false;
+        for i in (0..m){
+            if matrix[i][0] == 0{
+                col_clear = true;
+            } 
+            for j in (1..n){
+                if matrix[i][j] == 0 {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-    }
-    
-    fn bump_i(&mut self, i:usize){
-        for j in (0..self.matrix[0].len()){
-            if self.matrix[i][j] == 0 && !self.zeros_j.contains(&j){
-                //println!("bump_i:{}",i);
-                self.zeros_j.insert(j);
-                self.bump_j(j);
-            } else{
-                self.matrix[i][j] = 0;
+        println!("{:?}",matrix);
+        for i in (1..m){
+            for j in (1..n){
+                if matrix[0][j] == 0 || matrix[i][0] == 0 {
+                    matrix[i][j] = 0;
+                }
             }
         }
-    }
-    fn bump_j(&mut self, j:usize){
-        for i in (0..self.matrix.len()){
-            if self.matrix[i][j] == 0 && !self.zeros_i.contains(&i){
-                //println!("bump_j:{}",j);
-                self.zeros_i.insert(i);
-                self.bump_i(i);
-            } else{
-                self.matrix[i][j] = 0;
+        
+        for j in (1..n){
+            if matrix[0][0] == 0 {
+                matrix[0][j] = 0;
             }
-        }  
+        }
+        
+        if col_clear{
+            for i in (0..m){
+                    matrix[i][0] = 0;
+                
+            }
+        }
     }
 }
