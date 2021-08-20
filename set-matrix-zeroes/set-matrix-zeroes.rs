1e34cba2bb038874d2f7de1634777f52a6e06561
子風 <zpsyhapcst@gmail.com>
2021-08-20 11:10:41 +0800

    Time: 4 ms (99.08%), Space: 2.4 MB (52.29%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.rs
+++ set-matrix-zeroes/set-matrix-zeroes.rs
@@ -3,19 +3,21 @@ impl Solution {
     pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
         let m = matrix.len();
         let n = matrix[0].len();
-        let mut ans:Vec<Vec<i32>> = matrix.clone();
+        let mut zeros:Vec<(usize,usize)> = vec![];
         for i in (0..m){
             for j in (0..n){
                 if matrix[i][j] == 0 {
-                    for k in (0..m){
-                        ans[k][j] = 0;
-                    }
-                    for k in (0..n){
-                        ans[i][k] = 0;
-                    }
+                    zeros.push((i,j));
                 }
             }
         }
-        mem::swap(&mut ans, matrix);
+        for (i,j) in zeros{
+            for k in (0..m){
+                matrix[k][j] = 0;
+            }
+            for k in (0..n){
+                matrix[i][k] = 0;
+            }
+        }
     }
 }
