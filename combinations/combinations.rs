1b435736a4a1f5accd83558609722d51e6dfa72c
子風 <zpsyhapcst@gmail.com>
2021-08-21 23:52:18 +0800

    Time: 16 ms (27.91%), Space: 2.8 MB (69.77%) - LeetHub

--- combinations/combinations.rs
+++ combinations/combinations.rs
@@ -1,45 +1,24 @@
-// C(n,k)=C(n-1,k-1)+C(n-1,k)
-// https://zhidao.baidu.com/question/368034364206885564.html
-use std::collections::HashMap;
+use std::cmp::min;
 impl Solution {
     pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
-        let mut c = Combine::new();
-        c.combine(n,k)
+        let mut nums:Vec<i32> = (1..=n).collect();
+        let mut ans:Vec<Vec<i32>> = Vec::new();
+        let mut temp:Vec<i32> = Vec::new(); 
+        backtrack(&mut ans, &nums, &mut temp, 0, k as usize);
+        ans
     }
 }
 
-struct Combine{
-    dp:HashMap<(i32,i32),Vec<Vec<i32>>>,
-}
 
-impl Combine{
-    fn new() -> Self{
-        Combine{
-            dp: HashMap::new(),
-        }
+fn backtrack(ans: &mut Vec<Vec<i32>>, nums:&Vec<i32>, temp:&mut Vec<i32>, pos:usize, len:usize){
+    if temp.len() == len{
+        ans.push(temp.clone());
+        return;
     }
-    fn combine(&mut self, n: i32, k: i32) -> Vec<Vec<i32>>{
-        match self.dp.get(&(n,k)){
-            Some(v) => v.to_vec(),
-            None => {
-                if k == 0 {
-                    return vec![vec![]];
-                } else if k == n{
-                    return vec![(1..=n).collect()];
-                }
 
-                let mut result:Vec<Vec<i32>> = vec![];
-
-                for mut cmb in self.combine(n - 1, k - 1) {
-                    cmb.push(n);
-                    result.push(cmb);
-                }
-
-                result.append(&mut self.combine(n - 1, k));
-
-                self.dp.insert((n,k),result.clone());
-                result
-            }
-        }
+    for i in (pos..nums.len()){
+        temp.push(nums[i]);
+        backtrack(ans, nums, temp, i+1, len);
+        temp.pop();
     }
 }
