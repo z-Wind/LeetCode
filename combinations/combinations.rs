dc63ba28246059bb955b6a6ce9fd82b17007e1d6
子風 <zpsyhapcst@gmail.com>
2021-08-21 20:33:46 +0800

    Time: 8 ms (74.42%), Space: 3.1 MB (6.98%) - LeetHub

--- combinations/combinations.rs
+++ combinations/combinations.rs
@@ -1,22 +1,22 @@
-use std::cmp::min;
+// C(n,k)=C(n-1,k-1)+C(n-1,k)
+// https://zhidao.baidu.com/question/368034364206885564.html
 impl Solution {
     pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
-        let mut ans:Vec<Vec<i32>> = Vec::new();
-        let mut nums:Vec<i32> = Vec::new();
-        push(&mut ans, &mut nums, 0, k as usize, n as usize);
-        ans
-    }
-}
-
-fn push(ans: &mut Vec<Vec<i32>>, nums:&mut Vec<i32>, start:usize, len:usize, numlen:usize){
-    //println!("{:?} {}==?{}", nums, nums.len(), len);
-    if nums.len() == len{
-        ans.push(nums.clone());
-    } else{
-        for i in (start..numlen){
-            nums.push(i as i32 +1);
-            push(ans, nums, i+1, len, numlen);
-            nums.pop();
+        if k == 0 {
+            return vec![vec![]];
+        } else if k == n{
+            return vec![(1..=n).collect()];
         }
+        
+        let mut result:Vec<Vec<i32>> = vec![];
+        
+        for mut cmb in Self::combine(n - 1, k - 1) {
+            cmb.push(n);
+            result.push(cmb);
+        }
+        
+        result.append(&mut Self::combine(n - 1, k));
+        
+        result
     }
 }
