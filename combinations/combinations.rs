0d4f9cc8188bf0ce10e3d9488dea7d7ced8d12dd
子風 <zpsyhapcst@gmail.com>
2021-08-21 19:54:02 +0800

    Time: 15 ms (30.23%), Space: 3.1 MB (16.28%) - LeetHub

---
+++ combinations/combinations.rs
@@ -0,0 +1,21 @@
+use std::cmp::min;
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        let mut nums:Vec<i32> = (1..=n).collect();
+        let mut ans:Vec<Vec<i32>> = Vec::new();
+        push(&mut ans, &mut nums, 0, k as usize, 0);
+        ans
+    }
+}
+
+fn push(ans: &mut Vec<Vec<i32>>, nums:&mut Vec<i32>, pos:usize, len:usize, start:usize){
+    if pos == len{
+        ans.push(nums[..len].to_vec());
+    } else{
+        for i in (start..nums.len()){
+            nums.swap(pos,i);
+            push(ans, nums, pos+1, len, i+1);
+            nums.swap(pos,i);
+        }
+    }
+}
