4518968d7692dc5a6327bcf636577603addc5f62
子風 <zpsyhapcst@gmail.com>
2021-08-22 11:57:00 +0800

    Time: 110 ms (38.98%), Space: 2 MB (47.46%) - LeetHub

--- word-search/word-search.rs
+++ word-search/word-search.rs
@@ -1,8 +1,11 @@
 impl Solution {
     pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
-        for i in (0..board.len()){
-            for j in (0..board[0].len()){
-                if backtrack(&mut board, 0, &word.chars().collect(), i as i32,j as i32){
+        let m = board.len();
+        let n = board[0].len();
+        
+        for i in (0..m){
+            for j in (0..n){
+                if backtrack(&mut board, &(m,n), 0, &word.chars().collect(), i as i32,j as i32){
                     return true;
                 }    
             }
@@ -11,12 +14,12 @@ impl Solution {
     }
 }
 
-fn backtrack(board: &mut Vec<Vec<char>>, pos:usize, word:&Vec<char>, i:i32, j:i32) -> bool{
+fn backtrack(board: &mut Vec<Vec<char>>, size:&(usize,usize), pos:usize, word:&Vec<char>, i:i32, j:i32) -> bool{
     let x = i as usize;
     let y = j as usize;
     if pos == word.len(){
         return true;
-    } else if i<0 || j<0 || x>= board.len() || y>= board[0].len() || 
+    } else if i<0 || j<0 || x>= size.0 || y>= size.1 || 
                 board[x][y]=='.' || board[x][y] != word[pos]{
         return false;
     } 
@@ -24,10 +27,10 @@ fn backtrack(board: &mut Vec<Vec<char>>, pos:usize, word:&Vec<char>, i:i32, j:i3
     
     let c = board[x][y];
     board[x][y] = '.';
-    if backtrack(board, pos+1, word, i,j-1) || 
-       backtrack(board, pos+1, word, i,j+1) ||
-       backtrack(board, pos+1, word, i-1,j) ||
-       backtrack(board, pos+1, word, i+1,j) {
+    if backtrack(board, size, pos+1, word, i,j-1) || 
+       backtrack(board, size, pos+1, word, i,j+1) ||
+       backtrack(board, size, pos+1, word, i-1,j) ||
+       backtrack(board, size, pos+1, word, i+1,j) {
         return true;
     }
     board[x][y] = c;
