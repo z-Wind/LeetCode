cd220b0d893ba1a565576046ee2eaee0b6136b2c
子風 <zpsyhapcst@gmail.com>
2021-08-22 13:26:02 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (14.29%) - LeetHub

---
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.rs
@@ -0,0 +1,36 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> bool {
+        -1 != search(&nums, target, 0, nums.len()-1)
+    }
+}
+
+pub fn search(nums: &Vec<i32>, target: i32, start: usize, end: usize) -> i32 {
+    let middle = (start + end)/2;
+    if nums[middle] == target{
+        return middle as i32;
+    } else if nums[start] == target{
+        return start as i32;
+    } else if nums[end] == target{
+        return end as i32;
+    } else if start >= end{
+        return -1;
+    }
+    
+    
+    //println!("{},{},{},{:?}",start,middle,end, &nums[start..=end]);
+    
+    let mut i = -1;
+    if (nums[middle] > nums[start] && nums[start] < target && target < nums[middle]) ||
+       (nums[middle] < nums[start] && (nums[start] < target || nums[middle] > target)) {
+        i = search(&nums, target, start, middle);
+    } else if (nums[middle] < nums[end] && nums[end] > target && target > nums[middle]) ||
+       (nums[middle] > nums[end] && (nums[end] > target || nums[middle] < target)) {
+        i = search(&nums, target, middle+1, end);
+    } else {
+        i = search(&nums, target, start, middle);
+        if i == -1{
+            i = search(&nums, target, middle+1, end);
+        }
+    }
+    return i;
+}
