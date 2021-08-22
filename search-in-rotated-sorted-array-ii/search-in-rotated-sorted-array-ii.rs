e9fcbf95359f9292c6fa9610bb09a96bd5c054f6
子風 <zpsyhapcst@gmail.com>
2021-08-22 13:50:57 +0800

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.rs
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.rs
@@ -4,8 +4,17 @@ impl Solution {
     }
 }
 
-pub fn search(nums: &Vec<i32>, target: i32, start: usize, end: usize) -> i32 {
+pub fn search(nums: &Vec<i32>, target: i32, mut start: usize, mut end: usize) -> i32 {
     let middle = (start + end)/2;
+    while start < middle && nums[middle] == nums[start]{
+        start += 1;
+    }
+    while end > middle+1 && nums[middle+1] == nums[end]{
+        end -= 1;
+    }
+    
+    //println!("{},{},{},{:?}",start,middle,end, &nums[start..=end]);
+    
     if nums[middle] == target{
         return middle as i32;
     } else if nums[start] == target{
@@ -16,21 +25,12 @@ pub fn search(nums: &Vec<i32>, target: i32, start: usize, end: usize) -> i32 {
         return -1;
     }
     
-    
-    //println!("{},{},{},{:?}",start,middle,end, &nums[start..=end]);
-    
     let mut i = -1;
     if (nums[middle] > nums[start] && nums[start] < target && target < nums[middle]) ||
        (nums[middle] < nums[start] && (nums[start] < target || nums[middle] > target)) {
         i = search(&nums, target, start, middle);
-    } else if (nums[middle] < nums[end] && nums[end] > target && target > nums[middle]) ||
-       (nums[middle] > nums[end] && (nums[end] > target || nums[middle] < target)) {
-        i = search(&nums, target, middle+1, end);
     } else {
-        i = search(&nums, target, start, middle);
-        if i == -1{
-            i = search(&nums, target, middle+1, end);
-        }
+        i = search(&nums, target, middle+1, end);
     }
     return i;
 }
