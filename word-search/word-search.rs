a552a9d64e11c9191e3996d78fd7003d63c1ecc9
子風 <zpsyhapcst@gmail.com>
2021-08-22 11:53:35 +0800

    Time: 122 ms (38.98%), Space: 2 MB (47.46%) - LeetHub

--- word-search/word-search.rs
+++ word-search/word-search.rs
@@ -1,11 +1,8 @@
 impl Solution {
     pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
-        if board.len() == 1 && board[0].len() == 1{ 
-            return word.len() == 1 && board[0][0] == word.chars().next().unwrap();
-        }
         for i in (0..board.len()){
             for j in (0..board[0].len()){
-                if backtrack(&mut board, 0, &word.chars().collect(), i,j){
+                if backtrack(&mut board, 0, &word.chars().collect(), i as i32,j as i32){
                     return true;
                 }    
             }
@@ -14,36 +11,25 @@ impl Solution {
     }
 }
 
-fn backtrack(board: &mut Vec<Vec<char>>, pos:usize, word:&Vec<char>, i:usize, j:usize) -> bool{
+fn backtrack(board: &mut Vec<Vec<char>>, pos:usize, word:&Vec<char>, i:i32, j:i32) -> bool{
+    let x = i as usize;
+    let y = j as usize;
     if pos == word.len(){
         return true;
-    } else if i>= board.len() || j>= board[0].len() || board[i][j]=='.'{
+    } else if i<0 || j<0 || x>= board.len() || y>= board[0].len() || 
+                board[x][y]=='.' || board[x][y] != word[pos]{
         return false;
     } 
     //println!("{}:{} ==? board:{}",pos,word[pos],board[i][j]);
-    if board[i][j] == word[pos]{
-        let c = board[i][j];
-        board[i][j] = '.';
-        match i.checked_sub(1){
-            None => (),
-            Some(i) => {
-                if backtrack(board, pos+1, word, i,j){
-                    return true;
-                }
-            }
-        }
-        match j.checked_sub(1){
-            None => (),
-            Some(j) => {
-                if backtrack(board, pos+1, word, i,j){
-                    return true;
-                }
-            }
-        }
-        if backtrack(board, pos+1, word, i,j+1) | backtrack(board, pos+1, word, i+1,j){
-            return true;
-        }
-        board[i][j] = c;
+    
+    let c = board[x][y];
+    board[x][y] = '.';
+    if backtrack(board, pos+1, word, i,j-1) || 
+       backtrack(board, pos+1, word, i,j+1) ||
+       backtrack(board, pos+1, word, i-1,j) ||
+       backtrack(board, pos+1, word, i+1,j) {
+        return true;
     }
-    false
+    board[x][y] = c;
+    return false;
 }
