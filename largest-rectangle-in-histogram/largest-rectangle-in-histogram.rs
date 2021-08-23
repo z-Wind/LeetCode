9d13fb044d0b80284763b7f3720f3106372d12fc
子風 <zpsyhapcst@gmail.com>
2021-08-23 12:18:29 +0800

    Time: 12 ms (100.00%), Space: 3.7 MB (46.67%) - LeetHub

--- largest-rectangle-in-histogram/largest-rectangle-in-histogram.rs
+++ largest-rectangle-in-histogram/largest-rectangle-in-histogram.rs
@@ -1,37 +1,22 @@
-// https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28902/5ms-O(n)-Java-solution-explained-(beats-96)
-use std::cmp::max;
+// https://abhinandandubey.github.io/posts/2019/12/15/Largest-Rectangle-In-Histogram.html
 impl Solution {
-    pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+    pub fn largest_rectangle_area(mut heights: Vec<i32>) -> i32 {
         if (heights.len() == 0) {
             return 0;
         }
-        let mut lessFromLeft:Vec<i32> = vec![0;heights.len()]; // idx of the first bar the left that is lower than current
-        let mut lessFromRight:Vec<i32> = vec![0;heights.len()]; // idx of the first bar the right that is lower than current
-        lessFromRight[heights.len() - 1] = heights.len() as i32;
-        lessFromLeft[0] = -1;
-
-        for i in (1..heights.len() as i32) {
-            let mut p:i32 = i - 1;
-            while (p >= 0 && heights[p as usize] >= heights[i as usize]) {
-                p = lessFromLeft[p as usize];
-            }
-            lessFromLeft[i as usize] = p;
-        }
-
-        for i in (0..(heights.len()-1) as i32).rev() {
-            let mut p:i32 = i + 1;
-            while (p < heights.len() as i32 && heights[p as usize] >= heights[i as usize]) {
-                p = lessFromRight[p as usize];
-            }
-            lessFromRight[i as usize] = p;
-        }
-        
-        //println!("{:?} {:?}", lessFromLeft, lessFromRight);
         let mut maxArea = 0;
-        for i in (0..heights.len()) {
-            maxArea = max(maxArea, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        let mut stack:Vec<(i32,i32)> = vec![(-1,0)];
+        heights.push(0);
+        for (i,h) in heights.into_iter().enumerate(){
+            //println!("{:?}", stack);
+            while h < stack.last().unwrap().1{
+                let (_,h_stack) = stack.pop().unwrap();
+                let w = i as i32 - stack.last().unwrap().0 -1;
+                maxArea = maxArea.max(w*h_stack);
+            } 
+            stack.push((i as i32, h));
         }
-
+    
         return maxArea;
     }
 }
