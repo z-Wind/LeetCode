e705e1e4ff93d5b863c187cd4654ef741f91c8e5
子風 <zpsyhapcst@gmail.com>
2021-08-23 09:49:25 +0800

    Time: 16 ms (46.67%), Space: 3.3 MB (56.67%) - LeetHub

--- largest-rectangle-in-histogram/largest-rectangle-in-histogram.rs
+++ largest-rectangle-in-histogram/largest-rectangle-in-histogram.rs
@@ -1,30 +1,37 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28902/5ms-O(n)-Java-solution-explained-(beats-96)
 use std::cmp::max;
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        find(&heights[..])
-    }
-}
-
-fn find(heights: &[i32]) -> i32 {
-    if heights.len() == 0{
-        return 0;
-    }
-    let mut min_h:i32 = i32::MAX;
-    let mut min_i = 0;
-    let mut count = 0;
-    for (i,h) in heights.iter().enumerate(){
-        if h < &min_h{
-            min_h = *h;
-            min_i = i;
-            count = 1;
-        } else if h == &min_h{
-            count += 1;
+        if (heights.len() == 0) {
+            return 0;
         }
+        let mut lessFromLeft:Vec<i32> = vec![0;heights.len()]; // idx of the first bar the left that is lower than current
+        let mut lessFromRight:Vec<i32> = vec![0;heights.len()]; // idx of the first bar the right that is lower than current
+        lessFromRight[heights.len() - 1] = heights.len() as i32;
+        lessFromLeft[0] = -1;
+
+        for i in (1..heights.len() as i32) {
+            let mut p:i32 = i - 1;
+            while (p >= 0 && heights[p as usize] >= heights[i as usize]) {
+                p = lessFromLeft[p as usize];
+            }
+            lessFromLeft[i as usize] = p;
+        }
+
+        for i in (0..(heights.len()-1) as i32).rev() {
+            let mut p:i32 = i + 1;
+            while (p < heights.len() as i32 && heights[p as usize] >= heights[i as usize]) {
+                p = lessFromRight[p as usize];
+            }
+            lessFromRight[i as usize] = p;
+        }
+        
+        //println!("{:?} {:?}", lessFromLeft, lessFromRight);
+        let mut maxArea = 0;
+        for i in (0..heights.len()) {
+            maxArea = max(maxArea, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+
+        return maxArea;
     }
-    let area = min_h * heights.len() as i32;
-    if count == heights.len(){
-        return area;
-    }
-    let split_area = max(find(&heights[..min_i]), find(&heights[min_i+1..]));
-    max(area, split_area)
 }
