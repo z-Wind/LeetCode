01f255d9270dc974bf9fa45e4852e8a9e80d5a01
子風 <zpsyhapcst@gmail.com>
2021-08-26 12:26:08 +0800

    Time: 25 ms (7.38%), Space: 2.8 MB (22.95%) - LeetHub

--- gray-code/gray-code.rs
+++ gray-code/gray-code.rs
@@ -1,20 +1,10 @@
-//16 8 4 => repeat
-// 0 0 0
-// 0 0 1
-// 0 1 1
-// 0 1 0
-// 1 
-// 1
-// 1
-// 1
+// 2^b * x + 2^(b-1) = i
 impl Solution {
     pub fn gray_code(n: i32) -> Vec<i32> {
-        let mut ans:Vec<i32> = vec![];
-        for i in (0..2i32.pow(n as u32)){
-            let mut num = 0;
-            for j in (0..n){
-                num |= (get_bit(i>>j) << j)
-            }
+        let mut ans:Vec<i32> = vec![0];
+        let mut num = ans[0];
+        for i in (1..2i32.pow(n as u32)){
+            num = change_bit(&num, i, n);
             //println!("{} => {:0n$b}", i, num, n=n as usize);
             ans.push(num);
         }
@@ -22,10 +12,16 @@ impl Solution {
     }
 }
 
-fn get_bit(n: i32) -> i32{
-    match n%4{
-        0 | 3 => 0,
-        1 | 2 => 1,
-        _ => panic!(),
+fn change_bit(num: &i32, i:i32, n:i32) -> i32{
+    let mut bit_n:i32 = 1;
+    // 2^b * x + 2^(b-1) = i
+    for b in (1..=n){
+        if (i-(1<<(b-1))) % (1<<b) == 0{
+            //println!("{} => b:{}",i, b);
+            bit_n = (1 << (b-1));
+            break;
+        }
     }
+    //println!("{:0n$b} ^ {:0n$b} = {:0n$b}", num, bit_n, *num ^ bit_n,n=n as usize);
+    *num ^ bit_n
 }
