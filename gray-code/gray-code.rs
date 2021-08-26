12d2cc906c8a4b96a126b35ae10d63b17a6c9377
子風 <zpsyhapcst@gmail.com>
2021-08-26 14:40:58 +0800

    Time: 22 ms (7.38%), Space: 2.7 MB (50.82%) - LeetHub

--- gray-code/gray-code.rs
+++ gray-code/gray-code.rs
@@ -1,16 +1,25 @@
-// 0 -> 1
-// 0_0 0_1 -> 1_1 1_0
-// 0_00 0_01 0_11 0_10 -> 1_10 1_11 1_01 1_00 
+// https://stackoverflow.com/questions/4166106/the-nth-gray-code/4166615#4166615
+// G(i) = i^ (i/2)
+ //    N                    N>>1                  gray
+ // 0000           .        0000           .      0000           .
+ //    | >xor = 0001             >xor = 0000           >xor = 0001
+ // 0001          .         0000          .       0001          .
+ //   || >xor = 0011           | >xor = 0001           >xor = 0010
+ // 0010           .        0001           .      0011           .
+ //    | >xor = 0001             >xor = 0000           >xor = 0001
+ // 0011         .          0001         .        0010         .
+ //  ||| >xor = 0111          || >xor = 0011           >xor = 0100
+ // 0100                    0010                  0110
+// (i xor i+1) xor (i>>1 xor i+1>>1) == (i xor i>>1) xor (i+1 xor i+1>>1) == gray (i) xor gray (i+1)
 impl Solution {
     pub fn gray_code(n: i32) -> Vec<i32> {
-        let mut ans:Vec<i32> = vec![0];
-        for b in (0..n){
-            //println!("{}: {:?}", b, ans);
-            for i in (0..ans.len()).rev(){
-                ans.push(ans[i] | 1<<b);
-            }
+        let length = 2_i32.pow(n as u32);
+        let mut result = vec![0; length as usize];
+        
+        for i in 0..length {
+            result[i as usize] = i ^ (i >> 1);
         }
-        ans
+        return result;
     }
 }
 
