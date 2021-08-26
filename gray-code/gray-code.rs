8290a457df7f9d1d5b6a299516d94dbd70a44cfd
子風 <zpsyhapcst@gmail.com>
2021-08-26 13:31:24 +0800

    Time: 8 ms (98.36%), Space: 2.8 MB (22.95%) - LeetHub

--- gray-code/gray-code.rs
+++ gray-code/gray-code.rs
@@ -1,27 +1,16 @@
-// 2^b * x + 2^(b-1) = i
+// 0 -> 1
+// 0_0 0_1 -> 1_1 1_0
+// 0_00 0_01 0_11 0_10 -> 1_10 1_11 1_01 1_00 
 impl Solution {
     pub fn gray_code(n: i32) -> Vec<i32> {
         let mut ans:Vec<i32> = vec![0];
-        let mut num = ans[0];
-        for i in (1..2i32.pow(n as u32)){
-            num = change_bit(&num, i, n);
-            //println!("{} => {:0n$b}", i, num, n=n as usize);
-            ans.push(num);
+        for b in (0..n){
+            //println!("{}: {:?}", b, ans);
+            for i in (0..ans.len()).rev(){
+                ans.push(ans[i] | 1<<b);
+            }
         }
         ans
     }
 }
 
-fn change_bit(num: &i32, i:i32, n:i32) -> i32{
-    let mut bit_n:i32 = 1;
-    // 2^b * x + 2^(b-1) = i
-    for b in (1..=n){
-        if (i-(1<<(b-1))) % (1<<b) == 0{
-            //println!("{} => b:{}",i, b);
-            bit_n = (1 << (b-1));
-            break;
-        }
-    }
-    //println!("{:0n$b} ^ {:0n$b} = {:0n$b}", num, bit_n, *num ^ bit_n,n=n as usize);
-    *num ^ bit_n
-}
