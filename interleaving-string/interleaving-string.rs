c05a61aa6f0bff3e3056e938a64a00ba60699a6b
子風 <zpsyhapcst@gmail.com>
2021-08-30 15:00:46 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (50.00%) - LeetHub

--- interleaving-string/interleaving-string.rs
+++ interleaving-string/interleaving-string.rs
@@ -1,32 +1,31 @@
-use std::collections::HashMap;
+// dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+//         || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1))
+//
+//            dp[i-1][j]
+// dp[i][j-1] dp[i][j]
+//         
+//          dp[j]
+// dp[j-1]  dp[j](update)
 impl Solution {
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
+         if s1.len() + s2.len() != s3.len() {
+            return false;
+        }
         let s1 = s1.as_bytes();
         let s2 = s2.as_bytes();
         let s3 = s3.as_bytes();
-        let mut dp:HashMap<(usize,usize),bool> = HashMap::new();
-        is_interleave(s1,s2,s3,0,0,&mut dp)
+        let mut dp = vec![false; s2.len() + 1]; // include empty string
+        for i in 0..=s1.len() {
+            for j in 0..=s2.len() {
+                dp[j] = match (i, j) {
+                    (0, 0) => true,
+                    (0, _) => dp[j - 1] && s2[j - 1] == s3[i + j - 1],
+                    (_, 0) => dp[j] && s1[i - 1] == s3[i + j - 1],
+                    _ => (dp[j - 1] && s2[j - 1] == s3[i + j - 1]) || 
+                         (dp[j]     && s1[i - 1] == s3[i + j - 1]),
+                }
+            }
+        }
+        dp[s2.len()]
     }
-}
-
-fn is_interleave(s1: &[u8], s2: &[u8], s3: &[u8], i:usize, j:usize, dp:&mut HashMap<(usize,usize),bool>) -> bool{
-    if dp.contains_key(&(i,j)){
-        return *dp.get(&(i,j)).unwrap();
-    }
-    if s1.len() + s2.len() != s3.len(){
-        return false;
-    } else if i == s1.len() && j == s2.len(){
-        return true;
-    }
-    
-    let mut check = false;
-    if i<s1.len() && s3[i+j] == s1[i]{
-        check = check || is_interleave(s1, s2, s3, i+1, j, dp);
-    }
-    if j<s2.len() && s3[i+j] == s2[j]{
-        check = check || is_interleave(s1, s2, s3, i, j+1, dp);
-    }
-    
-    dp.insert((i,j),check);
-    check
 }
