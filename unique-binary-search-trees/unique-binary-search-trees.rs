dde5e3db20e8ccc395a0454fa6fa74fb8d41b645
子風 <zpsyhapcst@gmail.com>
2021-08-30 16:38:41 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (75%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.rs
+++ unique-binary-search-trees/unique-binary-search-trees.rs
@@ -1,27 +1,25 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-use std::collections::HashMap;
+// https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+// G(n): the number of unique BST for a sequence of length n.
+// F(i, n), 1 <= i <= n: the number of unique BST, where the number i is the root of BST, and the sequence ranges from 1 to n.
+// G(n) = F(1, n) + F(2, n) + ... + F(n, n). 
+// length 1 (only a root) or 0 (empty tree)
+// G(0)=1, G(1)=1. 
+// F(3, 7): the number of unique BST tree with number 3 as its root. To construct an unique BST out of the entire sequence [1, 2, 3, 4, 5, 6, 7] with 3 as the root, which is to say, we need to construct an unique BST out of its left subsequence [1, 2] and another BST out of the right subsequence [4, 5, 6, 7], and then combine them together (i.e. cartesian product). The tricky part is that we could consider the number of unique BST out of sequence [1,2] as G(2), and the number of of unique BST out of sequence [4, 5, 6, 7] as G(4). Therefore, F(3,7) = G(2) * G(4).
+// F(i, n) = G(i-1) * G(n-i)	1 <= i <= n 
+// G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0) 
 impl Solution {
     pub fn num_trees(n: i32) -> i32 {
-        let mut dp:HashMap<(i32,i32),i32> = HashMap::new();
-        num_trees(1, n, &mut dp)
-    }
-}
+        let n = n as usize;
+        let mut G = vec![0;n+1];
+        G[0] = 1; 
+        G[1] = 1;
 
-fn num_trees(left:i32 ,right:i32, dp:&mut HashMap<(i32,i32),i32>) -> i32{
-    if dp.contains_key(&(left,right)){
-        return *dp.get(&(left,right)).unwrap();
+        for i in (2..=n) {
+            for j in (1..=i) {
+              G[i] += G[j-1] * G[i-j];
+            }
+        }
+        //println!("{:?}", G);
+        return G[n];
     }
-    //println!("{},{}", left, right);
-    if left > right || left == right {
-        return 1;
-    }
-    
-    let mut count = 0;
-    for root in (left..=right){
-        count += (1 * num_trees(left,root-1,dp) * num_trees(root+1,right,dp));
-    }
-    
-    dp.insert((left,right), count);
-    count
 }
