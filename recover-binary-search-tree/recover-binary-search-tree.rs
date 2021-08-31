2a26a24595d3a584e3439ebbe0572706bf39d114
子風 <zpsyhapcst@gmail.com>
2021-08-31 14:59:42 +0800

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

--- recover-binary-search-tree/recover-binary-search-tree.rs
+++ recover-binary-search-tree/recover-binary-search-tree.rs
@@ -5,7 +5,7 @@
 //   pub left: Option<Rc<RefCell<TreeNode>>>,
 //   pub right: Option<Rc<RefCell<TreeNode>>>,
 // }
-// 
+//
 // impl TreeNode {
 //   #[inline]
 //   pub fn new(val: i32) -> Self {
@@ -16,38 +16,70 @@
 //     }
 //   }
 // }
-use std::rc::Rc;
+// https://leetcode.com/problems/recover-binary-search-tree/discuss/32559/Detail-Explain-about-How-Morris-Traversal-Finds-two-Incorrect-Pointer
 use std::cell::RefCell;
 use std::mem::swap;
+use std::rc::Rc;
 impl Solution {
     pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        let mut v:Vec<Rc<RefCell<TreeNode>>> = Vec::new();
-        traversal(root,&mut v);
-        //println!("{:?}",v);
-        
-        let mut swap_a:Option<Rc<RefCell<TreeNode>>>=None;
-        let mut swap_b:Option<Rc<RefCell<TreeNode>>>=None;
-        for pairs in v.windows(2) {
-            if swap_a.is_none() && pairs[0].borrow().val>=pairs[1].borrow().val{
-                swap_a = Some(Rc::clone(&pairs[0]));
-            }
-            if pairs[0].borrow().val>=pairs[1].borrow().val{
-                swap_b = Some(Rc::clone(&pairs[1]));
+        let mut root: Option<Rc<RefCell<TreeNode>>> = root.clone();
+        let mut pre: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut first: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut second: Option<Rc<RefCell<TreeNode>>> = None;
+        // Morris Traversal
+        let mut temp: Option<Rc<RefCell<TreeNode>>> = None;
+        while root.is_some() {
+            if root.as_ref().unwrap().borrow().left.is_some() {
+                // connect threading for root
+                temp = root.as_ref().unwrap().borrow().left.clone();
+                while (temp.as_ref().unwrap().borrow().right.is_some()
+                    && temp.as_ref().unwrap().borrow().right.as_ref().unwrap().borrow().val != root.as_ref().unwrap().borrow().val)
+                {
+                    let tt = temp.as_ref().unwrap().borrow().right.clone();
+                    temp = tt;
+                }
+                // the threading already exists
+                if temp.as_ref().unwrap().borrow().right.is_some() {
+                    if pre.is_some()
+                        && pre.as_ref().unwrap().borrow().val > root.as_ref().unwrap().borrow().val
+                    {
+                        if first.is_none() {
+                            first = pre.clone();
+                            second = root.clone();
+                        } else {
+                            second = root.clone();
+                        }
+                    }
+                    pre = root.clone();
+
+                    temp.as_ref().unwrap().borrow_mut().right = None;
+                    let rr = root.as_ref().unwrap().borrow().right.clone();
+                    root = rr;
+                } else {
+                    // construct the threading
+                    temp.as_ref().unwrap().borrow_mut().right = root.clone();
+                    let rr = root.as_ref().unwrap().borrow().left.clone();
+                    root = rr;
+                }
+            } else {
+                if pre.is_some()
+                    && pre.as_ref().unwrap().borrow().val > root.as_ref().unwrap().borrow().val
+                {
+                    if first.is_none() {
+                        first = pre.clone();
+                        second = root.clone();
+                    } else {
+                        second = root.clone();
+                    }
+                }
+                pre = root.clone();
+                let rr = root.as_ref().unwrap().borrow().right.clone();
+                root = rr;
             }
         }
-        swap(&mut swap_a.unwrap().borrow_mut().val, &mut swap_b.unwrap().borrow_mut().val);
+        // swap two node values;
+        if (first.is_some() && second.is_some()) {
+            swap(&mut first.as_ref().unwrap().borrow_mut().val, &mut second.as_ref().unwrap().borrow_mut().val);
+        }
     }
-}
-
-fn traversal<'a>(root: &'a Option<Rc<RefCell<TreeNode>>>, v:&'a mut Vec<Rc<RefCell<TreeNode>>>){
-    if root.is_none(){
-        return;
-    }
-    let mut r = root.as_ref().unwrap().borrow();
-    let left:&Option<Rc<RefCell<TreeNode>>> = &r.left;
-    traversal(left,v);
-    //println!("{}", r.val);
-    v.push(Rc::clone(root.as_ref().unwrap()));
-    let right:&Option<Rc<RefCell<TreeNode>>> = &r.right;
-    traversal(right,v);
 }
