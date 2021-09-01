74ce7aa137f15c428d379925d1865e37b31fd5df
子風 <zpsyhapcst@gmail.com>
2021-09-01 09:09:27 +0800

    Time: 0 ms (100.00%), Space: 2.7 MB (50.79%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.rs
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.rs
@@ -23,28 +23,10 @@ impl Solution {
         if root.is_none(){
             return 0;
         }
-        let mut stack:Vec<(i32, Option<Rc<RefCell<TreeNode>>>)> = Vec::new();
-        let mut depth:i32 = 1;
-        let mut max_depth:i32 = 1;
-        while root.is_some(){            
-            let left = root.as_ref().unwrap().borrow().left.clone();
-            let right = root.as_ref().unwrap().borrow().right.clone();
+                 
+        let left = root.as_ref().unwrap().borrow().left.clone();
+        let right = root.as_ref().unwrap().borrow().right.clone();
             
-            if left.is_some(){
-                stack.push((depth+1,left));    
-            }
-            if right.is_some(){
-                stack.push((depth+1,right));    
-            }
-            
-            let pairs = stack.pop();
-            if pairs.is_none(){
-                break;
-            }
-            depth = pairs.as_ref().unwrap().0;
-            root = pairs.unwrap().1;
-            max_depth = max_depth.max(depth);
-        }
-        max_depth
+        1+Self::max_depth(left).max(Self::max_depth(right))
     }
 }
