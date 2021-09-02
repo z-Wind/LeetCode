d7b33844730203d70095f38fd77b525982a969a2
子風 <zpsyhapcst@gmail.com>
2021-09-02 15:51:56 +0800

    Time: 2 ms (33.33%), Space: 2 MB (66.67%) - LeetHub

--- distinct-subsequences/distinct-subsequences.rs
+++ distinct-subsequences/distinct-subsequences.rs
@@ -17,22 +17,31 @@
 // dp[i][j] = dp[i][j-1]                    when s[j-1] != t[i-1]
 // dp[i][j] = dp[i][j-1]   +   dp[i-1][j-1] when s[j-1] == t[i-1]
 //           exclude s[j-1]   include s[j-1]
+//
+// dp[i-1][j-1]  dp[i-1][j]
+// dp[i][j-1]    dp[i][j]
+//
+// pre      dp[i-1]
+// dp[i]    dp[i](update)
 impl Solution {
     pub fn num_distinct(s: String, t: String) -> i32 {
         let s = s.as_bytes();
         let t = t.as_bytes();
         let n = s.len();
         let m = t.len();
-        let mut dp:Vec<Vec<i32>> = vec![vec![0;n+1];m+1];
-        for j in (0..=n) { dp[0][j] = 1;}
+        let mut dp:Vec<i32> = vec![0;m+1];
+        dp[0] = 1;
         for j in (1..=n){
+            let mut pre = dp[0];
             for i in (1..=m){
-                dp[i][j] = match s[j-1] == t[i-1]{
-                    false => dp[i][j-1],
-                    true => dp[i][j-1] + dp[i-1][j-1],
-                }
+                let temp = dp[i];
+                dp[i] += match s[j-1] == t[i-1]{
+                    false => 0,
+                    true => pre,
+                };
+                pre = temp;
             }
         }
-        return dp[m][n];
+        return dp[m];
     }
 }
