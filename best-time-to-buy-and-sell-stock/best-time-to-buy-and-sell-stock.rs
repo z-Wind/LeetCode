ac9d360e2a519c71f55383c89125d6c76894377f
子風 <zpsyhapcst@gmail.com>
2021-09-03 13:35:29 +0800

    Time: 33 ms (5.52%), Space: 2.8 MB (89.57%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
@@ -1,22 +1,14 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut profit = 0;
-        let mut min_buy = (0, i32::MAX);
-        let mut max_sell = (0, i32::MIN);
+        let mut max_profit = 0;
+        let mut min_buy = i32::MAX;
         for i in (0..prices.len()){
-            if max_sell.1 < prices[i]{
-                max_sell = (i, prices[i]);
-                if max_sell.0 > min_buy.0 {
-                    profit = profit.max(prices[i]-min_buy.1);
-                }
-            }
-            if min_buy.1 > prices[i]{
-                min_buy = (i, prices[i]);
-                if i>max_sell.0{
-                    max_sell = (0, i32::MIN);
-                }
+            if min_buy > prices[i]{
+                min_buy = prices[i];
+            } else if prices[i] - min_buy > max_profit{
+                max_profit = prices[i] - min_buy;
             }
         }
-        profit
+        max_profit
     }
 }
