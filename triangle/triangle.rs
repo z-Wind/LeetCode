5599fcb80ff32c4cfc24d8b4bedfa25e3f7bbbd6
子風 <zpsyhapcst@gmail.com>
2021-09-03 10:48:33 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (18.75%) - LeetHub

--- triangle/triangle.rs
+++ triangle/triangle.rs
@@ -1,20 +1,22 @@
 use std::collections::HashMap;
 impl Solution {
-    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-        let mut dp:HashMap<(usize,usize),i32> = HashMap::new();
-        minimum_total(&mut dp, &triangle, 0, 0)
+    pub fn minimum_total(mut triangle: Vec<Vec<i32>>) -> i32 {
+        let sum = minimum_total(&mut triangle, 0, 0);
+        //println!("{:?}",triangle);
+        sum
     }
 }
 
-fn minimum_total(dp:&mut HashMap<(usize,usize),i32>, triangle: &Vec<Vec<i32>>, row:usize, col:usize) -> i32{
+fn minimum_total(triangle: &mut Vec<Vec<i32>>, row:usize, col:usize) -> i32{
     if row == triangle.len()-1{
         return triangle[row][col];
     }
-    if dp.contains_key(&(row,col)){
-        return *dp.get(&(row,col)).unwrap()
+    let mut sum=0;
+    if col == 0{
+        sum = triangle[row][col] + minimum_total(triangle, row+1,col).min(minimum_total(triangle, row+1,col+1));
+    } else {
+        sum = triangle[row][col] + triangle[row+1][col].min(minimum_total(triangle, row+1,col+1));
     }
-    
-    let sum = triangle[row][col] + minimum_total(dp, triangle, row+1,col).min(minimum_total(dp, triangle, row+1,col+1));
-    dp.insert((row,col), sum);
-    sum
+    triangle[row][col] = sum;
+    sum    
 }
