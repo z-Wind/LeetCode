f98062b00b38c1ce250a1c633e78bdd6b4417cef
子風 <zpsyhapcst@gmail.com>
2021-09-06 16:33:20 +0800

    Time: 88 ms (66.67%), Space: 3 MB (66.67%) - LeetHub

--- word-ladder/word-ladder.rs
+++ word-ladder/word-ladder.rs
@@ -1,46 +1,55 @@
-use std::collections::HashSet;
 use std::collections::VecDeque;
 impl Solution {
-    pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
-        let mut words = HashSet::new();
-
-        for word in word_list {
-            words.insert(word);
+    pub fn ladder_length(begin_word: String, end_word: String, mut word_list: Vec<String>) -> i32 {
+        let mut ans:Vec<Vec<String>> = Vec::new();
+        let mut queue:VecDeque<Vec<String>> = VecDeque::new();
+        
+        match word_list.iter().position(|x| x == &begin_word){
+            None => queue.push_back(vec![begin_word]),
+            Some(i) => queue.push_back(vec![word_list.remove(i)]),
         }
-        if !words.contains(&end_word) {
-            return 0;
-        }
-
-        let mut distance: i32 = 1;
-        let mut q = VecDeque::new();
-        q.push_back(begin_word.clone());
-        let mut reached = HashSet::new();
-        reached.insert(begin_word);
-        let letters = (b'a'..=b'z').map(char::from).collect::<Vec<_>>();
-        while q.len() > 0 {
-            let q_size = q.len();
-            for _i in 0..q_size {
-                let curr_word = q.pop_front().unwrap();
-                for j in 0..curr_word.len() {
-                    for k in &letters {
-                        let new_word = (&curr_word[0..j]).to_string() + &(k.to_string()) + &curr_word[j+1..];
-                        if new_word == end_word {
-                            return distance+1;
+        
+        'outer: loop{
+            let len = queue.len();
+            if len == 0{
+                break;
+            }
+            // println!("queue:{:?}", queue);
+            // println!("word_list:{:?}", word_list);
+            for _ in (0..len){
+                let mut seq = queue.pop_front().unwrap();
+                if !ans.is_empty() && seq.len() >= ans[0].len() {
+                    break 'outer;
+                }
+                // println!("seq:{:?}",seq);
+                let key = seq.last().unwrap();
+                for i in (0..word_list.len()).rev(){
+                    if is_single(key.as_bytes(), word_list[i].as_bytes()){
+                        let word = word_list.remove(i);
+                        if word == end_word{
+                            return seq.len() as i32 + 1;
                         }
 
-                        if !words.contains(&new_word) || new_word == curr_word {
-                            continue;
-                        }
-
-                        if !reached.contains(&new_word) {
-                            q.push_back(new_word.clone());
-                            reached.insert(new_word);
-                        }
+                        let mut tmp_s = seq.clone();
+                        tmp_s.push(word.clone());
+                        queue.push_back(tmp_s);
                     }
                 }
             }
-            distance += 1;
         }
         0
     }
+}
+
+fn is_single(s1:&[u8], s2:&[u8]) -> bool{
+    let mut count = 0;
+    for i in (0..s1.len()){
+        if s1[i] != s2[i]{
+            count += 1;
+        }
+        if count > 1{
+            return false;
+        }
+    }
+    count == 1
 }
