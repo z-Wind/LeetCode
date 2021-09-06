3af99b327d808e47ae3502d6e00a80734d9c0fc2
子風 <zpsyhapcst@gmail.com>
2021-09-06 09:11:52 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (98.39%) - LeetHub

--- valid-palindrome/valid-palindrome.rs
+++ valid-palindrome/valid-palindrome.rs
@@ -1,26 +1,18 @@
 impl Solution {
     pub fn is_palindrome(mut s: String) -> bool {
-        s.make_ascii_lowercase();
         let s = s.as_bytes();
         let mut i = 0;
         let mut j = s.len()-1;
         while i < j{
-            match s[i]{
-                b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' => (),
-                _ => {
-                    i+=1;
-                    continue;
-                },
+            if !s[i].is_ascii_alphanumeric(){
+                i+=1;
+                continue
             }
-            match s[j]{
-                b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' => (),
-                _ => {
-                    j-=1;
-                    continue;
-                },
+            if !s[j].is_ascii_alphanumeric(){
+                j-=1;
+                continue
             }
-            
-            if s[i] != s[j] {
+            if !s[i].eq_ignore_ascii_case(&s[j]) {
                 return false;
             }
             i+=1;
