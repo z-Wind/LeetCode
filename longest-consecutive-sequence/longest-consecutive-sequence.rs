274941e5a112339cee2bc0bace469e2e1e68dc2b
子風 <zpsyhapcst@gmail.com>
2021-09-07 10:30:57 +0800

    Time: 12 ms (73.97%), Space: 3.6 MB (52.05%) - LeetHub

--- longest-consecutive-sequence/longest-consecutive-sequence.rs
+++ longest-consecutive-sequence/longest-consecutive-sequence.rs
@@ -1,23 +1,36 @@
+use std::collections::HashSet;
 impl Solution {
     pub fn longest_consecutive(mut nums: Vec<i32>) -> i32 {
         if nums.is_empty(){
             return 0;
         }
-        nums.sort();
-        // println!("{:?}", nums);
-        let mut max_count = 0;
-        let mut count = 1;
-        for w in nums.windows(2){
-            if (w[0] + 1) == w[1]{
-                count += 1;
-            } else if w[0] == w[1]{
-                continue;
-            } else {
-                max_count = max_count.max(count);
-                count = 1;
-            }
+        
+        let mut set:HashSet<i32> = HashSet::new();
+        for num in nums{
+            set.insert(num);
         }
-        max_count = max_count.max(count);
+        // println!("{:?}", set);
+        let mut max_count = 1;
+        while !set.is_empty(){
+            let mut count = 1;
+            let num = set.iter().next().unwrap().clone();
+            set.remove(&num);
+            
+            let mut num_add = num + 1;
+            while let Some(_) = set.take(&num_add){
+                count+=1;
+                num_add+=1;
+            }
+            
+            let mut num_sub = num - 1;
+            while let Some(_) = set.take(&num_sub){
+                count+=1;
+                num_sub-=1;
+            }
+            
+            max_count = max_count.max(count);
+            // println!("{}: {:?}",num, set);
+        }
         max_count
     }
 }
