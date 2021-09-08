f8e1d4a702413aafb08de2ba10413034ea51890a
子風 <zpsyhapcst@gmail.com>
2021-09-08 10:40:14 +0800

    Time: 100 ms (94.29%), Space: 20.4 MB (94.29%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.rs
+++ palindrome-partitioning/palindrome-partitioning.rs
@@ -3,31 +3,29 @@ impl Solution {
     pub fn partition(s: String) -> Vec<Vec<String>> {
         let mut ans:Vec<Vec<String>> = Vec::new();
         let mut temp:Vec<String> = Vec::new();
-        backtrack(&mut ans, &mut temp, &s, 0);
+        let mut dp:Vec<Vec<bool>> = vec![vec![false;s.len()];s.len()];
+        backtrack(&mut dp, &mut ans, &mut temp, &s, 0);
         ans
     }
 }
 
-fn backtrack(ans:&mut Vec<Vec<String>>,temp: &mut Vec<String>, s:&str, start:usize){
+fn backtrack(dp:&mut Vec<Vec<bool>>, ans:&mut Vec<Vec<String>>,temp: &mut Vec<String>, s:&str, start:usize){
     if start >= s.len(){
         ans.push(temp.clone());
         return;
     }
     
     for i in (start..s.len()){
-        if is_palindrome(&s.as_bytes()[start..=i]){
+        if is_palindrome(dp, &s.as_bytes(), start, i){
             temp.push(String::from(&s[start..=i]));
-            backtrack(ans,temp,s,i+1);
+            backtrack(dp,ans,temp,s,i+1);
             temp.pop();
         }
     }
 }
 
-fn is_palindrome(s:&[u8]) -> bool{
-    for i in (0..s.len()/2){
-        if s[i] != s[s.len()-1-i]{
-            return false;
-        }
-    }
-    true
+fn is_palindrome(dp:&mut Vec<Vec<bool>>, s:&[u8], start:usize, end:usize) -> bool{
+    let check = s[start] == s[end] && (end-start <=2 || dp[start+1][end-1]);
+    dp[start][end] = check;
+    check
 }
