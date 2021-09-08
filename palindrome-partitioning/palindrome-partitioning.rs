dd766942c27eb01466ac6afc29aa190c4068275f
子風 <zpsyhapcst@gmail.com>
2021-09-08 10:18:14 +0800

    Time: 96 ms (100.00%), Space: 20.7 MB (40.00%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.rs
+++ palindrome-partitioning/palindrome-partitioning.rs
@@ -1,41 +1,26 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn partition(s: String) -> Vec<Vec<String>> {
-        let mut dp:HashMap<usize, Vec<Vec<String>>> = HashMap::new();
-        backward(&mut dp, &s, 0)
+        let mut ans:Vec<Vec<String>> = Vec::new();
+        let mut temp:Vec<String> = Vec::new();
+        backtrack(&mut ans, &mut temp, &s, 0);
+        ans
     }
 }
 
-fn backward(dp:&mut HashMap<usize,Vec<Vec<String>>>, s:&str, start:usize) -> Vec<Vec<String>>{
-    let mut ans:Vec<Vec<String>> = Vec::new();
+fn backtrack(ans:&mut Vec<Vec<String>>,temp: &mut Vec<String>, s:&str, start:usize){
     if start >= s.len(){
-        return ans;
-    }
-    if dp.contains_key(&start){
-        return dp.get(&start).unwrap().to_vec();
+        ans.push(temp.clone());
+        return;
     }
     
     for i in (start..s.len()){
         if is_palindrome(&s.as_bytes()[start..=i]){
-            let mut words = backward(dp,s,i+1);
-            if words.is_empty(){
-                let mut temp:Vec<String> = Vec::new();
-                temp.push(String::from(&s[start..=i]));
-                ans.push(temp);
-                continue;
-            }
-            for mut word in words{
-                let mut temp:Vec<String> = Vec::new();
-                temp.push(String::from(&s[start..=i]));
-                temp.append(&mut word);
-                ans.push(temp);
-            };
+            temp.push(String::from(&s[start..=i]));
+            backtrack(ans,temp,s,i+1);
+            temp.pop();
         }
     }
-    
-    dp.insert(start, ans.clone());
-    // println!("{}: {:?}", &s[start..],ans);
-    ans
 }
 
 fn is_palindrome(s:&[u8]) -> bool{
