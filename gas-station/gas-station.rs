944c26ea2daed575e99aad346ed0cad385f045ad
子風 <zpsyhapcst@gmail.com>
2021-09-09 16:20:38 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (55.00%) - LeetHub

--- gas-station/gas-station.rs
+++ gas-station/gas-station.rs
@@ -2,24 +2,18 @@ use std::collections::VecDeque;
 
 impl Solution {
     pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
-        let mut queue: VecDeque<(usize,usize, i32)> = VecDeque::new();
-        
-        for i in (0..gas.len()){
-            if cost[i] <= gas[i]{
-                let end = i.checked_sub(1).unwrap_or(gas.len()-1);
-                queue.push_back((i,end,gas[i]-cost[i]))
-            }
-        }
-        while !queue.is_empty(){
-            let len = queue.len();
-            for _ in (0..len){
-                let (mut cur,end,mut rest) = queue.pop_front().unwrap();
-                let i = (cur + 1) % gas.len();
-                rest += gas[i]-cost[i];
-                if i == end && rest >= 0{
-                    return ((end+1) % gas.len()) as i32;
-                } else if rest >= 0{
-                    queue.push_back((i,end,rest));
+        let mut n = 0;       
+        let mut rest = 0;
+        for i in (0..gas.len()*2){
+            let i = i % gas.len();
+            rest += gas[i]-cost[i];
+            if rest < 0{
+                rest = 0;
+                n = 0;
+            } else {
+                n += 1;
+                if n == gas.len(){
+                    return ((i+1) % gas.len()) as i32;
                 }
             }
         }
