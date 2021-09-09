ac5c919ac006cc9c3e24be9f63387941205bcc91
子風 <zpsyhapcst@gmail.com>
2021-09-09 16:30:44 +0800

    Time: 3 ms (55.00%), Space: 2.2 MB (75.00%) - LeetHub

--- gas-station/gas-station.rs
+++ gas-station/gas-station.rs
@@ -1,5 +1,14 @@
-use std::collections::VecDeque;
-
+// Facts:
+// 1. A cannot reach B
+// 2. There are C1,C2, ..., Ck between A and B
+// 3. A can reach C1, C2, ..., Ck
+// A --- C1 --- C2  --- ... Ck --- B
+// Proof by contradiction:
+// Assume: C1 can reach B
+// A can reach C1 (by Fact3) & C1 can reach B => A can reach B (Contradict with Fact1 !)
+// => assumption is wrong, C1 cannot reach B
+// Same proof could be applied to C2 ~ Ck
+// => any station between A and B that A can reach cannot reach B
 impl Solution {
     pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
         let mut n = 0;       
