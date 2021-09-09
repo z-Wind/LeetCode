6f25bf1b4e9f2937bd44bfc65b059329b10d54b0
子風 <zpsyhapcst@gmail.com>
2021-09-09 13:20:59 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (92.94%) - LeetHub

--- palindrome-partitioning-ii/palindrome-partitioning-ii.rs
+++ palindrome-partitioning-ii/palindrome-partitioning-ii.rs
@@ -1,29 +1,25 @@
 // https://leetcode.com/problems/palindrome-partitioning-ii/discuss/42198/My-solution-does-not-need-a-table-for-palindrome-is-it-right-It-uses-only-O(n)-space.
-use std::cmp::min;
 impl Solution {
     pub fn min_cut(s: String) -> i32 {
         let s = s.as_bytes();
         let n = s.len();
-        let mut cut = vec![0;n+1];  // number of cuts for the first k characters
-        for i in (0..=n) {
-            cut[i] = i-1;
-        }
-        for i in (0..n) {
+        let mut cut:Vec<i32> = (-1..=n as i32).collect();  // number of cuts for the first k characters
+        for mid in (0..n) {
             
             // odd length palindrome
-            let mut j = 0;
-            while i >= j && i+j < n && s[i-j]==s[i+j] {
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j]);
-                j+=1;
+            let mut shift = 0;
+            while mid >= shift && mid+shift < n && s[mid-shift]==s[mid+shift] {
+                cut[mid+shift+1] = cut[mid+shift+1].min(1+cut[mid-shift]);
+                shift+=1;
             }
             
             // even length palindrome
-            j = 1;
-            while i+1 >= j && i+j < n && s[i-j+1] == s[i+j] {  
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j+1]);
-                j+=1;
+            shift = 1;
+            while mid+1 >= shift && mid+shift < n && s[mid-shift+1] == s[mid+shift] {  
+                cut[mid+shift+1] = cut[mid+shift+1].min(1+cut[mid-shift+1]);
+                shift+=1;
             }
         }
-        cut[n] as i32
+        cut[n]
     }
 }
