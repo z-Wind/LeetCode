a43196dc0e44a07e4efce7487fb2c7eae01e0694
子風 <zpsyhapcst@gmail.com>
2021-09-10 11:08:07 +0800

    Time: 6 ms (41.67%), Space: 2.2 MB (100.00%) - LeetHub

--- candy/candy.rs
+++ candy/candy.rs
@@ -29,32 +29,25 @@ impl Solution {
             }
                     
             match (&pre_state, &state){
+                (Order::UNKNOW, Order::ASC) | 
+                (Order::ASC, Order::ASC) | 
+                (Order::UNKNOW, Order::DESC) |
+                (Order::DESC, Order::DESC) |
+                (Order::EQ, Order::ASC) | 
+                (Order::EQ, Order::DESC) => (),
+                // 1 2 3 2 1 
+                (Order::ASC,Order::DESC) => {
+                    mid = i;
+                },
                 // 1 2 2
                 // 2 2 1
-                (Order::ASC,Order::EQ) | 
-                (Order::UNKNOW,Order::EQ) => {
-                    if left == mid{
-                        mid = i;
-                    }
-                    sum += candy_sum(left,mid,right);
-                    left = right + 1;
-                    mid = right + 1;  
-                },
-                // 2 2 2
-                (Order::EQ,Order::EQ) => {
-                    sum += 1;  
-                    left = right + 1;
-                    mid = right + 1; 
-                },
                 // 1 3 2 2
+                (Order::ASC,Order::EQ) | 
+                (Order::UNKNOW,Order::EQ) |
                 (Order::DESC,Order::EQ) => {
                     sum += candy_sum(left,mid,right);
                     left = right + 1;
                     mid = right + 1;  
-                },
-                // 1 2 3 2 1 
-                (Order::ASC,Order::DESC) => {
-                    mid = i;
                 },
                 // 1 2 3 2 1 2
                 (Order::DESC,Order::ASC) => {
@@ -63,18 +56,18 @@ impl Solution {
                     left = right;
                     mid = right; 
                 },
-                (Order::UNKNOW, Order::ASC) | 
-                (Order::ASC, Order::ASC) | 
-                (Order::UNKNOW, Order::DESC) |
-                (Order::DESC, Order::DESC) |
-                (Order::EQ, Order::ASC) | 
-                (Order::EQ, Order::DESC) => (),
+                // 2 2 2
+                (Order::EQ,Order::EQ) => {
+                    sum += 1;  
+                    left = right + 1;
+                    mid = right + 1; 
+                },
                 (pre,cur) => panic!(format!("{:?} {},{},{} => {:?},{:?}",ratings,left,mid,right,pre,cur)),
             }
             
             pre_state = state;
         }
-        //for 1 2 3 or 3 2 1
+        // for 1 2 3 or 3 2 1
         sum += candy_sum(left,mid,ratings.len()-1);
         
         sum
@@ -88,9 +81,9 @@ fn candy_sum(left:usize, mid:usize, right:usize) -> i32 {
     let mut sum = 0;
     
     if left_n >= right_n{
-        sum = (left_n+1)*left_n/2+(right_n-1)*right_n/2;
+        sum = (left_n+1)*left_n/2 + (right_n-1)*right_n/2;
     } else {
-        sum = (right_n+1)*right_n/2+(left_n-1)*left_n/2;
+        sum = (right_n+1)*right_n/2 + (left_n-1)*left_n/2;
     }
     
     // println!("{},{},{} => {}",left,mid,right,sum);
