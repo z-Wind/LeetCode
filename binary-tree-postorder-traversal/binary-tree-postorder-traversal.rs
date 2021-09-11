62b238c2bd4aab021669662d24e466c76b4cd24b
子風 <zpsyhapcst@gmail.com>
2021-09-11 12:48:42 +0800

    Time: 0 ms (100.00%), Space: 1.9 MB (92.31%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.rs
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.rs
@@ -19,9 +19,36 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
-    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {     
+        if root.is_none(){
+            return vec![];
+        }
         let mut ans:Vec<i32> = Vec::new();
-        postorder_traversal(&mut ans, root);
+        let mut stack:Vec<Option<Rc<RefCell<TreeNode>>>> = Vec::new();
+        stack.push(root);
+        while !stack.is_empty(){
+            let root = stack.pop().unwrap();
+            let left = root.as_ref().unwrap().borrow_mut().left.take();
+            let right = root.as_ref().unwrap().borrow_mut().right.take();
+            match (left, right){
+                (None, None) => {
+                    let val = root.as_ref().unwrap().borrow().val;
+                    //println!("{}",val);
+                    ans.push(val);
+                }
+                (Some(x),None) | (None, Some(x)) => {
+                    stack.push(root);
+                    stack.push(Some(x));    
+                }
+                (Some(l),Some(r)) => {
+                    stack.push(root);
+                    stack.push(Some(r));    
+                    stack.push(Some(l));    
+                }
+            }
+        }
+        
+        
         ans
     }
 }
