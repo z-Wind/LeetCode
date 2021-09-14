469d6dbad7be986378f53dbb7878783953d5ebd7
子風 <zpsyhapcst@gmail.com>
2021-09-14 15:06:22 +0800

    Time: 3 ms (33.33%), Space: 2.1 MB (83.33%) - LeetHub

--- find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.rs
+++ find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.rs
@@ -1,35 +1,47 @@
-use std::cmp::min;
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/discuss/48808/My-pretty-simple-code-to-solve-it/281823
+//
+// nums[start] vs nums[mid]
+// case   nums[start]   ?   nums[mid]   ?   nums[end]    example      min_location
+// 1                    <               <              [1 2 3 3 4]      left
+// 2                    <               =              [1 2 3 3 3]      left
+// 3                    <               >              [3 4 5 1 2]      right
+// 4                    =               <              [1 1 1 2 3]      left
+// 5                    =               =              [3 3 3 2 3]      right
+// 6                    =               >              [3 3 3 1 2]      right
+// 7                    >               <              [4 2 3 3 4]      left
+// 8                    >               =              [4 2 3 3 3]      left
+// 9                    >               >              [5 4 3 2 1] => impossible
+//
+// nums[end] vs nums[mid]
+// case   nums[start]   ?   nums[mid]   ?   nums[end]    example      min_location
+// 1                    <               <              [1 2 3 3 4]      left
+// 4                    =               <              [1 1 1 2 3]      left
+// 7                    >               <              [4 2 3 3 4]      left
+// 2                    <               =              [1 2 3 3 3]      left
+// 5                    =               =              [3 3 3 2 3]      right
+// 8                    >               =              [4 2 3 3 3]      left
+// 3                    <               >              [3 4 5 1 2]      right
+// 6                    =               >              [3 3 3 1 2]      right
+// 9                    >               >              [5 4 3 2 1] => impossible
+//
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
-        search(&nums,0,nums.len()-1)
-    }
-}
-
-fn search(nums: &Vec<i32>, start: usize, end: usize) -> i32 {
-    if start == end{
-        return nums[start]
-    }
-    let mid = (start + end)/2;
-
-    // 1 2 2 4 5
-    if (nums[end] > nums[start]){
-         search(&nums, start, mid)
-    // 3 4 5 1 2 or 2 1
-    } else if (nums[end] < nums[start]){
-        // 3 4 5
-        if nums[mid] >= nums[start]{
-            search(&nums, mid+1, end)
-        // 3 4 1
-        } else {//if nums[mid] < nums[start] {
-            search(&nums, start, mid)           
+        let mut start = 0;
+        let mut end = nums.len()-1;
+        let mut mid = 0;
+        while start < end {
+            mid = (start + end) / 2;
+            
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            }
+            else if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            }
+            else { // if nums[mid] == nums[end]
+                end-=1;
+            }
         }
-    } else { // 3 4 5 2 3 or 3 4 1 2 2 2 3 or 4 1 2 3 
-        if nums[mid] > nums[start]{
-            search(&nums, mid+1, end)
-        } else if nums[mid] < nums[start] {
-            search(&nums, start, mid)
-        } else {
-            min(search(&nums, start, mid), search(&nums, mid+1, end))   
-        }
+        nums[start]
     }
 }
