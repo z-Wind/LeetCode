01da7c170d1ff244a3695b65226755ae891e311b
子風 <zpsyhapcst@gmail.com>
2021-09-14 08:59:25 +0800

    Time: 3 ms (15.38%), Space: 2.3 MB (38.46%) - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.rs
+++ reverse-words-in-a-string/reverse-words-in-a-string.rs
@@ -1,8 +1,7 @@
 impl Solution {
     pub fn reverse_words(s: String) -> String {
         s.split_ascii_whitespace().rev()
-            .fold(String::new(), |acc, s| acc + s + " ")
-            .trim_end()
-            .to_string()
+            .collect::<Vec<&str>>()
+            .join(" ")
     }
 }
