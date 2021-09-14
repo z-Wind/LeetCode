3e06081dd3c7b0c18b682ebeb3c53e5e85e9b7b2
子風 <zpsyhapcst@gmail.com>
2021-09-14 15:55:49 +0800

    Time: 2 ms (18.18%), Space: 1.9 MB (90.91%) - LeetHub

--- find-peak-element/find-peak-element.rs
+++ find-peak-element/find-peak-element.rs
@@ -7,11 +7,16 @@ impl Solution {
             return len as i32 - 1;
         }
         
-        for (i,w) in nums.windows(3).enumerate(){
-            if w[0] < w[1] && w[1] > w[2]{
-                return i as i32 + 1;
+        let mut start = 0;
+        let mut end = len-1;
+        while end > start{
+            let mid = (start+end)/2;
+            if nums[mid+1] > nums[mid]{
+                start = mid+1;
+            } else {
+                end = mid;
             }
         }
-        -1
+        start as i32
     }
 }
