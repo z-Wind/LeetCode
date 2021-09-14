f90317aecd957c2803fdcace0d7a26a5c7727025
子風 <zpsyhapcst@gmail.com>
2021-09-14 10:32:18 +0800

    Time: 0 ms (100.00%), Space: 2 MB (96.67%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.rs
+++ maximum-product-subarray/maximum-product-subarray.rs
@@ -1,23 +1,28 @@
+use std::mem::swap;
 impl Solution {
     pub fn max_product(nums: Vec<i32>) -> i32 {
-        if nums.len() == 1{
-            return nums[0];
-        }
-        
-        let mut max_product = i32::MIN;
-        for i in (0..nums.len()){
-            let mut product = nums[i];
-            if product>max_product{
-                max_product = product;
+        // store the result that is the max we have found so far
+        let mut r = nums[0];
+
+        // imax/imin stores the max/min product of
+        // subarray that ends with the current number A[i]
+        let mut imax = r;
+        let mut imin = r;
+        for i in (1..nums.len()) {
+            // multiplied by a negative makes big number smaller, small number bigger
+            // so we redefine the extremums by swapping them
+            if (nums[i] < 0) {
+                swap(&mut imax, &mut imin);
             }
-            for j in (i+1..nums.len()){
-                product*=nums[j];
-                if product>max_product{
-                    max_product = product;
-                }
-            }
+
+            // max/min product for the current number is either the current number itself
+            // or the max/min by the previous number times the current one
+            imax = nums[i].max(imax * nums[i]);
+            imin = nums[i].min(imin * nums[i]);
+
+            // the newly computed max value is a candidate for our global result
+            r = r.max(imax);
         }
-        
-        max_product
+        r
     }
 }
