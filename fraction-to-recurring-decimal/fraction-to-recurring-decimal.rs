d06b2070dca72ea701ef7e4b81ae8ca587e39cc3
子風 <zpsyhapcst@gmail.com>
2021-09-15 16:09:04 +0800

    Time: 0 ms (100.00%), Space: 2.4 MB (33.33%) - LeetHub

--- fraction-to-recurring-decimal/fraction-to-recurring-decimal.rs
+++ fraction-to-recurring-decimal/fraction-to-recurring-decimal.rs
@@ -1,57 +1,43 @@
+// https://leetcode.com/problems/fraction-to-recurring-decimal/discuss/51106/My-clean-Java-solution
+use std::collections::HashMap;
 impl Solution {
     pub fn fraction_to_decimal(numerator: i32, denominator: i32) -> String {
         if numerator == 0{
-            return String::from("0");
+            return 0.to_string();
         }
+        
+        let mut result = String::new();
         let sign = match (numerator<0,denominator<0){
-            (false,false) | (true,true) => 1,
-            _ => -1,
+            (false,false) | (true,true) => (),
+            _ => result.push('-'),
         };
         
         let mut numerator = (numerator as i64).abs();
         let mut denominator = (denominator as i64).abs();
         
-        let i = numerator/denominator;
+        result.push_str(&(numerator/denominator).to_string());
         numerator %= denominator;
         if numerator == 0{
-            return add_sign(sign, i.to_string());
+            return result;
         }
+        result.push('.');
         
-        let mut s = vec![];
+        let mut map = HashMap::new();
+        let mut i = result.len();
         while numerator != 0{
             numerator *= 10;
-            s.push((numerator,denominator));
-            match check_repeat(&s[..]){
-                None => (),
-                Some((start,end)) => {
-                    let f = s.iter().map(|(num,den)| (num/den).to_string()).collect::<Vec<String>>().join("");
-                    let r = &f[start..=end];
-                    return add_sign(sign,format!("{}.{}({})",i,f.replace(r,""),r));
-                },
+            if map.contains_key(&numerator){
+                i = *map.get(&numerator).unwrap();
+                result.insert(i,'(');
+                result.push(')');
+                break;
             }
-            
+            map.insert(numerator,i);
+            result.push_str(&(numerator/denominator).to_string());            
+            i+=1;
             numerator %= denominator;
         }
         
-        let f = s.iter().map(|(num,den)| (num/den).to_string()).collect::<Vec<String>>().join("");
-        add_sign(sign, i.to_string() + "." + &f)
+        result
     }
-}
-
-fn check_repeat(s:&[(i64,i64)]) -> Option<(usize,usize)>{
-    let len = s.len();
-    for i in (1..=len/2){        
-        // println!("{:?} ==? {:?}", &s[len-i..], &s[len-i*2..len-i]);
-        if s[len-i..] == s[len-i*2..len-i]{
-            return Some((len-i,len-1));
-        }
-    }
-    return None;
-}
-
-fn add_sign(sign:i32, s:String) -> String{
-    if sign == 1{
-        return s
-    }
-    "-".to_owned() + &s
 }
