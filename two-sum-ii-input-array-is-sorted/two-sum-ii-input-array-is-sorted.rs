918e9b78ed3ee51cdd9e14825b290078d704f80e
子風 <zpsyhapcst@gmail.com>
2021-09-16 08:37:17 +0800

    Time: 56 ms (5.68%), Space: 2.3 MB (31.82%) - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -1,8 +1,11 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
         for i in (0..numbers.len()){
+            let rest = target - numbers[i];
             for j in (i+1..numbers.len()){
-                if numbers[i] + numbers[j] == target{
+                if numbers[j] > rest{
+                    break;
+                } else if rest == numbers[j]{
                     return vec![i as i32 + 1,j as i32 + 1];
                 }
             }
