7209ef751d2734a0ab08e2b1fd4b2edf30b3a716
子風 <zpsyhapcst@gmail.com>
2021-09-16 09:50:02 +0800

    Time: 1 ms (26.32%), Space: 2.1 MB (36.84%) - LeetHub

--- excel-sheet-column-title/excel-sheet-column-title.rs
+++ excel-sheet-column-title/excel-sheet-column-title.rs
@@ -1,17 +1,10 @@
 impl Solution {
     pub fn convert_to_title(column_number: i32) -> String {
-        let mut result = String::new();
+        if column_number == 0{
+            return String::from("");
+        }
         
-        let mut q = column_number;
-        while q != 0{
-            let r = (q-1) % 26;
-            result.push((b'A' + r as u8) as char);
-            match q%26{
-                0 => q=q/26-1,
-                _ => q/=26,
-            }
-        } 
-        
-        result.chars().rev().collect::<String>()
+        let q = column_number-1;
+        Self::convert_to_title(q / 26) + &((b'A' + (q%26) as u8) as char).to_string()
     }
 }
