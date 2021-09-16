eaf7201a082c17884177604271ecf5c56b650f03
子風 <zpsyhapcst@gmail.com>
2021-09-16 19:04:48 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (60.00%) - LeetHub

--- dungeon-game/dungeon-game.rs
+++ dungeon-game/dungeon-game.rs
@@ -5,29 +5,29 @@
 // dp[i,j]   dp[i][j+1]
 // dp[i+1,j]
 //
+// dp[j](update) dp[j+1]
+// dp[j]
+//
 
 impl Solution {
     pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
         let m = dungeon.len();
         let n = dungeon[0].len();
-        let mut dp = vec![vec![0;n];m];
+        let mut dp = vec![0;n];
         
-        dp[m-1][n-1] = health(-dungeon[m-1][n-1] + 1);
-        
-        for i in (0..m-1).rev(){
-            dp[i][n-1] = health(dp[i+1][n-1]-dungeon[i][n-1]);
-        }
+        dp[n-1] = health(-dungeon[m-1][n-1] + 1);
         for j in (0..n-1).rev(){
-            dp[m-1][j] = health(dp[m-1][j+1]-dungeon[m-1][j]);
+            dp[j] = health(dp[j+1]-dungeon[m-1][j]);
         }
         
         for i in (0..m-1).rev(){
+            dp[n-1] = health(dp[n-1]-dungeon[i][n-1]);
             for j in (0..n-1).rev(){
-                dp[i][j] = health(dp[i+1][j].min(dp[i][j+1]) - dungeon[i][j]);
+                dp[j] = health(dp[j].min(dp[j+1]) - dungeon[i][j]);
             }
         }
-        // println!("{:?}", dp);
-        dp[0][0]
+
+        dp[0]
     }
 }
 
