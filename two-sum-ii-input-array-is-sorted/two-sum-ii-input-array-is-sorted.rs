860c359e9e009e46bb4810a677038c1361f313f8
子風 <zpsyhapcst@gmail.com>
2021-09-16 08:58:20 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (64.77%) - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -1,20 +1,13 @@
 use std::collections::BTreeMap;
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map:BTreeMap<i32,Vec<i32>> = BTreeMap::new();
+        let mut map:BTreeMap<i32,usize> = BTreeMap::new();
         for (i,num) in numbers.into_iter().enumerate(){
-            map.entry(num).or_insert(vec![]).push(i as i32 + 1);
-        }
-        for (num,i) in map.iter(){
-            let rest = target - num;
-            if map.contains_key(&rest){
-                for j in map.get(&rest).unwrap(){
-                    if i[0] != *j{
-                        return vec![i[0],*j];
-                    }
-                }
+            if let Some((_,&j)) = map.get_key_value(&num){
+                return vec![j as i32 + 1,i as i32 + 1];
+            } else {
+                map.insert(target-num,i);
             }
-            
         }
         vec![]
     }
