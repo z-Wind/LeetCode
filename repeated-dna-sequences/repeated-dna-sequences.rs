e24a7b2202542ed792869c5a6663fbeebd14abec
子風 <zpsyhapcst@gmail.com>
2021-09-17 11:42:15 +0800

    Time: 8 ms (72.22%), Space: 7.3 MB (38.89%) - LeetHub

--- repeated-dna-sequences/repeated-dna-sequences.rs
+++ repeated-dna-sequences/repeated-dna-sequences.rs
@@ -1,18 +1,19 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+        let mut ans = vec![];
         if s.len() < 11{
-            return vec![];
+            return ans;
         }
         let mut map = HashMap::new();
-        let s = s.as_bytes();
         for i in (0..s.len()-9){
             // println!("{:?}",String::from_utf8(s[i..i+10].to_vec()).unwrap());
-            *map.entry(&s[i..i+10]).or_insert(0) += 1;
+            let mut entry = map.entry(&s[i..i+10]).or_insert(0);
+            *entry += 1;
+            if *entry == 2{
+                ans.push(s[i..i+10].to_string());
+            }
         }
-        map.iter()
-            .filter(|(key,val)| **val > 1)
-            .map(|(x,_)| String::from_utf8(x.to_vec()).unwrap())
-            .collect()
+        ans
     }
 }
