893b7df7fbdb7d28b061b83128482b2307b64d77
子風 <zpsyhapcst@gmail.com>
2021-09-17 16:31:17 +0800

    Time: 0 ms (100.00%), Space: 2 MB (43.48%) - LeetHub

--- house-robber/house-robber.rs
+++ house-robber/house-robber.rs
@@ -1,21 +1,21 @@
+// dp[i] = nums[i] + max(dp[i+2] + dp[i+3])
+
 use std::cmp::max;
-use std::collections::HashMap;
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let mut dp:HashMap<usize,i32> = HashMap::new();
-        max(backtrack(&mut dp, &nums,0), backtrack(&mut dp, &nums,1))
+        let n = nums.len();
+        if n < 3{
+            return *nums.iter().max().unwrap();
+        }
+        let mut dp:Vec<i32> = vec![0;n];
+        
+        dp[n-1] = nums[n-1];
+        dp[n-2] = nums[n-2];
+        dp[n-3] = nums[n-3] + dp[n-1];
+        for i in (0..n-3).rev(){
+            dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
+        }
+        
+        max(dp[0], dp[1])
     }
-}
-
-fn backtrack(dp:&mut HashMap<usize,i32>, nums: &Vec<i32>, start:usize) -> i32 {
-    if start >= nums.len(){
-        return 0
-    }
-    if dp.contains_key(&start){
-        return *dp.get(&start).unwrap();
-    }
-    
-    let ans = nums[start] + max(backtrack(dp,nums,start+2), backtrack(dp,nums,start+3));
-    dp.insert(start,ans);
-    ans
 }
