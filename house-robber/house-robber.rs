8988984b004f31ca9795697d2a90082b04391b10
子風 <zpsyhapcst@gmail.com>
2021-09-17 16:54:23 +0800

    Time: 2 ms (17.39%), Space: 1.9 MB (84.78%) - LeetHub

--- house-robber/house-robber.rs
+++ house-robber/house-robber.rs
@@ -1,20 +1,23 @@
-// dp[i] = nums[i] + max(dp[i+2], dp[i+3])
-use std::cmp::max;
+// (1) d[i] = d[i-2] + nums[i] if robbing, 
+// (2) d[i] = d[i-1]           if not robbing
+// d[i] = max(dp[i-1], d[i-2] + nums[i])
+// 
+// d[i-2] d[i-1] d[i]
+//
+// pre    cur    cur(update)
+//
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        if n < 3{
-            return *nums.iter().max().unwrap();
-        }
-        let mut dp:Vec<i32> = vec![0;n];
         
-        dp[n-1] = nums[n-1];
-        dp[n-2] = nums[n-2];
-        dp[n-3] = nums[n-3] + dp[n-1];
-        for i in (0..n-3).rev(){
-            dp[i] = nums[i] + max(dp[i+2], dp[i+3]);
+        let mut pre = 0;
+        let mut cur = 0;
+        for i in (0..n).rev(){
+            let temp = cur;
+            cur = cur.max(pre+nums[i]);
+            pre = temp;
         }
         
-        max(dp[0], dp[1])
+        cur
     }
 }
