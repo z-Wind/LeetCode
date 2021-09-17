767d7cd3ff1ad3123986414f0295ff90669c93d1
子風 <zpsyhapcst@gmail.com>
2021-09-17 15:08:49 +0800

    Time: 2 ms (28.57%), Space: 2.2 MB (52.38%) - LeetHub

--- best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.rs
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.rs
@@ -4,12 +4,24 @@
 
 impl Solution {
     pub fn max_profit(k: i32, prices: Vec<i32>) -> i32 {
-        if (prices.len() == 0) { return 0 };
+        let n = prices.len();
+        if n == 0 { return 0 };
         
         let mut k_n = k as usize;
+        //if k_n >= n/2, then you can make maximum number of transactions.
+        if k_n >=  n/2 {
+            let mut maxPro = 0;
+            for i in (1..n) {
+                if prices[i] > prices[i-1] {
+                    maxPro += prices[i] - prices[i-1];
+                }
+            }
+            return maxPro;
+        }
+        
         let mut dp = vec![0;k_n+1];
         let mut min = vec![prices[0];k_n+1];
-        for i in (1..prices.len()) {
+        for i in (1..n) {
             for k in (1..=k_n) {
                 min[k] = min[k].min(prices[i] - dp[k-1]);
                 dp[k] = dp[k].max(prices[i] - min[k]);
