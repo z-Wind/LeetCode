565da0988a1b48af4d9554145cc268995d7aeae3
子風 <zpsyhapcst@gmail.com>
2021-09-17 20:59:16 +0800

    Time: 0 ms (100.00%), Space: 2 MB (30.77%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.rs
+++ binary-tree-right-side-view/binary-tree-right-side-view.rs
@@ -21,29 +21,20 @@ use std::cell::RefCell;
 use std::collections::VecDeque;
 impl Solution {
     pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        if root.is_none(){
-            return vec![];
-        }
-        let mut queue = VecDeque::new();
-        queue.push_back(root);
-        let mut ans = Vec::new();
-        while !queue.is_empty(){
-            let len = queue.len();
-            for i in (0..len){
-                let root = queue.pop_front().unwrap();
-                if i == len-1{
-                    ans.push(root.as_ref().unwrap().borrow().val)
-                }
-                let left = root.as_ref().unwrap().borrow_mut().left.take();
-                if left.is_some(){
-                    queue.push_back(left);
-                }
-                let right = root.as_ref().unwrap().borrow_mut().right.take();
-                if right.is_some(){
-                    queue.push_back(right);
-                }
-            }
-        }
-        ans
+        let mut result = Vec::new();
+        rightView(root, &mut result, 0);
+        result
     }
+}
+
+fn rightView(curr:Option<Rc<RefCell<TreeNode>>>, result:&mut Vec<i32>, currDepth:usize){
+    if curr.is_none(){
+        return;
+    }
+    if(currDepth == result.len()){
+        result.push(curr.as_ref().unwrap().borrow().val);
+    }
+
+    rightView(curr.as_ref().unwrap().borrow_mut().right.take(), result, currDepth + 1);
+    rightView(curr.as_ref().unwrap().borrow_mut().left.take(), result, currDepth + 1);
 }
