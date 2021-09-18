4c94a332abd3237b049e2cb83061d250e7b34433
子風 <zpsyhapcst@gmail.com>
2021-09-18 13:36:55 +0800

    Time: 12 ms (72.73%), Space: 1.9 MB (81.82%) - LeetHub

--- bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.rs
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.rs
@@ -1,13 +1,14 @@
 impl Solution {
-    pub fn range_bitwise_and(mut left: i32, right: i32) -> i32 {
+    pub fn range_bitwise_and(mut left: i32, mut right: i32) -> i32 {
         if left == 0{
             return 0;
         }
-        let mut ans = left & right;
-        let mut diff = right-left;
-        let bits = 32 - diff.leading_zeros();
-        ans >>= bits;
-        ans <<= bits;
-        ans
+        let mut count = 0;
+        while left != right{
+            left >>= 1;
+            right >>= 1;
+            count += 1;
+        }
+        left << count
     }
 }
