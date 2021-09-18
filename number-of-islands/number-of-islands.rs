37a519be143da0b6466bc9dda2577e34e8bf3099
子風 <zpsyhapcst@gmail.com>
2021-09-18 12:39:24 +0800

    Time: 12 ms (51.76%), Space: 9 MB (35.29%) - LeetHub

--- number-of-islands/number-of-islands.rs
+++ number-of-islands/number-of-islands.rs
@@ -1,10 +1,12 @@
 impl Solution {
     pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
         let mut count = 0;
+        let m = grid.len();
+        let n = grid[0].len();
         for i in (0..grid.len()){
             for j in (0..grid[0].len()){
                 if grid[i][j] == '1'{
-                    island_connect(&mut grid, i ,j);
+                    island_connect(&mut grid, m, n, i, j);
                     count += 1;
                 }
             }
@@ -14,15 +16,15 @@ impl Solution {
     }
 }
 
-fn island_connect(grid: &mut Vec<Vec<char>>, i:usize, j:usize){
-    if i == grid.len() || j == grid[0].len() || grid[i][j] != '1'{
+fn island_connect(grid: &mut Vec<Vec<char>>, m:usize, n:usize, i:usize, j:usize){
+    if i >= m || j >= n || grid[i][j] != '1'{
         return;
     }
     
     grid[i][j] = '_';
-    island_connect(grid,i.saturating_sub(1),j);
-    island_connect(grid,i+1,j);
-    island_connect(grid,i,j.saturating_sub(1));
-    island_connect(grid,i,j+1);
+    island_connect(grid,m,n,i-1,j);
+    island_connect(grid,m,n,i+1,j);
+    island_connect(grid,m,n,i,j-1);
+    island_connect(grid,m,n,i,j+1);
 
 }
