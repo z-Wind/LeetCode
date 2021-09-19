fb806913951bb004754b3572fd9c01aa892de288
子風 <zpsyhapcst@gmail.com>
2021-09-19 12:30:20 +0800

    Time: 2 ms (17.60%), Space: 2.3 MB (76.80%) - LeetHub

--- reverse-linked-list/reverse-linked-list.rs
+++ reverse-linked-list/reverse-linked-list.rs
@@ -16,23 +16,13 @@
 // }
 impl Solution {
     pub fn reverse_list(head:Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        if head.is_none(){
-            return None;
+        let mut prev: Option<Box<ListNode>> = None;
+        let mut next = head;
+        while let Some(mut cur) = next {
+            next = cur.next.take();
+            cur.next = prev;
+            prev = Some(cur);
         }
-        let mut v:Vec<Option<Box<ListNode>>> = Vec::new();
-        let mut h = head;
-        while h.is_some(){
-            let mut next = h.as_mut().unwrap().next.take();
-            v.push(h);
-            h = next;
-        }
-        
-        h = Some(Box::new(ListNode::new(0)));
-        let mut p = &mut h;
-        while let Some(node) = v.pop(){
-            p.as_mut().unwrap().next = node;
-            p = &mut p.as_mut().unwrap().next;
-        }
-        h.as_mut().unwrap().next.take()
+        return prev;
     }
 }
