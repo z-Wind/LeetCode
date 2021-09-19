1cbfd0acfdd524cd22ef48750d7ccf5089bb51b9
子風 <zpsyhapcst@gmail.com>
2021-09-19 11:48:28 +0800

    Time: 77 ms (38.24%), Space: 6.8 MB (55.88%) - LeetHub

--- count-primes/count-primes.rs
+++ count-primes/count-primes.rs
@@ -1,17 +1,25 @@
 impl Solution {
     pub fn count_primes(n: i32) -> i32 {
+        if n <= 2 {
+            return 0;
+        }
         let n = n as usize;
-        let mut is_prime = vec![false;n];
-        let mut count = 0;
-        for i in (2..n) {
-            if !is_prime[i] {
-                count+=1;
-                for j in (i..).take_while(|j| i*j<n) {
-                    is_prime[i*j] = true;
+        
+        let mut is_prime = vec![true;n];
+        is_prime[0] = false;
+        is_prime[1] = false;
+        
+        let sqrted_n = (n as f64).sqrt() as usize;
+        for i in (2..=sqrted_n) {
+            if is_prime[i] {
+                let mut j = i*i;
+                while j<n {
+                    is_prime[j] = false;
+                    j+=i;
                 }
             }
         }
         
-        count
+        is_prime.iter().filter(|x| **x).count() as i32
     }
 }
