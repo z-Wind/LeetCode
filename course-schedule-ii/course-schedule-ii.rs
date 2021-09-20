267f5e2e30b5e2890b6208b57d182da254b58977
子風 <zpsyhapcst@gmail.com>
2021-09-20 13:28:33 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (92.31%) - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.rs
@@ -0,0 +1,46 @@
+// http://web.ntnu.edu.tw/~algo/DirectedAcyclicGraph.html
+// topological sorting
+use std::collections::VecDeque;
+impl Solution {
+    pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
+        let num_courses = num_courses as usize;
+        let mut edges:Vec<i32> = vec![0;num_courses];
+        let mut adj:Vec<Vec<usize>> = vec![vec![];num_courses];
+        for prer in prerequisites{
+            // [a, b] means that you to take 'a' you need to take 'b' first  
+            let a = prer[0] as usize;
+            let b = prer[1] as usize;
+            //  b -> a
+            adj[b].push(a);
+            edges[a] += 1;
+        }
+        
+        let mut queue = VecDeque::new();
+        for i in (0..num_courses){
+            if edges[i] == 0{
+                queue.push_back(i);
+            } 
+        }
+        
+        let mut result = vec![];
+        for i in (0..num_courses){
+            if queue.is_empty(){
+                break;
+            }
+            let s = queue.pop_front().unwrap();
+            edges[s] = -1;
+            result.push(s as i32);
+            for &t in adj[s].iter(){
+                edges[t] -= 1;
+                if edges[t] == 0{
+                    queue.push_back(t);
+                } 
+            }
+        }
+        
+        if result.len() != num_courses{
+            return vec![];
+        }
+        result
+    }
+}
