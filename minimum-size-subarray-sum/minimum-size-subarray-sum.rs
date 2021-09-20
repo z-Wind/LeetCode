a9a458c3ba19ff075d4309a5e281730b3243d5b0
子風 <zpsyhapcst@gmail.com>
2021-09-20 12:10:15 +0800

    Time: 4 ms (15.00%), Space: 2.4 MB (60.00%) - LeetHub

---
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
+        let mut left:usize = 0;
+        let mut right:usize = 0;
+        let mut sum = 0;
+        let mut min_len:Option<i32> = None;
+        while left < nums.len(){
+            if right < nums.len() && sum < target{
+                sum+=nums[right];
+                right+=1;
+            } else if sum >= target{
+                let len = (right-left) as i32;
+                min_len = Some(min_len.unwrap_or(len).min(len));
+                sum-=nums[left];
+                left+=1;
+            } else {
+                break;
+            }
+            // println!("{:?} {} => {}",&nums[left..right],sum,min_len);
+        }
+        min_len.unwrap_or(0)
+    }
+}
