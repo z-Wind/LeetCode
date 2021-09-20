f715bc492bf0800654e6437c3a259505f2e7852a
子風 <zpsyhapcst@gmail.com>
2021-09-20 12:12:29 +0800

    Time: 0 ms (100.00%), Space: 2.4 MB (60.00%) - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.rs
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.rs
@@ -3,14 +3,14 @@ impl Solution {
         let mut left:usize = 0;
         let mut right:usize = 0;
         let mut sum = 0;
-        let mut min_len:Option<i32> = None;
+        let mut min_len = i32::MAX;
         while left < nums.len(){
             if right < nums.len() && sum < target{
                 sum+=nums[right];
                 right+=1;
             } else if sum >= target{
                 let len = (right-left) as i32;
-                min_len = Some(min_len.unwrap_or(len).min(len));
+                min_len = min_len.min(len);
                 sum-=nums[left];
                 left+=1;
             } else {
@@ -18,6 +18,9 @@ impl Solution {
             }
             // println!("{:?} {} => {}",&nums[left..right],sum,min_len);
         }
-        min_len.unwrap_or(0)
+        if min_len == i32::MAX{
+            return 0;
+        }
+        min_len
     }
 }
