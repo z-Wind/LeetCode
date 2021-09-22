f7e59d213ba2f51fd137d405dcdb78a71e5128f2
子風 <zpsyhapcst@gmail.com>
2021-09-22 16:36:52 +0800

    Time: 20 ms (80.00%), Space: 3.2 MB (20.00%) - LeetHub

---
+++ shortest-palindrome/shortest-palindrome.rs
@@ -0,0 +1,65 @@
+// Manacher’s Algorithm
+// https://havincy.github.io/blog/post/ManacherAlgorithm/
+// https://medium.com/hoskiss-stand/manacher-299cf75db97e
+//
+// https://github.com/z-Wind/LeetCode/blob/main/longest-palindromic-substring/longest-palindromic-substring.rs
+//
+
+use std::cmp;
+
+fn insert_symbol_in_string(mut s:String) -> String {    
+    for i in (0..=s.len()).rev() {
+        s.insert(i, '_');
+    }
+    
+    format!("^{}$", s)
+}
+
+fn radius_string(s: &str) -> (Vec<usize>, usize){
+    let mut max_index = 0;
+    let mut v: Vec<usize> = Vec::with_capacity(s.len());
+    v.push(0); //^
+    v.push(0); //_
+    
+    let chars:Vec<char> = s.chars().collect();
+    let (mut center,mut center_R) = (0,0);
+    for i in (2..s.len()-1){
+        let mirror_i = center-(i-center);
+        let mut r = if i < center_R{
+            cmp::min(v[mirror_i], center_R-i)
+        } else {
+            0
+        };
+        
+        if i>=center_R || 
+           mirror_i <= 2 || 
+           i + v[mirror_i] >= center_R{
+            // calculate radius
+            while chars[i-r-1] == chars[i+r+1]{
+                r+=1
+            }
+            
+            center = i;
+            center_R = i+r;
+        }
+        v.push(r);
+        
+        if v[max_index] < r && i-r <= 1{
+            max_index = i;
+        }
+    }
+    
+    (v, max_index)
+}
+
+impl Solution {
+    pub fn shortest_palindrome(s: String) -> String {
+        let si = insert_symbol_in_string(s.clone());
+        let (v, max_index) = radius_string(&si);
+        
+        //println!("{}\n{}\n{:?}\n{}\n", s, si, v, max_index);
+        let r = v[max_index];
+        let right = (max_index-r)/2+r;
+        format!("{}{}", s[right..].chars().rev().collect::<String>(), s)
+    }
+}
