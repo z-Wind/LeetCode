c4337f71bedadb060b5abd77b7951ea120b1a2dd
子風 <zpsyhapcst@gmail.com>
2021-09-22 10:20:12 +0800

    Time: 116 ms (88.89%), Space: 2.2 MB (61.11%) - LeetHub

--- word-search-ii/word-search-ii.rs
+++ word-search-ii/word-search-ii.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 #[derive(Debug, Default)]
 struct WordDictionary {
     word: String,
@@ -19,35 +18,27 @@ impl WordDictionary {
         }
         curr.word = word;
     }
-    
-    fn search_char(&self, c: u8) -> &Option<Box<WordDictionary>> {
-        let mut curr = self;
- 
-        let i = (c - b'a') as usize;
-        return &curr.children[i]
-    }
 }
 
 impl Solution {
     pub fn find_words(mut board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
-        let mut words_dict = WordDictionary::new();
+        let mut words_dict = Box::new(WordDictionary::new());
         for word in words{
             words_dict.add_word(word);
         }
         
-        let mut ans:HashSet<String> = HashSet::new();
-        let words_dict = Some(Box::new(words_dict));
+        let mut ans:Vec<String> = Vec::new();
         for i in (0..board.len()){
             for j in (0..board[0].len()){
-                find_words(&mut ans, &mut board, &words_dict, i, j) 
+                find_words(&mut ans, &mut board, &mut words_dict, i, j) 
             }
         }
         
-        ans.into_iter().collect()
+        ans
     }
 }
 
-fn find_words(ans: &mut HashSet<String>, board: &mut Vec<Vec<char>>, node: &Option<Box<WordDictionary>>, i:usize, j:usize) {
+fn find_words(ans: &mut Vec<String>, board: &mut Vec<Vec<char>>, node: &mut Box<WordDictionary>, i:usize, j:usize) {
     // println!("{},{}: {}",i,j,temp);
     if i >= board.len() || j >= board[0].len(){
         return;
@@ -57,10 +48,9 @@ fn find_words(ans: &mut HashSet<String>, board: &mut Vec<Vec<char>>, node: &Opti
         '_' => return,
         c => {
             board[i][j] = '_';
-            let next_node = node.as_ref().unwrap().search_char(c as u8);
-            if next_node.is_some(){
-                if !next_node.as_ref().unwrap().word.is_empty(){
-                    ans.insert(next_node.as_ref().unwrap().word.clone());
+            if let Some(next_node) = &mut node.children[(c as u8 - b'a') as usize]{
+                if !next_node.word.is_empty(){
+                    ans.push(next_node.word.drain(..).collect());
                 }
                 find_words(ans, board, next_node,i-1,j);
                 find_words(ans, board, next_node,i+1,j);
