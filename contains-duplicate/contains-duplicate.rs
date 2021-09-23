add19e21eebbf6a4d6e4130eff53c8dff4409830
子風 <zpsyhapcst@gmail.com>
2021-09-23 12:15:32 +0800

    Time: 4 ms (77.78%), Space: 2.8 MB (45.61%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.rs
@@ -0,0 +1,13 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut set = HashSet::new();
+        for num in nums{
+            if set.contains(&num){
+                return true;
+            }
+            set.insert(num);
+        }
+        false
+    }
+}
