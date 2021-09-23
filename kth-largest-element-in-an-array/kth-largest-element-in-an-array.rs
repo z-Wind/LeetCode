7eb813f08f1b18e19a77579eccaf723e43cd0cff
子風 <zpsyhapcst@gmail.com>
2021-09-23 11:01:07 +0800

    Time: 4 ms (34.88%), Space: 2.1 MB (58.14%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.rs
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.rs
@@ -1,17 +1,37 @@
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
 impl Solution {
-    pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
-        let k = k as usize;
-        let mut heap = BinaryHeap::with_capacity(k);
-        for num in nums{
-            if heap.len() < k{
-                heap.push(Reverse(num));
-            } else if num > heap.peek().unwrap().0{
-                heap.pop();
-                heap.push(Reverse(num));
+    pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+        let k = nums.len() - k as usize;
+        // println!("k:{}",k);
+        let mut left = 0;
+        let mut right = nums.len()-1;
+        while left < right{
+            // println!("{},{}:{:?}",left, right, nums);
+            let i = partition(&mut nums, left, right);
+            // println!("{} => {}:{:?}",i,nums[i], nums);
+            if i < k{
+                left = i + 1;
+            } else if i > k {
+                right = i - 1;
+            } else {
+                break;
             }
         }
-        heap.peek().unwrap().0
+        nums[k]
     }
+}
+
+fn partition(nums: &mut Vec<i32>, mut left:usize, mut right:usize) -> usize{
+    let pivot = left;
+    let mut i = left + 1;
+    let mut j = right;
+    while true{
+        while i<right && nums[i] <= nums[pivot] {i+=1}
+        while j>left && nums[j] > nums[pivot] {j-=1}
+        if i >= j{
+            break;
+        }
+        nums.swap(i,j);
+    }
+    nums.swap(pivot,j);
+    j
 }
