46435e89474e3adb323bc7109aea4adb0e91a073
子風 <zpsyhapcst@gmail.com>
2021-09-24 15:48:11 +0800

    Time: 12 ms (100.00%), Space: 2.5 MB (66.67%) - LeetHub

--- contains-duplicate-iii/contains-duplicate-iii.rs
+++ contains-duplicate-iii/contains-duplicate-iii.rs
@@ -1,48 +1,21 @@
+use std::collections::BTreeSet;
 impl Solution {
     pub fn contains_nearby_almost_duplicate(nums: Vec<i32>, k: i32, t: i32) -> bool {
-        let w_size = k as usize + 1;
-        if nums.len() < w_size{
-            for i in (0..nums.len()){
-                for j in (i+1..nums.len()){
-                    if sub_abs_compare(nums[i], nums[j], t){
-                        return true;
-                    }
-                }
+        let k = k as usize;
+        let mut bts: BTreeSet<i32> = BTreeSet::new();
+        for i in 0..nums.len() {
+            if i > k {
+                bts.remove(&nums[i - 1 - k]);
             }
-            return false;
-        }
-        
-        for (i,w) in nums.windows(w_size).enumerate(){
-            // println!("{}: {:?}",i,w);
-            if i == 0{
-                for j in (0..w_size){
-                    for k in (j+1..w_size){
-                        if sub_abs_compare(w[j], w[k], t){
-                            return true;
-                        }
-                    }
-                }
-            } else {
-                for j in (0..w_size-1){
-                    if sub_abs_compare(w[j], w[w_size-1], t){
-                        return true;
-                    }
-                }
+            if bts
+                .range(nums[i].saturating_sub(t)..=nums[i].saturating_add(t))
+                .next()
+                .is_some()
+            {
+                return true;
             }
+            bts.insert(nums[i]);
         }
         false
-    }
-}
-
-fn sub_abs_compare(a:i32, b:i32, t:i32) -> bool{
-    match a.overflowing_sub(b){
-        (_,true) => false,
-        (x,false) => {
-            if (x < 0 && x >= -t) ||
-               (x >= 0 && x <= t) {
-                   return true;
-            } 
-            false
-        }
     }
 }
