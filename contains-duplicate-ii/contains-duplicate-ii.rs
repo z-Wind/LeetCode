24249c16a5044f48fb92c79a38a6e35613801b73
子風 <zpsyhapcst@gmail.com>
2021-09-24 14:11:34 +0800

    Time: 24 ms (67.65%), Space: 6.8 MB (20.59%) - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.rs
+++ contains-duplicate-ii/contains-duplicate-ii.rs
@@ -3,15 +3,12 @@ impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
         let mut map:HashMap<i32,usize> = HashMap::new();
         for (i,num) in nums.into_iter().enumerate(){
-            match map.get_mut(&num){
-                None => {
-                    map.insert(num, i);
-                },
+            match map.insert(num, i){
+                None => (),
                 Some(j) => {
-                    if i-*j <= k as usize{
+                    if i-j <= k as usize{
                         return true;
                     }
-                    *j = i;
                 },
             }
         }
