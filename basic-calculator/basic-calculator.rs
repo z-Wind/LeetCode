e6944f4101e0aa778c59d3b25b45865ae49bbe4a
子風 <zpsyhapcst@gmail.com>
2021-09-25 20:44:47 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (65.52%) - LeetHub

--- basic-calculator/basic-calculator.rs
+++ basic-calculator/basic-calculator.rs
@@ -1,46 +1,39 @@
+// https://leetcode.com/problems/basic-calculator/discuss/62361/Iterative-Java-solution-with-stack/64037
+
 impl Solution {
     pub fn calculate(s: String) -> i32 {
-        calculate(s.as_bytes())
-    }
-}
-
-fn calculate(s: &[u8]) -> i32 {
-    // println!("{}", String::from_utf8(s.to_vec()).unwrap());
-    let mut sign = 1;
-    let mut lhs = String::new();
-    let mut paren = 0;
-    let mut i = 0;
-    while i < s.len(){
-        match s[i]{
-            b'+' if paren == 0 => {
-                return calculate(lhs.as_bytes()) * sign + calculate(&s[i+1..]);
-            },
-            b'-' if paren == 0 => {
-                if lhs.is_empty(){
-                    sign = -1;
-                } else {
-                    return calculate(lhs.as_bytes()) * sign + calculate(&s[i..]);
-                }
-            },
-            b'(' => {
-                if paren != 0{
-                    lhs.push('(')
-                }
-                paren += 1;
-            },
-            b')' => {
-                paren -= 1;
-                if paren != 0{
-                    lhs.push(')')
-                }
-            }
-            b' ' => (),
-            c => lhs.push(c as char),
+        if s.is_empty() {
+            return 0;
         }
-        i+=1;
-    }
-    match lhs.parse::<i32>(){
-        Ok(num) => num * sign,
-        Err(_) => calculate(lhs.as_bytes()) * sign,
+        
+        let mut result:i32 = 0;
+        let mut sign:i32 = 1;
+        let mut num:i32 = 0;
+
+        let mut stack:Vec<i32> = Vec::new();
+        stack.push(sign);
+
+        for c in s.as_bytes() {
+            match c{
+                b'0'..=b'9'=> num = num * 10 + (c - b'0') as i32,
+                b'+' => {
+                    result += sign * num;
+                    sign = *stack.last().unwrap(); 
+                    num = 0;
+                },
+                b'-' => {
+                    result += sign * num;
+                    sign = -*stack.last().unwrap(); 
+                    num = 0;
+                },
+                b'(' => stack.push(sign),
+                b')' => {stack.pop();},
+                b' ' => (),
+                _ => panic!(),
+            }
+        }
+
+        result += sign * num;
+        result
     }
 }
