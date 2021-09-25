5a5d6f17daa0402aebdba9f2bb053eafe96bbdb6
子風 <zpsyhapcst@gmail.com>
2021-09-25 13:05:40 +0800

    Time: 4 ms (71.43%), Space: 4.8 MB (35.71%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.rs
+++ count-complete-tree-nodes/count-complete-tree-nodes.rs
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/count-complete-tree-nodes/discuss/61958/Concise-Java-solutions-O(log(n)2)
+//
 // Definition for a binary tree node.
 // #[derive(Debug, PartialEq, Eq)]
 // pub struct TreeNode {
@@ -19,57 +21,26 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
-    pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if root.is_none(){
-            return 0;
+    pub fn count_nodes(mut root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut nodes = 0;
+        let mut h = height(&root);
+        while let Some(node) = root {
+            if height(&node.borrow().right) == h - 1 {
+                nodes += 1 << h; // 1*(2^(h-1) - 1)/(2-1) + 1 = 2^(h-1) = 1 << h
+                root = node.borrow_mut().right.take();
+            } else {
+                nodes += 1 << h-1; // 1*(2^(h-2) - 1)/(2-1) + 1 = 2^(h-2) = 1 << (h-1)
+                root = node.borrow_mut().left.take();
+            }
+            h-=1;
         }
-        
-        let (check, h) = is_complete(&root);
-        // println!("{:?}",(check, h));
-        if check{
-            return 2_i32.pow(h)-1;
-        }
-        
-        let mut count = 1;
-        let left = root.as_ref().unwrap().borrow_mut().left.take();
-        let right = root.as_ref().unwrap().borrow_mut().right.take();
-        
-        let (check, h) = is_complete(&left);
-        if check{
-            count += 2_i32.pow(h)-1;
-        } else {
-            count += Self::count_nodes(left);
-        }
-        
-        let (check, h) = is_complete(&right);
-        if check{
-            count += 2_i32.pow(h)-1;
-        } else {
-            count += Self::count_nodes(right);
-        }
-        count
+        return nodes;
     }
 }
 
-fn is_complete(root: &Option<Rc<RefCell<TreeNode>>>) -> (bool,u32){
-    if root.is_none(){
-        return (true,0);
-    }
-    let left = &root.as_ref().unwrap().borrow().left;
-    let right = &root.as_ref().unwrap().borrow().right;
-    match (left, right){
-        (None,None) => (true, 1),
-        (x,None) => (false,0),
-        (left, right) => {
-            let (check_left, h_left) = is_complete(left);
-            let (check_right, h_right) = is_complete(right);
-            // println!("{:?}, {:?}",(check_left, h_left),(check_right, h_right));
-            if check_left && check_right && h_left == h_right{
-                (true, h_left+1)
-            } else {
-                (false, 0)
-            }
-        },
-        (None,x) => panic!(),
+fn height(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match root{
+        None => -1,
+        Some(root) => 1 + height(&root.borrow().left),
     }
 }
