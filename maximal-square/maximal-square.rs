8d06afdb7dd1b4a368fa58e9c2176f6f8d6625cf
子風 <zpsyhapcst@gmail.com>
2021-09-25 11:21:03 +0800

    Time: 8 ms (100.00%), Space: 4.9 MB (82.35%) - LeetHub

--- maximal-square/maximal-square.rs
+++ maximal-square/maximal-square.rs
@@ -3,40 +3,46 @@
 //
 // dp(i-1,j-1) dp(i-1,j)
 // dp(i,j-1)   dp(i,j)
-
+//
+// pre     dp(j)
+// dp(j-1) dp(j)(update)
+//
 use std::cmp::min;
 impl Solution {
     pub fn maximal_square(matrix: Vec<Vec<char>>) -> i32 {
         let m = matrix.len();
         let n = matrix[0].len();
-        let mut dp:Vec<Vec<i32>> = vec![vec![0;n];m];
+        let mut dp:Vec<i32> = vec![0;n];
         let mut max_side = 0;
-        for i in (0..m){
-            match matrix[i][0]{
-                '0' => dp[i][0] = 0,
-                '1' => dp[i][0] = 1,
-                _ => panic!(),
-            }
-            max_side = max_side.max(dp[i][0]);
-        }
         for j in (0..n){
-            match matrix[0][j]{
-                '0' => dp[0][j] = 0,
-                '1' => dp[0][j] = 1,
+            dp[j] = match matrix[0][j]{
+                '0' => 0,
+                '1' => 1,
                 _ => panic!(),
-            }
-            max_side = max_side.max(dp[0][j]);
+            };
+            max_side = max_side.max(dp[j]);
         }
+        // println!("{:?}",dp);
         
         for i in (1..m){
+            let mut pre = dp[0];
+            dp[0] = match matrix[i][0]{
+                '0' => 0,
+                '1' => 1,
+                _ => panic!(),
+            };
+            max_side = max_side.max(dp[0]);
             for j in (1..n){
-                match matrix[i][j]{
-                    '1' => dp[i][j] = min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1])) + 1,
-                    '0' => dp[i][j] = 0,
+                let temp = dp[j];
+                dp[j] = match matrix[i][j]{
+                    '1' => min(dp[j],min(dp[j-1],pre)) + 1,
+                    '0' => 0,
                     _ => panic!(),
-                }
-                max_side = max_side.max(dp[i][j]);
+                };
+                pre = temp;
+                max_side = max_side.max(dp[j]);
             }
+            // println!("{:?}",dp);
         }
         max_side * max_side
     }
