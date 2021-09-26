08c77ed786eeba6870e079f1467ad9109f5fa5df
子風 <zpsyhapcst@gmail.com>
2021-09-26 13:09:27 +0800

    Time: 0 ms (100.00%), Space: 2 MB (60.00%) - LeetHub

--- summary-ranges/summary-ranges.rs
+++ summary-ranges/summary-ranges.rs
@@ -1,40 +1,21 @@
 impl Solution {
-    pub fn summary_ranges(mut nums: Vec<i32>) -> Vec<String> {
-        if nums.is_empty(){
-            return vec![];
-        }
-        
-        let mut result:Vec<String> = Vec::new();
-        let mut start = None;
-        for w in nums.windows(2){
-            match start{
-                None => {
-                    if w[1] != w[0]+1{
-                        result.push(format!("{}",w[0]));
-                        start = Some(w[1]);
-                    } else {
-                        start = Some(w[0]);
-                    }
-                },
-                Some(x) => {
-                    if w[1] != w[0]+1{
-                        if x == w[0]{
-                            result.push(format!("{}",x));    
-                        } else {
-                            result.push(format!("{}->{}",x,w[0]));
-                        }
-                        start = Some(w[1]);
-                    }
-                }
+    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {        
+        let mut list:Vec<String> = Vec::new();
+        let mut start = 0;
+        let mut end = 0;
+        while end < nums.len(){
+            while(end < nums.len() - 1 && nums[end] + 1 == nums[end + 1]){
+                end+=1;
             }
-        }
-        match start{
-            Some(x) if x == nums[nums.len()-1] => result.push(format!("{}",x)),
-            Some(x) if x != nums[nums.len()-1] => result.push(format!("{}->{}",x,nums[nums.len()-1])),
-            None => result.push(format!("{}",nums[nums.len()-1])),
-            _ => panic!(),
+            if(start == end){
+                list.push(format!("{}",nums[start]));
+            } else {
+                list.push(format!("{}->{}",nums[start],nums[end]));
+            }
+            end+=1;
+            start = end;
         }
         
-        result
+        list
     }
 }
