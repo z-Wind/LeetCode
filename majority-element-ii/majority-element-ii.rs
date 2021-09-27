35b0002cfbb448f6ff8581edb0572b8f2943f419
子風 <zpsyhapcst@gmail.com>
2021-09-27 09:45:04 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- majority-element-ii/majority-element-ii.rs
+++ majority-element-ii/majority-element-ii.rs
@@ -1,16 +1,57 @@
-use std::collections::HashMap;
+// https://leetcode.com/problems/majority-element-ii/discuss/63520/Boyer-Moore-Majority-Vote-algorithm-and-my-elaboration
+// https://leetcode.com/problems/majority-element-ii/discuss/63537/My-understanding-of-Boyer-Moore-Majority-Vote
+// Boyer-Moore Majority Vote Algorithm
+
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len()/3;
-        let mut map = HashMap::new();
-        let mut ans = Vec::new();
-        for num in nums{
-            let entry = map.entry(num).or_insert(0);
-            *entry += 1;
-            if *entry == n+1{
-                ans.push(num);
+        if nums.is_empty(){
+            return vec![];
+        }
+        let mut candicate1 = 0;
+        let mut count1 = 0;
+        let mut candicate2 = 1;
+        let mut count2 = 0;
+        for &num in nums.iter(){
+            match num{
+                x if x == candicate1 => count1+=1,
+                x if x == candicate2 => count2+=1,
+                _ => {
+                    match (count1,count2){
+                        (0,_) => {
+                            candicate1 = num;
+                            count1 = 1;
+                        },
+                        (_,0) => {
+                            candicate2 = num;
+                            count2 = 1;
+                        },
+                        (_,_) => {
+                            count1 -= 1;
+                            count2 -= 1;
+                        }
+                    }
+                }
             }
         }
-        ans
+        // println!("{}:{}, {}:{}",candicate1,count1,candicate2,count2);
+        let mut result = vec![];
+        count1 = 0;
+        count2 = 0;
+        let n = nums.len()/3;
+        for num in nums{
+            if num == candicate1{
+                count1 += 1
+            } else if num == candicate2{
+                count2 += 1
+            } 
+        }
+        if count1 > n{
+            result.push(candicate1)
+        }
+        if count2 > n{
+            result.push(candicate2)
+        }
+        result
+        
     }
 }
