f909e4855d01d7869dc0deac9c07640197814686
子風 <zpsyhapcst@gmail.com>
2021-09-29 09:38:29 +0800

    Time: 52 ms (63.33%), Space: 6.8 MB (86.67%) - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.rs
@@ -0,0 +1,38 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+        let mut v = Vec::new();
+        let mut h = head;
+        while h.is_some(){
+            let next = h.as_mut().unwrap().next.take();
+            v.push(h);
+            h = next;
+        }
+        
+        let mut left = 0;
+        let mut right = v.len()-1;
+        while left < right{
+            if v[left] != v[right]{
+                return false;
+            }
+            left+=1;
+            right-=1;
+        }
+        true
+    }
+}
