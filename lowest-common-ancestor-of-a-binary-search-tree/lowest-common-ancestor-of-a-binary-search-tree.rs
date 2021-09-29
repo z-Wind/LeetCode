5cb896dc85338e1aa4fc1c5172c44cc5e84f128a
子風 <zpsyhapcst@gmail.com>
2021-09-29 11:35:40 +0800

    Time: 4 ms (100.00%), Space: 3.3 MB (50.00%) - LeetHub

---
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.rs
@@ -0,0 +1,60 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn lowest_common_ancestor(root: Option<Rc<RefCell<TreeNode>>>, p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let p_val = p.as_ref().unwrap().borrow().val;
+        let q_val = q.as_ref().unwrap().borrow().val;
+        if root.as_ref().unwrap().borrow().val == p_val
+        || root.as_ref().unwrap().borrow().val == q_val{
+            return root;
+        }
+    
+        let (pa, _) = lowest_common_ancestor(root, p_val, q_val, 0);
+        pa
+    }
+}
+
+fn lowest_common_ancestor(root: Option<Rc<RefCell<TreeNode>>>, p:i32, q:i32, h:i32) -> (Option<Rc<RefCell<TreeNode>>>,i32) {
+    let val = root.as_ref().unwrap().borrow().val;
+    let left = root.as_ref().unwrap().borrow().left.clone();
+    let right = root.as_ref().unwrap().borrow().right.clone();
+    
+    match (p<val, q<val){
+        (true,true) => {
+            if left.as_ref().unwrap().borrow().val == p
+            || left.as_ref().unwrap().borrow().val == q{
+                return (left,h+1);
+            }
+            return lowest_common_ancestor(left, p, q, h+1);
+        },
+        (false,false) => {
+            if right.as_ref().unwrap().borrow().val == p
+            || right.as_ref().unwrap().borrow().val == q{
+                return (right,h+1);
+            }
+            return lowest_common_ancestor(right, p, q, h+1);
+        },
+        (true,false) | (false,true) =>{
+            return (root,h);
+        },
+    }
+}
