e4589903f445e4812472708461473cebfe044b08
子風 <zpsyhapcst@gmail.com>
2021-09-29 15:37:17 +0800

    Time: 8 ms (100.00%), Space: 3.2 MB (60.34%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.rs
+++ product-of-array-except-self/product-of-array-except-self.rs
@@ -1,22 +1,18 @@
 impl Solution {
     pub fn product_except_self(mut nums: Vec<i32>) -> Vec<i32> {
         let n = nums.len();
-        let mut left_prod = vec![nums[0];n];
+        let mut left_prod = vec![1;n];
         for i in (1..n){
-            left_prod[i] = left_prod[i-1] * nums[i];
+            left_prod[i] = left_prod[i-1] * nums[i-1];
         }
-        let mut right_prod = vec![nums[n-1];n];
-        for i in (0..n-1).rev(){
-            right_prod[i] = right_prod[i+1] * nums[i];
-        }
-        // println!("left :{:?}", left_prod);
-        // println!("right:{:?}", right_prod);
+        // println!("{:?}", left_prod);
         
-        nums[0] = right_prod[1];
-        nums[n-1] = left_prod[n-2];
-        for i in (1..n-1){
-            nums[i] = left_prod[i-1] * right_prod[i+1];
+        let mut right_prod = 1;
+        for i in (0..n).rev(){
+            left_prod[i] *= right_prod;
+            right_prod *= nums[i];
         }
-        nums
+        
+        left_prod
     }
 }
