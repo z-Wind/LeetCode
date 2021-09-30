8ff8cacc5685f292234338714be734ee53c0f028
子風 <zpsyhapcst@gmail.com>
2021-09-30 10:23:02 +0800

    Time: 4 ms (42.86%), Space: 2.7 MB (9.52%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.rs
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.rs
@@ -1,33 +1,20 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/66140/My-concise-O(m%2Bn)-Java-solution
+
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
         let m = matrix.len();
         let n = matrix[0].len();
         
         let mut i = 0;
-        let mut j = 0;
-        while i < m{
-            let order = if matrix[i][j] <= target{
-                1
+        let mut j = n-1;
+        while i < m && j < n{
+            // println!("{},{}",i,j);
+            if matrix[i][j] == target{
+                return true;
+            } else if matrix[i][j] > target{
+                j-=1;
             } else {
-                -1
-            };
-            while j < n{
-                // println!("{},{}",i,j);
-                if matrix[i][j]  == target{
-                    return true
-                }
-                
-                match (order, matrix[i][j] < target){
-                    (1,true) => j+=1,
-                    (-1,false) => j-=1,
-                    _ => break,
-                }
-            }
-            i+=1;
-            if j == n{
-                j = n-1;
-            } else if j > n{
-                j = 0;
+                i+=1;
             }
         }
         false
