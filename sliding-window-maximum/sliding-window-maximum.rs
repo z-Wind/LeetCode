5935f01bae45447db961863108fbec8cdb050c41
子風 <zpsyhapcst@gmail.com>
2021-09-30 09:52:45 +0800

    Time: 76 ms (82.35%), Space: 3.3 MB (88.24%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.rs
+++ sliding-window-maximum/sliding-window-maximum.rs
@@ -1,23 +1,48 @@
-use std::collections::BTreeMap;
+// https://leetcode.com/problems/sliding-window-maximum/discuss/1455296/Rust-Using-a-monotonically-decreasing-queue.-With-comments
+
+use std::collections::VecDeque;
+
 impl Solution {
     pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let k = k as usize;
-        if nums.len() < k{
-            return vec![nums.into_iter().max().unwrap()];
-        }
-        
-        let mut bt:BTreeMap<i32,i32> = BTreeMap::new();
-        nums[0..k].iter().for_each(|&x| *bt.entry(x).or_insert(0)+=1);
-        let mut v:Vec<i32> = vec![*bt.keys().last().unwrap()];
-        for i in (k..nums.len()){
-            *bt.entry(nums[i]).or_insert(0) +=1;
-            let x = bt.get_mut(&nums[i-k]).unwrap();
-            *x -= 1;
-            if *x == 0{
-                bt.remove(&nums[i-k]);
+        let mut result = Vec::with_capacity(nums.len() - k + 1);
+		
+		// We'll store the index of the number in the queue. We'll build a monotonically decreasing queue
+        let mut queue = VecDeque::with_capacity(k * 2);
+
+        for idx in 0..nums.len() {
+            // remove old values 
+			// if the stored idx at the front (corresponding to the largest number) is not in 
+			// the current window, then we have to remove it from the queue
+            while let Some(&pos) = queue.front() {
+                if pos + k-1 >= idx {
+                    break; // the biggest number is within the current window
+                }
+
+                queue.pop_front(); // remove number from a previous window
             }
-            v.push(*bt.keys().last().unwrap());
+
+            // remove all values smaller than the current one
+			// By doing this we'll maintain our queue in a monotonically decreasing order
+            while let Some(&pos) = queue.back() { 
+                // as we store indexes, we have to get the actual old value
+                if nums[pos] > nums[idx] {
+                    break; // there are no more smaller values
+                }
+
+                queue.pop_back();  // remove smaller or equal number
+            }
+
+            queue.push_back(idx);
+
+            // the window's maximum is at the front of the queue, because it's *decreasing*
+            if idx >= k-1{ // push a result only if we have processed at least one full window
+                if let Some(&max) = queue.front() { // always true, because of the `push_back()` above
+                    result.push(nums[max]);
+                }
+            }
         }
-        v
+
+        result
     }
 }
