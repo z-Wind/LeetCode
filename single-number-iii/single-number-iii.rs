178ff04eb42721cc3348a2add140c1d83ccb9a44
子風 <zpsyhapcst@gmail.com>
2021-10-01 10:41:36 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (66.67%) - LeetHub

--- single-number-iii/single-number-iii.rs
+++ single-number-iii/single-number-iii.rs
@@ -1,13 +1,19 @@
-use std::collections::HashSet;
+// https://leetcode.com/problems/single-number-iii/discuss/68900/Accepted-C%2B%2BJava-O(n)-time-O(1)-space-Easy-Solution-with-Detail-Explanations
+
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
-        let mut set = HashSet::new();
+        let xor = nums.iter().fold(0,|acc, x| acc^x);
+        let group_condition = xor & -xor;
+        
+        let mut result = vec![0,0];
         for num in nums{
-            match set.contains(&num){
-                true => set.remove(&num),
-                false => set.insert(num),
-            };
+            if num & group_condition == 0{
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
         }
-        set.into_iter().collect()
+        
+        result
     }
 }
