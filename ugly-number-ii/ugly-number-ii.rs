b984c0c8c6d2d713f1b8b6dd4cd145bddcb27e5c
子風 <zpsyhapcst@gmail.com>
2021-10-01 13:35:18 +0800

    Time: 0 ms (100.00%), Space: 2 MB (94.12%) - LeetHub

--- ugly-number-ii/ugly-number-ii.rs
+++ ugly-number-ii/ugly-number-ii.rs
@@ -1,17 +1,34 @@
-use std::collections::BTreeSet;
+// https://leetcode.com/problems/ugly-number-ii/discuss/69364/My-16ms-C%2B%2B-DP-solution-with-short-explanation
+// nums[1] = min( nums[0]x2, nums[0]x3, nums[0]x5)
+// nums[2] = min( nums[1]x2, nums[0]x3, nums[0]x5)
+// And so on. Be careful about the cases such as 6, in which we need to forward both pointers of 2 and 3.
+
 impl Solution {
     pub fn nth_ugly_number(n: i32) -> i32 {
+        if n == 1{
+            return 1;    
+        }
         let n = n as usize;
-        let mut nums = vec![];
-        let mut set:BTreeSet<usize> = BTreeSet::new();
-        set.insert(1);
+        let mut nums = vec![1];
+        
+        let mut i = 0;
+        let mut j = 0;
+        let mut k = 0;
         while nums.len() < n{
-            let num = *set.iter().next().unwrap();
-            nums.push(num as i32);
-            set.remove(&num);
-            set.insert(num*2);
-            set.insert(num*3);
-            set.insert(num*5);
+            let x = nums[i]*2;
+            let y = nums[j]*3;
+            let z = nums[k]*5;
+            let num = x.min(y).min(z);
+            nums.push(num);
+            if x == num{
+                i+=1;
+            }
+            if y == num{
+                j+=1;
+            }
+            if z == num{
+                k+=1;
+            }
         }
         // println!("{:?}", nums);
         nums[n-1]
