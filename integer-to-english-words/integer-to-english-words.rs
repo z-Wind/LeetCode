ecb5bf7511f77c90bd9ed839594008d07aa1bcab
子風 <zpsyhapcst@gmail.com>
2021-10-02 12:15:15 +0800

    Time: 5 ms (100.00%), Space: 2.1 MB (57.14%) - LeetHub

---
+++ integer-to-english-words/integer-to-english-words.rs
@@ -0,0 +1,84 @@
+impl Solution {
+    pub fn number_to_words(mut num: i32) -> String {
+        if num == 0{
+            return String::from("Zero");
+        }
+        let mut ans:Vec::<String> = Vec::new();
+        let mut digits = 0;
+        while num != 0{
+            let mut n = num%1000;
+            if n != 0{  
+                match digits{
+                    0 => (),
+                    3 => ans.push(number_to_word(1000)),
+                    6 => ans.push(number_to_word(1000_000)),
+                    9 => ans.push(number_to_word(1000_000_000)),
+                    _ => panic!("digits:{}", digits),
+                }
+                match n{
+                    1..=99 => match_1_99_to_word(&mut ans,n),
+                    100..=999 => {
+                        match_1_99_to_word(&mut ans, n%100);
+                        ans.push(number_to_word(100));
+                        ans.push(number_to_word(n/100));
+                    },
+                    _ => panic!("n:{}", n),
+                }
+            }
+            // println!("{}:{:?}",n,ans);
+            num /= 1000;
+            digits += 3;
+        }
+        ans.reverse();
+        ans.join(" ")
+    }
+}
+
+fn match_1_99_to_word(ans:&mut Vec<String>, num: i32){
+    match num{
+        0 => (),
+        1..=20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 => ans.push(number_to_word(num)),
+        21..=99 => {
+            ans.push(number_to_word(num%10));
+            ans.push(number_to_word(num/10 * 10));
+        },
+        x => panic!("n:{}", x),
+    }
+}
+
+fn number_to_word(num: i32) -> String {
+    match num{
+        1  => String::from("One"),
+        2  => String::from("Two"),
+        3  => String::from("Three"),
+        4  => String::from("Four"),
+        5  => String::from("Five"),
+        6  => String::from("Six"),
+        7  => String::from("Seven"),
+        8  => String::from("Eight"),
+        9  => String::from("Nine"),
+        10 => String::from("Ten"),
+        11 => String::from("Eleven"),
+        12 => String::from("Twelve"),
+        13 => String::from("Thirteen"),
+        14 => String::from("Fourteen"),
+        15 => String::from("Fifteen"),
+        16 => String::from("Sixteen"),
+        17 => String::from("Seventeen"),
+        18 => String::from("Eighteen"),
+        19 => String::from("Nineteen"),
+        20 => String::from("Twenty"),
+        30 => String::from("Thirty"),
+        40 => String::from("Forty"),
+        50 => String::from("Fifty"),
+        60 => String::from("Sixty"),
+        70 => String::from("Seventy"),
+        80 => String::from("Eighty"),
+        90 => String::from("Ninety"),
+        100 => String::from("Hundred"),
+        1000 => String::from("Thousand"),
+        1000_000 => String::from("Million"),
+        1000_000_000 => String::from("Billion"),
+        x => panic!("{}", x),
+    }
+}
