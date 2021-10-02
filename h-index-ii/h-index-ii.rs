7309ff0f9648aaeea2065091ead428bb7acd0d5d
子風 <zpsyhapcst@gmail.com>
2021-10-02 15:29:18 +0800

    Time: 0 ms (100.00%), Space: 2.7 MB (100.00%) - LeetHub

--- h-index-ii/h-index-ii.rs
+++ h-index-ii/h-index-ii.rs
@@ -1,23 +1,20 @@
+// https://leetcode.com/problems/h-index-ii/discuss/71063/Standard-binary-search
+
 impl Solution {
     pub fn h_index(citations: Vec<i32>) -> i32 {
-        let mut h = citations.len() as i32;
-        let mut count = 0;
-        for &c in citations.iter().rev(){
-            if c < h{
-                while h > c{
-                    h-=1;
-                    if count >= h{
-                        return h;
-                    }        
-                }
-            }
-            count += 1;
-            // println!("{} => h:{} count:{}", c, h, count);
-            
-            if count >= h{
-                return h;
+        let n = citations.len();
+        let mut left = 0;
+        let mut right = citations.len()-1;
+        while left <= right{
+            let mid = (left+right)/2;
+            // println!("{},{},{}",left,mid,right);
+            if citations[mid] as usize >= n - mid{
+                right = mid - 1;
+                if right >= n { break; }
+            } else {
+                left = mid + 1;
             }
         }
-        0
+        (n - left) as i32
     }
 }
