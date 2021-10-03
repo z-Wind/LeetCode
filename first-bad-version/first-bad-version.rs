37972b79d170b7ddeba8b09cd980e04423eb6bff
子風 <zpsyhapcst@gmail.com>
2021-10-03 10:43:35 +0800

    Time: 1 ms (30.08%), Space: 2 MB (84.55%) - LeetHub

---
+++ first-bad-version/first-bad-version.rs
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// isBadVersion(versions:i32)-> bool;
+// to call it use self.isBadVersion(versions)
+
+impl Solution {
+    pub fn first_bad_version(&self, n: i32) -> i32 {
+		let mut left = 1;
+        let mut right = n;
+        while left <= right{
+            // println!("{},{}",left, right);
+            let mid = left+(right-left)/2;
+            if self.isBadVersion(mid){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        left
+    }
+}
