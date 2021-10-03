d31dfc2e279f7aabaa0c3326c58b652df3320d09
子風 <zpsyhapcst@gmail.com>
2021-10-03 11:27:05 +0800

    Time: 46 ms (47.83%), Space: 2.1 MB (43.48%) - LeetHub

--- perfect-squares/perfect-squares.rs
+++ perfect-squares/perfect-squares.rs
@@ -1,27 +1,20 @@
 // dp(n) = 1 + dp(n-x*x)
 
-use std::collections::HashMap;
 impl Solution {
     pub fn num_squares(n: i32) -> i32 {
-        let mut dp:HashMap<i32,i32> = HashMap::new();
-        num_squares(&mut dp, n)
+        if n < 4{
+            return n;
+        }
+        let n = n as usize;
+        let mut dp:Vec<i32> = vec![i32::MAX; n +1];
+        dp[0] = 0;
+        for i in (1..=n){
+            let mut j = 1;
+            while j*j <= i{
+                dp[i] = dp[i].min(1+dp[i-j*j]);
+                j+=1;
+            }
+        }
+        dp[n]
     }
-}
-
-fn num_squares(dp:&mut HashMap<i32,i32>, n: i32) -> i32 {
-    if n < 4{
-        return n;
-    }
-    if dp.contains_key(&n){
-        return *dp.get(&n).unwrap();
-    }
-    
-    let mut ans = i32::MAX;
-    let mut i = 1;
-    while i*i <= n{
-        ans = ans.min(1+num_squares(dp, n-i*i));
-        i+=1
-    }
-    dp.insert(n,ans);
-    ans
 }
