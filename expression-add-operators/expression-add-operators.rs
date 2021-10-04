b744aee2219b9aa229062ebe0136058a0d7c5cdd
子風 <zpsyhapcst@gmail.com>
2021-10-04 16:45:42 +0800

    Time: 158 ms (27.59%), Space: 2.4 MB (41.38%) - LeetHub

--- expression-add-operators/expression-add-operators.rs
+++ expression-add-operators/expression-add-operators.rs
@@ -1,118 +1,36 @@
-use std::collections::VecDeque;
-
-#[derive(Debug, Clone)]
-struct Result{
-    s:Vec<char>,
-    val:i64,
-    curr_num:Option<i64>,
-    last_num:i64,
-    op:char,
-}
-impl Result{
-    fn new() -> Self{
-        Self{
-            s:vec![],
-            val:0,
-            curr_num:None,
-            last_num:0,
-            op:'+',
-        }
-    }
-    
-    fn cal(&mut self){
-        match self.op{
-            '+' => {
-                self.val += self.last_num;
-                self.last_num = self.curr_num.unwrap();
-            },
-            '-' => {
-                self.val += self.last_num;
-                self.last_num = -self.curr_num.unwrap();
-            },
-            '*' => {
-                self.last_num *= self.curr_num.unwrap();
-            },
-            _ => panic!(),
-        }
-        self.val+=self.last_num;
-    }
-    
-    fn operation(&mut self, c:char){
-        self.s.push(c);
-        match c{
-            '0'..='9' => {
-                self.curr_num = match self.curr_num{
-                    None => Some( (c as u8 - b'0') as i64 ),
-                    Some(x) => Some(x*10 + (c as u8 - b'0') as i64),
-                };
-            },
-            '+' | '-' | '*' => {
-                match self.op{
-                    '+' => {
-                        self.val += self.last_num;
-                        self.last_num = self.curr_num.unwrap();
-                    },
-                    '-' => {
-                        self.val += self.last_num;
-                        self.last_num = -self.curr_num.unwrap();
-                    },
-                    '*' => {
-                        self.last_num *= self.curr_num.unwrap();
-                    },
-                    _ => panic!(),
-                }
-                self.op = c;
-                self.curr_num = None;
-            },
-            _ => panic!(),
-        }
-    }
-}
 impl Solution {
     pub fn add_operators(num: String, target: i32) -> Vec<String> {
-        let mut results:VecDeque<Result> = VecDeque::new();
-        results.push_back(Result::new());
-        for (i, c) in num.chars().enumerate(){
-            // println!("{:?}", results);
-            if i == 0{
-                results[0].operation(c);
-                continue;
-            }
-            for _ in (0..results.len()){
-                let result = results.pop_front().unwrap();
-                // println!("{}: {:?}", c, result);
-                // None
-                if !(result.curr_num.is_some() && result.curr_num.unwrap() == 0){
-                    let mut temp = result.clone();
-                    temp.operation(c);
-                    results.push_back(temp);
-                }
-                // +
-                let mut temp = result.clone();
-                temp.operation('+');
-                temp.operation(c);
-                results.push_back(temp);
-                // -
-                let mut temp = result.clone();
-                temp.operation('-');
-                temp.operation(c);
-                results.push_back(temp);
-                // *
-                let mut temp = result.clone();
-                temp.operation('*');
-                temp.operation(c);
-                results.push_back(temp);
-            }
+        let mut rst = Vec::new();
+        if num.is_empty() {
+            return rst
+        };
+        
+        helper(&mut rst, String::new(), &num, target as i64, 0, 0, 0);
+        rst
+    }
+}
+
+fn helper(rst:&mut Vec<String>, path:String, num:&str, target:i64, pos:usize, eval:i64, multed:i64){
+    if(pos == num.len()){
+        if(target == eval){
+            rst.push(path);
         }
-        let mut ans = vec![];
-        for mut result in results{
-            result.cal();
-            // println!("{}={}",result.s.clone().into_iter().collect::<String>(),result.val);
-            if result.val == target as i64{
-                ans.push(result.s.into_iter().collect());
-            }
+        return;
+    }
+    for i in (pos..num.len()){
+        if i != pos && &num[pos..pos+1] == "0"{
+            break;  
+        } 
+        let cur:i64 = num[pos..i + 1].parse().unwrap();
+        if pos == 0 {
+            helper(rst, format!("{}{}", path, cur), num, target, i + 1, cur, cur);
         }
-    
-        ans
+        else{
+            helper(rst, format!("{}+{}", path, cur), num, target, i + 1, eval + cur , cur);
+
+            helper(rst, format!("{}-{}", path, cur), num, target, i + 1, eval - cur, -cur);
+
+            helper(rst, format!("{}*{}", path, cur), num, target, i + 1, eval - multed + multed * cur, multed * cur );
+        }
     }
 }
