906d3b0a4ce1dddffaedf1fb140d98b82846512a
子風 <zpsyhapcst@gmail.com>
2021-10-04 15:28:20 +0800

    Time: 0 ms (100.00%), Space: 2.5 MB (90.00%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.rs
+++ basic-calculator-ii/basic-calculator-ii.rs
@@ -1,13 +1,6 @@
-#[derive(Debug)]
-enum Cal{
-    Num(i32),
-    Op(u8),
-}
 impl Solution {
     pub fn calculate(s: String) -> i32 {
-        let mut stack:Vec<Cal> = Vec::new();
         let mut result = 0;
-        let mut sign = 1;
         let mut op = &b'+';
         let mut curr_num = 0;
         let mut last_num = 0;
