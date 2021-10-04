83bbc227089ec24481e6101c68524935e54111e3
子風 <zpsyhapcst@gmail.com>
2021-10-04 16:50:41 +0800

    Time: 172 ms (24.14%), Space: 2.5 MB (17.24%) - LeetHub

--- expression-add-operators/expression-add-operators.rs
+++ expression-add-operators/expression-add-operators.rs
@@ -3,25 +3,26 @@ impl Solution {
         let mut rst = Vec::new();
         if num.is_empty() {
             return rst
-        };
+        }
         
-        helper(&mut rst, String::new(), &num, target as i64, 0, 0, 0);
+        helper(&mut rst, String::new(), num.as_bytes(), target as i64, 0, 0, 0);
         rst
     }
 }
 
-fn helper(rst:&mut Vec<String>, path:String, num:&str, target:i64, pos:usize, eval:i64, multed:i64){
+fn helper(rst:&mut Vec<String>, path:String, num:&[u8], target:i64, pos:usize, eval:i64, multed:i64){
     if(pos == num.len()){
         if(target == eval){
             rst.push(path);
         }
         return;
     }
+    let mut cur:i64 = 0;
     for i in (pos..num.len()){
-        if i != pos && &num[pos..pos+1] == "0"{
+        if i != pos && num[pos] == b'0'{
             break;  
         } 
-        let cur:i64 = num[pos..i + 1].parse().unwrap();
+        cur = 10*cur + (num[i] - b'0') as i64; 
         if pos == 0 {
             helper(rst, format!("{}{}", path, cur), num, target, i + 1, cur, cur);
         }
