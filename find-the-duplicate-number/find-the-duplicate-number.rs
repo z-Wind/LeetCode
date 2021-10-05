73e9055b55662781da8958617584677c797eb6b1
子風 <zpsyhapcst@gmail.com>
2021-10-05 11:09:44 +0800

    Time: 32 ms (16.13%), Space: 3.2 MB (67.74%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.rs
+++ find-the-duplicate-number/find-the-duplicate-number.rs
@@ -1,12 +1,39 @@
+// Floyd's algorithm
+// F: the number of nodes to circle
+// a: the intersection which from start of circule
+// rabbit is twice as fast as tortoise
+//
+// Phase 1: from start
+// tortoise walk F + a distance
+// rabbit walk F + nC + a distance
+// 2(F+a) = F + nC + a
+// F+a = nC
+//
+// Phase 2: tortoise from start, rabbit move one step from intersection
+// because F+a = nC
+// the intersection would be the start of circule
+
 impl Solution {
-    pub fn find_duplicate(mut nums: Vec<i32>) -> i32 {
-        for i in (0..nums.len()){
-            let j = nums[i].abs() as usize - 1;
-            if nums[j] < 0{
-                return j as i32 + 1;
+    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+        // phase 1
+        let mut tortoise = nums[0];
+        let mut rabbit = nums[0];
+        loop{
+            tortoise = nums[tortoise as usize];
+            rabbit = nums[nums[rabbit as usize] as usize];
+            if tortoise == rabbit{
+                break;
             }
-            nums[j] = -nums[j] 
         }
-        0
+        // println!("from start, F+a:{}, F+nC+a:{}", tortoise, rabbit);
+        
+        // phase 2
+        tortoise = nums[0];
+        while tortoise != rabbit{
+            tortoise = nums[tortoise as usize];
+            rabbit = nums[rabbit as usize];
+        }
+        
+        tortoise
     }
 }
