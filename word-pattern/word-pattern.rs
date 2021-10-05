c205cf8cb2ae38518e04b88792d89ff4f144a3f6
子風 <zpsyhapcst@gmail.com>
2021-10-05 16:50:19 +0800

    Time: 2 ms (18.42%), Space: 2.2 MB (26.32%) - LeetHub

--- word-pattern/word-pattern.rs
+++ word-pattern/word-pattern.rs
@@ -2,24 +2,24 @@ use std::collections::HashMap;
 impl Solution {
     pub fn word_pattern(pattern: String, s: String) -> bool {
         let pattern = pattern.as_bytes();
-        let mut v:Vec<&str> = s.split(' ').collect();
-        if pattern.len() != v.len(){
+        let mut words:Vec<&str> = s.split(' ').collect();
+        if pattern.len() != words.len(){
             return false;
         }
         
-        let mut map_p:HashMap<u8,&str> = HashMap::new();
-        let mut map_s:HashMap<&str,u8> = HashMap::new();
+        let mut map_p:HashMap<u8,usize> = HashMap::new();
+        let mut map_s:HashMap<&str,usize> = HashMap::new();
         
-        for i in (0..v.len()){
+        for i in (0..words.len()){
             let p = pattern[i];
-            let s = v[i];
+            let s = words[i];
             match (map_p.get(&p), map_s.get(&s)){
                 (None,None) =>{
-                    map_p.insert(pattern[i], &s);
-                    map_s.insert(s, pattern[i]);
+                    map_p.insert(pattern[i], i);
+                    map_s.insert(s, i);
                 },
-                (Some(s2),Some(p2)) => {
-                    if &s != s2 || &p != p2{
+                (Some(x),Some(y)) => {
+                    if x != y{
                         return false;
                     }
                 },
