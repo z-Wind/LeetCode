4840b892face12d79a07782d1934413520dd695a
子風 <zpsyhapcst@gmail.com>
2021-10-05 08:45:49 +0800

    Time: 10 ms (13.10%), Space: 2.3 MB (38.10%) - LeetHub

--- move-zeroes/move-zeroes.rs
+++ move-zeroes/move-zeroes.rs
@@ -1,5 +1,11 @@
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
-        nums.sort_by_key(|&k| k==0);
+        let mut lastNonZeroFoundAt = 0;
+        for cur in (0..nums.len()) {
+            if (nums[cur] != 0) {
+                nums.swap(lastNonZeroFoundAt, cur);
+                lastNonZeroFoundAt += 1;
+            }
+        }
     }
 }
