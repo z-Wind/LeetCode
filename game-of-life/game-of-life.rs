f776738d53242af961e3fd1cfd242d1b39e7268c
子風 <zpsyhapcst@gmail.com>
2021-10-05 14:20:41 +0800

    Time: 1 ms (29.41%), Space: 2.1 MB (58.82%) - LeetHub

--- game-of-life/game-of-life.rs
+++ game-of-life/game-of-life.rs
@@ -1,8 +1,15 @@
+// https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation
+// [2nd bit, 1st bit] = [next state, current state]
+// - 00  dead (next) <- dead (current)
+// - 01  dead (next) <- live (current)  
+// - 10  live (next) <- dead (current)  
+// - 11  live (next) <- live (current) 
+
+
 impl Solution {
     pub fn game_of_life(board: &mut Vec<Vec<i32>>) {
         let m = board.len();
         let n = board[0].len();
-        let mut lifes = vec![vec![0;n];m];
         for i in (0..m){
             for j in (0..n){
                 // println!("{},{}",i,j);
@@ -10,20 +17,20 @@ impl Solution {
                 for k in (i.saturating_sub(1)..=(m-1).min(i+1)){
                     for l in (j.saturating_sub(1)..=(n-1).min(j+1)){
                         // println!("  {},{}",k,l);
-                        life += board[k][l];
+                        life += board[k][l] & 1;
                     }
                 }
-                lifes[i][j] = life - board[i][j]; 
+                match life - (board[i][j] & 1){
+                    2 if (board[i][j] & 1) == 1 => board[i][j] = 0b11,
+                    3 => board[i][j] |= 0b10,
+                    _ => (),
+                }
             }
         }
-        // println!("{:?}", lifes);
+        // println!("{:?}", board);
         for i in (0..m){
             for j in (0..n){
-                match lifes[i][j]{
-                    2 => (),
-                    3 => board[i][j] = 1,
-                    _ => board[i][j] = 0,
-                }
+                board[i][j] >>= 1;
             }
         }
     }
