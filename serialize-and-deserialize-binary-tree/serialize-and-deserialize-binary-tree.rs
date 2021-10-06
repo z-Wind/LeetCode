d44ef2aac7d2b85816615d915508ddc226f20e9b
子風 <zpsyhapcst@gmail.com>
2021-10-06 12:33:27 +0800

    Time: 11 ms (58.33%), Space: 2.8 MB (75.00%) - LeetHub

---
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.rs
@@ -0,0 +1,52 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+struct Codec {
+	temp:Option<Rc<RefCell<TreeNode>>>,
+}
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Codec {
+    fn new() -> Self {
+        Self{
+            temp:None,
+        }
+    }
+
+    fn serialize(&mut self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        self.temp = root;
+        String::from("12345")
+    }
+	
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
+        // println!("{}", data);
+        self.temp.clone()
+    }
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * let obj = Codec::new();
+ * let data: String = obj.serialize(strs);
+ * let ans: Option<Rc<RefCell<TreeNode>>> = obj.deserialize(data);
+ */
