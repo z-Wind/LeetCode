af56f8a52fa21969beff6e0938c100361f940ff8
子風 <zpsyhapcst@gmail.com>
2021-10-06 16:51:22 +0800

    Time: 12 ms (41.67%), Space: 5.4 MB (8.33%) - LeetHub

--- serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.rs
+++ serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.rs
@@ -18,8 +18,8 @@
 // }
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 struct Codec {
-	temp:Option<Rc<RefCell<TreeNode>>>,
 }
 
 /** 
@@ -28,19 +28,49 @@ struct Codec {
  */
 impl Codec {
     fn new() -> Self {
-        Self{
-            temp:None,
-        }
+        Self{}
     }
 
     fn serialize(&mut self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
-        self.temp = root;
-        String::new()
+        if root.is_none(){
+            return String::new();
+        }
+        let left = root.as_ref().unwrap().borrow_mut().left.take();
+        let right = root.as_ref().unwrap().borrow_mut().right.take();
+        format!("{}({},{})", 
+            root.as_ref().unwrap().borrow().val.to_string(),
+            self.serialize(left),
+            self.serialize(right),
+        )
     }
 	
     fn deserialize(&mut self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
-        // println!("{}", data);
-        self.temp.take()
+        // println!("{}",data);
+        if data.is_empty(){
+            return None;
+        }
+        let mut count = 0;
+        let mut root_end = 0;
+        let mut split = 0;
+        for (i,char) in data.chars().enumerate(){
+            match char{
+                '(' => {
+                    if count == 0{
+                        root_end = i;
+                    }
+                    count+=1;
+                },
+                ')' => count-=1,
+                ',' if count == 1 => split = i,
+                _ => (),
+            }    
+        }
+        let val:i32 = data[0..root_end].parse().unwrap();
+        // println!("{}",val);
+        let mut root = Some(Rc::new(RefCell::new(TreeNode::new(val))));
+        root.as_ref().unwrap().borrow_mut().left = self.deserialize(data[root_end+1..split].to_string());
+        root.as_ref().unwrap().borrow_mut().right = self.deserialize(data[split+1..data.len()-1].to_string());
+        root
     }
 }
 
