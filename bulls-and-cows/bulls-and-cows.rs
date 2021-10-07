5ebf803362505d0f3f4f493b28719c0b5ba08946
子風 <zpsyhapcst@gmail.com>
2021-10-07 13:29:21 +0800

    Time: 0 ms (100.00%), Space: 2 MB (73.33%) - LeetHub

---
+++ bulls-and-cows/bulls-and-cows.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn get_hint(secret: String, guess: String) -> String {
+        let mut digits = vec![0;10];
+        let mut bulls = 0;
+        let mut cows = 0;
+        let secret = secret.as_bytes();
+        let guess = guess.as_bytes();
+        for i in (0..secret.len()){
+            if secret[i] == guess[i]{
+                bulls += 1;
+            }
+            let idx = (secret[i] - b'0') as usize;
+            if digits[idx] < 0{
+                cows+=1;
+            }
+            digits[idx] += 1;
+            let idx = (guess[i] - b'0') as usize;
+            if digits[idx] > 0{
+                cows+=1;
+            }
+            digits[idx] -= 1;
+        }
+        
+        format!("{}A{}B",bulls,cows-bulls)
+    }
+}
