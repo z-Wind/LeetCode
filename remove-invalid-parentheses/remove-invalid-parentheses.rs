d6b03ab25ac62bcc1f76db5a903c22f4bdb63ed5
子風 <zpsyhapcst@gmail.com>
2021-10-08 14:22:50 +0800

    Time: 0 ms (100.00%), Space: 2 MB (44.44%) - LeetHub

--- remove-invalid-parentheses/remove-invalid-parentheses.rs
+++ remove-invalid-parentheses/remove-invalid-parentheses.rs
@@ -1,73 +1,42 @@
-use std::collections::HashSet;
+// https://leetcode.com/problems/remove-invalid-parentheses/discuss/75027/Easy-Short-Concise-and-Fast-Java-DFS-3-ms-solution
 
 impl Solution {
     pub fn remove_invalid_parentheses(s: String) -> Vec<String> {
-        let mut max_left = 0;
-        let mut max_right = 0;
-        let mut count:i32 = 0;
-        for c in s.chars(){
-            match c{
-                '(' => {
-                    count+=1;
-                },
-                ')' => {
-                    if count <= 0{
-                        max_right += 1;
-                        count = 0;
-                    } else {
-                        count-=1;   
-                    }
-                },
-                _ => (),
-            }
-        }
-        if count >= 0{
-            max_left += count;
-        } else {
-            max_right -= count;
-        }
+        let mut ans:Vec<String> = Vec::new();
+        backtrack(s, &mut ans, 0, 0, [b'(', b')']);
         
-        // println!("{}", max_remove);
-        if max_left + max_right == 0{
-            return vec![s];
-        } else if max_left + max_right == s.len() as i32{
-            return vec![String::new()];
-        }
-        
-        let mut ans:HashSet<String> = HashSet::new();
-        let mut temp = String::new();
-        backtrack(&mut ans, &mut temp, s.as_bytes(), 0, 0, max_left, max_right);
-        ans.into_iter().collect()
+        ans
     }
 }
 
-fn backtrack(ans:&mut HashSet<String>, temp:&mut String, s:&[u8], start:usize, count:i32, max_left:i32, max_right:i32){
-    // println!("{}:{} {} {}", count, temp, max_left, max_right);
-    if max_left < 0 || max_right < 0 || count < 0{
-        return;
-    } else if start == s.len(){
-        if count == 0{
-            ans.insert(temp.clone());
+fn backtrack(s:String, ans:&mut Vec<String>, last_i:usize, last_j:usize, par:[u8;2]) {
+    // println!("par:{:?}, s:{}, {},{}", par, s, last_i, last_j);
+    let mut stack:i32 = 0;
+    let b = s.as_bytes();
+    for i in (last_i..s.len()) {
+        if b[i] == par[0] { 
+            stack+=1; 
+        } else if b[i] == par[1] { 
+            stack-=1;
+        }
+        if stack >= 0 { 
+            continue; 
+        }
+        
+        for j in (last_j..=i){
+            if (b[j] == par[1] && (j == last_j || b[j-1] != par[1])){
+                let mut temp = s.clone();
+                temp.remove(j);
+                backtrack(temp, ans, i, j, par);
+            }
         }
         return;
     }
-    match s[start]{
-        b'(' => {
-            temp.push('(');
-            backtrack(ans, temp, s, start+1, count+1, max_left, max_right);
-            temp.pop();
-            backtrack(ans, temp, s, start+1, count, max_left-1, max_right);
-        },
-        b')' => {
-            temp.push(')');
-            backtrack(ans, temp, s, start+1, count-1, max_left, max_right);
-            temp.pop();
-            backtrack(ans, temp, s, start+1, count, max_left, max_right-1);
-        },
-        c => {
-            temp.push(c as char);
-            backtrack(ans, temp, s, start+1, count, max_left, max_right);
-            temp.pop();
-        },
+    
+    let reversed = s.chars().rev().collect::<String>();
+    if par[0] == b'(' {// finished left to right
+        backtrack(reversed, ans, 0, 0, [b')', b'(']);
+    } else {// finished right to left
+        ans.push(reversed);
     }
 }
