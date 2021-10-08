8922ca14201eba761a7a9dbf76425b38ac4d7ddb
子風 <zpsyhapcst@gmail.com>
2021-10-08 12:07:09 +0800

    Time: 56 ms (11.11%), Space: 2.3 MB (44.44%) - LeetHub

--- remove-invalid-parentheses/remove-invalid-parentheses.rs
+++ remove-invalid-parentheses/remove-invalid-parentheses.rs
@@ -2,7 +2,8 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn remove_invalid_parentheses(s: String) -> Vec<String> {
-        let mut max_remove = 0;
+        let mut max_left = 0;
+        let mut max_right = 0;
         let mut count:i32 = 0;
         for c in s.chars(){
             match c{
@@ -11,7 +12,7 @@ impl Solution {
                 },
                 ')' => {
                     if count <= 0{
-                        max_remove += 1;
+                        max_right += 1;
                         count = 0;
                     } else {
                         count-=1;   
@@ -20,22 +21,27 @@ impl Solution {
                 _ => (),
             }
         }
-        max_remove += count.abs();
+        if count >= 0{
+            max_left += count;
+        } else {
+            max_right -= count;
+        }
+        
         // println!("{}", max_remove);
-        if max_remove == 0{
+        if max_left + max_right == 0{
             return vec![s];
-        } else if max_remove == s.len() as i32{
+        } else if max_left + max_right == s.len() as i32{
             return vec![String::new()];
         }
         
         let mut ans:HashSet<String> = HashSet::new();
         let mut temp = String::new();
-        backtrack(&mut ans, &mut temp, &s, 0, 0, max_remove);
+        backtrack(&mut ans, &mut temp, &s, 0, 0, max_left, max_right);
         ans.into_iter().collect()
     }
 }
 
-fn backtrack(ans:&mut HashSet<String>, temp:&mut String, s:&str, start:usize, count:i32, max_remove:i32){
+fn backtrack(ans:&mut HashSet<String>, temp:&mut String, s:&str, start:usize, count:i32, max_left:i32, max_right:i32){
     // println!("{}:{} {}", count, temp, max_remove);
     if start == s.len(){
         if count == 0{
@@ -43,27 +49,27 @@ fn backtrack(ans:&mut HashSet<String>, temp:&mut String, s:&str, start:usize, co
         }
         return;
     }
-    if max_remove < 0{
+    if max_left < 0 || max_right < 0{
         return;
     }
     match &s[start..start+1]{
         "(" => {
             temp.push('(');
-            backtrack(ans, temp, s, start+1, count+1, max_remove);
+            backtrack(ans, temp, s, start+1, count+1, max_left, max_right);
             temp.pop();
-            backtrack(ans, temp, s, start+1, count, max_remove-1);
+            backtrack(ans, temp, s, start+1, count, max_left-1, max_right);
         },
         ")" => {
             if count > 0{
                 temp.push(')');
-                backtrack(ans, temp, s, start+1, count-1, max_remove);
+                backtrack(ans, temp, s, start+1, count-1, max_left, max_right);
                 temp.pop();
             }
-            backtrack(ans, temp, s, start+1, count, max_remove-1);
+            backtrack(ans, temp, s, start+1, count, max_left, max_right-1);
         },
         c => {
             temp.push_str(c);
-            backtrack(ans, temp, s, start+1, count, max_remove);
+            backtrack(ans, temp, s, start+1, count, max_left, max_right);
             temp.pop();
         },
     }
