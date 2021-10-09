20a9124b7d1b1bda1bfd8f7ddf39ccc1ac4b650f
子風 <zpsyhapcst@gmail.com>
2021-10-09 14:43:00 +0800

    Time: 164 ms (50.00%), Space: 22.7 MB (80.00%) - LeetHub

--- range-sum-query-mutable/range-sum-query-mutable.rs
+++ range-sum-query-mutable/range-sum-query-mutable.rs
@@ -1,5 +1,81 @@
+// Ref: https://en.wikipedia.org/wiki/Segment_tree
+
+#[derive(Debug)]
+struct SegmentTree {
+    pub start: usize,
+    pub end: usize,
+    pub sum: i32,
+    pub left: Option<Box<SegmentTree>>,
+    pub right: Option<Box<SegmentTree>>,
+}
+
+impl SegmentTree {
+    // O(n)
+    fn new(start: usize, end: usize, vals: &[i32]) -> Self {
+        if start == end {
+            return Self {
+                start,
+                end,
+                sum: vals[start],
+                left: None,
+                right: None,
+            };
+        }
+        let mid = start + (end - start) / 2;
+        let left = Self::new(start, mid, vals);
+        let right = Self::new(mid + 1, end, vals);
+        let sum = left.sum + right.sum;
+        Self {
+            start,
+            end,
+            sum,
+            left: Some(Box::new(left)),
+            right: Some(Box::new(right)),
+        }
+    }
+
+    // O(logn)
+    fn update(&mut self, index: usize, val: i32) {
+        // NOTE: If is leaf, update itself
+        if self.start == self.end && self.end == index {
+            self.sum = val;
+            return;
+        }
+        // NOTE: If is not leaf, update left or right
+        let mid = self.start + (self.end - self.start) / 2;
+        if index <= mid {
+            self.left.as_mut().unwrap().update(index, val);
+        } else {
+            self.right.as_mut().unwrap().update(index, val);
+        }
+        // NOTE: After update children, update self
+        self.sum = self.left.as_ref().unwrap().sum + self.right.as_ref().unwrap().sum;
+    }
+
+    // O(logn)
+    fn query(&self, start: usize, end: usize) -> i32 {
+        // NOTE: Exact match
+        if self.start == start && self.end == end {
+            return self.sum;
+        }
+        let mid = self.start + (self.end - self.start) / 2;
+        // NOTE: Range on the left or right
+        if end <= mid{
+            return self.left.as_ref().unwrap().query(start, end);
+        // NOTE: Range on the right
+        } else if start > mid {
+            return self.right.as_ref().unwrap().query(start, end);
+        // NOTE: Range on both sides
+        } else {
+            return self.left.as_ref().unwrap().query(start, mid)
+                + self.right.as_ref().unwrap().query(mid + 1, end);
+        }
+    }
+}
+
+#[derive(Debug)]
 struct NumArray {
-    nums:Vec<i32>,
+    sums:SegmentTree,
 }
 
 
@@ -11,20 +87,16 @@ impl NumArray {
 
     fn new(nums: Vec<i32>) -> Self {
         Self{
-            nums,
+            sums: SegmentTree::new(0, nums.len()-1, &nums),
         }
     }
     
     fn update(&mut self, index: i32, val: i32) {
-        let index = index as usize;
-        self.nums[index] = val;
+        self.sums.update(index as usize, val);
     }
     
     fn sum_range(&self, left: i32, right: i32) -> i32 {
-        let left = left as usize;
-        let right = right as usize;
-        
-        self.nums[left..=right].iter().sum()
+        self.sums.query(left as usize, right as usize)
     }
 }
 
