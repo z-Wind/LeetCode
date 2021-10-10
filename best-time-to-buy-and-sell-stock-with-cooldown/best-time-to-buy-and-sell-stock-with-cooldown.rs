b9ba669d83cd062a85cdf96425f8a81ca1e5f0b5
子風 <zpsyhapcst@gmail.com>
2021-10-10 12:29:47 +0800

    Time: 3 ms (18.18%), Space: 2.1 MB (100.00%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.rs
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.rs
@@ -1,74 +1,31 @@
-// https://github.com/z-Wind/LeetCode/blob/main/best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.rs
-// dp[k, i] = max(dp[k, i-1], prices[i] - prices[j] + dp[k-1, j-2]), j=[0..=i-1]
-//                  nothing     buy at j, sell at i
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75928/Share-my-DP-solution-(By-State-Machine-Thinking)
+//
+// three state
+// s0: stay or cooldown from s2
+// s1: stay or buy from s0
+// s2: sell from s1
+//
+// three lists mean total money at i
+// s0[i] = max(s0[i - 1], s2[i - 1]); // Stay at s0, or rest from s2
+// s1[i] = max(s1[i - 1], s0[i - 1] - prices[i]); // Stay at s1, or buy from s0
+// s2[i] = s1[i - 1] + prices[i]; // Only one way from s1
+
 use std::cmp::max;
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        MaxProfit_V3(prices)
+        let n = prices.len();
+        if n <= 1 { return 0; }
+		let mut s0 = vec![0;n];
+		let mut s1 = vec![0;n];
+		let mut s2 = vec![0;n];
+		s1[0] = -prices[0];
+		s0[0] = 0;
+		s2[0] = i32::MIN;
+		for i in 1..n {
+			s0[i] = max(s0[i - 1], s2[i - 1]);
+			s1[i] = max(s1[i - 1], s0[i - 1] - prices[i]);
+			s2[i] = s1[i - 1] + prices[i];
+		}
+		return max(s0[n - 1], s2[n - 1]);
     }
-}
-
-// Version 1
-// Time complexity is O(kn^2), space complexity is O(kn)
-fn MaxProfitDp_V1(prices: Vec<i32>) -> i32 {
-    if (prices.len() <= 1) { return 0 };
-    let max_k = if prices.len()%3 == 0 {prices.len()/3} else {prices.len()/3+1};
-    let mut dp = vec![vec![0;prices.len()];max_k+1];
-    for k in (1..=max_k) {
-        for i in (1..prices.len()) {
-            let mut min = prices[0].min(prices[1]);
-            for j in (2..=i-1){
-                min = min.min(prices[j] - dp[k-1][j-2]);
-            }
-            dp[k][i] = max(dp[k][i-1], prices[i] - min);
-        }
-    }
-
-    return dp[max_k][prices.len() - 1];
-}
-
-// Version 2: Get rid of repeating calculation of min
-// because dp[k][i] should be the maximum value
-// Time complexity is O(kn), space complexity is O(kn)
-pub fn MaxProfitDp_V2(prices: Vec<i32>) -> i32 {
-    if (prices.len() <= 1) { return 0 };
-    let max_k = if prices.len()%3 == 0 {prices.len()/3} else {prices.len()/3+1};
-    let mut dp = vec![vec![0;prices.len()];max_k+1];
-    for k in (1..=max_k) {
-        dp[k][1] = max(dp[k][0], prices[1] - prices[0]);
-        
-        let mut min = prices[0].min(prices[1]);
-        for i in (2..prices.len()) {
-            min = min.min(prices[i] - dp[k-1][i-2]);
-            dp[k][i] = max(dp[k][i-1], prices[i] - min);
-        }
-    }
-
-    return dp[max_k][prices.len() - 1];
-}
-
-// Version 3: compact k
-//
-// dp[k-1][i-2]
-// dp[k][i-1]   dp[k][i]
-//
-// dp[i-2]
-// dp[i-1]      dp[i]
-//
-// Time complexity is O(kn), space complexity is O(n).
-pub fn MaxProfit_V3(prices: Vec<i32>) -> i32 {
-    if (prices.len() <= 1) { return 0 };
-    let max_k = if prices.len()%3 == 0 {prices.len()/3} else {prices.len()/3+1};
-    let mut dp = vec![0;prices.len()];
-    for k in (1..=max_k) {
-        dp[1] = max(dp[0], prices[1] - prices[0]);
-        
-        let mut min = prices[0].min(prices[1]);
-        for i in (2..prices.len()) {
-            min = min.min(prices[i] - dp[i-2]);
-            dp[i] = max(dp[i-1], prices[i] - min);
-        }
-    }
-
-    return dp[prices.len() - 1];
 }
