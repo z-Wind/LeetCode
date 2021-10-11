f748fa6c057fabfaec953f433fb8623d57a74fe5
子風 <zpsyhapcst@gmail.com>
2021-10-11 11:04:18 +0800

    Time: 8 ms (100.00%), Space: 3.2 MB (90.00%) - LeetHub

--- minimum-height-trees/minimum-height-trees.rs
+++ minimum-height-trees/minimum-height-trees.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::collections::VecDeque;
 impl Solution {
     pub fn find_min_height_trees(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
@@ -7,20 +6,23 @@ impl Solution {
             return (0..n as i32).collect();
         }
         
-        let mut links:Vec<HashSet<usize>> = vec![HashSet::new();n];
+        let mut links:Vec<Vec<usize>> = vec![Vec::new();n];
+        let mut edge_num:Vec<i32> = vec![0;n];
         for edge in edges{
             // a-b
             let a = edge[0] as usize;
             let b = edge[1] as usize;
             
-            links[a].insert(b);
-            links[b].insert(a);
+            links[a].push(b);
+            links[b].push(a);
+            edge_num[a] += 1;
+            edge_num[b] += 1;
         }
         
         // println!("{:?}", links);
         let mut leaves = VecDeque::new();
         for i in (0..n){
-            if links[i].len() == 1{
+            if edge_num[i] == 1{
                 leaves.push_back(i);
             }
         }
@@ -29,10 +31,11 @@ impl Solution {
             n-=leaves.len();
             for _ in (0..leaves.len()){
                 let i = leaves.pop_front().unwrap();
-                let node = links[i].drain().next().unwrap();
-                links[node].remove(&i);
-                if links[node].len() == 1{
-                    leaves.push_back(node);
+                for &node in links[i].iter(){
+                    edge_num[node] -= 1;
+                    if edge_num[node] == 1{
+                        leaves.push_back(node);
+                    }
                 }
             }
         }
