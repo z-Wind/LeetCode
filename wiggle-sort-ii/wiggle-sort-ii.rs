e9eb3e6e795b1642b61b75571a55b6f99c57b349
子風 <zpsyhapcst@gmail.com>
2021-10-23 00:08:18 +0800

    Time: 16 ms (100.00%), Space: 2.4 MB (100.00%) - LeetHub

--- wiggle-sort-ii/wiggle-sort-ii.rs
+++ wiggle-sort-ii/wiggle-sort-ii.rs
@@ -1,41 +1,19 @@
-use std::collections::VecDeque;
-use std::iter::FromIterator;
-impl Solution {
-    pub fn wiggle_sort(nums: &mut Vec<i32>) {  
-        let mut vdq = VecDeque::from_iter(nums.clone());
-        for _ in 0..nums.len(){
-            let num = vdq.pop_front().unwrap();
-            match wiggle_sort(&mut vdq, num, true){
-                None => (),
-                Some(mut x) => {
-                    x.push(num);
-                    x.reverse();
-                    *nums = x;
-                    return;
-                }
-            }
-            vdq.push_back(num);
-        }
-    }
-}
+// https://leetcode.com/problems/wiggle-sort-ii/discuss/77678/3-lines-Python-with-Explanation-Proof
 
-fn wiggle_sort(nums: &mut VecDeque<i32>, prev:i32, odd:bool) -> Option<Vec<i32>>{
-    // println!("{},{}, {:?}", num, odd, nums);
-    if nums.is_empty(){
-        return Some(vec![]);
-    }
-    for i in 0..nums.len(){
-        if (odd && nums[i] > prev) || (!odd && nums[i] < prev){
-            let cur = nums.swap_remove_front(i).unwrap();
-            match wiggle_sort(nums, cur, !odd){
-                None => (),
-                Some(mut x) => {
-                    x.push(cur);
-                    return Some(x);
-                }
-            }
-            nums.push_front(cur);
+impl Solution {
+    pub fn wiggle_sort(nums: &mut Vec<i32>) {
+        let mut temp = nums.clone();
+        temp.sort();
+        let n = temp.len();
+        let mid = (n+1)/2;
+        // println!("{} {} {:?}", n,n/2,temp);
+        for i in (0..temp.len()-1).step_by(2){
+            // println!("{},{} => {},{}",i,i+1,temp[mid-1-i/2],temp[n-1-i/2]);
+            nums[i] = temp[mid-1-i/2];
+            nums[i+1] = temp[n-1-i/2];
+        }
+        if n%2 == 1{
+            nums[n-1] = temp[0];
         }
     }
-    None
 }
