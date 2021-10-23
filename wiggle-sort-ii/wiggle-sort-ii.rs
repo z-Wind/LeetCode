963c4638a7fd7105e0250356a5c89cca3dc4a537
子風 <zpsyhapcst@gmail.com>
2021-10-23 13:42:04 +0800

    Time: 40 ms (50.00%), Space: 2.9 MB (50.00%) - LeetHub

--- wiggle-sort-ii/wiggle-sort-ii.rs
+++ wiggle-sort-ii/wiggle-sort-ii.rs
@@ -1,19 +1,77 @@
-// https://leetcode.com/problems/wiggle-sort-ii/discuss/77678/3-lines-Python-with-Explanation-Proof
+// https://leetcode.com/problems/wiggle-sort-ii/discuss/77677/O(n)%2BO(1)-after-median-Virtual-Indexing
+//
+// find mid
+// three-way partitioning to 1 3 5 7 0 2 4 6
+// so larger at 1 3 5, mid at 7 0, smaller at 2 4 6
+
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 impl Solution {
     pub fn wiggle_sort(nums: &mut Vec<i32>) {
-        let mut temp = nums.clone();
-        temp.sort();
-        let n = temp.len();
-        let mid = (n+1)/2;
-        // println!("{} {} {:?}", n,n/2,temp);
-        for i in (0..temp.len()-1).step_by(2){
-            // println!("{},{} => {},{}",i,i+1,temp[mid-1-i/2],temp[n-1-i/2]);
-            nums[i] = temp[mid-1-i/2];
-            nums[i+1] = temp[n-1-i/2];
-        }
-        if n%2 == 1{
-            nums[n-1] = temp[0];
+        let n = nums.len();
+    
+        // Find a median.
+        let mid = nth_element(nums, n/2);
+        // println!("mid:{}", mid);
+        
+        // Index-rewiring.
+        // 0 1 2 3 4 5
+        // 1 3 5 0 2 4
+        let A = |i| {
+            (1 + 2 * i) % (n | 1)
+        };
+
+        // 3-way-partition-to-wiggly in O(n) time with O(1) space.
+        let mut i = 0;
+        let mut j = 0;
+        let mut k = n - 1;
+        while j <= k {
+            // println!("i:{}=>{}, j:{}=>{}, k:{}=>{}, {:?}", i, A(i), j, A(j), k, A(k), nums);
+            if nums[A(j)] > mid{
+                nums.swap(A(i), A(j));
+                i+=1;
+                j+=1;
+            } else if nums[A(j)] < mid{
+                nums.swap(A(j), A(k));
+                k-=1;
+            }
+            else{
+                j+=1;
+            }
         }
     }
+}
+
+fn nth_element(nums: &mut Vec<i32>, k: usize) -> i32 {
+    nums.shuffle(&mut thread_rng());
+    let mut left = 0;
+    let mut right = nums.len()-1;
+    while left < right{
+        let i = partition(nums, left, right);
+        if i < k{
+            left = i + 1;
+        } else if i > k {
+            right = i - 1;
+        } else {
+            break;
+        }
+    }
+    nums[k]
+}
+
+fn partition(nums: &mut Vec<i32>, mut left:usize, mut right:usize) -> usize{
+    let pivot = left;
+    let mut i = left + 1;
+    let mut j = right;
+    loop{
+        while i<right && nums[i] <= nums[pivot] {i+=1}
+        while j>left && nums[j] > nums[pivot] {j-=1}
+        if i >= j{
+            break;
+        }
+        nums.swap(i,j);
+    }
+    nums.swap(pivot,j);
+    j
 }
