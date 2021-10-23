fc5841f54b939c2f1f683dca1c4b6d0c94d2729a
子風 <zpsyhapcst@gmail.com>
2021-10-23 14:23:00 +0800

    Time: 8 ms (89.29%), Space: 2 MB (78.57%) - LeetHub

--- power-of-three/power-of-three.rs
+++ power-of-three/power-of-three.rs
@@ -1,5 +1,6 @@
 impl Solution {
     pub fn is_power_of_three(n: i32) -> bool {
-        return n > 0 && 1162261467 % n == 0;
+        // 1162261467 is 3^19,  3^20 is bigger than i32::MAX
+        n > 0 && 1162261467 % n == 0
     }
 }
