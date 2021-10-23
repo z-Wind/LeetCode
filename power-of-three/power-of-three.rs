e7d44effc5cceb417739e0c8ca8d4f22bcb48864
子風 <zpsyhapcst@gmail.com>
2021-10-23 14:21:47 +0800

    Time: 15 ms (57.14%), Space: 1.9 MB (78.57%) - LeetHub

--- power-of-three/power-of-three.rs
+++ power-of-three/power-of-three.rs
@@ -1,15 +1,5 @@
 impl Solution {
     pub fn is_power_of_three(n: i32) -> bool {
-        if n >= 1162261467{
-            return n == 1162261467;
-        }
-        
-        let mut m = 1;
-        while m < n{
-            m = m+m+m;
-            // println!("{}",m);
-        }
-        
-        m == n
+        return n > 0 && 1162261467 % n == 0;
     }
 }
