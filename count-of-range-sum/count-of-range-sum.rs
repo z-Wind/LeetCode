cc95c8f9a28fc1a0dd0dc29beb14720e84dde418
子風 <zpsyhapcst@gmail.com>
2021-10-24 15:12:42 +0800

    Time: 92 ms (100.00%), Space: 4.8 MB (50.00%) - LeetHub

--- count-of-range-sum/count-of-range-sum.rs
+++ count-of-range-sum/count-of-range-sum.rs
@@ -1,3 +1,13 @@
+// https://leetcode.com/problems/count-of-range-sum/discuss/77990/Share-my-solution
+//
+// Recall count smaller number after self where we encountered the problem
+// count[i] = count of nums[j] - nums[i] < 0 with j > i
+// Here, after we did the preprocess, we need to solve the problem
+// count[i] = count of a <= S[j] - S[i] <= b with j > i
+// ans = sum(count[:])
+//
+// like https://github.com/z-Wind/LeetCode/blob/main/count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.rs
+
 impl Solution {
     pub fn count_range_sum(nums: Vec<i32>, lower: i32, upper: i32) -> i32 {
         let mut c = CountRangeSum::new(lower as i64, upper as i64);
@@ -24,41 +34,39 @@ impl CountRangeSum{
         // n+1 for only one nums, like nums[0..=0]
         let mut sum = vec![0;n+1];
         sum[0] = 0;
-        let mut temp = vec![0;n+1];
         for i in 1..=n {
             sum[i] = sum[i-1] + nums[i-1] as i64;
         }
         
         // println!("orig : {:?}", sum);
-        self.mergesort(&mut sum , 0, n, &mut temp);
+        self.mergesort(&mut sum , 0, n);
         // println!("final: {:?}", sum);
         self.count
     }
     
-    fn mergesort(&mut self, sum:&mut Vec<i64>, start:usize, end:usize, temp:&mut Vec<i64>) {
+    fn mergesort(&mut self, sum:&mut Vec<i64>, start:usize, end:usize) {
         // println!("merge: {:?}", &sum[start..=end]);
         if start >= end {
             return;
         }
         let mid = start + (end - start) / 2;
-        self.mergesort(sum, start, mid, temp);
-        self.mergesort(sum, mid + 1, end, temp);
-        self.merge(sum, start, mid, end, temp);
+        self.mergesort(sum, start, mid);
+        self.mergesort(sum, mid + 1, end);
+        self.merge(sum, start, mid, end);
     }
     
-    fn merge(&mut self,sum:&mut Vec<i64>, start:usize, mid:usize, end:usize, temp:&mut Vec<i64>) {
+    fn merge(&mut self,sum:&mut Vec<i64>, start:usize, mid:usize, end:usize) {
         let mut right = mid + 1;
-        let mut index = start;
         let mut low = mid + 1;
         let mut high = mid + 1;
+        let mut merged:Vec<i64> = Vec::with_capacity(end-start+1);
+        
         for left in start..=mid{
             while right <= end && sum[right] < sum[left] {
-                temp[index] = sum[right];
-                index += 1;
+                merged.push(sum[right]);
                 right += 1;
             }
-            temp[index] = sum[left];
-            index += 1;
+            merged.push(sum[left]);
             
             // extra action for count
             while low <= end && sum[low] - sum[left] < self.lower {
@@ -70,13 +78,16 @@ impl CountRangeSum{
             self.count += (high - low) as i32;
         }
         while right <= end {
-            temp[index] = sum[right];
-            index += 1;
+            merged.push(sum[right]);
             right += 1;
         }
         
-        for i in start..=end {
-            sum[i] = temp[i];
+        // part of normal merge sort
+        // copy back merged result into array
+        let mut pos = start;
+        for num in merged {
+            sum[pos] = num;
+            pos+=1;
         }
         // println!("{}: merged: {:?}", self.count, &sum[start..=end]);
     }
