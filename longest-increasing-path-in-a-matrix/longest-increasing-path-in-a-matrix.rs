109333ad0472c10d941122c47877f9cde01396ba
子風 <zpsyhapcst@gmail.com>
2021-10-25 21:44:04 +0800

    Time: 18 ms (50.00%), Space: 2.2 MB (100.00%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.rs
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.rs
@@ -1,73 +1,77 @@
+use std::cmp::max;
 use std::cmp::Ordering;
 impl Solution {
     pub fn longest_increasing_path(mut matrix: Vec<Vec<i32>>) -> i32 {
         let m = matrix.len();
         let n = matrix[0].len();
-        let mut dp = vec![vec![1;n];m];
-        let mut map = vec![vec![true;n];m];
-        
+        let mut dp = vec![vec![0; n]; m];
+        let mut map = vec![vec![true; n]; m];
+
         let mut ans = 0;
-        for i in 0..m{
-            for j in 0..n{
-                longest_increasing_path(&mut map, &mut ans, &mut dp, &matrix, i, j);      
+        for i in 0..m {
+            for j in 0..n {
+                longest_increasing_path(&mut map, &mut ans, &mut dp, &matrix, i, j);
             }
         }
         // println!("[");
-        // for i in 0..m{
-        //     println!("{:?}", dp[i]);    
+        // for i in 0..m {
+        //     println!("{:?}", dp[i]);
         // }
         // println!("]");
-        
+
         ans
     }
 }
 
-fn longest_increasing_path(map:&mut Vec<Vec<bool>>, ans:&mut i32, dp:&mut Vec<Vec<i32>>, matrix: &Vec<Vec<i32>>, i:usize, j:usize) { 
-    if !map[i][j]{
-        return;
-    }
-    map[i][j] = false;
-    
+fn longest_increasing_path(
+    map: &mut Vec<Vec<bool>>,
+    ans: &mut i32,
+    dp: &mut Vec<Vec<i32>>,
+    matrix: &Vec<Vec<i32>>,
+    i: usize,
+    j: usize,
+) -> i32 {
     let m = matrix.len();
-    let n = matrix[0].len();   
-    
-    // up
-    if i > 0{
-        update(map, ans, dp, matrix, (i,j), (i-1,j));
-    }
-    // down
-    if i < m-1{
-        update(map, ans, dp, matrix, (i,j), (i+1,j));
-    }
-    // left
-    if j > 0{
-        update(map, ans, dp, matrix, (i,j), (i,j-1));
-    }
-    // right
-    if j < n-1{
-        update(map, ans, dp, matrix, (i,j), (i,j+1));
-    }
-    
-    map[i][j] = true;
-    *ans = (*ans).max(dp[i][j]);
-}
+    let n = matrix[0].len();
 
-fn update(map:&mut Vec<Vec<bool>>, ans:&mut i32, dp:&mut Vec<Vec<i32>>, matrix: &Vec<Vec<i32>>, cur:(usize,usize), to:(usize,usize)){      
-    // println!("{},{} => {},{}", cur.0, cur.1, to.0, to.1);
-    match matrix[to.0][to.1].cmp(&matrix[cur.0][cur.1]){
-        Ordering::Greater => {
-            if dp[to.0][to.1] <= dp[cur.0][cur.1]{
-                dp[to.0][to.1] = 1 + dp[cur.0][cur.1];    
-                longest_increasing_path(map, ans, dp, matrix, to.0, to.1);
-            }
-        },
-        Ordering::Less => (),
-        Ordering::Equal => (),
-    }
-    
+    // println!("{},{}",i,j);
     // println!("[");
-    // for i in 0..matrix.len(){
-    //     println!("{:?}", dp[i]);    
+    // for i in 0..m{
+    //     println!("{:?}", dp[i]);
     // }
     // println!("]");
+
+    if i >= m || j >= n || !map[i][j] {
+        return 0;
+    } else if dp[i][j] != 0 {
+        return dp[i][j];
+    }
+
+    map[i][j] = false;
+
+    let mut update = |k: usize, l: usize| {
+        if k >= m || l >= n {
+            return 0;
+        }
+        // println!("{},{} => {},{}",i,j,k,l);
+        match matrix[k][l].cmp(&matrix[i][j]) {
+            Ordering::Greater => longest_increasing_path(map, ans, dp, matrix, k, l),
+            Ordering::Less => 0,
+            Ordering::Equal => 0,
+        }
+    };
+
+    dp[i][j] = 1+
+        // up
+        update(i-1,j).max(
+        // down
+        update(i+1,j)).max(
+        // left
+        update(i,j-1)).max(
+        // right
+        update(i,j+1));
+
+    map[i][j] = true;
+    *ans = (*ans).max(dp[i][j]);
+    dp[i][j]
 }
