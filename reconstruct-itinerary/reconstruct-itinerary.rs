b711e2654565ded90271993f760301c8dd17077b
子風 <zpsyhapcst@gmail.com>
2021-11-01 09:45:25 +0800

    Time: 4 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- reconstruct-itinerary/reconstruct-itinerary.rs
+++ reconstruct-itinerary/reconstruct-itinerary.rs
@@ -1,40 +1,31 @@
+// https://leetcode.com/problems/reconstruct-itinerary/discuss/78768/Short-Ruby-Python-Java-C%2B%2B
+
 use std::collections::BTreeMap;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
 impl Solution {
     pub fn find_itinerary(tickets: Vec<Vec<String>>) -> Vec<String> {        
-        let mut map:BTreeMap<String,Vec<String>> = BTreeMap::new();
-        let mut len = 1;
+        let mut targets = BTreeMap::new();
         for ticket in tickets{
-            map.entry(ticket[0].clone()).or_insert(Vec::new()).push(ticket[1].clone());
-            len += 1;
+            targets.entry(ticket[0].clone())
+            .or_insert(BinaryHeap::new())
+            .push(Reverse(ticket[1].clone()));
         }
-        for v in map.values_mut(){
-            v.sort_unstable_by(|a, b| b.cmp(a));
-        }
-        // println!("{}: {:?}", len, map);
-        
-        let ans = find_itinerary(&mut Vec::new(), map, String::from("JFK"), len);
-        ans.unwrap()
+        // println!("{:?}", targets);
+        let mut route = Vec::new();
+        visit(&mut route, &mut targets, String::from("JFK"));
+        route.reverse();
+        route
     }
 }
 
-fn find_itinerary(temp:&mut Vec<String>, mut map: BTreeMap<String,Vec<String>>, start:String, len:usize) -> Option<Vec<String>>{
-    temp.push(start.clone());
-    // println!("{:?} => {:?}", temp, map);
-    if temp.len() == len{
-        return Some(temp.clone());
-    }
-    
-    if let Some(v) = map.get(&start){
-        for i in (0..v.len()).rev(){
-            let mut map_clone = map.clone();
-            let next = map_clone.get_mut(&start).unwrap().remove(i);
-            match find_itinerary(temp, map_clone, next, len){
-                None => (),
-                x => return x,
-            }
+fn visit (route:&mut Vec<String>, targets:&mut BTreeMap<String,BinaryHeap<Reverse<String>>>, airport:String){
+    while let Some(locs) = targets.get_mut(&airport){
+        if locs.is_empty(){
+            break;
         }
+        let loc = locs.pop().unwrap().0;
+        visit(route, targets, loc);
     }
-    
-    temp.pop();
-    None
+    route.push(airport);
 }
