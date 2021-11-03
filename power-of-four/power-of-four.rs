5731413d171edf4df8cebd3fca3fee06fb4880e0
子風 <zpsyhapcst@gmail.com>
2021-11-03 15:46:27 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (47.06%) - LeetHub

---
+++ power-of-four/power-of-four.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        let zeros = n.trailing_zeros();
+        let ones = n.count_ones();
+        // println!("{}:{}", n, zeros);
+        ones == 1 && zeros & 1 == 0
+    }
+}
