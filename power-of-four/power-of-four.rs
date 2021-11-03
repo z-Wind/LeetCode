37e7c21d03d187ce384fc4c7886a78007cea8810
子風 <zpsyhapcst@gmail.com>
2021-11-03 15:47:34 +0800

    Time: 0 ms (100.00%), Space: 2 MB (88.24%) - LeetHub

--- power-of-four/power-of-four.rs
+++ power-of-four/power-of-four.rs
@@ -3,6 +3,6 @@ impl Solution {
         let zeros = n.trailing_zeros();
         let ones = n.count_ones();
         // println!("{}:{}", n, zeros);
-        ones == 1 && zeros & 1 == 0
+        ones == 1 && zeros & 1 == 0 // zeros % 2 == 0
     }
 }
