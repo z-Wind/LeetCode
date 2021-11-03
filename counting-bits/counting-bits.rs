d02536b2988e84e039a81489222db4ceb539b2f0
子風 <zpsyhapcst@gmail.com>
2021-11-03 11:38:12 +0800

    Time: 4 ms (98.53%), Space: 2.5 MB (47.06%) - LeetHub

--- counting-bits/counting-bits.rs
+++ counting-bits/counting-bits.rs
@@ -1,9 +1,21 @@
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
+        let n = n as usize + 1;
         let mut ans = Vec::with_capacity(n as usize + 1);
-        for i in 0..=n{
-            ans.push(i.count_ones() as i32);
+        ans.push(0);
+        let mut i = 1;
+        while i < n{
+            if i&1 == 0{
+                for j in 0..i{
+                    ans.push(ans[j]+1);
+                    i+=1;
+                }
+            } else {
+                ans.push(ans[i-1]+1);
+                i+=1;
+            }
+            // println!("{}:{:?}", i, ans);
         }
-        ans
+        ans[..n].to_vec()
     }
 }
