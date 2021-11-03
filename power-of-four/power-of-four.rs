ca282d88a2c3b5016849d3caba246a9b5a8c95d6
子風 <zpsyhapcst@gmail.com>
2021-11-03 15:59:25 +0800

    Time: 0 ms (100.00%), Space: 2 MB (88.24%) - LeetHub

--- power-of-four/power-of-four.rs
+++ power-of-four/power-of-four.rs
@@ -1,8 +1,15 @@
+// https://leetcode.com/problems/power-of-four/discuss/80457/Java-1-line-(cheating-for-the-purpose-of-not-using-loops)
+
 impl Solution {
     pub fn is_power_of_four(n: i32) -> bool {
-        let zeros = n.trailing_zeros();
-        let ones = n.count_ones();
-        // println!("{}:{}", n, zeros);
-        ones == 1 && zeros & 1 == 0 // zeros % 2 == 0
+        //  1: 0b000000001
+        //  4: 0b000000100
+        // 16: 0b000010000
+        // 32: 0b001000000
+        // at odd location
+        // use 0x55555555 to check
+        
+        n > 0 && (n&(n-1)) == 0 && (n & 0x55555555) != 0
+        //      check only one 1        
     }
 }
