e0ee84390cf9058594093032ecc865aa198e4791
子風 <zpsyhapcst@gmail.com>
2021-11-04 12:29:09 +0800

    Time: 0 ms (100.00%), Space: 2.5 MB (96.15%) - LeetHub

--- reverse-vowels-of-a-string/reverse-vowels-of-a-string.rs
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.rs
@@ -3,26 +3,27 @@ impl Solution {
         let mut i = 0;
         let mut j = s.len() - 1;
         let mut s = s.as_bytes().to_vec();
+        
+        let is_vowel = |c|{
+            matches!(
+                c,
+                b'a' | b'e' | b'i' | b'o' | b'u' | b'A' | b'E' | b'I' | b'O' | b'U'
+            )
+        };
+        
         while i < j {
-            let bool_i = matches!(
-                s[i],
-                b'a' | b'e' | b'i' | b'o' | b'u' | b'A' | b'E' | b'I' | b'O' | b'U'
-            );
-            let bool_j = matches!(
-                s[j],
-                b'a' | b'e' | b'i' | b'o' | b'u' | b'A' | b'E' | b'I' | b'O' | b'U'
-            );
-            if bool_i && bool_j {
-                s.swap(i, j);
+            if !is_vowel(s[i]){
                 i += 1;
+                continue;
+            }
+            if !is_vowel(s[j]){
                 j -= 1;
+                continue;
             }
-            if !bool_i {
-                i += 1;
-            }
-            if !bool_j {
-                j -= 1;
-            }
+            
+            s.swap(i, j);
+            i += 1;
+            j -= 1;
         }
 
         String::from_utf8(s).unwrap()
