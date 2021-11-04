9e576ed63beffb41e0e756ca45d78541bcd6481f
子風 <zpsyhapcst@gmail.com>
2021-11-04 18:21:38 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (37.41%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.rs
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.rs
@@ -3,24 +3,19 @@ use std::collections::HashMap;
 impl Solution {
     pub fn intersect(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> Vec<i32> {
         let mut map1:HashMap<i32,usize> = HashMap::new();
-        let mut map2:HashMap<i32,usize> = HashMap::new();
         if nums1.len() > nums2.len(){
             std::mem::swap(&mut nums1, &mut nums2);
         }
         for num in nums1{
             *map1.entry(num).or_insert(0) += 1;
         }
-        for num in nums2{
-            *map2.entry(num).or_insert(0) += 1;
-        }
         
         let mut ans:Vec<i32> = Vec::new();
-        for (&key,&val1) in map1.iter(){
-            match map2.get(&key){
-                Some(&val2) => {
-                    for _ in 0..val1.min(val2){
-                        ans.push(key);
-                    }
+        for num in nums2{
+            match map1.get_mut(&num){
+                Some(val) if *val > 0 => {
+                    ans.push(num);
+                    *val-=1;
                 },
                 _ => (),
             }
