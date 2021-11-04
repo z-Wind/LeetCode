029642b79138cf9d67080583152c8699acc3cd7d
子風 <zpsyhapcst@gmail.com>
2021-11-04 15:36:13 +0800

    Time: 4 ms (67.86%), Space: 3.2 MB (8.93%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.rs
+++ top-k-frequent-elements/top-k-frequent-elements.rs
@@ -1,30 +1,28 @@
+// https://leetcode.com/problems/top-k-frequent-elements/discuss/81602/Java-O(n)-Solution-Bucket-Sort
+
 use std::collections::HashMap;
-use std::collections::BTreeMap;
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let n = nums.len();
         let k = k as usize;
         
-        let mut map = HashMap::new();
+        let mut map:HashMap<i32,usize> = HashMap::new();
         for num in nums{
             *map.entry(num).or_insert(0) += 1;
         }
         // println!("{:?}", map);
         
-        let mut order_map = BTreeMap::new();
-        for (key, val) in map.iter(){
-            order_map.entry(val).or_insert(vec![]).push(key);
+        let mut bucket:Vec<Vec<i32>> = vec![vec![];n+1];
+        for (&key, &val) in map.iter(){
+            bucket[val].push(key);
         }
         // println!("{:?}", order_map);
         
         let mut ans:Vec<i32> = Vec::with_capacity(k);
-        let mut i = 0;
-        'outer: for (key, vals) in order_map.iter().rev(){
-            for &&val in vals{
-                ans.push(val);
-                i+=1;
-                if i == k{
-                    break 'outer;
-                }
+        for mut v in bucket.into_iter().rev(){
+            ans.append(&mut v);
+            if ans.len() >= k{
+                break;
             }
         }
         
