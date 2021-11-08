31bbca2c42ee45bd313b9d1090218527363f05b0
子風 <zpsyhapcst@gmail.com>
2021-11-08 11:10:52 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (50.00%) - LeetHub

--- count-numbers-with-unique-digits/count-numbers-with-unique-digits.rs
+++ count-numbers-with-unique-digits/count-numbers-with-unique-digits.rs
@@ -1,44 +1,29 @@
-// x    => a:10
-// xx   => ab:10*9
-//         0b:c(1,1) => means 00
-// xxx  => abc:10*9*8
-//         0bc:c(2,1)*9 => means 00x or 0x0
-//         00c:c(1,1) => means 000
-// xxxx => abcd:10*9*8*7
-//         0bcd:c(3,1)*9*8 => means 00xx or 0x0x or 0xx0
-//         00cd:c(2,1)*9 => means 000x, or 00x0
-//         000d:c(1,1) => means 0000
-impl Solution {
-    pub fn count_numbers_with_unique_digits(n: i32) -> i32 {
-        let mut count = 0;
-        
-        // all diff: abcd..
-        let mut ans = 1;
-        for i in 0..n{
-            ans *= (10-i);
-        }
-        count += ans;
-        // println!("10*9*8..= {}", ans);
-        
-        for i in (1..=n-1).rev(){
-            ans = c(i,1);
-            for num in 10-(i-1)..=9{
-                ans *= num;    
-            }
-            // println!("c({},1) * {:?} = {}", i, 9-(n-1-i)+1..=9, ans);
-            count += ans;
-        }
-        count
-    }
-}
+// https://leetcode.com/problems/count-numbers-with-unique-digits/discuss/83041/JAVA-DP-O(1)-solution.
+// Let f(n) = count of number with unique digits of length n.
+// f(1) = 10. (0, 1, 2, 3, ...., 9)
+// f(2) = 9 * 9
+// f(3) = f(2) * 8 = 9 * 9 * 8
+// f(4) = f(3) * 7 = 9 * 9 * 8 * 7
+// ...
 
-fn c(n:i32,m:i32) -> i32{
-    let mut ans = 1;
-    for i in n-m+1..=n{
-        ans *= i;
+// f(10) = 9 * 9 * 8 * 7 * 6 * ... * 1
+// f(11) = 0 = f(12) = f(13)....
+
+impl Solution {
+    pub fn count_numbers_with_unique_digits(mut n: i32) -> i32 {
+        if n == 0 {
+            return 1;   
+        }
+        
+        let mut res = 10;
+        let mut uniqueDigits = 9;
+        let mut availableNumber = 9;
+        while n > 1 && availableNumber > 0 {
+            uniqueDigits = uniqueDigits * availableNumber;
+            res += uniqueDigits;
+            availableNumber-=1;
+            n -= 1;
+        }
+        return res;
     }
-    for i in 2..=m{
-        ans /= i;
-    }
-    ans
 }
