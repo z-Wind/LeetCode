b762b6f44beea08e56f717ee3079a3106bad2faf
子風 <zpsyhapcst@gmail.com>
2021-11-09 14:10:15 +0800

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

--- water-and-jug-problem/water-and-jug-problem.rs
+++ water-and-jug-problem/water-and-jug-problem.rs
@@ -1,40 +1,23 @@
-use std::collections::HashSet;
+// https://leetcode.com/problems/water-and-jug-problem/discuss/83715/Math-solution-Java-solution
+// Bézout's lemma
+// Bézout's identity (also called Bézout's lemma) is a theorem in the elementary theory of numbers:
+// let a and b be nonzero integers and let d be their greatest common divisor. Then there exist integers x and y such that ax+by=d
+// In addition, the greatest common divisor d is the smallest positive integer that can be written as ax + by
+// every integer of the form ax + by is a multiple of the greatest common divisor d.
+
 impl Solution {
     pub fn can_measure_water(jug1_capacity: i32, jug2_capacity: i32, target_capacity: i32) -> bool {
-        if jug1_capacity + jug2_capacity < target_capacity {
-            return false;
-        }
-        let mut set = HashSet::new();
-        can_measure_water(
-            &mut set,
-            jug1_capacity,
-            jug1_capacity,
-            jug2_capacity,
-            jug2_capacity,
-            target_capacity,
-        )
+        jug1_capacity == target_capacity
+            || jug2_capacity == target_capacity
+            || jug1_capacity + jug2_capacity == target_capacity
+            || (jug1_capacity + jug2_capacity > target_capacity
+                && (target_capacity % gcd(jug1_capacity, jug2_capacity)) == 0)
     }
 }
 
-fn can_measure_water(set:&mut HashSet<(i32,i32)>, j1_max: i32, j1_r: i32, j2_max: i32, j2_r: i32, target: i32) -> bool {
-    if set.contains(&(j1_r,j2_r)){
-        return false;
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        return a;
     }
-    set.insert((j1_r,j2_r));
-    
-    // println!("{},{}",j1_r,j2_r);
-    if j1_r < 0 || j2_r < 0 || j1_r > j1_max || j2_r > j2_max{
-        return false;
-    }
-    if j1_r + j2_r == target || j1_r == target || j2_r == target {
-        return true;
-    }
-    // put water from jug2 to jug1
-    can_measure_water(set, j1_max, j1_max, j2_max, j2_r-j1_max, target)
-    || can_measure_water(set, j1_max, j2_r, j2_max, j2_max, target)
-    || can_measure_water(set, j1_max, j1_max, j2_max, j2_max-(j1_max-j1_r), target)
-    // put water from jug1 to jug2
-    || can_measure_water(set, j1_max, j1_r-j2_max, j2_max, j2_max, target)
-    || can_measure_water(set, j1_max, j1_max, j2_max, j1_r, target)
-    || can_measure_water(set, j1_max, j1_max-(j2_max-j2_r), j2_max, j2_max, target)
+    gcd(b, a % b)
 }
