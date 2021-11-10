7807823df373e3acc750422a9217fc5ab390e881
子風 <zpsyhapcst@gmail.com>
2021-11-10 19:42:57 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (18.75%) - LeetHub

--- valid-perfect-square/valid-perfect-square.rs
+++ valid-perfect-square/valid-perfect-square.rs
@@ -1,12 +1,17 @@
+// https://leetcode.com/problems/valid-perfect-square/discuss/83874/A-square-number-is-1%2B3%2B5%2B7%2B...-JAVA-code
+
 impl Solution {
     pub fn is_perfect_square(num: i32) -> bool {
-        // 46340^2 < 2147483647 < 46341^2
-        for i in 1..=46340{
-            let x = i*i;
-            if x == num{
-                return true;
-            } else if x > num{
-                break;
+        let num = num as i64;
+        let mut low = 1;
+        let mut high = num;
+        while low <= high {
+            let mid = low + ((high-low)>>1);
+            // println!("{},{} => {}",low, high, mid);
+            match (mid * mid).cmp(&num) {
+                std::cmp::Ordering::Equal => return true,
+                std::cmp::Ordering::Less => low = mid + 1,
+                std::cmp::Ordering::Greater => high = mid - 1,
             }
         }
         false
