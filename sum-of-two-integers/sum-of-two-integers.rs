485f72d50d9d175185a1795c9f7db992b4961b55
子風 <zpsyhapcst@gmail.com>
2021-11-12 10:17:48 +0800

    Time: 0 ms (100.00%), Space: 2 MB (58.14%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.rs
+++ sum-of-two-integers/sum-of-two-integers.rs
@@ -1,5 +1,19 @@
 impl Solution {
-    pub fn get_sum(a: i32, b: i32) -> i32 {
-        a+b
+    pub fn get_sum(mut a: i32, mut b: i32) -> i32 {
+        let mut ans = 0;
+        let mut carry = 0;
+        let mut sum = 0;
+        for i in 0..32{
+            let bit_a = a & 1;
+            let bit_b = b & 1;
+            let carry_t = (bit_a & bit_b) | (carry & (bit_a ^ bit_b));
+            let sum_t = carry ^ bit_a ^ bit_b;
+            carry = carry_t;
+            sum = sum_t;
+            ans |= sum << i;
+            a >>= 1;
+            b >>= 1;
+        }
+        ans
     }
 }
