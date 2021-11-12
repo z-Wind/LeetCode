89e7479bf1ef166b4941d0a6a99e60459807840d
子風 <zpsyhapcst@gmail.com>
2021-11-12 14:22:24 +0800

    Time: 284 ms (12.50%), Space: 3.6 MB (75.00%) - LeetHub

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.rs
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.rs
@@ -1,50 +1,25 @@
-use std::collections::BinaryHeap;
-
-struct Ele(i32, i32);
-impl Ord for Ele {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        (self.0 + self.1).cmp(&(other.0 + other.1)).reverse()
-    }
-}
-
-impl PartialOrd for Ele {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Ele {
-    fn eq(&self, other: &Self) -> bool {
-        (self.0, self.1) == (other.0, other.1)
-    }
-}
-
-impl Eq for Ele { }
-
 impl Solution {
     pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
-        let mut heap:BinaryHeap<Ele> = BinaryHeap::new();
-        let mut ans: Vec<Vec<i32>> = Vec::new();
-        let mut iters:Vec<(i32,_)> = Vec::new();
-        for num in nums1 {
-            iters.push((num, nums2.iter().peekable()));
-        }
-        
-        let mut i = 0;
-        while i < k {
-            for j in 0..iters.len() {
-                match iters[j].1.next(){
-                    Some(&x) => heap.push(Ele(iters[j].0, x)),
-                    None => (),
-                }    
+        let k = k as usize;
+        let n1 = nums1.len();
+        let n2 = nums2.len();
+        let mut ans: Vec<Vec<i32>> = Vec::with_capacity(k);
+        let mut idx = vec![0;n1];
+        for _ in 0..k {
+            let mut ele:Vec<i32> = vec![i32::MAX>>1, i32::MAX>>1];
+            let mut min_i=0;
+            for i in 0..n1 {
+                if idx[i] < n2 && nums1[i] + nums2[idx[i]] < ele[0] + ele[1]{
+                    ele[0] = nums1[i];
+                    ele[1] = nums2[idx[i]];
+                    min_i = i;
+                }
             }
-            match heap.pop(){
-                Some(ele) => {
-                    ans.push(vec![ele.0, ele.1]);
-                    i+=1;
-                },
-                None => break,
+            if ele[0] == i32::MAX>>1{
+                break;
             }
+            ans.push(ele);
+            idx[min_i] += 1;
         }
         
         ans
