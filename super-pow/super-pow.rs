515f87a481abbb7085bfe3530ae3e6289afc2c25
子風 <zpsyhapcst@gmail.com>
2021-11-12 11:36:42 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (15.79%) - LeetHub

--- super-pow/super-pow.rs
+++ super-pow/super-pow.rs
@@ -3,23 +3,22 @@
 // xy mod b => (bq+r)(bq'+r') = b^2qq' + bqr' + bq'r + rr' = b(bqq' + qr' + q'r) + rr'
 
 impl Solution {
-    pub fn super_pow(a: i32, b: Vec<i32>) -> i32 {
-        let mut n = 0;
+    pub fn super_pow(a: i32, mut b: Vec<i32>) -> i32 {
         let mut r = a%1337;
-        let mut ans = 1;
-        for (i,num) in b.into_iter().rev().enumerate(){
-            if i > 0{
-                // r = r^10 mod 1337
-                let mut temp = 1;
-                for _ in 0..10{
-                    temp = (temp * r) % 1337;
-                }
-                r = temp;
-            }
-            for _ in 0..num{
-                ans = (ans * r) % 1337;
-            }
+        let mut ans = power_mod(r, b.pop().unwrap());
+        while let Some(n) = b.pop(){
+            // r = r^10 mod 1337
+            r = power_mod(r, 10);
+            ans = (ans * power_mod(r, n)) % 1337;
         }
         ans
     }
+}
+
+fn power_mod(x:i32, n:i32) -> i32{
+    let mut ans = 1;
+    for _ in 0..n{
+        ans = (ans * x) % 1337;
+    }
+    ans
 }
