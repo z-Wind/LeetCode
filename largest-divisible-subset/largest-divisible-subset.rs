efd0cbdcc8e3d27d0a33ded698d526acc00df7b4
子風 <zpsyhapcst@gmail.com>
2021-11-12 10:08:13 +0800

    Time: 12 ms (85.71%), Space: 2.1 MB (100.00%) - LeetHub

--- largest-divisible-subset/largest-divisible-subset.rs
+++ largest-divisible-subset/largest-divisible-subset.rs
@@ -4,22 +4,24 @@ impl Solution {
     pub fn largest_divisible_subset(mut nums: Vec<i32>) -> Vec<i32> {
         let n = nums.len();
         nums.sort();
-        
-        let mut ans = n-1;
-        let mut dp:Vec<Vec<i32>> = vec![vec![];n];
-        for i in (0..n).rev(){
+
+        let mut ans = n - 1;
+        let mut dp: Vec<Vec<i32>> = vec![vec![]; n];
+        for i in (0..n).rev() {
             dp[i].push(nums[i]);
-            let mut max_j = i;
-            for j in i+1..n{
-                if nums[j] % nums[i] == 0 && (max_j==i || dp[j].len() > dp[max_j].len()){
-                    max_j = j;    
+            let mut max_j: Option<usize> = None;
+            for j in i + 1..n {
+                if nums[j] % nums[i] == 0
+                    && (max_j.is_none() || dp[j].len() > dp[max_j.unwrap()].len())
+                {
+                    max_j = Some(j);
                 }
             }
-            if max_j != i{
-                let mut temp = dp[max_j].clone();
+            if let Some(j) = max_j {
+                let mut temp = dp[j].clone();
                 dp[i].append(&mut temp);
             }
-            if dp[ans].len() < dp[i].len(){
+            if dp[ans].len() < dp[i].len() {
                 ans = i
             }
         }
