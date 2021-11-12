00e872ae9b82c69f79446fe334bfa9502688ab81
子風 <zpsyhapcst@gmail.com>
2021-11-12 14:11:39 +0800

    Time: 1424 ms (12.50%), Space: 485.1 MB (12.50%) - LeetHub

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.rs
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.rs
@@ -1,26 +1,50 @@
+use std::collections::BinaryHeap;
+
+struct Ele(i32, i32);
+impl Ord for Ele {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (self.0 + self.1).cmp(&(other.0 + other.1)).reverse()
+    }
+}
+
+impl PartialOrd for Ele {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Ele {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0, self.1) == (other.0, other.1)
+    }
+}
+
+impl Eq for Ele { }
+
 impl Solution {
     pub fn k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+        let mut heap:BinaryHeap<Ele> = BinaryHeap::new();
         let mut ans: Vec<Vec<i32>> = Vec::new();
         let mut iters:Vec<(i32,_)> = Vec::new();
         for num in nums1 {
             iters.push((num, nums2.iter().peekable()));
         }
-
-        for _ in 0..k {
-            let mut ele = vec![i32::MAX>>1, i32::MAX>>1];
-            let mut min_i = 0;
-            for i in 0..iters.len() {
-                if iters[i].1.peek().is_some() && iters[i].0 + **iters[i].1.peek().unwrap() < ele[0] + ele[1] {
-                    min_i = i;
-                    ele[0] = iters[i].0;
-                    ele[1] = **iters[i].1.peek().unwrap();
-                }
+        
+        let mut i = 0;
+        while i < k {
+            for j in 0..iters.len() {
+                match iters[j].1.next(){
+                    Some(&x) => heap.push(Ele(iters[j].0, x)),
+                    None => (),
+                }    
             }
-            if ele[0] == i32::MAX>>1{
-                break;
+            match heap.pop(){
+                Some(ele) => {
+                    ans.push(vec![ele.0, ele.1]);
+                    i+=1;
+                },
+                None => break,
             }
-            ans.push(ele);
-            iters[min_i].1.next();
         }
         
         ans
