97d2f2dab0700bf8cb9064a0c50ae66fae0da27b
子風 <zpsyhapcst@gmail.com>
2021-11-17 10:52:47 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (58.33%) - LeetHub

--- wiggle-subsequence/wiggle-subsequence.rs
+++ wiggle-subsequence/wiggle-subsequence.rs
@@ -1,43 +1,21 @@
-// dp[prev][start][pos] = if nums[prev] < nums[start] 1+dp[start][next][neg] else 0
-// dp[prev][start][neg] = if nums[prev] > nums[start] 1+dp[start][next][pos] else 0
+// dp[i][pos] = max(if nums[i] < nums[j] 1+dp[j][neg]) for j in i+1..n
+// dp[i][neg] = max(if nums[i] > nums[j] 1+dp[j][pos]) for j in i+1..n
 
-use std::cmp::max;
-use std::collections::HashMap;
 impl Solution {
     pub fn wiggle_max_length(nums: Vec<i32>) -> i32 {
-        if nums.len() == 1{
-            return 1;
-        }
-        let mut dp:HashMap<(usize,usize,bool),i32> = HashMap::new();
+        let n = nums.len();
+        let mut dp:Vec<Vec<i32>> = vec![vec![1,1];n];
         let mut ans = 0;
-        for i in 1..nums.len(){
-            ans = ans.max(1+max(
-                wiggle_max_length(&mut dp, &nums, i, i-1, true),
-                wiggle_max_length(&mut dp, &nums, i, i-1, false),
-            ))
+        for i in (0..n-1).rev(){
+            for j in (i+1..n).rev(){
+                if nums[i] < nums[j]{
+                    dp[i][0] = dp[i][0].max(1+dp[j][1]) 
+                } else if nums[i] > nums[j]{
+                    dp[i][1] = dp[i][1].max(1+dp[j][0]) 
+                }
+            }
         }
-        ans
+        // println!("{:?}", dp);
+        dp[0].iter().max().cloned().unwrap()
     }
-}
-
-fn wiggle_max_length(dp:&mut HashMap<(usize,usize,bool),i32>, nums: &Vec<i32>, start: usize, prev: usize, is_larger: bool) -> i32 {
-    if start == nums.len() {
-        return 0;
-    }
-    if dp.contains_key(&(prev,start,is_larger)){
-        return *dp.get(&(prev,start,is_larger)).unwrap();
-    }
-    
-    // println!("{:2} vs {:2}: {}", nums[prev], nums[start], is_larger);
-
-    let mut ans = 0;
-    if is_larger && nums[start] > nums[prev] {
-        ans = 1 + wiggle_max_length(dp, nums, start + 1, start, !is_larger);
-    } else if !is_larger && nums[start] < nums[prev] {
-        ans = 1 + wiggle_max_length(dp, nums, start + 1, start, !is_larger);
-    }
-    ans = ans.max(wiggle_max_length(dp, nums, start + 1, prev, is_larger));
-    dp.insert((prev,start,is_larger), ans);
-    // println!("{}:{:?}", nums[start], dp);
-    ans
 }
