b631d6e0f5a193f04f6ac320017bcbd7cbedec2e
子風 <zpsyhapcst@gmail.com>
2021-11-19 10:40:35 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (18.18%) - LeetHub

--- combination-sum-iv/combination-sum-iv.rs
+++ combination-sum-iv/combination-sum-iv.rs
@@ -2,26 +2,28 @@
 // comb[target] = sum(comb[target - nums[i]]), where 0 <= i < nums.length, and target >= nums[i]
 // comb[4] = comb[4-1] + comb[4-2] + comb[4-3] = comb[3] + comb[2] + comb[1]
 
+use std::collections::HashMap;
 impl Solution {
     pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
-        let mut dp = vec![-1;target as usize + 1];
+        let mut dp:HashMap<i32, i32> = HashMap::with_capacity(target as usize);
         combination_sum4(&mut dp, &nums, target)
     }
 }
 
-fn combination_sum4(dp:&mut Vec<i32>, nums: &Vec<i32>, target: i32) -> i32{
+fn combination_sum4(dp:&mut HashMap<i32, i32>, nums: &Vec<i32>, target: i32) -> i32{
     if target == 0 {
         return 1;
     }
-    if dp[target as usize] != -1{
-        return dp[target as usize];
+    if dp.contains_key(&target){
+        return *dp.get(&target).unwrap();
     }
     let mut res = 0;
     for i in 0..nums.len() {
         if target >= nums[i] {
-            let idx = (target - nums[i]) as usize;
-            dp[idx] = combination_sum4(dp, nums, target - nums[i]);
-            res += dp[idx];
+            let new_target = target - nums[i];
+            let comb = combination_sum4(dp, nums, new_target);
+            dp.insert(new_target, comb);
+            res += comb;
         }
     }
     return res;
