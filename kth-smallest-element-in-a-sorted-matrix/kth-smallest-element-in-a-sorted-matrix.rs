df1108ddc991d81b4a191abc234ec54a59b8fd13
子風 <zpsyhapcst@gmail.com>
2021-11-20 10:58:56 +0800

    Time: 0 ms (100.00%), Space: 2.6 MB (80.00%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.rs
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.rs
@@ -1,22 +1,33 @@
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
+// https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1322101/C%2B%2BJavaPython-MaxHeap-MinHeap-Binary-Search-Picture-Explain-Clean-and-Concise
+
 impl Solution {
     pub fn kth_smallest(matrix: Vec<Vec<i32>>, k: i32) -> i32 {
-        let n = matrix.len();
-        let mut heap = BinaryHeap::new();
-        for i in 0..n{
-            heap.push(Reverse((matrix[i][0], (i,1))));    
-        }
-        let mut ans = 0;
-        for _ in 0..k{
-            if let Some(Reverse((x, (i,j)))) = heap.pop(){
-                // println!("{}", x);
-                if j < n{
-                    heap.push(Reverse((matrix[i][j], (i,j+1))));
-                }
-                ans = x;
+        let k = k as usize;
+        let (m, n) = (matrix.len(), matrix[0].len()); // For general, the matrix need not be a square
+
+        let countLessOrEqual = |x| {
+            let mut cnt = 0;
+            let mut c = n - 1; // start with the rightmost column
+            for r in 0..m {
+                while c < n && matrix[r][c] > x {
+                    c -= 1
+                } // decrease column until matrix[r][c] <= x
+                cnt += (c + 1);
+            }
+            return cnt;
+        };
+
+        let (mut left, mut right) = (matrix[0][0], matrix[m - 1][n - 1]);
+        let mut ans = -1;
+        while left <= right {
+            let mid = (left + right) / 2;
+            if countLessOrEqual(mid) >= k {
+                ans = mid;
+                right = mid - 1; // try to looking for a smaller value in the left side
+            } else {
+                left = mid + 1; // try to looking for a bigger value in the right side
             }
         }
-        ans
+        return ans;
     }
 }
