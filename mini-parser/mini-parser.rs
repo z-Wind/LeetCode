13ea21ccc1baee89c44d0dbe5992e3d8f6287c5f
子風 <zpsyhapcst@gmail.com>
2021-11-23 10:57:44 +0800

    Time: 0 ms (100.00%), Space: 2.8 MB (50.00%) - LeetHub

--- mini-parser/mini-parser.rs
+++ mini-parser/mini-parser.rs
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/mini-parser/discuss/86066/An-Java-Iterative-Solution
+
 // #[derive(Debug, PartialEq, Eq)]
 // pub enum NestedInteger {
 //   Int(i32),
@@ -5,39 +7,51 @@
 // }
 impl Solution {
     pub fn deserialize(s: String) -> NestedInteger {
-        deserialize(&s)
-    }
-}
-
-fn deserialize(s: &str) -> NestedInteger {
-    // println!("'{}'", s);
-    match &s[0..1]{
-        "[" => {
-            let mut parn = 1;
-            let mut start = 1;
-            let mut v:Vec<NestedInteger> = Vec::new();
-            for i in 1..s.len(){
-                match &s[i..i+1]{
-                    "[" => parn+=1,
-                    "]" => {
-                        parn-=1;
-                        if parn == 0 && start != i{
-                            v.push(deserialize(&s[start..i]));
-                        }
-                    },
-                    "," => {
-                        if parn == 1{
-                            v.push(deserialize(&s[start..i]));
-                            start = i+1;
-                        }
-                    },
-                    _ => (),
-                }
-            }
-            return NestedInteger::List(v);
-        },
-        _ => {
+        if &s[0..1] != "[" {
             return NestedInteger::Int(s.parse().expect("should be a number"));
-        },
-    } 
+        }
+
+        let mut stack: Vec<NestedInteger> = Vec::new();
+        let mut curr: Option<Vec<NestedInteger>> = None;
+        let mut l = 0; // l shall point to the start of a number substring;
+                       // r shall point to the end+1 of a number substring
+        for (r, ch) in s.chars().enumerate() {
+            match ch {
+                '[' => {
+                    if curr.is_some() {
+                        stack.push(NestedInteger::List(curr.unwrap()));
+                    }
+                    curr = Some(Vec::new());
+                    l = r + 1;
+                }
+                ']' => {
+                    let num = &s[l..r];
+                    if !num.is_empty() {
+                        curr = curr.map(|mut e| {
+                            e.push(NestedInteger::Int(num.parse().expect("should be a number")));
+                            e
+                        });
+                    }
+                    if let Some(NestedInteger::List(mut pop)) = stack.pop() {
+                        pop.push(NestedInteger::List(curr.unwrap()));
+                        curr = Some(pop);
+                    }
+                    l = r + 1;
+                }
+                ',' => {
+                    if &s[r - 1..r] != "]" {
+                        let num = &s[l..r];
+                        curr = curr.map(|mut e| {
+                            e.push(NestedInteger::Int(num.parse().expect("should be a number")));
+                            e
+                        });
+                    }
+                    l = r + 1;
+                }
+                _ => (),
+            }
+        }
+
+        return NestedInteger::List(curr.unwrap());
+    }
 }
