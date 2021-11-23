e62b3d6586ae239f4124cf6fff9056d12bf3fdd0
子風 <zpsyhapcst@gmail.com>
2021-11-23 14:45:57 +0800

    Time: 2 ms (41.94%), Space: 2.3 MB (12.90%) - LeetHub

--- find-the-difference/find-the-difference.rs
+++ find-the-difference/find-the-difference.rs
@@ -1,17 +1,15 @@
+// https://leetcode.com/problems/find-the-difference/discuss/86825/Java-solution-using-bit-manipulation
+
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut counts = vec![0;26];
-        for c in s.bytes(){
-            let i = (c-b'a') as usize;
-            counts[i]+=1;
+        let s = s.as_bytes();
+        let t = t.as_bytes();
+        let n = t.len();
+        let mut ans:u8 = t[n-1];
+        for i in 0..n-1{
+            ans ^= s[i];
+            ans ^= t[i];
         }
-        for c in t.bytes(){
-            let i = (c-b'a') as usize;
-            if counts[i] == 0{
-                return c as char;
-            }
-            counts[i]-=1;
-        }
-        panic!("impossible");
+        ans as char
     }
 }
