ca8359da9607847c853c70a503bf99a695c7f05f
子風 <zpsyhapcst@gmail.com>
2021-11-24 08:55:58 +0800

    Time: 9 ms (100.00%), Space: 2.2 MB (66.67%) - LeetHub

--- elimination-game/elimination-game.rs
+++ elimination-game/elimination-game.rs
@@ -1,37 +1,18 @@
-// 1 2
-//   1   => n/2
+// https://leetcode.com/problems/elimination-game/discuss/87119/JAVA%3A-Easiest-solution-O(logN)-with-explanation
+// head = 1, left = true, step = 1 (times 2 each turn), remaining = n(24)
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 //
-// 1 2 3
-//   1   => n/2
+// head = 2, left = false, step = 1 * 2 = 2, remaining = remaining / 2 = 12
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 - > 2 4 6 8 10 12 14 16 18 20 22 24
 //
-// 1 2 3 4
-//   1   2  => n/2
-//   1      => (n+1)/2
+// head = 2, left = true, step = 2 * 2 = 4, remaining = remaining / 2 = 6
+// 2 4 6 8 10 12 14 16 18 20 22 24 - > 2 6 10 14 18 22
 //
-// 1 2 3 4 5
-//   1   2    => n/2
-//   1        => (n+1)/2
+// head = 6, left = false, step = 4 * 2 = 8, remaining = remaining / 2 = 3
+// 2 6 10 14 18 22 - > 6 14 22
 //
-// 1 2 3 4 5 6
-//   1   2   3  => n/2
-//       1      => n/2
-//
-// 1 2 3 4 5 6 7 8 9 10 11 12
-//   1   2   3   4    5     6   => n/2
-//   1       2        3         => (n+1)/2
-//           1                  => n/2
-//
-// 1  => 1
-// 2  => 2
-// 3  => 2
-// 4  => 2
-// 5  => 2
-// 6  => 4
-// 7  => 4
-// 8  => 6
-// 9  => 6
-// 10 => 8
-// 12 => 6
+// head = 14, left = true, step = 8 * 2 = 16, remaining = remaining / 2 = 1
+// 6 14 22 - > 14
 
 #[derive(Debug)]
 enum Order{
@@ -45,40 +26,34 @@ fn reverse_order(order:Order) -> Order{
     }
 }
 impl Solution {
-    pub fn last_remaining(mut n: i32) -> i32 {
+    pub fn last_remaining(n: i32) -> i32 {
+        let mut head = 1;
+        let mut step = 1;
         let mut order = Order::LtoR;
-        let mut cal = vec![];
-        
-        // top to down
-        while n >= 2{
-            // println!("{}:{:?}", n, order);
-            match (&order, n%2){
+        let mut remaining = n;
+        while remaining > 1{
+            // println!("head = {}, order = {:?}, step = {}, remaining = {}", head, order, step, remaining);
+            match (&order, remaining%2){
                 (Order::LtoR, 0) => {
-                    cal.push(0);
-                    n>>=1;
+                    head += step;
+                    step <<= 1;
                 },
                 (Order::LtoR, 1) => {
-                    cal.push(0);
-                    n>>=1;
+                    head += step;
+                    step <<= 1;
                 },
                 (Order::RtoL, 1) => {
-                    cal.push(0);
-                    n>>=1;
+                    head += step;
+                    step <<= 1;
                 },
                 (Order::RtoL, 0) => {
-                    cal.push(-1);
-                    n = (n+1)>>1;
+                    step <<= 1;
                 }
                 _ => panic!("impossible")
             }
             order = reverse_order(order);
+            remaining /= 2;
         }
-        // println!("{}:{:?}",n,cal);
-        
-        // down to top
-        while let Some(x) = cal.pop(){
-            n = (n<<1)+x;
-        }
-        n
+        head
     }
 }
