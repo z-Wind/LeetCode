74c7ae02f5339397aabff44abd6d6f2d53f17fdf
子風 <zpsyhapcst@gmail.com>
2021-11-24 11:34:45 +0800

    Time: 34 ms (37.50%), Space: 3 MB (25.00%) - LeetHub

--- perfect-rectangle/perfect-rectangle.rs
+++ perfect-rectangle/perfect-rectangle.rs
@@ -1,54 +1,46 @@
+// https://leetcode.com/problems/perfect-rectangle/discuss/87181/Really-Easy-Understanding-Solution(O(n)-Java)
+
 use std::collections::HashSet;
 impl Solution {
     pub fn is_rectangle_cover(rectangles: Vec<Vec<i32>>) -> bool {
-        let mut set_bottom_left:HashSet<(i32,i32)> = HashSet::new();
-        let mut set_bottom_right:HashSet<(i32,i32)> = HashSet::new();
-        let mut set_top_left:HashSet<(i32,i32)> = HashSet::new();
-        let mut set_top_right:HashSet<(i32,i32)> = HashSet::new();
-        for rec in rectangles{
-            let bottom_left = (rec[0],rec[1]);
-            if set_bottom_left.contains(&bottom_left){
-                return false;
-            }
-            let bottom_right = (rec[2],rec[1]);
-            if set_bottom_right.contains(&bottom_right){
-                return false;
-            }
-            let top_left = (rec[0],rec[3]);
-            if set_top_left.contains(&top_left){
-                return false;
-            }
-            let top_right = (rec[2],rec[3]);
-            if set_top_right.contains(&top_right){
-                return false;
-            }
-            
-            combine(bottom_left, &mut set_bottom_left, &mut set_bottom_right, &mut set_top_left);
-            combine(bottom_right, &mut set_bottom_right, &mut set_bottom_left, &mut set_top_right);
-            combine(top_left, &mut set_top_left, &mut set_bottom_left, &mut set_top_right);
-            combine(top_right, &mut set_top_right, &mut set_bottom_right, &mut set_top_left);
-            
-            // println!("bottom_left:{:?}", bottom_left);
-            // println!("bottom_right:{:?}", bottom_right);
-            // println!("top_left:{:?}", top_left);
-            // println!("top_right:{:?}", top_right);
-            // println!("set_bottom_left:{:?}", set_bottom_left);
-            // println!("set_bottom_right:{:?}", set_bottom_right);
-            // println!("set_top_left:{:?}", set_top_left);
-            // println!("set_top_right:{:?}", set_top_right);
-            // println!("");
-        }
-        
-        set_bottom_left.len() == 1 && set_bottom_right.len() == 1 && set_top_left.len() == 1 && set_top_right.len() == 1
-    }
-}
+        let mut x = i32::MAX;
+        let mut y = i32::MAX;
+        let mut a = i32::MIN;
+        let mut b = i32::MIN;
+        let mut area = 0;
 
-fn combine(xy:(i32,i32), set:&mut HashSet<(i32,i32)>, set1:&mut HashSet<(i32,i32)>, set2:&mut HashSet<(i32,i32)>){
-    if set1.contains(&xy){
-        set1.remove(&xy);
-    } else if set2.contains(&xy){
-        set2.remove(&xy);
-    } else {
-        set.insert(xy);
+        let mut set: HashSet<(i32, i32)> = HashSet::new();
+        for rec in rectangles {
+            x = x.min(rec[0]);
+            y = y.min(rec[1]);
+            a = a.max(rec[2]);
+            b = b.max(rec[3]);
+
+            area += (rec[2] - rec[0]) * (rec[3] - rec[1]);
+
+            let bottom_left = (rec[0], rec[1]);
+            if !set.insert(bottom_left) {
+                set.remove(&bottom_left);
+            }
+            let bottom_right = (rec[2], rec[1]);
+            if !set.insert(bottom_right) {
+                set.remove(&bottom_right);
+            }
+            let top_left = (rec[0], rec[3]);
+            if !set.insert(top_left) {
+                set.remove(&top_left);
+            }
+            let top_right = (rec[2], rec[3]);
+            if !set.insert(top_right) {
+                set.remove(&top_right);
+            }
+        }
+        // println!("{:?}", set);
+        set.len() == 4
+            && set.contains(&(x, y))
+            && set.contains(&(a, y))
+            && set.contains(&(x, b))
+            && set.contains(&(a, b))
+            && area == (a - x) * (b - y)
     }
 }
