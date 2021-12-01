c049ac99c10e0bc102471f8de16bfcaad4fffaed
子風 <zpsyhapcst@gmail.com>
2021-12-01 14:27:41 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (9.52%) - LeetHub

--- integer-replacement/integer-replacement.rs
+++ integer-replacement/integer-replacement.rs
@@ -1,29 +1,26 @@
-// 1 -> 2 -> 4 -> 8 -> 16 -> 32 -> 64 -> 128
-use std::collections::HashMap;
+// https://leetcode.com/problems/integer-replacement/discuss/87920/A-couple-of-Java-solutions-with-explanations
+// 111011 -> 111100 -> 11110 -> 1111 -> 10000 -> 1000 -> 100 -> 10 -> 1
+// the rightmost should have more zeros
+// like this
+// 1001 -> 1000        -> 100 -> 10 -> 1
+// 1001 -> 1010 -> 101 -> 100 -> 10 -> 1
+
 impl Solution {
     pub fn integer_replacement(n: i32) -> i32 {
-        let mut dp: HashMap<i64, i32> = HashMap::new();
-        integer_replacement(&mut dp, n as i64)
-    }
-}
-
-fn integer_replacement(dp: &mut HashMap<i64, i32>, n: i64) -> i32 {
-    // println!("{}", n);
-    if n == 1 {
-        return 0;
-    }
-    if dp.contains_key(&n) {
-        return *dp.get(&n).unwrap();
-    }
-
-    let count = {
-        if n & 1 == 0 {
-            // n%2 == 0
-            integer_replacement(dp, n >> 1)
-        } else {
-            integer_replacement(dp, n - 1).min(integer_replacement(dp, n + 1))
+        let mut n = n as i64;
+        let mut count = 0;
+        while n > 1{
+            // println!("{:b}", n);
+            if n & 1 == 0{ // even is fastest
+                n >>= 1;
+            // make num to 0b*00
+            } else if n == 3 || (n>>1) & 1 == 0{ // 0b11 or 0b*01
+                n-=1;
+            } else {
+                n+=1;
+            }
+            count += 1;
         }
-    } + 1;
-    dp.insert(n, count);
-    count
+        count
+    }
 }
