8fd2fb6f5ec0c4a05d97b2dfda5b6c13eb5df17f
子風 <zpsyhapcst@gmail.com>
2021-12-02 15:56:49 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (14.29%) - LeetHub

--- convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.rs
+++ convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.rs
@@ -1,5 +1,15 @@
+use std::char;
 impl Solution {
     pub fn to_hex(num: i32) -> String {
-        format!("{:x}",num)
+        if num == 0{
+            return String::from("0");
+        }
+        let mut num = num as u32;
+        let mut s:Vec<char> = Vec::new();
+        while num > 0{
+            s.push(char::from_digit(num%16, 16).unwrap());
+            num/=16;
+        }
+        s.into_iter().rev().collect()
     }
 }
