1c5526976b4eff50a8bba4da68ba440c288f5a5c
子風 <zpsyhapcst@gmail.com>
2021-12-02 10:42:48 +0800

    Time: 0 ms (100.00%), Space: 2 MB (42.86%) - LeetHub

--- nth-digit/nth-digit.rs
+++ nth-digit/nth-digit.rs
@@ -4,24 +4,19 @@
 
 impl Solution {
     pub fn find_nth_digit(n: i32) -> i32 {
-        find_nth_digit(n as i64, 1) as i32
+        find_nth_digit(n as i64, 1)
     }
 }
 
-fn find_nth_digit(n: i64, level: i64) -> i64 {
+fn find_nth_digit(n: i64, level: i64) -> i32 {
     let start = 10_i64.pow(level as u32 - 1);
     let all = 9 * level * start;
     if n > all {
         return find_nth_digit(n - all, level + 1);
     }
     
-    let idx = ((n - 1) / level) as usize;
-    let mut loc = level - 1 - (n - 1) % level;
-    let mut num = (start..).nth(idx).unwrap();
-    // println!("n:{} ({}..) => idx:{}, num:{}, loc:{}",n,start, idx, num, loc);
-    while loc > 0 {
-        num /= 10;
-        loc -= 1;
-    }
-    num % 10
+    let idx = (n - 1) / level;
+    let loc = ((n - 1) % level) as usize;
+    let num = start+idx;
+    (num.to_string().as_bytes()[loc] - b'0') as i32
 }
