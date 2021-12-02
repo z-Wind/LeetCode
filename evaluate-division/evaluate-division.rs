7cf3f419f492496125f7abdc5e659b1e9cb278e4
子風 <zpsyhapcst@gmail.com>
2021-12-02 09:51:10 +0800

    Time: 0 ms (100.00%), Space: 2 MB (88.24%) - LeetHub

--- evaluate-division/evaluate-division.rs
+++ evaluate-division/evaluate-division.rs
@@ -1,83 +1,71 @@
-use std::collections::HashMap;
+// https://leetcode.com/problems/evaluate-division/discuss/867362/Rust-DFS-Solution-or-0ms-or-Explained
+
+use std::collections::{HashMap, HashSet};
+type Graph = HashMap<String, HashMap<String, f64>>;
 impl Solution {
     pub fn calc_equation(
         equations: Vec<Vec<String>>,
         values: Vec<f64>,
         queries: Vec<Vec<String>>,
     ) -> Vec<f64> {
-        let mut map = HashMap::new();
-        for i in 0..equations.len() {
-            insert(&mut map, equations[i][0].clone(), equations[i][1].clone(), values[i]);
-        }
-
-        let mut ans: Vec<f64> = vec![-1.0; queries.len()];
-        for i in 0..queries.len() {
-            if let Some(x) = calc(&map, queries[i][0].clone(), queries[i][1].clone()){
-                ans[i] = x;
-            }
-        }
-        ans
+        let graph = build_graph(equations, values);
+        queries
+            .into_iter()
+            .map(|query| dfs(&graph, query, &mut HashSet::new(), None).map_or(-1.0, |v| v))
+            .collect()
     }
 }
 
-fn insert(map: &mut HashMap<String, HashMap<String, f64>>, x:String, y:String, val:f64) {
-    if x == y{
-        return;
-    }
-    if let Some(mx) = map.get(&x){
-        if mx.contains_key(&y){
-            return;
-        }
-    }
-    // println!("{}={}*{}",x,y,val);
-    
-    let mut temp:Vec<(String,String,f64)> = Vec::new();
-    
-    if let Some(my) = map.get(&y){
-        for (z,val_z) in my.iter(){
-            // x=val*y, y=val_z*z
-            temp.push((x.to_string(),z.to_string(),val*val_z));
-        }
-    }
-    if let Some(mx) = map.get(&x){
-        for (z,val_z) in mx.iter(){
-            // y=1/val*x, x=val_z*z
-            temp.push((y.to_string(),z.to_string(),1.0/val*val_z));
-        }
-    }
-    map.entry(x.clone())
-        .or_insert(HashMap::new())
-        .insert(y.clone(), val);    
-    map.entry(y)
-        .or_insert(HashMap::new())
-        .insert(x, 1.0 / val);
-    
-    
-    for (a,b,val) in temp{
-        insert(map, a, b, val);
-    }
+fn build_graph(equations: Vec<Vec<String>>, values: Vec<f64>) -> Graph {
+    equations
+        .into_iter()
+        .zip(values.into_iter())
+        .fold(HashMap::new(), |mut acc, (eq, val)| {
+            let entry = acc.entry(eq[0].clone()).or_default();
+            entry.insert(eq[1].clone(), val);
+
+            let entry = acc.entry(eq[1].clone()).or_default();
+            entry.insert(eq[0].clone(), 1.0 / val);
+
+            acc
+        })
 }
 
-fn calc(map: &HashMap<String, HashMap<String, f64>>, x: String, y: String) -> Option<f64> {
-    match map.get(&x){
-        None => return None,
-        Some(mx) => {
-            match mx.get(&y){
-                Some(val) => return Some(*val),
-                None => (),
+fn dfs(
+    graph: &Graph,
+    query: Vec<String>,
+    visited: &mut HashSet<String>,
+    current_sum: Option<f64>,
+) -> Option<f64> {
+    visited.insert(query[0].clone());
+
+    // Check the the node exists in the graph
+    let entry = graph.get(&query[0])?;
+
+    // a / a = 1.0
+    if query[0] == query[1] {
+        return Some(1.0);
+    }
+
+    // If our current node has a direct route to our target, return it
+    if let Some(&answer) = entry.get(&query[1]) {
+        return current_sum.map_or(Some(answer), |x| Some(x * answer));
+    };
+
+    // Otherwise perform a DFS of connected nodes (that we haven't already visited)
+    for path in entry.keys() {
+        if !visited.contains(path) {
+            if let Some(&val) = entry.get(path) {
+                let result = dfs(
+                    graph,
+                    vec![path.clone(), query[1].clone()],
+                    visited,
+                    current_sum.map_or(Some(val), |x| Some(x * val)),
+                );
+                if result.is_some() {
+                    return result;
+                }
             }
-        }
-    }
-    if map.get(&y).is_none(){
-        return None
-    }
-    
-    let mx = map.get(&x).unwrap();
-    let my = map.get(&y).unwrap();
-    for (s,val_x) in mx.iter(){
-        match my.get(s){
-            None => continue,
-            Some(val_y) => return Some(val_x/val_y),
         }
     }
     None
