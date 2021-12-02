0c3dd3d1f916ca20e2b84420050cd7a51882b862
子風 <zpsyhapcst@gmail.com>
2021-12-02 11:04:43 +0800

    Time: 0 ms (100.00%), Space: 2 MB (100.00%) - LeetHub

--- binary-watch/binary-watch.rs
+++ binary-watch/binary-watch.rs
@@ -1,26 +1,16 @@
+// https://leetcode.com/problems/binary-watch/discuss/88458/Simple-Python%2BJava
+
 impl Solution {
     pub fn read_binary_watch(turned_on: i32) -> Vec<String> {
-        let times = vec![8,4,2,1,32,16,8,4,2,1];
+        let turned_on = turned_on as u32;
         let mut ans = Vec::new();
-        read_binary_watch(&mut ans, turned_on, &times, 0, 0, 0);
-        ans
-    }
-}
-
-fn read_binary_watch(ans:&mut Vec<String>, n: i32, times:&Vec<i32>, start:usize, hour:i32, minute:i32){
-    if hour>11 || minute>59 || start > times.len(){
-        return;
-    }
-    if n == 0{
-        ans.push(format!("{}:{:02}", hour, minute));
-        return;
-    }    
-    
-    for i in start..times.len(){
-        if i < 4{
-            read_binary_watch(ans, n-1, times, i+1, hour+times[i], minute);
-        } else {
-            read_binary_watch(ans, n-1, times, i+1, hour, minute+times[i]);
+        for h in 0..12_i32 {
+            for m in 0..60_i32 {
+                if (h * 64 + m).count_ones() == turned_on {
+                    ans.push(format!("{}:{:02}", h, m));
+                }
+            }
         }
+        ans
     }
 }
