8b1937d442fe4bc78f41b1e93cc77a8cf07165da
子風 <zpsyhapcst@gmail.com>
2021-12-02 14:24:28 +0800

    Time: 12 ms (100.00%), Space: 4.7 MB (50.00%) - LeetHub

---
+++ frog-jump/frog-jump.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn can_cross(stones: Vec<i32>) -> bool {
+        let mut dp:HashMap<(usize,i32),bool> = HashMap::new();
+        if stones[1] != stones[0]+1{
+            return false;
+        }
+        can_cross(&mut dp, &stones, 1, 1)
+    }
+}
+
+fn can_cross(dp:&mut HashMap<(usize,i32),bool>,stones: &Vec<i32>, start:usize, step:i32) -> bool {
+    if start == stones.len()-1{
+        return true;
+    }
+    if dp.contains_key(&(start,step)){
+        return *dp.get(&(start,step)).unwrap();
+    }
+    
+    // println!("{} step:{}",stones[start],step);
+    let mut ans = false;
+    for i in start+1..stones.len(){
+        if stones[i] == stones[start] + step - 1 && can_cross(dp, &stones, i, step-1){
+            ans = true;
+            break;
+        } else if stones[i] == stones[start] + step && can_cross(dp, &stones, i, step){
+            ans = true;
+            break;
+        } else if stones[i] == stones[start] + step + 1 && can_cross(dp, &stones, i, step+1){
+            ans = true;
+            break;
+        } else if stones[i] > stones[start] + step + 1 {
+            break;
+        }
+    }
+    
+    dp.insert((start,step), ans);
+    ans
+}
