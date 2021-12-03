c07c05648245e61f40779f4bdca2e3237ca40d88
子風 <zpsyhapcst@gmail.com>
2021-12-03 10:32:15 +0800

    Time: 4 ms (100.00%), Space: 2.4 MB (28.57%) - LeetHub

--- queue-reconstruction-by-height/queue-reconstruction-by-height.rs
+++ queue-reconstruction-by-height/queue-reconstruction-by-height.rs
@@ -1,36 +1,31 @@
-// bubble sort
+// https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89345/Easy-concept-with-PythonC%2B%2BJava-Solution
+//
+// 1. Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+// 2. For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+// E.g.
+// input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+// subarray after step 1: [[7,0], [7,1]]
+// subarray after step 2: [[7,0], [6,1], [7,1]]
+
+use std::collections::{BTreeMap,BTreeSet};
 impl Solution {
     pub fn reconstruct_queue(mut people: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        people.sort_unstable_by_key(|e| e[1]);
-        // println!("{:?}", people);
+        // obtain everyone's info
+        // key=height, value=k-value, index in original array
+        let mut peopledct: BTreeMap<i32, BTreeSet<(usize, usize)>> = BTreeMap::new();
+        let mut res = Vec::new();
 
-        for i in 0..people.len(){
-            let mut count = 0;
-            for j in 0..i{
-                if people[j][0] >= people[i][0]{
-                    count += 1;
-                }
-            }
-            // println!("{:?} {}:{:?}", people[i], count, people);
-            let mut j = i;
-            while people[j][1] != count{
-                if count > people[j][1]{
-                    if people[j-1][0] >= people[j][0]{
-                        count -= 1;
-                    }
-                    people.swap(j-1,j);
-                    j = j-1;
-                } else {
-                    if people[j+1][0] >= people[j][0]{
-                        count += 1;
-                    }
-                    people.swap(j+1,j);
-                    j=j+1;
-                }
-                // println!("       {}:{:?}",count,people);
+        people.iter().enumerate().for_each(|(i, p)| {
+            peopledct.entry(p[0]).or_default().insert((p[1] as usize, i));
+        });
+
+        // sort from the tallest group
+        for set in peopledct.values().rev() {
+            for &(i, j) in set.iter() {
+                res.insert(i, people[j].clone());
             }
         }
 
-        people
+        return res;
     }
 }
