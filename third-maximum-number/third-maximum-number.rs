a4e4736470d08ab7b2c701e490a7ae9b261ebb09
子風 <zpsyhapcst@gmail.com>
2021-12-08 14:58:36 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (33.33%) - LeetHub

--- third-maximum-number/third-maximum-number.rs
+++ third-maximum-number/third-maximum-number.rs
@@ -1,28 +1,26 @@
+use std::cmp::Ordering;
 impl Solution {
     pub fn third_max(nums: Vec<i32>) -> i32 {
         // 1st 2nd 3rd
         let mut max_vals = vec![None;3];
         for num in nums{
+            let num = Some(num);
             for i in 0..3{
-                if max_vals[i].is_none(){
-                    max_vals[i] = Some(num);
-                    break;
-                } else if num > max_vals[i].unwrap(){
-                    for j in (i+1..3).rev(){
-                        max_vals[j] = max_vals[j-1];
-                    }
-                    max_vals[i] = Some(num);
-                    break;
-                } else if num == max_vals[i].unwrap(){
-                    break;
+                match num.cmp(&max_vals[i]){
+                    Ordering::Less => (),
+                    Ordering::Equal => break,
+                    Ordering::Greater => {
+                        for j in (i+1..3).rev(){
+                            max_vals[j] = max_vals[j-1];
+                        }
+                        max_vals[i] = num;
+                        break;
+                    },
                 }
             }
             // println!("{:?}", max_vals);
         }
         // println!("{:?}", max_vals);
-        match max_vals[2]{
-            Some(x) => x,
-            None => max_vals[0].unwrap()
-        }
+        max_vals[2].unwrap_or(max_vals[0].unwrap())
     }
 }
