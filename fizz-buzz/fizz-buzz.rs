0ea828b1c95b9ffb6bc569f1fe798482159032fb
子風 <zpsyhapcst@gmail.com>
2021-12-08 10:39:32 +0800

    Time: 0 ms (100.00%), Space: 2.6 MB (96.25%) - LeetHub

--- fizz-buzz/fizz-buzz.rs
+++ fizz-buzz/fizz-buzz.rs
@@ -2,12 +2,16 @@ impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut ans:Vec<String> = Vec::with_capacity(n as usize);
         for i in 1..=n{
-            match (i%3==0, i%5==0){
-                (true,true) => ans.push(String::from("FizzBuzz")),
-                (true,false) => ans.push(String::from("Fizz")),
-                (false,true) => ans.push(String::from("Buzz")),
-                (false,false) => ans.push(i.to_string()),
-            }
+            ans.push(i.to_string());
+        }
+        for i in (2..n).step_by(3){
+            ans[i as usize] = String::from("Fizz")
+        }
+        for i in (4..n).step_by(5){
+            ans[i as usize] = String::from("Buzz")
+        }
+        for i in (14..n).step_by(15){
+            ans[i as usize] = String::from("FizzBuzz")
         }
         ans
     }
