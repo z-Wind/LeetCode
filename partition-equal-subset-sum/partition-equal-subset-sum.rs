b154baed4f6f0bb2c0791f503aa645780005739f
子風 <zpsyhapcst@gmail.com>
2021-12-10 10:47:18 +0800

    Time: 4 ms (95.00%), Space: 2.2 MB (40.00%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.rs
+++ partition-equal-subset-sum/partition-equal-subset-sum.rs
@@ -1,69 +1,36 @@
-// https://leetcode.com/problems/partition-equal-subset-sum/discuss/90592/01-knapsack-detailed-explanation
+// https://leetcode.com/problems/partition-equal-subset-sum/discuss/276278/Python-DP-and-(DFS%2BMemo)
 
-// dp[i][j] means whether the specific sum j can be gotten from the first i numbers
-// nums[i-1] means ith num
-// dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
-
-// dp[i-1][j-nums[i-1]] dp[i-1][j] 
-//                      dp[i][j]
-
-// should reverse update
-// dp[j-nums[i-1]] dp[j]
-//                 dp[j](update)
-
+use std::collections::HashMap;
 impl Solution {
-    pub fn can_partition(nums: Vec<i32>) -> bool {
+    pub fn can_partition(mut nums: Vec<i32>) -> bool {
         let mut sum = nums.iter().fold(0, |acc, x| acc+x);
         // println!("{}", sum);
         // sum % 2 == 1
         if sum & 1 == 1{
             return false;
         }
-        sum >>= 1;
         
-        // can_partition_1(nums, sum as usize)
-        can_partition_2(nums, sum as usize)
+        nums.sort_unstable_by(|a,b| b.cmp(a));
+        let mut memo = HashMap::new();
+        memo.insert(0, true);
+        dfs(&mut memo, &nums, 0, sum>>1)
     }
 }
 
-fn can_partition_1 (nums: Vec<i32>, sum: usize) -> bool {
-    let n = nums.len();
-    let mut dp = vec![vec![false;sum+1];n+1];    
-    dp[0][0] = true;
-    
-    for i in 1..n+1 {
-        dp[i][0] = true;
-    }
-    // for j in 1..sum+1 {
-    //     dp[0][j] = false;
-    // }
-    
-    for i in 1..n+1 {
-        for j in 1..sum+1 {
-            dp[i][j] = dp[i-1][j];
-            let num = nums[i-1] as usize;
-            if j >= num{
-                dp[i][j] = (dp[i][j] || dp[i-1][j-num]);
+fn dfs(memo:&mut HashMap<i32,bool>, nums:&Vec<i32>, start:usize, x:i32) -> bool{
+    match memo.get(&x){
+        None => {
+            memo.insert(x, false);
+            if x > 0{
+                for i in start..nums.len(){
+                    if dfs(memo, nums, i+1, x-nums[i]){
+                        memo.insert(x, true);
+                        break;
+                    }
+                }
             }
-        }
+            *memo.get(&x).unwrap()
+        },
+        Some(can) => *can,
     }
-   
-    dp[n][sum]
-}
-
-fn can_partition_2 (nums: Vec<i32>, sum: usize) -> bool {
-    let n = nums.len();
-    let mut dp = vec![false;sum+1];    
-    dp[0] = true;
-    
-    for i in 1..n+1 {
-        for j in (1..sum+1).rev() {
-            let num = nums[i-1] as usize;
-            if j >= num{
-                dp[j] = (dp[j] || dp[j-num]);
-            }
-        }
-    }
-   
-    dp[sum]
 }
