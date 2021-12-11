8b775294f23b708953c59721754fd857d79921e2
子風 <zpsyhapcst@gmail.com>
2021-12-11 14:56:00 +0800

    Time: 4 ms (100.00%), Space: 2.4 MB (60.00%) - LeetHub

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.rs
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.rs
@@ -3,64 +3,44 @@ impl Solution {
         let m = heights.len();
         let n = heights[0].len();
 
-        let mut map: Vec<Vec<i32>> = vec![vec![-1; n]; m];
+        let mut map: Vec<Vec<i32>> = vec![vec![0; n]; m];
+        
+        for i in 0..m{
+            search(&mut map, &heights, i32::MIN, i, 0, 0b10);
+            search(&mut map, &heights, i32::MIN, i, n-1, 0b01);
+        }
+        for j in 0..n{
+            search(&mut map, &heights, i32::MIN, 0, j, 0b10);
+            search(&mut map, &heights, i32::MIN, m-1, j, 0b01);
+        }
+        // println!("{:?}", map);
+        
         let mut ans: Vec<Vec<i32>> = Vec::new();
         for i in 0..m {
             for j in 0..n {
-                // println!("");
-                // println!("{},{}==========================",i,j);
-                map[i][j] = search(&mut map, &heights, i, j);
-                // println!("{:02b}=============================",map[i][j]);
                 if map[i][j] == 0b11 {
                     ans.push(vec![i as i32, j as i32]);
                 }
             }
         }
 
-        // println!("{:?}", map);
         ans
     }
 }
 
-fn search(map: &mut Vec<Vec<i32>>, heights: &Vec<Vec<i32>>, i: usize, j: usize) -> i32 {
-    if map[i][j] >= 0 {
-        return map[i][j];
-    }
-    if map[i][j] == -2{
-        return 0;
-    }
-    map[i][j] = -2;
-    
+fn search(map: &mut Vec<Vec<i32>>, heights: &Vec<Vec<i32>>, height:i32, i: usize, j: usize, mark: i32) {
     let m = heights.len();
     let n = heights[0].len();
-    let mut ans = 0;
-    // Pacific
-    if i == 0 || j == 0 {
-        ans |= 0b10;
+    if i >= m || j >= n || map[i][j] & mark > 0 || heights[i][j] < height{
+        return;
     }
-    // Atlantic
-    if i == m - 1 || j == n - 1 {
-        ans |= 0b01;
-    }
-
+    map[i][j] |= mark;
     // up
-    if ans != 0b11 && i - 1 < m && heights[i][j] >= heights[i - 1][j] {
-        ans |= search(map, heights, i - 1, j);
-    }
+    search(map, heights, heights[i][j], i - 1, j, mark);
     // down
-    if ans != 0b11 && i + 1 < m && heights[i][j] >= heights[i + 1][j] {
-        ans |= search(map, heights, i + 1, j);
-    }
+    search(map, heights, heights[i][j], i + 1, j, mark);
     // left
-    if ans != 0b11 && j - 1 < n && heights[i][j] >= heights[i][j - 1] {
-        ans |= search(map, heights, i, j - 1);
-    }
+    search(map, heights, heights[i][j], i, j - 1, mark);
     // right
-    if ans != 0b11 && j + 1 < n && heights[i][j] >= heights[i][j + 1] {
-        ans |= search(map, heights, i, j + 1);
-    }
-    
-    map[i][j] = -1;
-    // println!("{},{}=>{:02b}",i,j,ans);
-    ans
+    search(map, heights, heights[i][j], i, j + 1, mark);
 }
