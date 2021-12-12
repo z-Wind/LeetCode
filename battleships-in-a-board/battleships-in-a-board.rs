5c1326668c882a61ca5b7f00224a1e8f4bca5e8c
子風 <zpsyhapcst@gmail.com>
2021-12-12 11:22:20 +0800

    Time: 0 ms (100.00%), Space: 3.8 MB (60.00%) - LeetHub

--- battleships-in-a-board/battleships-in-a-board.rs
+++ battleships-in-a-board/battleships-in-a-board.rs
@@ -1,31 +1,16 @@
+// https://leetcode.com/problems/battleships-in-a-board/discuss/90902/Simple-Java-Solution
+
 impl Solution {
     pub fn count_battleships(mut board: Vec<Vec<char>>) -> i32 {
         let mut count = 0;
         for i in 0..board.len(){
             for j in 0..board[0].len(){
-                if board[i][j] == 'X'{
-                    count += 1;
-                    mark_battleships(&mut board, i, j);   
-                }
+                if board[i][j] == '.' { continue; }
+                if i > 0 && board[i-1][j] == 'X' { continue; }
+                if j > 0 && board[i][j-1] == 'X' { continue; }
+                count+=1;
             }
         }
         count
-    }
-}
-
-fn mark_battleships(board: &mut Vec<Vec<char>>, i:usize, j:usize){
-    if i >= board.len() || j >= board[0].len(){
-        return;
-    }
-    
-    match board[i][j] {
-        'X' => {
-            board[i][j] = 'O';
-            mark_battleships(board, i-1, j);
-            mark_battleships(board, i+1, j);
-            mark_battleships(board, i, j-1);
-            mark_battleships(board, i, j+1);
-        },
-        _ => (),
     }
 }
