2b6740016fef682d5e1bdaf49f0ff47f101f2733
子風 <zpsyhapcst@gmail.com>
2021-12-15 10:18:33 +0800

    Time: 173 ms (25.00%), Space: 12.4 MB (25.00%) - LeetHub

--- maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.rs
+++ maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.rs
@@ -1,54 +1,82 @@
-// https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/91049/Java-O(n)-solution-using-bit-manipulation-and-HashMap/95535
+// https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/91059/Java-O(n)-solution-using-Trie
+// https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/91059/Java-O(n)-solution-using-Trie/275175
+// https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/849356/Rust-Trie-solution
 
-use std::collections::HashSet;
+// binary trie, where each trie node has only 2 child 
+// one child will represent bit "0" in the binary representation of num
+// one child will represent bit "1" in the binary representation of num
+#[derive(Default,Debug)]
+struct TrieNode{
+    // children[0]: represent bit "0" in the binary representation 
+    // children[1]: represent bit "1" in the binary representation 
+    children: [Option<Box<TrieNode>>;2],
+}
 impl Solution {
     pub fn find_maximum_xor(nums: Vec<i32>) -> i32 {
-        let mut maxResult = 0;
-        let mut mask = 0;
-        /*The maxResult is a record of the largest XOR we got so far. if it's 11100 at i = 2, it means
-        before we reach the last two bits, 11100 is the biggest XOR we have, and we're going to explore
-        whether we can get another two '1's and put them into maxResult
-
-        This is a greedy part, since we're looking for the largest XOR, we start
-        from the very begining, aka, the 31st postition of bits. */
-        for i in (0..32).rev() {
-            //The mask will grow like  100..000 , 110..000, 111..000,  then 1111...111
-            //for each iteration, we only care about the left parts
-            mask = mask | (1 << i);
-
-            let mut set = HashSet::new();
-            for &num in nums.iter() {
-                // we only care about the left parts, for example, if i = 2, then we have
-                // {1100, 1000, 0100, 0000} from {1110, 1011, 0111, 0010}
-                let leftPartOfNum = num & mask;
-                set.insert(leftPartOfNum);
+        // initialize the root node
+        let mut root:TrieNode = Default::default();
+        
+        // build the trie 
+        for &num in nums.iter() {
+            // starting from root;
+            let mut curr = &mut root;
+            
+            // since every num is positive, their 31th bit is always 0
+            // we can ignore that bit and directy build from the 30th bit 
+            for i in (0..31).rev() {
+                // get the ith bit (count from LSB, 0-based) of num
+                let bit = ((num >> i) & 1) as usize;
+                
+                // if current bit is 0, it will go to children[0]
+                // if current bit is 1, it will go to children[1]
+                curr = curr.children[bit].get_or_insert_with(Default::default);
             }
-
-            // if i = 1 and before this iteration, the maxResult we have now is 1100,
-            // my wish is the maxResult will grow to 1110, so I will try to find a candidate
-            // which can give me the greedyTry;
-            let greedyTry = maxResult | (1 << i);
-
-            for &leftPartOfNum in set.iter() {
-                //This is the most tricky part, coming from a fact that if a ^ b = c, then a ^ c = b;
-                // now we have the 'c', which is greedyTry, and we have the 'a', which is leftPartOfNum
-                // If we hope the formula a ^ b = c to be valid, then we need the b,
-                // and to get b, we need a ^ c, if a ^ c exisited in our set, then we're good to go
-                let anotherNum = leftPartOfNum ^ greedyTry;
-                if set.contains(&anotherNum) {
-                    // println!("{0:011b}:{0}",leftPartOfNum);
-                    // println!("{0:011b}:{0}",anotherNum);
-                    // println!("{0:011b}:{0}",greedyTry);
-                    // println!("");
-                    maxResult = greedyTry;
-                    break;
+        }
+        
+        let mut ans = i32::MIN;
+        
+        // iterate through each num again
+        // starting from those significant bits of num, we try the best 
+        // to go to the node that represent the negation of current bit 
+        // if such node doesn't exist, we have to go the node that represents
+        // the current bit 
+        for &num in nums.iter() {
+            // starting from root
+            let mut curr = &root;
+            
+            // keep track of the maximum result of XOR current num with 
+            // other num in the array
+            let mut rst = 0;
+            for i in (0..31).rev() {
+                let bit = ((num >> i) & 1) as usize;
+                
+                // check to see if node that represents the negation of 
+                // current bit exists or not. If exists, go to that way 
+                
+                // if current bit is 1, then we want to go to children[0] (which represents 0)
+                // if current bit is 0, then we want to go to children[1] (which represents 1)
+                if let Some(node) = &curr.children[1-bit] {
+                    curr = node;
+                    
+                    // if exists, then we will have a "1" at the current index
+                    // in the result of maximum XOR 
+                    rst |= (1 << i);
+                } 
+                // if not exists
+                else {
+                    curr = &curr.children[bit].as_ref().unwrap();
                 }
             }
-
-            // If unfortunately, we didn't get the greedyTry, we still have our max,
-            // So after this iteration, the max will stay at 1100.
+            
+            // keep track of global maximum 
+            ans = ans.max(rst);
+            // there is no need to continue when final result has reached max value 
+            if ans == i32::MAX {
+                break;  
+            } 
         }
-
-        return maxResult;
+        
+        return ans;
     }
 }
+
