98f672e31b7a51bacbed023bad315488db97cc3f
子風 <zpsyhapcst@gmail.com>
2021-12-17 10:22:18 +0800

    Time: 48 ms (100.00%), Space: 17.6 MB (50.00%) - LeetHub

--- all-oone-data-structure/all-oone-data-structure.rs
+++ all-oone-data-structure/all-oone-data-structure.rs
@@ -1,9 +1,10 @@
 use std::collections::{HashMap,BTreeMap,HashSet};
-
+use std::cmp::Reverse;
 #[derive(Default, Debug)]
 struct AllOne {
     map:HashMap<String,i32>,
     min_map:BTreeMap<i32,HashSet<String>>,
+    max_map:BTreeMap<Reverse<i32>,HashSet<String>>,
 }
 
 
@@ -19,16 +20,31 @@ impl AllOne {
     
     fn inc(&mut self, key: String) {
         let count = self.map.entry(key.clone()).or_default();
+        let pre_count = *count;
         *count += 1;
-        self.min_map.entry(*count).or_default().insert(key.clone());
-        if *count > 1{
-            let count = (*count-1);
-            let mut set = self.min_map.get_mut(&count).unwrap();
+        let count = *count;
+        
+        self.min_map.entry(count).or_default().insert(key.clone());
+        if pre_count != 0{
+            let mut set = self.min_map.get_mut(&pre_count).unwrap();
             set.remove(&key);    
             if set.is_empty(){
-                    self.min_map.remove(&count);
+                    self.min_map.remove(&pre_count);
             }
         }
+        
+        self.max_map.entry(Reverse(count)).or_default().insert(key.clone());
+        if pre_count != 0{
+            let mut set = self.max_map.get_mut(&Reverse(pre_count)).unwrap();
+            set.remove(&key);    
+            if set.is_empty(){
+                    self.max_map.remove(&Reverse(pre_count));
+            }
+        }
+        
+        // println!("map:{:?}", self.map);
+        // println!("min:{:?}", self.min_map);
+        // println!("max:{:?}", self.max_map);
     }
     
     fn dec(&mut self, key: String) {
@@ -40,6 +56,13 @@ impl AllOne {
                 if set.is_empty(){
                     self.min_map.remove(&x);
                 }
+                
+                let mut set = self.max_map.get_mut(&Reverse(*x)).unwrap();
+                set.remove(&key);
+                if set.is_empty(){
+                    self.max_map.remove(&Reverse(*x));
+                }
+                
                 *x -= 1;
                 *x
             },
@@ -47,12 +70,13 @@ impl AllOne {
         if val == 0{
             self.map.remove(&key);
         } else {
-            self.min_map.entry(val).or_default().insert(key);
+            self.min_map.entry(val).or_default().insert(key.clone());
+            self.max_map.entry(Reverse(val)).or_default().insert(key);
         }
     }
     
     fn get_max_key(&self) -> String {
-        match self.min_map.values().last(){
+        match self.max_map.values().next(){
             None => String::new(),
             Some(set) => match set.iter().next(){
                 None => String::new(),
