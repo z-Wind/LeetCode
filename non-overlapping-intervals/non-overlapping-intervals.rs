b35ffa26f68d7dd167838e8d838d9b29f603e737
子風 <zpsyhapcst@gmail.com>
2021-12-21 16:16:38 +0800

    Time: 44 ms (100.00%), Space: 9.2 MB (100.00%) - LeetHub

--- non-overlapping-intervals/non-overlapping-intervals.rs
+++ non-overlapping-intervals/non-overlapping-intervals.rs
@@ -5,13 +5,13 @@ impl Solution {
         let n = intervals.len();
         let mut map:BTreeMap<i32, i32> = BTreeMap::new();
         for interval in intervals{
-            match map.get_mut(&interval[0]){
+            match map.get_mut(&interval[1]){
                 None => {
-                    map.insert(interval[0], interval[1]);
+                    map.insert(interval[1], interval[0]);
                 },
                 Some(x) => {
-                    if *x > interval[1]{
-                        *x = interval[1];
+                    if *x < interval[0]{
+                        *x = interval[0];
                     }
                 },
             }
@@ -19,11 +19,9 @@ impl Solution {
         // println!("{:?}", map);
         
         let mut count = 1;
-        let mut min_end = i32::MAX;
-        for (&start, &end) in map.iter(){
-            if start < min_end && end < min_end{
-                min_end = end;
-            } else if start >= min_end {
+        let mut min_end = *map.keys().next().unwrap();
+        for (&end, &start) in map.iter(){
+            if start >= min_end {
                 count += 1;
                 min_end = end;
             }
