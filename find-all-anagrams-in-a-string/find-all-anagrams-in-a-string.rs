fa916903f019dda1efcb018193e871ab17ff0a8b
子風 <zpsyhapcst@gmail.com>
2021-12-22 14:44:17 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (50.00%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.rs
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.rs
@@ -1,11 +1,13 @@
+// https://github.com/z-Wind/LeetCode/blob/main/minimum-window-substring/minimum-window-substring.rs
+
 impl Solution {
     pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
         let m = s.len();
         let n = p.len();
-        if n > m{
+        if n > m {
             return Vec::new();
         }
-        
+
         let mut counts_p = [0; 26];
         for i in p.bytes().map(|c| (c - b'a') as usize) {
             counts_p[i] += 1;
@@ -15,40 +17,38 @@ impl Solution {
         let mut ans = Vec::new();
         let mut window_counts = [0; 26];
         let mut collected = 0;
+        let mut l = 0;
+        let mut r = 0;
         let s = s.as_bytes();
-        for i in 0..n{
-            let c = (s[i] - b'a') as usize;
+
+        while r < m {
+            let c = (s[r] - b'a') as usize;
             if counts_p[c] > 0 {
                 window_counts[c] += 1;
                 if counts_p[c] == window_counts[c] {
                     collected += 1;
                 }
             }
-        }
-        for i in 0..m-n{
-            if collected == done{
-                ans.push(i as i32);
-            }
-            
-            let c = (s[i+n] - b'a') as usize;
-            if counts_p[c] > 0 {
-                window_counts[c] += 1;
-                if counts_p[c] == window_counts[c] {
-                    collected += 1;
+
+            while collected == done {
+                if r - l + 1 == n {
+                    ans.push(l as i32);
                 }
-            }
-            
-            let c = (s[i] - b'a') as usize;
-            if counts_p[c] > 0 {
-                if counts_p[c] == window_counts[c] {
-                    collected -= 1;
+
+                let c = (s[l] - b'a') as usize;
+                if counts_p[c] > 0 {
+                    if counts_p[c] == window_counts[c] {
+                        collected -= 1;
+                    }
+                    window_counts[c] -= 1;
                 }
-                window_counts[c] -= 1;
+
+                l += 1;
             }
+
+            r += 1;
         }
-        if collected == done{
-            ans.push((m-n) as i32);
-        }
+
         ans
     }
 }
