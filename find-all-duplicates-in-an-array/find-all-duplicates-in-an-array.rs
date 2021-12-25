1d9de87a8038f423de25d508c7b4b465b2befe40
子風 <zpsyhapcst@gmail.com>
2021-12-25 12:07:25 +0800

    Time: 12 ms (85.00%), Space: 2.6 MB (75.00%) - LeetHub

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.rs
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.rs
@@ -1,37 +1,15 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/discuss/92387/Java-Simple-Solution
+
 impl Solution {
     pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {        
         let n = nums.len();
         let mut ans = vec![];
         for i in 0..n{
-            if nums[i] <= 0{
-                continue;
+            let idx = nums[i].abs() as usize -1;
+            if nums[idx] < 0{
+                ans.push(nums[i].abs());
             }
-            // println!("i:{}", i);
-            
-            let mut num = nums[i];
-            let mut idx = num as usize -1;
-            if idx == i{
-                nums[i] = 0;
-                continue;
-            } else {
-                nums[i] = -2;   
-            }
-            loop{
-                // println!("{}:{} {:?}", num, nums[idx], nums);
-                if nums[idx] == 0{
-                    ans.push(num);
-                    nums[idx] = -1;
-                    break;
-                } else {
-                    if nums[idx] == -2{
-                        nums[idx] = 0;
-                        break;
-                    }
-                    num = nums[idx];
-                    nums[idx] = 0;
-                    idx = num as usize -1;
-                }
-            }
+            nums[idx] = -nums[idx];
         }
         // println!("{:?}", nums);
         ans
