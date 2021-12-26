462f5d10e6c55ba3aad6734a5eb05258b952496c
子風 <zpsyhapcst@gmail.com>
2021-12-26 11:14:29 +0800

    Time: 4 ms (28.57%), Space: 2 MB (100.00%) - LeetHub

--- string-compression/string-compression.rs
+++ string-compression/string-compression.rs
@@ -3,37 +3,24 @@ impl Solution {
         let n = chars.len();
         
         let mut i = 0;
-        let mut start = 0;
-        let mut end = 0;
-        let mut count = 0;
-        while end < n {
-            if chars[start] == chars[end] {
+        let mut j = 0;
+        while j < n {
+            let curr = chars[j];
+            let mut count = 0;
+            while j < n && curr == chars[j] {
                 count += 1;
-                end += 1;
-            } else {
-                chars[i] = chars[start];
-                i += 1;
-                if count > 1 {
-                    for c in count.to_string().chars(){
-                        chars[i] = c;
-                        i += 1;   
-                    }
+                j += 1;
+            }
+            
+            chars[i] = curr;
+            i += 1;
+            if count > 1 {
+                for c in count.to_string().chars(){
+                    chars[i] = c;
+                    i += 1;   
                 }
-                count = 0;
-                start = end;
             }
         }
-        chars[i] = chars[start];
-        i += 1;
-        if count > 1 {
-            for c in count.to_string().chars(){
-                chars[i] = c;
-                i += 1;   
-            }
-        }
-        // for _ in i..n{
-        //     chars.pop();
-        // }
 
         i as i32
     }
