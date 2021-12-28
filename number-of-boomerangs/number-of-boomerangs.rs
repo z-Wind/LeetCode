6efe9e1ab32b6545cb498f80c141caae7664afc5
子風 <zpsyhapcst@gmail.com>
2021-12-28 10:22:14 +0800

    Time: 132 ms (25.00%), Space: 10.9 MB (25.00%) - LeetHub

--- number-of-boomerangs/number-of-boomerangs.rs
+++ number-of-boomerangs/number-of-boomerangs.rs
@@ -2,28 +2,26 @@ use std::collections::HashMap;
 impl Solution {
     pub fn number_of_boomerangs(points: Vec<Vec<i32>>) -> i32 {
         let n = points.len();
-        let mut map = vec![HashMap::new();n];
-        for i in 0..n{
-            for j in i+1..n{
-                let x:i64 = (points[i][0] - points[j][0]) as i64;
-                let y:i64 = (points[i][1] - points[j][1]) as i64;
-                let dis:i64 = x*x + y*y;
-                map[i].entry(dis).or_insert(Vec::new()).push(j);
-                map[j].entry(dis).or_insert(Vec::new()).push(i);
+        let mut map: Vec<HashMap<i64, i32>> = vec![HashMap::new(); n];
+        for i in 0..n {
+            for j in i + 1..n {
+                let x: i64 = (points[i][0] - points[j][0]) as i64;
+                let y: i64 = (points[i][1] - points[j][1]) as i64;
+                let dis: i64 = x * x + y * y;
+                *map[i].entry(dis).or_insert(0) += 1;
+                *map[j].entry(dis).or_insert(0) += 1;
             }
         }
         // println!("{:?}", map);
-        
+
         let mut ans = 0;
-        for i in 0..n{
-            for v in map[i].values(){
-                let len = v.len() as i32;
-                if len > 1{
-                    ans += len * (len - 1);
+        for m in map{
+            for &count in m.values(){
+                if count > 1{
+                    ans += count * (count - 1);
                 }
             }
         }
         ans
     }
 }
-
