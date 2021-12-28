5b95c8a7f7f53edafeb90cb1d5b9a348becadc3c
子風 <zpsyhapcst@gmail.com>
2021-12-28 10:31:37 +0800

    Time: 52 ms (50.00%), Space: 2.1 MB (50.00%) - LeetHub

--- number-of-boomerangs/number-of-boomerangs.rs
+++ number-of-boomerangs/number-of-boomerangs.rs
@@ -3,8 +3,8 @@ impl Solution {
     pub fn number_of_boomerangs(points: Vec<Vec<i32>>) -> i32 {
         let n = points.len();
         let mut ans = 0;
+        let mut map = HashMap::new();
         for i in 0..n {
-            let mut map = HashMap::new();
             for j in 0..n {
                 if i == j{
                     continue;
@@ -15,6 +15,7 @@ impl Solution {
                 *map.entry(dis).or_insert(0) += 1;
             }
             ans += map.values().map(|&count| count * (count - 1)).sum::<i32>();
+            map.clear();
         }
 
         ans
