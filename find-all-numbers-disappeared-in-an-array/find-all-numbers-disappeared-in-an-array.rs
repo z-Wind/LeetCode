b8626a8a8dd1ce9c370a9491cb0c050359d8cd25
子風 <zpsyhapcst@gmail.com>
2021-12-28 10:44:21 +0800

    Time: 12 ms (88.37%), Space: 2.7 MB (29.07%) - LeetHub

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
+        for i in 0..nums.len() {
+            let idx = (nums[i].abs() - 1) as usize;
+            nums[idx] = -nums[idx].abs();
+        }
+        // println!("{:?}", nums);
+        
+        nums.into_iter()
+            .enumerate()
+            .filter(|(_, x)| *x > 0)
+            .map(|(i, _)| i as i32 + 1)
+            .collect()
+    }
+}
