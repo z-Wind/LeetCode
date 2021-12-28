983bb36b2bcf2bcd44de66c8bdc36bc7a5425b92
子風 <zpsyhapcst@gmail.com>
2021-12-28 09:28:58 +0800

    Time: 128 ms (9.09%), Space: 15.6 MB (77.27%) - LeetHub

--- arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.rs
+++ arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.rs
@@ -1,79 +1,28 @@
-use std::collections::HashSet;
+// by Solution
+// Approach #2 Dynamic Programming [Accepted]
+// Weak arithmetic subsequences are subsequences that consist of at least two elements and if the difference between any two consecutive elements is the same.
+// There are two properties of weak arithmetic subsequences that are very useful:
+//      1. For any pair i, j (i != j), A[i] and A[j] can always form a weak arithmetic subsequence.
+//      2. If we can append a new element to a weak arithmetic subsequence and keep it arithmetic, then the new subsequence must be an arithmetic subsequence.
+// f[i][d] denotes the number of weak arithmetic subsequences that ends with A[i] and its common difference is d.
+// for all j < i, f[i][A[i] - A[j]] += (f[j][A[i] - A[j]] + 1).
+// The 1 appears here because of the property one, we can form a new weak arithmetic subsequence for the pair (i, j).
+
+use std::collections::HashMap;
 impl Solution {
     pub fn number_of_arithmetic_slices(nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        if n < 3{
-            return 0;
-        }
-        let mut set = HashSet::new();
-        let mut ans:i32 = 0;
-        'outer: for i in 0..n-2{
-            if set.contains(&nums[i]){
-                continue;
-            }
-            for j in i+1..n-1{
-                if nums[i] != nums[j]{
-                    continue;
-                }
-                for k in j+1..n{
-                    if nums[i] != nums[k]{
-                        continue;
-                    }
-                    set.insert(nums[i]);
-                    let mut count = 3;
-                    for l in k+1..n{
-                        if nums[i] == nums[l]{
-                            count += 1;
-                        }    
-                    }
-                    
-                    for i in 3..=count{
-                        ans += nChoosek(count,i);
-                    }
-                    continue 'outer;
-                }
+        let mut ans = 0;
+        let mut cnt:Vec<HashMap<i64,i32>> = vec![HashMap::new();n];
+        for i in 0..n {
+            for j in 0..i {
+                let delta = nums[i] as i64 - nums[j] as i64;
+                let sum = *cnt[j].get(&delta).unwrap_or(&0);
+                *cnt[i].entry(delta).or_insert(0) += sum + 1;
+                // sum means the number of at least 2 elements, and append nums[i] to greater or equal to 3 elements
+                ans += sum;
             }
         }
-        for i in 0..n-2{
-            for j in i+1..n-1{
-                for k in j+1..n{
-                    let prev = nums[j] as i64;
-                    let cur = nums[k] as i64;
-                    let diff = prev - nums[i] as i64;
-                    if diff != 0 && diff == cur - prev{
-                        number_of_arithmetic_slices(&mut ans, &nums, k+1, cur, diff);       
-                    }
-                }
-            }
-        }
-        
-        ans
+        return ans;       
     }
-}
-
-fn number_of_arithmetic_slices(ans:&mut i32, nums: &Vec<i32>, start:usize, prev:i64, diff:i64){
-    *ans += 1;
-    if start >= nums.len(){
-        return;
-    }
-    
-    for i in start..nums.len(){
-        let cur = nums[i] as i64;
-        if diff == cur - prev{
-            number_of_arithmetic_slices(ans, nums, i+1, cur, diff);
-        }
-    }
-}
-
-fn nChoosek(n: usize, mut k: usize) -> i32{
-    if k > n {return 0;}
-    if k * 2 > n {k = n-k;}
-    if k == 0 {return 1;}
-
-    let mut result = n;
-    for i in 2..=k {
-        result *= (n-i+1);
-        result /= i;
-    }
-    result as i32
 }
