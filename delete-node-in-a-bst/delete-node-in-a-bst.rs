b6a48d2cf8fd9203a67f64a19845dd04a7967116
子風 <zpsyhapcst@gmail.com>
2021-12-28 16:34:04 +0800

    Time: 4 ms (92.68%), Space: 3 MB (95.12%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.rs
+++ delete-node-in-a-bst/delete-node-in-a-bst.rs
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/delete-node-in-a-bst/discuss/1590868/JAVA-or-Recursive-or-Most-Intutive-or-Proper-Explanation-Using-Image
+
 // Definition for a binary tree node.
 // #[derive(Debug, PartialEq, Eq)]
 // pub struct TreeNode {
@@ -5,7 +7,7 @@
 //   pub left: Option<Rc<RefCell<TreeNode>>>,
 //   pub right: Option<Rc<RefCell<TreeNode>>>,
 // }
-// 
+//
 // impl TreeNode {
 //   #[inline]
 //   pub fn new(val: i32) -> Self {
@@ -16,49 +18,53 @@
 //     }
 //   }
 // }
-use std::rc::Rc;
 use std::cell::RefCell;
 use std::cmp::Ordering;
+use std::rc::Rc;
 
 impl Solution {
-    pub fn delete_node(mut root: Option<Rc<RefCell<TreeNode>>>, key: i32) -> Option<Rc<RefCell<TreeNode>>> {
-        delete_node(&mut root, key);
+    pub fn delete_node(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        key: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() {
+            return root;
+        }
+
+        let val = root.as_ref().unwrap().borrow().val;
+        // println!("{}", val);
+
+        match key.cmp(&val) {
+            Ordering::Less => {
+                let node = Self::delete_node(root.as_ref().unwrap().borrow_mut().left.take(), key);
+                root.as_ref().unwrap().borrow_mut().left = node;
+            }
+            Ordering::Greater => {
+                let node = Self::delete_node(root.as_ref().unwrap().borrow_mut().right.take(), key);
+                root.as_ref().unwrap().borrow_mut().right = node;
+            }
+            Ordering::Equal => {
+                if root.clone().as_ref().unwrap().borrow().left.is_none() {
+                    return root.as_ref().unwrap().borrow_mut().right.take();
+                }
+                if root.clone().as_ref().unwrap().borrow().right.is_none() {
+                    return root.as_ref().unwrap().borrow_mut().left.take();
+                }
+
+                let val = find_min(&root.as_ref().unwrap().borrow().right);
+                root.as_ref().unwrap().borrow_mut().val = val;
+                let node = Self::delete_node(root.as_ref().unwrap().borrow_mut().right.take(), val);
+                root.as_ref().unwrap().borrow_mut().right = node;
+                    
+            }
+        }
         root
     }
 }
 
-fn delete_node(root: &mut Option<Rc<RefCell<TreeNode>>>, key: i32) {
-    if root.is_none(){
-        return;
+fn find_min(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match root.as_ref().unwrap().borrow().left.clone() {
+        None => root.as_ref().unwrap().borrow().val,
+        node => find_min(&node),
     }
-    
-    let val = root.as_ref().unwrap().borrow().val;
-    // println!("{}", val);
-
-    match key.cmp(&val){
-        Ordering::Less => {
-            delete_node(&mut root.as_ref().unwrap().borrow_mut().left, key);
-        },
-        Ordering::Greater => {
-            delete_node(&mut root.as_ref().unwrap().borrow_mut().right, key);
-        },
-        Ordering::Equal => {
-            let mut left = root.as_ref().unwrap().borrow_mut().left.take();
-            let mut right = root.as_ref().unwrap().borrow_mut().right.take();
-            *root = combine_tree(left, right);
-        },
-    }    
-}
-
-fn combine_tree(mut left: Option<Rc<RefCell<TreeNode>>>, mut right: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-    if right.is_none(){
-        return left;
-    } else if left.is_none(){
-        return right;
-    }
-    
-    let mut r = left.as_ref().unwrap().borrow_mut().right.take();
-    left.as_ref().unwrap().borrow_mut().right = combine_tree(r, right);
-    
-    left
 }
