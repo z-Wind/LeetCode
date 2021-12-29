47fbde9475f844f97af4f67f448f943f3a1199f0
子風 <zpsyhapcst@gmail.com>
2021-12-29 09:29:35 +0800

    Time: 0 ms (100.00%), Space: 2.6 MB (30.77%) - LeetHub

--- sort-characters-by-frequency/sort-characters-by-frequency.rs
+++ sort-characters-by-frequency/sort-characters-by-frequency.rs
@@ -1,42 +1,40 @@
-use std::collections::BTreeMap;
+use std::collections::BinaryHeap;
 
 impl Solution {
     pub fn frequency_sort(s: String) -> String {
-        let mut counts = vec![0; 26*2+10];
+        let mut counts = vec![0; 26 * 2 + 10];
         for c in s.bytes() {
             let idx = if c.is_ascii_lowercase() {
                 (c - b'a') as usize
             } else if c.is_ascii_uppercase() {
                 (c - b'A') as usize + 26
             } else {
-                (c - b'0') as usize + 26*2
+                (c - b'0') as usize + 26 * 2
             };
             counts[idx] += 1;
         }
 
-        let mut map = BTreeMap::new();
-        for i in 0..26*2+10 {
+        let mut heap = BinaryHeap::new();
+        for i in 0..26 * 2 + 10 {
             if counts[i] == 0 {
                 continue;
             }
 
             let c = if i < 26 {
                 (i as u8 + b'a') as char
-            } else if i < 26*2 {
+            } else if i < 26 * 2 {
                 (i as u8 + b'A' - 26) as char
             } else {
-                (i as u8 + b'0' - 26*2) as char
+                (i as u8 + b'0' - 26 * 2) as char
             };
-            map.entry(counts[i]).or_insert(Vec::new()).push(c)
+            heap.push((counts[i], c.to_string()));
         }
-
-        map.into_iter()
-            .rev()
-            .map(|(count, v)| {
-                v.into_iter()
-                    .map(|c| vec![c; count].into_iter().collect::<String>())
-                    .collect::<String>()
-            })
-            .collect()
+        // println!("{:?}", heap);
+        
+        let mut ans = String::with_capacity(s.len());
+        while let Some((count, c)) = heap.pop(){
+            ans.push_str(&c.repeat(count)); 
+        }  
+        ans
     }
 }
