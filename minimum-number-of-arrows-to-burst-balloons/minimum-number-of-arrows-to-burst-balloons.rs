02d97e957b55569f80e3f7d82f27c27ab8a2b004
子風 <zpsyhapcst@gmail.com>
2021-12-29 14:36:26 +0800

    Time: 74 ms (33.33%), Space: 9.4 MB (50.00%) - LeetHub

--- minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.rs
+++ minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.rs
@@ -1,26 +1,16 @@
 impl Solution {
     pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
         points.sort_unstable();
+        // println!("{:?}", points);
+        
         let mut count = 1;
-        let mut cur = (points[0][0]..=points[0][1]);
+        let mut end = points[0][1];
         for p in &points[1..]{
-            // println!("{:?} include? {:?}", cur, p);
-            let start = p[0];
-            let end = p[1];
-            match (cur.contains(&start), cur.contains(&end)){
-                (true, true) => {
-                    cur = (start..=end);
-                },
-                (true, false) => {
-                    cur = (start..=*cur.end());
-                },
-                (false, true) => {
-                    cur = (*cur.start()..=end);
-                },
-                (false, false) => {
-                    count += 1;
-                    cur = (start..=end);
-                }
+            if end >= p[0]{
+                end = end.min(p[1]);
+            } else {
+                end = p[1];
+                count += 1;
             } 
         }
         count
