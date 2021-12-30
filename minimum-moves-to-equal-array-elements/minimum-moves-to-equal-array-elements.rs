2b9b32da8431fb041cffef5fb054bd7c3f50d6bd
子風 <zpsyhapcst@gmail.com>
2021-12-30 13:52:29 +0800

    Time: 11 ms (50.00%), Space: 2.4 MB (50.00%) - LeetHub

---
+++ minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.rs
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements/discuss/93817/It-is-a-math-question
+// sum + (n-1) * move = n * final_val
+// final_val = min_num + move 
+// the minum number will be incremented in every move. So, if the final number is x, it would be minNum + moves;
+// sum + (n-1) * move = n * (min_num + move)
+// sum + n * move - move = n * min_num + n * move
+// move = sum - n * min_num
+impl Solution {
+    pub fn min_moves(mut nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+        let mut min = i32::MAX;
+        let mut sum = 0;
+        for num in nums{
+            sum += num;
+            min = min.min(num);
+        }
+        
+        sum - n*min
+    }
+}
