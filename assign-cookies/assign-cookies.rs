395a3c71a8215aec399fcff8eb826b8beeff90cd
子風 <zpsyhapcst@gmail.com>
2021-12-31 14:49:38 +0800

    Time: 7 ms (43.48%), Space: 2.3 MB (65.22%) - LeetHub

--- assign-cookies/assign-cookies.rs
+++ assign-cookies/assign-cookies.rs
@@ -1,5 +1,11 @@
 impl Solution {
     pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
+        let g_n = g.len();
+        let s_n = s.len();
+        if s_n == 0{
+            return 0;
+        }
+        
         g.sort_unstable();
         s.sort_unstable();
         
@@ -9,7 +15,7 @@ impl Solution {
         let mut count = 0;
         let mut i = 0;
         let mut j = 0;
-        while i < g.len() && j < s.len(){
+        while i < g_n && j < s_n{
             if g[i] <= s[j]{
                 count += 1;
                 i += 1;
