a62d11a74e3579ab9c18986c69bb8cdd56e50489
子風 <zpsyhapcst@gmail.com>
2022-01-03 16:27:20 +0800

    Time: 2 ms (100.00%), Space: 2.1 MB (66.67%) - LeetHub

--- circular-array-loop/circular-array-loop.rs
+++ circular-array-loop/circular-array-loop.rs
@@ -1,15 +1,12 @@
-#[derive(Clone, Copy, Debug)]
-enum Status{
-    New,
-    Process,
-    Fail,
-}
+// Process: 1001 | -1001
+// Fail: i32::MAX
+
 impl Solution {
-    pub fn circular_array_loop(nums: Vec<i32>) -> bool {
+    pub fn circular_array_loop(mut nums: Vec<i32>) -> bool {
         let n = nums.len();
-        let mut done = vec![Status::New;n];
         for i in 0..n{
-            if circular_array_loop(&mut done, &nums, i, nums[i] > 0, 0){
+            let is_pos = nums[i] > 0;
+            if circular_array_loop(&mut nums, i, is_pos, 0){
                 return true;
             }
         }
@@ -17,26 +14,26 @@ impl Solution {
     }
 }
 
-fn circular_array_loop(done:&mut Vec<Status>, nums: &Vec<i32>, i:usize, is_pos:bool, count:i32) -> bool {
-    // println!("{}:{:?}", nums[i], done);
+fn circular_array_loop(nums: &mut Vec<i32>, i:usize, is_pos:bool, count:i32) -> bool {
+    // println!("{}:{:?}", nums[i], nums);
     if (nums[i] > 0) ^ is_pos{
         return false;
     }
-    match done[i]{
-        Status::Fail => false,
-        Status::Process => count > 1,
-        Status::New => {
+    match nums[i]{
+        i32::MAX => false,
+        1001 | -1001 => count > 1,
+        _ => {
             let n = nums.len();
-            done[i] = Status::Process;
             let idx = (n + i + nums[i] as usize) % n;
+            nums[i] = if is_pos { 1001 } else { -1001 };
             if i == idx{
-                done[i] = Status::Fail;
+                nums[i] = i32::MAX;
                 return false;
             }
-            if circular_array_loop(done, nums, idx, is_pos, count+1){
+            if circular_array_loop(nums, idx, is_pos, count+1){
                 return true;
             }
-            done[i] = Status::Fail;
+            nums[i] = i32::MAX;
             false       
         },
     }    
