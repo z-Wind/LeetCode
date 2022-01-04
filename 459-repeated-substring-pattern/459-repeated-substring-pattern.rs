575479740fc429c31dbf2a3c793e1a0b9d6fc9f8
子風 <zpsyhapcst@gmail.com>
2022-01-04 15:26:27 +0800

    Time: 7 ms (25.00%), Space: 2 MB (100.00%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.rs
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.rs
@@ -1,24 +1,8 @@
+// https://leetcode.com/problems/repeated-substring-pattern/discuss/94334/Easy-python-solution-with-explaination
+
 impl Solution {
     pub fn repeated_substring_pattern(s: String) -> bool {
-        let n = s.len();
-        let mut len = 1;
-        while len <= n>>1{
-            if n % len == 0 && check(&s, &s[..len]){
-                return true;
-            }
-            len += 1;
-        }
-        false
+        let ss = s.repeat(2);
+        ss[1..ss.len()-1].contains(&s)
     }
-}
-
-fn check(s: &str, sub: &str) -> bool {
-    // println!("{},{}", s, sub);
-    if s.starts_with(sub){
-        if s.len() > sub.len(){
-            return check(&s[sub.len()..], sub);
-        }
-        return true;
-    }
-    false
 }
