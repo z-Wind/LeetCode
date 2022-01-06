9f8ac806cbd4ceae3297262708a97dfbb1d5abfb
子風 <zpsyhapcst@gmail.com>
2022-01-06 15:41:30 +0800

    Time: 32 ms (13.64%), Space: 2.1 MB (81.82%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.rs
+++ 463-island-perimeter/463-island-perimeter.rs
@@ -1,30 +1,33 @@
+// https://leetcode.com/problems/island-perimeter/discuss/95001/clear-and-easy-java-solution
+
 impl Solution {
-    pub fn island_perimeter(mut grid: Vec<Vec<i32>>) -> i32 {
-        let mut perimeter = 0;
-        for i in 0..grid.len(){
-            for j in 0..grid[0].len(){
-                if grid[i][j] == 1{
-                    island_perimeter(&mut perimeter, &mut grid, i, j);
-                    return perimeter   
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+
+        let mut islands = 0;
+        let mut neighbours = 0;
+
+        for i in 0..m {
+            for j in 0..n {
+                if (grid[i][j] == 1) {
+                    islands += 1; // count islands
+                    if (i - 1 < m && grid[i - 1][j] == 1) {
+                        neighbours += 1; // count up neighbours
+                    }
+                    if (i + 1 < m && grid[i + 1][j] == 1) {
+                        neighbours += 1; // count down neighbours
+                    }
+                    if (j - 1 < n && grid[i][j - 1] == 1) {
+                        neighbours += 1; // count left neighbours
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == 1) {
+                        neighbours += 1; // count right neighbours
+                    }
                 }
             }
         }
-        panic!("impossible");
-    }
-}
 
-fn island_perimeter(perimeter:&mut i32, grid: &mut Vec<Vec<i32>>, i:usize, j:usize){
-    if i >= grid.len() || j >= grid[0].len() || grid[i][j] == 0{
-        *perimeter += 1;
-        return;
+        return islands * 4 - neighbours;
     }
-    if grid[i][j] == 2{
-        return;
-    }
-    grid[i][j] = 2;
-    
-    island_perimeter(perimeter, grid, i-1, j);
-    island_perimeter(perimeter, grid, i+1, j);
-    island_perimeter(perimeter, grid, i, j-1);
-    island_perimeter(perimeter, grid, i, j+1);
 }
