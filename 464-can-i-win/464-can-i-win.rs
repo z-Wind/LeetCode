12c455bdf722453b39a0b05f5636b74c5d6e4e9c
子風 <zpsyhapcst@gmail.com>
2022-01-11 10:34:24 +0800

    Time: 497 ms (50.00%), Space: 20.5 MB (50.00%) - LeetHub

--- 464-can-i-win/464-can-i-win.rs
+++ 464-can-i-win/464-can-i-win.rs
@@ -10,48 +10,32 @@ impl Solution {
             return false;
         }
         
-        let mut nums:Vec<bool> = vec![true;max_choosable_integer as usize];
         let mut dp = HashMap::new();
-        check(&mut dp, &mut nums, desired_total, 1)
+        check(&mut dp, max_choosable_integer, 0, desired_total)
     }
 }
 
-fn check(dp:&mut HashMap<i32, bool>, nums: &mut Vec<bool>, desired_total: i32, player: i32) -> bool{    
-    let s = format(nums);
-    if dp.contains_key(&s){
-        return *dp.get(&s).unwrap();
+fn check(dp:&mut HashMap<i32, bool>, n: i32, state: i32, desired_total: i32) -> bool{    
+    if dp.contains_key(&state){
+        return *dp.get(&state).unwrap();
     }
     
     let mut ans = false;
-    let n = nums.len();
     // println!("{}: {}, {:?}", player, desired_total, nums);
-    for i in (0..n).rev() {
-        if !nums[i]{
-            continue;
-        }
-        nums[i] = false;
+    for i in 0..n {
+        let shift = (1<<i);
+        if (state & shift) != 0 {
+            continue;  
+        } 
+        
         let desired_total = desired_total - (i+1) as i32;
-        if desired_total <= 0 || !check(dp, nums, desired_total, player%2 + 1){
+        if desired_total <= 0 || !check(dp, n, state | shift, desired_total){
             ans = true;
-            nums[i] = true;
             break;
         }
-        nums[i] = true;
     }
     
-    dp.insert(s, ans);
+    dp.insert(state, ans);
     
     ans
-}
-
-// transfer boolean[] to an Integer 
-fn format(used: &Vec<bool>) -> i32{
-    let mut num = 0;
-    for &b in used {
-        num <<= 1;
-        if b {
-            num |= 1;
-        }
-    }
-    return num;
 }
