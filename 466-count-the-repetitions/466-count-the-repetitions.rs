8fd933145b91d84c82e1c26922de186706a4da2b
子風 <zpsyhapcst@gmail.com>
2022-01-12 09:28:22 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (50.00%) - LeetHub

--- 466-count-the-repetitions/466-count-the-repetitions.rs
+++ 466-count-the-repetitions/466-count-the-repetitions.rs
@@ -1,56 +1,41 @@
+// https://leetcode.com/problems/count-the-repetitions/discuss/95398/C%2B%2B-solution-inspired-by-%4070664914-with-organized-explanation
+// Time: O(|s1| * n1) where |s1| is the length of s1
+// Space: O(n1)
+
+use std::collections::HashMap;
 impl Solution {
     pub fn get_max_repetitions(s1: String, n1: i32, s2: String, n2: i32) -> i32 {
-        let s1 = clear(s1, s2.as_bytes());
-        
-        let s1_len = s1.len();
+        let s1 = s1.as_bytes();
+        let s2 = s2.as_bytes();
         let n1 = n1 as usize;
-        let s2_len = s2.len();
         let n2 = n2 as usize;
         
-        let mut s1_iter = s1.bytes().cycle().take(s1_len * n1);
-        let s2 = s2.as_bytes();
-        let mut i = 0;
+        let mut kToRepeatCount:HashMap<usize,usize> = HashMap::new();
+        let mut nextIndexToK:HashMap<usize,usize> = HashMap::new();
+        kToRepeatCount.insert(0,0);
+        nextIndexToK.insert(0,0);
         let mut j = 0;
-        let mut k = 0;
-        let mut dp = Vec::new();
-        dp.push(0);
-        while i < n1{
-            for c in s1.bytes(){
-                if s2[k] == c {
-                    k += 1;
-                }
-                if k == s2_len {
-                    k = 0;
+        let mut cnt = 0;
+        for k in 1..=n1 {
+            for i in 0..s1.len() {
+                if s1[i] == s2[j] {
                     j += 1;
+                    if j == s2.len() {
+                        j = 0;
+                        cnt += 1;
+                    }
                 }
             }
-            i += 1;
-            dp.push(j);
-            if k == 0 && j % n2 == 0{
-                break;
+            if let Some(start) = nextIndexToK.get(&j) {
+                let prefixCount = *kToRepeatCount.get(start).unwrap();
+                let patternCount = (n1 - start) / (k - start) * (cnt - prefixCount);
+                let key = start + (n1 - start) % (k - start);
+                let suffixCount = *kToRepeatCount.get(&key).unwrap() - prefixCount;
+                return ((prefixCount + patternCount + suffixCount) / n2) as i32;
             }
+            kToRepeatCount.insert(k,cnt);
+            nextIndexToK.insert(j,k);
         }
-        
-        let n = n1 / i;
-        let mut ans = n * (j / n2);
-        let idx = n1 - n * i;
-        // println!("i:{}, j:{}, idx:{} {:?}", i, j, idx, dp);
-        (ans + dp[idx] / n2) as i32
+        return (*kToRepeatCount.get(&n1).unwrap() / n2) as i32;
     }
-}
-
-fn clear(s1:String, s2:&[u8]) -> String{
-    let mut count = vec![0;26];
-    for c in s2{
-        let idx = (c - b'a') as usize;
-        count[idx] += 1;
-    }
-    
-    s1.chars().fold(String::new(), |mut s, c| {
-        if count[(c as u8 - b'a') as usize] > 0{
-            s.push(c);
-        }
-        s
-    })
-    
 }
