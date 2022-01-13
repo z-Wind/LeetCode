7849018fbe9fdc759b34ea590b188ef5514c5396
子風 <zpsyhapcst@gmail.com>
2022-01-13 11:29:36 +0800

    Time: 4 ms (50.00%), Space: 2 MB (100.00%) - LeetHub

--- 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.rs
+++ 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.rs
@@ -1,27 +1,90 @@
+// p: abcbcdcde
+//    *
+// count: 1
+//         o
+// counts: abcde...
+// ===================
+// abcbcdcde
+//  *
+// count: 2
+//          o
+//         oo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//   *
+// count: 3
+//           o
+//          oo
+//         ooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//    *
+// count: 1
+//           o
+//          oo
+//         ooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//     *
+// count: 2
+//           o
+//          oo
+//         ooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//      *
+// count: 3
+//           oo
+//          ooo
+//         oooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//       *
+// count: 1
+//           oo
+//          ooo
+//         oooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//        *
+// count: 2
+//           oo
+//          ooo
+//         oooo
+// counts: abcde...
+// ===================
+// abcbcdcde
+//         *
+// count: 3
+//           ooo
+//          oooo
+//         ooooo
+// counts: abcde...
+// ===================
 impl Solution {
     pub fn find_substring_in_wrapround_string(p: String) -> i32 {
         let mut counts:[i32;26] = [0;26];
         let p = p.as_bytes();
         let n = p.len();
-        let mut i = 0;
-        while i < n {
-            let mut count = 1;
-            let c = (p[i] - b'a') as usize;
-            counts[c] = counts[c].max(count);
-            let mut pre = p[i];
-            let mut end = i;
-            for j in i+1..n{
-                if p[j] == pre + 1 || (pre == b'z' && p[j] == b'a'){
-                    count += 1;
-                    end += 1;
-                    let c = (p[j] - b'a') as usize;
-                    counts[c] = counts[c].max(count);
-                } else {
-                    break;
-                }
-                pre = p[j];
+        
+        let mut c = (p[0] - b'a') as usize;
+        counts[c] = 1;
+        let mut count = 1;
+        for i in 1..n{
+            if p[i] - p[i-1] == 1 || p[i-1] - p[i] == 25 {
+                count += 1;
+            } else {
+                count = 1;
             }
-            i = end + 1;
+            
+            c = (p[i] - b'a') as usize;
+            counts[c] = counts[c].max(count);
         }
         // println!("{:?}", counts);
         counts.iter().sum()
