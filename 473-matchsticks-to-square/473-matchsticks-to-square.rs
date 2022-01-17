a284a50d5d4d85ec11dcdfaf5858a1aac63dcd8a
子風 <zpsyhapcst@gmail.com>
2022-01-17 13:29:38 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (50.00%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.rs
+++ 473-matchsticks-to-square/473-matchsticks-to-square.rs
@@ -1,53 +1,57 @@
+// https://leetcode.com/problems/matchsticks-to-square/discuss/95744/cpp-6ms-solution-with-DFS
+
 impl Solution {
     pub fn makesquare(mut matchsticks: Vec<i32>) -> bool {
-        matchsticks.sort_unstable();
-        let sum: i32 = matchsticks.iter().sum();
-        let target = if sum & 0b00 == 0 {
-            sum >> 2
-        } else {
+        if matchsticks.len() < 4 {
             return false;
-        };
-        
-        explore(target, &matchsticks, Vec::new(), 0, 0, 0)
+        }
+
+        let sum: i32 = matchsticks.iter().sum();
+        // sum % 4
+        if sum & 0b00 != 0 {
+            return false;
+        }
+        let target = sum / 4;
+
+        matchsticks.sort_unstable_by(|a, b| b.cmp(a));
+        if matchsticks[0] > target {
+            return false;
+        }
+
+        let mut sidesLength = vec![0; 4];
+        dfs(&mut sidesLength, &matchsticks, 0, target)
     }
 }
 
-fn explore(
-    target: i32,
-    matchsticks: &Vec<i32>,
-    mut temp: Vec<i32>,
-    i_start: usize,
-    mut cur_sum: i32,
-    level: i32,
-) -> bool {
-    if cur_sum > target {
-        return false;
-    } else if cur_sum == target {
-        temp.extend_from_slice(&matchsticks[i_start..]);
-        if level == 2{
-            return temp.iter().sum::<i32>() == target;
-        } else {
-            return explore(target, &temp, Vec::new(), 0, 0, level + 1);
-        }
+fn dfs(sidesLength: &mut Vec<i32>, matches: &Vec<i32>, index: usize, target: i32) -> bool {
+    if index == matches.len() {
+        return sidesLength[0] == sidesLength[1]
+            && sidesLength[1] == sidesLength[2]
+            && sidesLength[2] == sidesLength[3];
     }
-    
-    // println!("{},{}:{} {:?} {:?}", target, level, cur_sum, &matchsticks[i_start..], temp);
 
-    for i in i_start..matchsticks.len() {
-        if i > i_start && matchsticks[i] == matchsticks[i - 1] {
-        } else {
-            if explore(
-                target,
-                matchsticks,
-                temp.clone(),
-                i + 1,
-                cur_sum + matchsticks[i],
-                level,
-            ) {
-                return true;
-            }
+    for i in 0..4 {
+        if sidesLength[i] + matches[index] > target {
+            continue;
         }
-        temp.push(matchsticks[i]);
+
+        //         lets say sides are currently : [5,7,5,9] , the next element is 3.
+        //         Therefore we will be applying dfs on the following:
+        //         [5+3,7  ,5  ,9  ]
+        //         [5  ,7+3,5  ,9  ]
+        //         [5  ,7  ,5+3,9  ]
+        //         [5  ,7  ,5  ,9+3]
+        //         First and third are basically the same, only in a different order. If in the first case, dfs returned false, the third case will return false too.
+        //         So we don't run dfs in third case.
+        if sidesLength[..i].iter().any(|&x| x == sidesLength[i]) {
+            continue;
+        }
+
+        sidesLength[i] += matches[index];
+        if dfs(sidesLength, matches, index + 1, target) {
+            return true;
+        }
+        sidesLength[i] -= matches[index];
     }
-    false
+    return false;
 }
