f277137cf07fb6dcd73914d5d04cdc0bb5d56161
子風 <zpsyhapcst@gmail.com>
2022-01-17 14:59:42 +0800

    Time: 73 ms (50.00%), Space: 2.1 MB (100.00%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.rs
+++ 474-ones-and-zeroes/474-ones-and-zeroes.rs
@@ -1,68 +1,34 @@
+// https://leetcode.com/problems/ones-and-zeroes/discuss/95814/c%2B%2B-DP-solution-with-comments
+
 impl Solution {
     pub fn find_max_form(strs: Vec<String>, m: i32, n: i32) -> i32 {
-        let mut zeros_ones: Vec<(i32, i32)> = strs
-            .iter()
-            .map(|s| {
-                s.chars().fold((0, 0), |acc, c| {
-                    if c == '0' {
-                        (acc.0 + 1, acc.1)
-                    } else {
-                        (acc.0, acc.1 + 1)
-                    }
-                })
-            })
-            .collect();
-        zeros_ones.sort_unstable();
-        // println!("{:?}", zeros_ones);
-        
-        let mut ans = 0;
-        find_max_form(&mut ans, 0, (0,0), &zeros_ones, 0, m, n);
-        ans
-    }
-}
+        let m = m as usize;
+        let n = n as usize;
 
-fn find_max_form(
-    ans: &mut i32,
-    level: i32,
-    counts: (i32, i32),
-    zeros_ones: &Vec<(i32, i32)>,
-    start: usize,
-    m: i32,
-    n: i32,
-) {
-    if counts.0 > m || counts.1 > n{
-        return;
-    }
-    *ans = (*ans).max(level);
-    
-    let len = zeros_ones.len();
-    if start >= len {
-        return;
-    }
-    
-    // println!("{} {:?} {:?}", level, counts, &zeros_ones[start..]);
+        let mut memo = vec![vec![0; n + 1]; m + 1];
+        for s in strs {
+            // count number of zeroes and ones in current string
+            let (numZeroes, numOnes) = s.chars().fold((0, 0), |acc, c| {
+                if c == '0' {
+                    (acc.0 + 1, acc.1)
+                } else {
+                    (acc.0, acc.1 + 1)
+                }
+            });
+            // println!("{}:({},{})", s, numZeroes, numOnes);
 
-    for i in start..len {
-        if i > start && zeros_ones[i] == zeros_ones[i-1]{
-            continue;
+            // memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
+            // from the first few strings up to the current string s
+            // Catch: have to go from bottom right to top left
+            // Why? If a cell in the memo is updated(because s is selected),
+            // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
+            // If we go from top left to bottom right, we would be using results from this iteration => overcounting
+            for i in (numZeroes..=m).rev() {
+                for j in (numOnes..=n).rev() {
+                    memo[i][j] = memo[i][j].max(memo[i - numZeroes][j - numOnes] + 1);
+                }
+            }
         }
-        let zeros = counts.0 + zeros_ones[i].0;
-        let ones = counts.1 + zeros_ones[i].1;
-        if zeros > m {
-            break;
-        }
-        if ones > n {
-            continue;
-        }
-        
-        find_max_form(
-            ans,
-            level + 1,
-            (zeros, ones),
-            zeros_ones,
-            i + 1,
-            m,
-            n,
-        )
+        return memo[m][n];
     }
 }
