ac4a66e9039a8fed475b47b937905d03a32478d8
子風 <zpsyhapcst@gmail.com>
2022-01-18 15:09:17 +0800

    Time: 8 ms (22.22%), Space: 2.5 MB (88.89%) - LeetHub

--- 475-heaters/475-heaters.rs
+++ 475-heaters/475-heaters.rs
@@ -1,32 +1,19 @@
-impl Solution {
-    pub fn find_radius(mut houses: Vec<i32>, mut heaters: Vec<i32>) -> i32 {
-        let m = houses.len();
-        let n = heaters.len();
+// https://leetcode.com/problems/heaters/discuss/95886/Short-and-Clean-Java-Binary-Search-Solution
 
-        houses.sort_unstable();
+impl Solution {
+    pub fn find_radius(houses: Vec<i32>, mut heaters: Vec<i32>) -> i32 {
+        let n = heaters.len();
         heaters.sort_unstable();
 
-        let mut i = 0;
-        let mut j = 0;
-        let mut radius = 0;
-        let mut h1 = None;
-        let mut h2 = Some(heaters[j]);
-        while i < m {
-            // println!("{}: h1:{:?}, h2:{:?}, radius:{}", houses[i], h1, h2, radius);
-            if h1 <= Some(houses[i]) && Some(houses[i]) <= h2 {
-                let r = match (h1, h2) {
-                    (None, Some(x)) => x - houses[i],
-                    (Some(x1), Some(x2)) => (houses[i] - x1).min(x2 - houses[i]),
-                    x => panic!("{:?}", x),
-                };
-                radius = radius.max(r);
-            } else if Some(houses[i]) > h2 {
-                h1 = h2;
-                j += 1;
-                h2 = Some(if j == n { i32::MAX } else { heaters[j] });
-                continue;
-            }
-            i += 1;
+        let mut radius = 0;        
+        for house in houses {
+            let r = match heaters.binary_search(&house).unwrap_or_else(|x| x){
+                0 => heaters[0] - house,
+                i if i == n => house - heaters[n-1],
+                i => (heaters[i] - house).min(house - heaters[i-1]),
+            };
+            
+            radius = radius.max(r);
         }
 
         radius
