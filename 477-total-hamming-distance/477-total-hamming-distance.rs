f1b3af3d7ea7f8a97ba33db86e3a86051b2d081d
子風 <zpsyhapcst@gmail.com>
2022-01-19 14:16:52 +0800

    Time: 3 ms (85.71%), Space: 2 MB (100.00%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.rs
+++ 477-total-hamming-distance/477-total-hamming-distance.rs
@@ -3,11 +3,9 @@
 impl Solution {
     pub fn total_hamming_distance(nums: Vec<i32>) -> i32 {
         let n = nums.len() as i32;
-        let mut total:i32 = 0;
-        for i in 0..32 {
-            let bitCount = nums.iter().fold(0, |acc, &num| acc + ((num >> i) & 1));
-            total += bitCount * (n - bitCount);
-        }
-        return total;
+        (0..32).fold(0, |total, i| {
+            let bitCount = nums.iter().fold(0, |acc, num| acc + ((num >> i) & 1));
+            total + bitCount * (n - bitCount)
+        })
     }
 }
