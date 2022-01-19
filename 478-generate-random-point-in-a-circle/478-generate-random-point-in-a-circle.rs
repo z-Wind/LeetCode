ab4eb2f8f2cc08f1f091cbd7d7f28d8dcbef32bb
子風 <zpsyhapcst@gmail.com>
2022-01-19 16:25:28 +0800

    Time: 27 ms (33.33%), Space: 9.4 MB (100.00%) - LeetHub

--- 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.rs
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.rs
@@ -1,12 +1,13 @@
 // https://leetcode.com/problems/generate-random-point-in-a-circle/discuss/1113679/Python-Polar-coordinates-explained-with-diagrams-and-math
 // https://leetcode.com/problems/generate-random-point-in-a-circle/discuss/1113679/Python-Polar-coordinates-explained-with-diagrams-and-math/879669
 
-use rand::{thread_rng, Rng};
+use rand::{thread_rng, Rng, rngs::ThreadRng};
 
 struct Solution {
     r: f64,
     x: f64,
     y: f64,
+    rng: ThreadRng, 
 }
 
 /**
@@ -19,22 +20,22 @@ impl Solution {
             r: radius,
             x: x_center,
             y: y_center,
+            rng: rand::thread_rng(),
         }
     }
 
-    fn rand_point(&self) -> Vec<f64> {
-        let mut rng = rand::thread_rng();
+    fn rand_point(&mut self) -> Vec<f64> {
         let pi = std::f64::consts::PI;
-        let theta = 2.0 * pi * rng.gen::<f64>();
+        let theta = 2.0 * pi * self.rng.gen::<f64>();
 
         // Uniformly pick a random area between 0 and maximum possible area
         // area = pi * self.r * self.r
         // R = (rng.gen_range(0.0, area) / pi).sqrt()
         // omit pi
         let area = self.r * self.r;
-        let r = rng.gen_range(0.0, area).sqrt();
+        let R = self.rng.gen_range(0.0, area).sqrt();
 
-        vec![self.x + r * theta.cos(), self.y + r * theta.sin()]
+        vec![self.x + R * theta.cos(), self.y + R * theta.sin()]
     }
 }
 
