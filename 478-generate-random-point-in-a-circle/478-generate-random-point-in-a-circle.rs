8192efef9d99aca1b3412c3f2c0e8e5c8855c2d4
子風 <zpsyhapcst@gmail.com>
2022-01-19 16:21:56 +0800

    Time: 37 ms (33.33%), Space: 9.5 MB (33.33%) - LeetHub

--- 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.rs
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.rs
@@ -1,36 +1,40 @@
+// https://leetcode.com/problems/generate-random-point-in-a-circle/discuss/1113679/Python-Polar-coordinates-explained-with-diagrams-and-math
+// https://leetcode.com/problems/generate-random-point-in-a-circle/discuss/1113679/Python-Polar-coordinates-explained-with-diagrams-and-math/879669
+
 use rand::{thread_rng, Rng};
 
 struct Solution {
-    r: f64, 
-    x: f64, 
-    y: f64
+    r: f64,
+    x: f64,
+    y: f64,
 }
 
-
-/** 
+/**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Solution {
-
     fn new(radius: f64, x_center: f64, y_center: f64) -> Self {
-        Self{
+        Self {
             r: radius,
             x: x_center,
             y: y_center,
-        }   
+        }
     }
-    
+
     fn rand_point(&self) -> Vec<f64> {
         let mut rng = rand::thread_rng();
-        loop {
-            let x = 2.0 * rng.gen::<f64>() - 1.0;
-            let y = 2.0 * rng.gen::<f64>() - 1.0;
-            
-            if x*x + y*y <= 1.0{
-                return vec![self.r * x + self.x, self.r * y + self.y];
-            }
-        }
+        let pi = std::f64::consts::PI;
+        let theta = 2.0 * pi * rng.gen::<f64>();
+
+        // Uniformly pick a random area between 0 and maximum possible area
+        // area = pi * self.r * self.r
+        // R = (rng.gen_range(0.0, area) / pi).sqrt()
+        // omit pi
+        let area = self.r * self.r;
+        let r = rng.gen_range(0.0, area).sqrt();
+
+        vec![self.x + r * theta.cos(), self.y + r * theta.sin()]
     }
 }
 
