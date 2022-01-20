b45117d95e370acc522e7a0e66cc9c7a5d8be427
子風 <zpsyhapcst@gmail.com>
2022-01-20 15:05:44 +0800

    Time: 208 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 479-largest-palindrome-product/479-largest-palindrome-product.rs
+++ 479-largest-palindrome-product/479-largest-palindrome-product.rs
@@ -1,15 +1,50 @@
+// https://leetcode.com/problems/largest-palindrome-product/discuss/96306/Java-solutions-with-two-different-approaches
+
 impl Solution {
     pub fn largest_palindrome(n: i32) -> i32 {
-        match n {
-            1 => 9,
-            2 => 987,
-            3 => 123,
-            4 => 597,
-            5 => 677,
-            6 => 1218,
-            7 => 877,
-            8 => 475,
-            _ => panic!("{}", n),
+        if n == 1{
+            return 9;
         }
+        
+        let n = n as u32;
+        let start = 10_i64.pow(n-1);
+        let end = 10_i64.pow(n);
+        for prefix in (start..end).rev() {
+            let num = prefix * end + reverse(prefix);
+            // println!("{}", num);
+            for x in (start..end).rev() {
+                if num / x > x {
+                    break;
+                } else if num % x == 0 {
+                    println!("{}", num);
+                    return (num % 1337) as i32;
+                }
+            }
+        }
+        
+        panic!("no answer");
     }
+}
+
+// https://github.com/z-Wind/LeetCode/blob/main/reverse-integer/reverse-integer.rs
+fn reverse(x: i64) -> i64 {
+    if -10 < x && x < 10{
+        return x;
+    }
+
+    let mut temp = x;
+    let mut ans = 0;
+    while temp != 0{
+        let pop = temp % 10;
+        temp /= 10;
+
+        if (x >=0 && ans > (i64::MAX - pop)/10) || 
+           (x < 0 && ans < (i64::MIN - pop)/10) {
+            return 0;
+        }
+
+        ans = ans * 10 + pop;
+    }
+
+    ans
 }
