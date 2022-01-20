7e0537b85b93aa6956273275d5f8baf0224ab275
子風 <zpsyhapcst@gmail.com>
2022-01-20 15:14:06 +0800

    Time: 316 ms (100.00%), Space: 2.1 MB (100.00%) - LeetHub

--- 479-largest-palindrome-product/479-largest-palindrome-product.rs
+++ 479-largest-palindrome-product/479-largest-palindrome-product.rs
@@ -2,13 +2,11 @@
 
 impl Solution {
     pub fn largest_palindrome(n: i32) -> i32 {
-        if n == 1{
-            return 9;
-        }
-        
         let n = n as u32;
-        let start = 10_i64.pow(n-1);
+        let start = 10_i64.pow(n - 1);
         let end = 10_i64.pow(n);
+        
+        // 2n palindrome 911 => 911119
         for prefix in (start..end).rev() {
             let num = prefix * end + reverse(prefix);
             // println!("{}", num);
@@ -16,30 +14,43 @@ impl Solution {
                 if num / x > x {
                     break;
                 } else if num % x == 0 {
-                    println!("{}", num);
+                    // println!("{}", num);
                     return (num % 1337) as i32;
                 }
             }
         }
         
+        // 2n-1 palindrome 911 => 91119
+        for prefix in (start..end).rev() {
+            let num = prefix * start + reverse(prefix) % start;
+            println!("{}", num);
+            for x in (start..end).rev() {
+                if num / x > x {
+                    break;
+                } else if num % x == 0 {
+                    // println!("{}", num);
+                    return (num % 1337) as i32;
+                }
+            }
+        }
+
         panic!("no answer");
     }
 }
 
 // https://github.com/z-Wind/LeetCode/blob/main/reverse-integer/reverse-integer.rs
 fn reverse(x: i64) -> i64 {
-    if -10 < x && x < 10{
+    if -10 < x && x < 10 {
         return x;
     }
 
     let mut temp = x;
     let mut ans = 0;
-    while temp != 0{
+    while temp != 0 {
         let pop = temp % 10;
         temp /= 10;
 
-        if (x >=0 && ans > (i64::MAX - pop)/10) || 
-           (x < 0 && ans < (i64::MIN - pop)/10) {
+        if (x >= 0 && ans > (i64::MAX - pop) / 10) || (x < 0 && ans < (i64::MIN - pop) / 10) {
             return 0;
         }
 
