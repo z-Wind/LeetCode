67fb2338f5002077b074fb27033ed14ed655b525
子風 <zpsyhapcst@gmail.com>
2022-01-24 11:31:12 +0800

    Time: 13 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.rs
+++ 480-sliding-window-median/480-sliding-window-median.rs
@@ -1,53 +1,39 @@
-use std::collections::BTreeMap;
+// https://leetcode.com/problems/sliding-window-median/discuss/1341219/Rust-Binary-Search-%2B-Vec-Heap-beats-100-mem-and-speed
 
 impl Solution {
     pub fn median_sliding_window(nums: Vec<i32>, k: i32) -> Vec<f64> {
-        let k = k as usize;
-        let mut map:BTreeMap<i32,usize> = BTreeMap::new();
-        let mut ans = Vec::new();
-        for i in 0..k{
-            *map.entry(nums[i]).or_insert(0) += 1;
-        }
-        ans.push(get_median(&map, k));
-        for i in 1..nums.len()-(k-1){
-            if let Some(x) = map.remove(&nums[i-1]){
-                if x > 1 {
-                    map.insert(nums[i-1], x-1);
-                }
-            }
-            *map.entry(nums[i+k-1]).or_insert(0) += 1;
-            
-            ans.push(get_median(&map, k));
-        }
+        // println!("{:?}", nums);
         
-        ans
+        let k = k as usize;
+        let mut res: Vec<f64> = vec![];
+        let mut window:Vec<i32> = Vec::new();
+        window.extend_from_slice(&nums[..k]);
+        window.sort_unstable();
+        res.push(get_median(&window, k));
+        
+        for i in 1..nums.len()-(k-1) {
+            // println!("{} {:?} {:?}", nums[i-1], window, window.binary_search(&nums[i-1]));
+            // remove previous elem
+            window.remove(window.binary_search(&nums[i-1]).unwrap());
+            
+            // find where to insert
+            let num = nums[i+k-1];
+            let idx = window.binary_search(&num).unwrap_or_else(|x| x);
+            // add new elem
+            window.insert(idx, num);
+            
+            // get median
+            res.push(get_median(&window, k));
+        }
+        res
     }
 }
 
-fn get_median(map: &BTreeMap<i32,usize>, k:usize) -> f64{
-    // println!("{:?}", map);
-    let mut counts = 0;
+fn get_median(window:&Vec<i32>, k:usize) -> f64{
+    let mid = k / 2;
     if k % 2 == 1 {
-        let mid = k/2 + 1;
-        for (&num, count) in map.iter() {
-            counts += count;
-            if counts >= mid{
-                return num as f64;
-            }
-        }
+        window[mid] as f64
     } else {
-        let mid1 = k/2;
-        let mid2 = k/2 + 1;
-        let mut median = None;
-        for (&num, count) in map.iter() {
-            counts += count;
-            if median.is_none() && counts >= mid1 {
-                median = Some(num as f64);
-            }
-            if median.is_some() && counts >= mid2 {
-                return (median.unwrap() + num as f64) / 2.0;
-            }
-        }
+        (window[mid - 1] as f64 + window[mid] as f64) / 2.0
     }
-    panic!("no answer");
 }
