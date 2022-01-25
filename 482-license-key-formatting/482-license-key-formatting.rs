0c1a1fd954f253bec8a28a64b810efd8f25aeedf
子風 <zpsyhapcst@gmail.com>
2022-01-25 09:55:11 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.rs
+++ 482-license-key-formatting/482-license-key-formatting.rs
@@ -1,29 +1,20 @@
 impl Solution {
     pub fn license_key_formatting(s: String, k: i32) -> String {
+        let k = k as usize;
         let mut res = Vec::with_capacity(s.len());
-        let mut count = 0;
         for c in s.chars().rev(){
             match c {
                 '-' => continue,
                 _ => {
-                    res.push(c.to_ascii_uppercase());
-                    count += 1;
-                    if count == k{
+                    if res.len() % (k+1) == k {
                         res.push('-');
-                        count = 0;
                     }
+                    res.push(c.to_ascii_uppercase());
                 }
             }
         }
         // println!("{:?}", res);
-        if res.is_empty(){
-            return String::new();
-        }
         
-        if res[res.len()-1] == '-'{
-            res[..res.len()-1].into_iter().rev().collect()    
-        } else {
-            res.into_iter().rev().collect()
-        }
+        res.into_iter().rev().collect()
     }
 }
