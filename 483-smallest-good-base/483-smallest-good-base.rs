8472ff7c98cd6314b519399a2031915b059e11d3
子風 <zpsyhapcst@gmail.com>
2022-01-26 14:03:29 +0800

    Time: 2 ms (33.33%), Space: 2.8 MB (33.33%) - LeetHub

--- 483-smallest-good-base/483-smallest-good-base.rs
+++ 483-smallest-good-base/483-smallest-good-base.rs
@@ -1,49 +1,17 @@
-// Geometric progression
-//   1 + r + r^2 + ... + r^(N-1)
-// = (r^N - 1)/(r - 1)
-// = num
+// https://leetcode.com/problems/smallest-good-base/discuss/96587/Python-solution-with-detailed-mathematical-explanation-and-derivation
 
 impl Solution {
     pub fn smallest_good_base(n: String) -> String {
-        let num: u128 = n.parse().unwrap();
-        // println!("{}", num);
-        let mut ans = u128::MAX;
-        for i in 1.. {
-            // println!("{}======", i);
-            if sum(2, i) > num {
-                break;
-            }
-            if let Some(x) = binary_search(num, i) {
-                ans = ans.min(x);
+        let n: u128 = n.parse().unwrap();
+        let max_m = (n as f64).log2() as u32; // Refer [7]
+        for m in (2..=max_m).rev() {
+            let k = (n as f64).powf(1.0 / m as f64) as u128; // Refer [6]
+            if (k.pow(m + 1) - 1) / (k - 1) == n {
+                // Refer [3]
+                return k.to_string();
             }
         }
-        return ans.to_string();
-    }
-}
 
-fn binary_search(num: u128, n: u32) -> Option<u128> {
-    let mut left = 2;
-    let mut right = num - 1;
-    while left <= right {
-        // println!("{},{}", left, right);
-        if sum(left, n) == num {
-            return Some(left);
-        } else if sum(right, n) == num {
-            return Some(right);
-        }
-        let mid = left + (right - left) / 2;
-        let res = sum(mid, n);
-        if res == num {
-            return Some(mid);
-        } else if res > num {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
+        return (n - 1).to_string();
     }
-    None
-}
-
-fn sum(r: u128, n: u32) -> u128 {
-    (r.pow(n) - 1) / (r - 1)
 }
