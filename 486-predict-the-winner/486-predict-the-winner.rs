9528cc0ada89334d156ecdbf7057623ef9c112b8
子風 <zpsyhapcst@gmail.com>
2022-01-27 09:37:16 +0800

    Time: 2 ms (71.43%), Space: 1.9 MB (71.43%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.rs
+++ 486-predict-the-winner/486-predict-the-winner.rs
@@ -1,41 +1,21 @@
-use std::collections::VecDeque;
+// https://leetcode.com/problems/predict-the-winner/discuss/96828/JAVA-9-lines-DP-solution-easy-to-understand-with-improvement-to-O(N)-space-complexity.
 
 impl Solution {
     pub fn predict_the_winner(nums: Vec<i32>) -> bool {
-        if nums.len() <= 2 {
-            return true;
+        let n = nums.len();
+        if (n & 1) == 0 { 
+            return true; 
         }
-        let mut nums:VecDeque<i32> = nums.into_iter().collect();
-        predict_the_winner(&mut nums, 0, 0, 1)
+        let mut dp = vec![0;n];
+        for i in (0..n).rev() {
+            for j in i..n {
+                if i == j {
+                    dp[i] = nums[i];
+                } else {
+                    dp[j] = (nums[i] - dp[j]).max(nums[j] - dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1] >= 0;
     }
-}
-
-fn predict_the_winner(nums: &mut VecDeque<i32>, score1: i32, score2: i32, player: i32) -> bool {
-    if nums.is_empty() {
-        return score1 >= score2;
-    }
-    
-    let num = nums.pop_front().unwrap();
-    let res1 = if player == 1 {
-        predict_the_winner(nums, score1 + num, score2, player ^ 0b11)
-    } else {
-        predict_the_winner(nums, score1, score2 + num, player ^ 0b11)
-    };
-    nums.push_front(num);
-    if player == 1 && res1 {
-        return true
-    }
-    
-    let num = nums.pop_back().unwrap();
-    let res2 = if player == 1 {
-        predict_the_winner(nums, score1 + num, score2, player ^ 0b11)
-    } else {
-        predict_the_winner(nums, score1, score2 + num, player ^ 0b11)
-    };
-    nums.push_back(num);
-    if player == 1 && res2 {
-        return true
-    }
-    
-    res1 && res2
 }
