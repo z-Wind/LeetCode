bb9755a44e0848e0cbaffc20ae87d8084151db9a
子風 <zpsyhapcst@gmail.com>
2022-01-27 10:24:57 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (14.29%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.rs
+++ 486-predict-the-winner/486-predict-the-winner.rs
@@ -1,9 +1,27 @@
 // https://leetcode.com/problems/predict-the-winner/discuss/96828/JAVA-9-lines-DP-solution-easy-to-understand-with-improvement-to-O(N)-space-complexity.
 
+// dp[i][j] saves how much more scores that the first-in-action player will get from i to j than the second player
+// dp[i][i] = nums[i]
+// dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+//
+// dp[i][j-1]  dp[i][j]
+//             dp[i+1][j]
+//
+// dp[j-1]     dp[j] (update)
+//             dp[j]
+
 impl Solution {
     pub fn predict_the_winner(nums: Vec<i32>) -> bool {
         let n = nums.len();
-        if (n & 1) == 0 { 
+        
+        // [1,2,3,4,5,6,7,8]
+        //  o x o x o x o x
+        // player 1 could always select the even elements or odd elements
+        // if even elements larger or equal than odd elements
+        // select even elements or select odd elements
+        // so player 1 always wins if the lengh of nums is even
+        //            n % 2 == 0
+        if n == 1 || (n & 1) == 0 { 
             return true; 
         }
         let mut dp = vec![0;n];
