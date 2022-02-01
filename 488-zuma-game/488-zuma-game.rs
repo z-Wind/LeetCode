31c97709803afe1a31f39eedf05bcb8b40708b2c
zwindr <zpsyhapcst@gmail.com>
2022-02-01 22:16:18 +0800

    Time: 160 ms (100.00%), Space: 17.5 MB (100.00%) - LeetHub

--- 488-zuma-game/488-zuma-game.rs
+++ 488-zuma-game/488-zuma-game.rs
@@ -1,102 +1,87 @@
+// https://leetcode.com/problems/zuma-game/discuss/1226417/Two-well-explained-BFS-and-DFS-C%2B%2B-solutions-that-pass-all-35-test-cases-(17th-Nov-21)
+
 use std::collections::HashMap;
 
 impl Solution {
     pub fn find_min_step(board: String, hand: String) -> i32 {
         let board = board.as_bytes();
-        let mut map = HashMap::new();
-        for c in [b'R', b'Y', b'B', b'G', b'W']{
-            map.insert(c, 0);
+        let mut freq = [0; 26];
+        for c in hand.bytes() {
+            let idx = (c - b'A') as usize;
+            freq[idx] += 1;
         }
-        for c in hand.bytes(){
-            *map.get_mut(&c).unwrap() += 1;
+
+        let mut cache = HashMap::new();
+        let ans = find_min_step(board, &mut freq, &mut cache);
+        if ans == i32::MAX {
+            return -1;
         }
-        let mut ans = i32::MAX;
-        // find_min_step(&mut ans, 0, board, &mut map);
-        find_min_step_all(&mut ans, 0, board, &mut map);
-        if ans == i32::MAX{
-            -1
-        } else {
-            ans
-        }
+        ans
     }
 }
 
-fn find_min_step(ans:&mut i32, count:i32, mut board: &[u8], map:&mut HashMap<u8,i32>) {
-    if count >= *ans {
-        return;
-    }
-    
+fn find_min_step(
+    mut board: &[u8],
+    freq: &mut [i32; 26],
+    cache: &mut HashMap<(Vec<u8>, [i32; 26]), i32>,
+) -> i32 {
     // println!("{}", String::from_utf8(board.to_vec()).unwrap());
     let board = clear(board);
-    if board.is_empty(){
-        *ans = (*ans).min(count);
-        return
+    if board.is_empty() {
+        return 0;
     }
-    // println!("=> {}:{:?}", String::from_utf8(board.clone()).unwrap(), map);
-    
-    let n = board.len();
-    let mut prev = 0;
-    for i in 1..=n  {
-        if i < n && board[i] == board[prev] {
-            continue;
-        }
-        
-        let x = map.get(&board[prev]).cloned().unwrap();
-        if x == 0 {
-            prev = i;
-            continue;
-        }
+    // println!("=> {}:{:?}", String::from_utf8(board.clone()).unwrap(), freq);
 
-        map.insert(board[prev], x-1);
-        find_min_step(ans, count+1, &[&board[..i], &[board[prev]], &board[i..]].concat(), map);
-        map.insert(board[prev], x);
-        prev = i;
+    let key = (board.clone(), freq.clone());
+    if let Some(&x) = cache.get(&key) {
+        return x;
     }
-}
 
-fn find_min_step_all(ans:&mut i32, count:i32, mut board: &[u8], map:&mut HashMap<u8,i32>) {
-    if count >= *ans {
-        return;
-    }
-    
-    // println!("{}", String::from_utf8(board.to_vec()).unwrap());
-    let board = clear(board);
-    if board.is_empty(){
-        *ans = (*ans).min(count);
-        return
-    }
-    // println!("=> {}:{:?}", String::from_utf8(board.clone()).unwrap(), map);
-    
-    for i in 0..=board.len() {
-        for (&c,&x) in map.clone().iter(){
-            if x == 0 {
+    let mut ans = i32::MAX;
+    for i in 0..board.len() {
+        for c in [b'R', b'Y', b'B', b'G', b'W'] {
+            let idx = (c - b'A') as usize;
+            if freq[idx] == 0 {
                 continue;
             }
-            map.insert(c, x-1);
-            find_min_step(ans, count+1, &[&board[..i], &[c], &board[i..]].concat(), map);
-            map.insert(c, x);
+
+            // "RRWWRRBBRR"
+            // "WB"
+            if !((board[i] == c) || (board[i] != c && i > 0 && board[i] == board[i - 1])) {
+                continue;
+            }
+
+            freq[idx] -= 1;
+            ans = ans.min(find_min_step(
+                &[&board[..i], &[c], &board[i..]].concat(),
+                freq,
+                cache,
+            ));
+            freq[idx] += 1;
         }
     }
+    if ans != i32::MAX {
+        ans = ans + 1;
+    }
+    cache.insert(key, ans);
+    ans
 }
 
 fn clear(board: &[u8]) -> Vec<u8> {
     let mut s = board.to_vec();
-    'outer: loop {
-        let mut prev = 0;
-        for i in 1..=s.len() {
-            if i < s.len() && s[i] == s[prev] {
-                continue;
-            }
-            
-            // println!("{}", String::from_utf8(s.to_vec()).unwrap());
-            // println!("{},{}", prev, i);
-            if i - prev >= 3 {
-                s = [&s[..prev], &s[i..]].concat();
-                continue 'outer;
-            };
-            prev = i;
+
+    let mut prev = 0;
+    for i in 1..=s.len() {
+        if i < s.len() && s[i] == s[prev] {
+            continue;
         }
-        break;
+
+        if i - prev >= 3 {
+            let s2 = [&board[..prev], &board[i..]].concat();
+            return clear(&s2);
+        };
+        prev = i;
     }
-    s
+
+    board.to_vec()
 }
