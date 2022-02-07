43608813c5e1b5d020e9236bd8f01fee6ae69415
子風 <zpsyhapcst@gmail.com>
2022-02-07 10:43:17 +0800

    Time: 46 ms (42.86%), Space: 5.1 MB (85.71%) - LeetHub

--- 491-increasing-subsequences/491-increasing-subsequences.rs
+++ 491-increasing-subsequences/491-increasing-subsequences.rs
@@ -1,37 +1,30 @@
-use std::collections::HashSet;
 impl Solution {
     pub fn find_subsequences(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut ans = HashSet::new();
+        let mut ans = Vec::new();
         let mut temp = Vec::new();
         find_subsequences(&mut ans, &mut temp, &nums, 0);
 
-        ans.into_iter().collect()
+        ans
     }
 }
 
-fn find_subsequences(ans: &mut HashSet<Vec<i32>>, temp: &mut Vec<i32>, nums: &Vec<i32>, start: usize) {
-    if ans.contains(temp){
-        return;
-    }
-    if temp.len() >= 2 {
-        ans.insert(temp.clone());
-    }
-    if start == nums.len() {
-        return;
-    }
-
-    let x = if let Some(&x) = temp.last() {
-        x
-    } else {
-        i32::MIN
-    };
-    for i in start..nums.len() {
-        if x > nums[i] || (i != start && nums[i - 1] == nums[i]) {
-            continue;
+fn find_subsequences(ans: &mut Vec<Vec<i32>>, temp: &mut Vec<i32>, nums: &Vec<i32>, i: usize) {
+    if i == nums.len() {
+        if temp.len() > 1 {
+            ans.push(temp.clone());
         }
-
+        return;
+    }
+    
+    if temp.is_empty() || nums[i] >= temp.last().cloned().unwrap(){
         temp.push(nums[i]);
         find_subsequences(ans, temp, nums, i + 1);
         temp.pop();
+    } 
+    
+    if temp.len() > 0 && nums[i] == temp.last().cloned().unwrap() {
+        return;
     }
+    
+    find_subsequences(ans, temp, nums, i + 1);
 }
