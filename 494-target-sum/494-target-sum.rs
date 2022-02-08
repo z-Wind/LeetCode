cae2d0a460225bd2213d2ff8ebd331f4a5eb5186
子風 <zpsyhapcst@gmail.com>
2022-02-08 11:04:28 +0800

    Time: 52 ms (76.47%), Space: 2.7 MB (11.76%) - LeetHub

--- 494-target-sum/494-target-sum.rs
+++ 494-target-sum/494-target-sum.rs
@@ -1,27 +1,38 @@
+// https://leetcode.com/problems/target-sum/discuss/455024/DP-IS-EASY!-5-Steps-to-Think-Through-DP-Questions.
+
+use std::collections::HashMap;
+
 impl Solution {
     pub fn find_target_sum_ways(nums: Vec<i32>, target: i32) -> i32 {
-        let mut count = 0;
-        find_target_sum_ways(&nums, target, vec![0], &mut count);
-        count
+        let mut memo = HashMap::new();
+        find_target_sum_ways(&mut memo, &nums, target, nums.len() - 1, 0)
     }
 }
 
-fn find_target_sum_ways(nums: &[i32], target: i32, sums:Vec<i32>, count:&mut i32) {
-    // println!("{:?}: {:?}", nums, sums);
-    if nums.is_empty() {
-        for sum in sums {
-            if sum == target{
-                *count += 1;
-            }
+fn find_target_sum_ways(
+    memo: &mut HashMap<(usize, i32), i32>,
+    nums: &[i32],
+    target: i32,
+    index: usize,
+    curr_sum: i32,
+) -> i32 {
+    let key = (index, curr_sum);
+    if memo.contains_key(&key) {
+        return *memo.get(&key).unwrap();
+    }
+
+    if index >= nums.len() {
+        if curr_sum == target {
+            return 1;
+        } else {
+            return 0;
         }
-        return
     }
-    let mut temp = Vec::with_capacity(sums.len()*2);
-    for sum in sums {
-        temp.push(sum + nums[0]);
-        temp.push(sum - nums[0]);
-    }
-    
-    
-    find_target_sum_ways(&nums[1..], target, temp, count);
+
+    let positive = find_target_sum_ways(memo, nums, target, index - 1, curr_sum + nums[index]);
+    let negative = find_target_sum_ways(memo, nums, target, index - 1, curr_sum - nums[index]);
+
+    let res = positive + negative;
+    memo.insert(key, res);
+    return res;
 }
