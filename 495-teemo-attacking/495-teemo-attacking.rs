7686d2b8e1eae5e83e0a6dc048cf6a0629c9da0b
子風 <zpsyhapcst@gmail.com>
2022-02-08 13:37:12 +0800

    Time: 10 ms (20.00%), Space: 2.5 MB (10.00%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.rs
+++ 495-teemo-attacking/495-teemo-attacking.rs
@@ -1,18 +1,10 @@
 impl Solution {
     pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
-        let mut i = 0;
-        let mut poisoned_time = 0;
-        let mut t = 0;
-        while i < time_series.len() {
-            if t <= time_series[i] {
-                poisoned_time += duration;
-            } else {
-                poisoned_time += (time_series[i] - time_series[i - 1]);
-            }
-            t = time_series[i] + duration;
-            i += 1;
+        let mut poisoned_time = duration; // last
+        for i in 0..time_series.len() - 1 {
+            poisoned_time += duration.min(time_series[i + 1] - time_series[i]);
         }
-        
+
         poisoned_time
     }
 }
