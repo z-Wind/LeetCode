0634b535ec9918abe7277f5a1ae3ddb330523a47
子風 <zpsyhapcst@gmail.com>
2022-02-08 14:11:09 +0800

    Time: 0 ms (100.00%), Space: 2.2 MB (28.57%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.rs
+++ 496-next-greater-element-i/496-next-greater-element-i.rs
@@ -1,25 +1,22 @@
+// https://leetcode.com/problems/next-greater-element-i/discuss/97595/Java-10-lines-linear-time-complexity-O(n)-with-explanation
+
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for i in 0..nums2.len(){
-            map.insert(nums2[i], i);
-        }
-        
-        let mut ans = Vec::new();
-        'outer: for num in nums1 {
-            if let Some(start) = map.get(&num) {
-                for i in start+1..nums2.len() {
-                    if nums2[i] > num {
-                        ans.push(nums2[i]);
-                        continue 'outer;
-                    }
-                }
-                ans.push(-1);
+    pub fn next_greater_element(mut nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new(); // map from x to next greater element of x
+        let mut stack: Vec<i32> = Vec::new();
+        for num in nums2 {
+            while !stack.is_empty() && *stack.last().unwrap() < num {
+                map.insert(stack.pop().unwrap(), num);
             }
+            stack.push(num);
         }
-        
-        ans
+
+        for num in nums1.iter_mut() {
+            *num = map.get(num).cloned().unwrap_or(-1);
+        }
+
+        return nums1;
     }
 }
