b3cb347a4bd7350e10a39de66f4536490d43f028
子風 <zpsyhapcst@gmail.com>
2022-02-09 14:13:18 +0800

    Time: 13 ms (33.33%), Space: 2.5 MB (66.67%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.rs
+++ 498-diagonal-traverse/498-diagonal-traverse.rs
@@ -1,38 +1,33 @@
+// https://leetcode.com/problems/diagonal-traverse/discuss/581868/Easy-Python-NO-DIRECTION-CHECKING
+
 impl Solution {
     pub fn find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
         let m = mat.len();
         let n = mat[0].len();
         let mut ans = Vec::with_capacity(m * n);
 
-        let mut up = true;
         let mut i = 0;
         let mut j = 0;
         for _ in 0..m * n {
             // println!("{},{}", i, j);
             ans.push(mat[i][j]);
 
-            match (up, i, j) {
-                (true, ..) if j == n - 1 => {
+            // (i + j) % 2 == 0
+            if (i + j) & 1 == 0 {
+                if j == n - 1 {
                     i += 1;
-                    up = false;
-                }
-                (true, ..) if i == 0 => {
+                } else if i == 0 {
                     j += 1;
-                    up = false;
-                }
-                (true, ..) => {
+                } else {
                     i -= 1;
                     j += 1;
                 }
-                (false, ..) if i == m - 1 => {
+            } else {
+                if i == m - 1 {
                     j += 1;
-                    up = true;
-                }
-                (false, ..) if j == 0 => {
+                } else if j == 0 {
                     i += 1;
-                    up = true;
-                }
-                (false, ..) => {
+                } else {
                     i += 1;
                     j -= 1;
                 }
