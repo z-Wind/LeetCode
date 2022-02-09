fe71ad6a4be7545e81b64350e6cfc436ff4d777b
子風 <zpsyhapcst@gmail.com>
2022-02-09 14:39:11 +0800

    Time: 1 ms (35.71%), Space: 2 MB (71.43%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.rs
+++ 500-keyboard-row/500-keyboard-row.rs
@@ -1,37 +1,18 @@
 use std::collections::HashSet;
 impl Solution {
     pub fn find_words(words: Vec<String>) -> Vec<String> {
-        let mut set1: HashSet<char> = "qwertyuiop".chars().collect();
-        let mut set2: HashSet<char> = "asdfghjkl".chars().collect();
-        let mut set3: HashSet<char> = "zxcvbnm".chars().collect();
+        let rows:Vec<HashSet<char>> = vec![
+            "qwertyuiop".chars().collect(),
+            "asdfghjkl".chars().collect(),
+            "zxcvbnm".chars().collect(),
+        ];
 
-        let mut ans = Vec::new();
-        'outer: for word in words {
-            let c = word.chars().next().unwrap().to_ascii_lowercase();
-            if set1.contains(&c) {
-                for c in word.chars() {
-                    if !set1.contains(&c.to_ascii_lowercase()) {
-                        continue 'outer;
-                    }
-                }
-            } else if set2.contains(&c) {
-                for c in word.chars() {
-                    if !set2.contains(&c.to_ascii_lowercase()) {
-                        continue 'outer;
-                    }
-                }
-            } else if set3.contains(&c) {
-                for c in word.chars() {
-                    if !set3.contains(&c.to_ascii_lowercase()) {
-                        continue 'outer;
-                    }
-                }
-            } else {
-                continue;
-            }
-
-            ans.push(word);
-        }
-        ans
+        words
+            .into_iter()
+            .filter(|word| {
+                rows.iter()
+                    .any(|set| word.to_ascii_lowercase().chars().all(|c| set.contains(&c)))
+            })
+            .collect()
     }
 }
