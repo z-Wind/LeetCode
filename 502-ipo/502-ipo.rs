071769de98e9c1738d11db57490abeb0505a3c95
子風 <zpsyhapcst@gmail.com>
2022-02-11 14:21:01 +0800

    Time: 63 ms (50.00%), Space: 4.7 MB (50.00%) - LeetHub

---
+++ 502-ipo/502-ipo.rs
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/ipo/discuss/98210/Very-Simple-(Greedy)-Java-Solution-using-two-PriorityQueues
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn find_maximized_capital(k: i32, mut w: i32, profits: Vec<i32>, capital: Vec<i32>) -> i32 {
+        let mut pqCap: BinaryHeap<Reverse<(i32, i32)>> = capital
+            .into_iter()
+            .zip(profits.into_iter())
+            .map(|ele| Reverse(ele))
+            .collect();
+        let mut pqPro: BinaryHeap<(i32, i32)> = BinaryHeap::new();
+
+        for _ in 0..k {
+            while let Some(Reverse((cap, pro))) = pqCap.peek() {
+                if *cap > w {
+                    break;
+                }
+                pqPro.push((*pro, *cap));
+                pqCap.pop();
+            }
+
+            match pqPro.pop() {
+                None => break,
+                Some((pro, _)) => w += pro,
+            }
+        }
+
+        w
+    }
+}
