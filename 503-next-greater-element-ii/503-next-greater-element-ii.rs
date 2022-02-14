d77e0241e78f1dbb935e522961db737c3af51446
子風 <zpsyhapcst@gmail.com>
2022-02-14 10:14:55 +0800

    Time: 8 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.rs
+++ 503-next-greater-element-ii/503-next-greater-element-ii.rs
@@ -1,28 +1,20 @@
 // https://github.com/z-Wind/LeetCode/blob/main/496-next-greater-element-i/496-next-greater-element-i.rs
 
-use std::collections::HashMap;
-use std::collections::VecDeque;
 impl Solution {
     pub fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
         let n = nums.len();
-        let mut map = HashMap::new();
+        let mut ans = vec![-1; n];
         let mut stack = Vec::new();
-        for &num in nums.iter().cycle().take(n * 2) {
-            while !stack.is_empty() && *stack.last().unwrap() < num {
-                map.entry(stack.pop().unwrap()).or_insert(VecDeque::new()).push_back(num);
+        for i in (0..n).rev().cycle().take(n * 2) {
+            while !stack.is_empty() && *stack.last().unwrap() <= nums[i] {
+                stack.pop();
             }
-            stack.push(num);
-        }
-        // println!("{:?}", map);
-        
-        
-        let mut ans = Vec::with_capacity(n);
-        for num in nums.iter() {
-            match map.get_mut(num) {
-                None => ans.push(-1),
-                Some(v) => ans.push(v.pop_front().unwrap()),
+            if let Some(&x) = stack.last() {
+                ans[i] = x;
             }
+            stack.push(nums[i]);
         }
+
         ans
     }
 }
