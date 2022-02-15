00a5d4b0a99e3c8254b4dc7ff84d4195b85a738d
子風 <zpsyhapcst@gmail.com>
2022-02-15 12:54:42 +0800

    Time: 3 ms (12.50%), Space: 2 MB (62.50%) - LeetHub

--- 504-base-7/504-base-7.rs
+++ 504-base-7/504-base-7.rs
@@ -1,20 +1,16 @@
 impl Solution {
-    pub fn convert_to_base7(mut num: i32) -> String {
+    pub fn convert_to_base7(num: i32) -> String {
         if num == 0 {
             return String::from("0");
         }
+        
         let mut s = String::new();
-        let is_neg = num.is_negative();
-        let mut num = if is_neg {
-            -num as u32
-        } else {
-            num as u32
-        };
-        while num != 0 {
-            s.push(char::from_digit(num % 7, 10).unwrap());
-            num /= 7;
+        let mut n = num.abs();
+        while n != 0 {
+            s.push_str(&(n % 7).to_string());
+            n /= 7;
         }
-        if is_neg {
+        if num.is_negative() {
             s.push('-');
         }
         
