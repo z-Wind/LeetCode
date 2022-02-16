9c025135c4fd0e37eb4f82ac4b1b3e94472e2b6b
子風 <zpsyhapcst@gmail.com>
2022-02-16 09:40:01 +0800

    Time: 9 ms (60.00%), Space: 2.4 MB (80.00%) - LeetHub

--- 506-relative-ranks/506-relative-ranks.rs
+++ 506-relative-ranks/506-relative-ranks.rs
@@ -1,21 +1,22 @@
-use std::collections::BTreeMap;
+// https://leetcode.com/problems/relative-ranks/discuss/98468/Easy-Java-Solution-Sorting.
 
 impl Solution {
     pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
-        let mut map = BTreeMap::new();
-        for &n in score.iter() {
-            map.insert(n, String::from(""));
-        }
-        
-        for (i, value) in map.values_mut().rev().enumerate() {
-            *value = match i+1 {
+        let n = score.len();
+        let mut index: Vec<usize> = (0..n).collect();
+        index.sort_unstable_by_key(|&i| -score[i]);
+
+        let mut result = vec![String::new(); n];
+
+        for i in 0..n {
+            result[index[i]] = match i + 1 {
                 1 => String::from("Gold Medal"),
                 2 => String::from("Silver Medal"),
                 3 => String::from("Bronze Medal"),
-                x => x.to_string()
+                x => x.to_string(),
             }
         }
-        
-        score.iter().map(|x| map.get(x).unwrap().clone()).collect()
+
+        return result;
     }
 }
