f64eb1dd76dd0bcfb5bf4603728f24b907b92220
子風 <zpsyhapcst@gmail.com>
2022-02-17 08:41:14 +0800

    Time: 3 ms (20.00%), Space: 2.1 MB (80.00%) - LeetHub

--- 507-perfect-number/507-perfect-number.rs
+++ 507-perfect-number/507-perfect-number.rs
@@ -1,19 +1,18 @@
+// https://leetcode.com/problems/perfect-number/discuss/98594/Simple-Java-Solution/102848
+
 impl Solution {
     pub fn check_perfect_number(num: i32) -> bool {
         if num == 1 {
             return false;
         }
         
-        let max = (num as f64).sqrt() as i32;
-        let mut div = 2;
         let mut sum = 1;
-        while div <= max {
+        let max = (num as f64).sqrt() as i32;
+        for div in 2..=max {
             if num % div == 0 {
                 sum += div;
                 sum += num / div;
             }
-
-            div += 1;
         }
 
         sum == num
