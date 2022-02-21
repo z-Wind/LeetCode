252d82e1bc0640723719e8fd7fcdd42ac455f445
子風 <zpsyhapcst@gmail.com>
2022-02-21 13:30:58 +0800

    Time: 13 ms (75.00%), Space: 2.2 MB (50.00%) - LeetHub

---
+++ 514-freedom-trail/514-freedom-trail.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_rotate_steps(ring: String, key: String) -> i32 {
+        let mut ring_vec = vec![Vec::new(); 26];
+        for (i, c) in ring.bytes().enumerate() {
+            let idx = (c - b'a') as usize;
+            ring_vec[idx].push(i);
+        }
+        let key: Vec<u8> = key.bytes().collect();
+
+        let mut map = HashMap::new();
+        find_rotate_steps(&ring_vec, ring.len(), &key, 0, 0, &mut map)
+    }
+}
+
+fn find_rotate_steps(
+    ring: &Vec<Vec<usize>>,
+    n: usize,
+    key: &Vec<u8>,
+    ring_idx: usize,
+    key_idx: usize,
+    map: &mut HashMap<(usize, usize), i32>,
+) -> i32 {
+    if key_idx == key.len() {
+        return 0;
+    }
+    if let Some(step) = map.get(&(ring_idx, key_idx)) {
+        return *step;
+    }
+
+    let c = (key[key_idx] - b'a') as usize;
+    let step = ring[c]
+        .iter()
+        .map(|&next_ring_idx| {
+            let r =
+                ((next_ring_idx + n - ring_idx) % n).min((ring_idx + n - next_ring_idx) % n) as i32;
+            1 + r + find_rotate_steps(ring, n, key, next_ring_idx, key_idx + 1, map)
+        })
+        .min()
+        .unwrap();
+    // println!("{},{} => {}", ring_idx, key_idx, step);
+    map.insert((ring_idx, key_idx), step);
+    step
+}
