d820489d0aac4987bf6fb1087a49aef8f468f3fb
子風 <zpsyhapcst@gmail.com>
2022-02-23 14:03:45 +0800

    Time: 51 ms (50.00%), Space: 6 MB (66.67%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.rs
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.rs
@@ -1,28 +1,21 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn longest_palindrome_subseq(s: String) -> i32 {
+        let n = s.len();
         let s: Vec<char> = s.chars().collect();
-        let mut map = HashMap::new();
-        compare_subseq(&mut map, &s, 0, s.len() - 1)
+        let mut map = vec![vec![0; n]; n];
+        compare_subseq(&mut map, &s, 0, n - 1)
     }
 }
 
-fn compare_subseq(
-    map: &mut HashMap<(usize, usize), i32>,
-    s: &Vec<char>,
-    i: usize,
-    j: usize,
-) -> i32 {
+fn compare_subseq(map: &mut Vec<Vec<i32>>, s: &Vec<char>, i: usize, j: usize) -> i32 {
     if i == j {
         return 1;
     } else if i > j {
         return 0;
     }
 
-    let key = (i, j);
-    if map.contains_key(&key) {
-        return *map.get(&key).unwrap();
+    if map[i][j] != 0 {
+        return map[i][j];
     }
 
     let ans = if s[i] == s[j] {
@@ -30,6 +23,7 @@ fn compare_subseq(
     } else {
         compare_subseq(map, s, i, j - 1).max(compare_subseq(map, s, i + 1, j))
     };
-    map.insert(key, ans);
+    map[i][j] = ans;
     ans
 }
+
