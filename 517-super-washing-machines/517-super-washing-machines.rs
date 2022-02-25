cf8b88262ee18b4cd6c4191af0f5a0dd43677459
子風 <zpsyhapcst@gmail.com>
2022-02-25 13:35:55 +0800

    Time: 2 ms (100.00%), Space: 2.3 MB (100.00%) - LeetHub

--- 517-super-washing-machines/517-super-washing-machines.rs
+++ 517-super-washing-machines/517-super-washing-machines.rs
@@ -1,39 +1,39 @@
 // https://leetcode.com/problems/super-washing-machines/discuss/99177/Very-intuitive-O(n)-solution
+// https://leetcode.com/problems/super-washing-machines/discuss/99181/C++-16ms-O(n)-solution-(with-trivial-proof)/103242
 
 impl Solution {
     pub fn find_min_moves(machines: Vec<i32>) -> i32 {
         let n = machines.len();
-        let sum: usize = machines.iter().sum::<i32>() as usize;
-        if sum % n != 0 {
+        let sum: i32 = machines.iter().sum();
+        if sum % n as i32 != 0 {
             return -1;
         }
 
-        let avg = sum / n;
+        let avg = sum / n as i32;
+        
         let mut leftSums = vec![0; n];
-        let mut rightSums = vec![0; n];
         for i in 1..n {
-            leftSums[i] = leftSums[i - 1] + machines[i - 1] as usize;
-        }
-        for i in (0..n - 1).rev() {
-            rightSums[i] = rightSums[i + 1] + machines[i + 1] as usize;
+            leftSums[i] = leftSums[i - 1] + machines[i - 1];
         }
         // println!("{:?}", leftSums);
+        
+        let mut rightSums = vec![0; n];
+        for i in (0..n - 1).rev() {
+            rightSums[i] = rightSums[i + 1] + machines[i + 1];
+        }
         // println!("{:?}", rightSums);
         
         let mut mov = 0;
         for i in 0..n {
-            let expLeft = i * avg;
-            let expRight = (n - i - 1) * avg;
-            let mut left = 0;
-            let mut right = 0;
-            if expLeft > leftSums[i] {
-                left = expLeft - leftSums[i];
-            } 
-            if expRight > rightSums[i] {
-                right = expRight - rightSums[i];
-            }
+            let expLeft = i as i32 * avg;
+            let left = 0.max(expLeft - leftSums[i]);
+            
+            let expRight = (n - i - 1) as i32 * avg;
+            let right = 0.max(expRight - rightSums[i]);
+            
             mov = mov.max(left + right);
         }
-        mov as i32
+        
+        mov
     }
 }
