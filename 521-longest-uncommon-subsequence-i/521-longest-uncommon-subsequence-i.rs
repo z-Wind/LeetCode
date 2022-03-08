06e23a45ad3d62dd3c0caad8bd9ab15eec7f3d0c
子風 <zpsyhapcst@gmail.com>
2022-03-08 13:24:07 +0800

    Time: 2 ms (18.18%), Space: 2.2 MB (9.09%) - LeetHub

--- 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.rs
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.rs
@@ -1,13 +1,9 @@
 impl Solution {
     pub fn find_lu_slength(a: String, b: String) -> i32 {
-        if a.len() != b.len() {
-            return a.len().max(b.len()) as i32;
+        if a == b {
+            return -1
         }
         
-        if a == b {
-            return -1;
-        } else {
-            return a.len() as i32;
-        }
+        a.len().max(b.len()) as i32
     }
 }
