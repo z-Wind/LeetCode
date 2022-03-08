4f09cb12c13874d892dfa2f46d491f4b350c9337
子風 <zpsyhapcst@gmail.com>
2022-03-08 10:55:11 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (20.45%) - LeetHub

--- 520-detect-capital/520-detect-capital.rs
+++ 520-detect-capital/520-detect-capital.rs
@@ -1,17 +1,15 @@
 impl Solution {
     pub fn detect_capital_use(word: String) -> bool {
-        let (lower, upper) = word.chars().fold((0, 0), |mut count, c| {
-            if c.is_ascii_lowercase() {
-                count.0 += 1;
+        let upper = word.chars().fold(0, |count, c| {
+            if c.is_ascii_uppercase() {
+                count + 1
             } else {
-                count.1 += 1;
+                count
             }
-
-            count
         });
 
         upper == word.len()
-            || lower == word.len()
+            || upper == 0
             || (upper == 1 && word.chars().next().unwrap().is_ascii_uppercase())
     }
 }
