53f5af9e84ea6c2642f83207991ace25cd686a7c
子風 <zpsyhapcst@gmail.com>
2022-03-08 10:40:17 +0800

    Time: 0 ms (100.00%), Space: 3 MB (100.00%) - LeetHub

--- 519-random-flip-matrix/519-random-flip-matrix.rs
+++ 519-random-flip-matrix/519-random-flip-matrix.rs
@@ -1,10 +1,60 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+// https://leetcode.com/problems/random-flip-matrix/discuss/155341/Intuitive-Algorithm-Explanation-Step-By-Step.
+// https://leetcode.com/problems/random-flip-matrix/discuss/155341/Intuitive-Algorithm-Explanation-Step-By-Step./558400
+
+// For example, row = 2, cols = 3, total = 6
+//
+// random select from 0 1 2 3 4 5
+// r = 5
+// total: 5,
+// x = 5
+// map[5] = 5
+// This case is simple. Next time the number will be generated within 0-4, so 5 will never appear again.
+// The values {5}
+//
+// random select from 0 1 2 3 4
+// r = 2
+// total: 4
+// x = 2
+// map[5] = 5, map[2] = 4
+// The values {5, 2}
+//
+// random select from 0 1 2 3
+// r = 2, so 2 appears again,
+// total: 3
+// as map[2] = 4, so x = 4
+// map[5] = 5, map[2] = 3(update)
+// The values {5, 2, 4}
+//
+// random select from 0 1 2
+// r = 1
+// total: 2
+// x = 1
+// map[5] = 5, map[2] = 3, map[1] = 3(2 -> map[2])
+// The values {5, 2, 4, 1}
+//
+// random select from 0 1
+// r = 1
+// total: 1
+// as map[1] = 3, so x = 3
+// map[5] = 5, map[2] = 3, map[1] = 3(1 -> map[1])
+// The values {5, 2, 4, 1, 3}
+//
+// random select from 0
+// r = 0
+// total: 0
+// x = 0
+// map[5] = 5, map[2] = 3, map[1] = 3, map[0] = 0
+// The values {5, 2, 4, 1, 3, 0}
+
+use rand::{rngs::ThreadRng, thread_rng, Rng};
+use std::collections::HashMap;
 
 struct Solution {
-    m: i32,
-    n: i32,
-    ij: Vec<Vec<i32>>,
+    map: HashMap<i32, i32>,
+    rows: i32,
+    cols: i32,
+    total: i32,
+    rng: ThreadRng,
 }
 
 /**
@@ -12,41 +62,31 @@ struct Solution {
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Solution {
-    fn init(m: i32, n: i32) -> Vec<Vec<i32>> {
-        let mut rng = thread_rng();
-        let mut i:Vec<i32> = (0..m).collect();
-        i.shuffle(&mut rng);
-        let mut j:Vec<i32> = (0..n).collect();
-        j.shuffle(&mut rng);
-        
-        let mut ij = Vec::with_capacity(1000);
-        for x in i {
-            for &y in j.iter() {
-                ij.push(vec![x, y]);
-                if ij.len() > 1000 {
-                    return ij;
-                }
-            }
-        }
-        return ij;
-    }
-    
     fn new(m: i32, n: i32) -> Self {
-        let ij = Self::init(m, n);
-        // println!("{:?}", ij);
         Self {
-            m,
-            n,
-            ij,
+            map: HashMap::new(),
+            rows: m,
+            cols: n,
+            total: m * n,
+            rng: rand::thread_rng(),
         }
     }
 
     fn flip(&mut self) -> Vec<i32> {
-        self.ij.pop().unwrap()
+        // generate index, decrease total number of values
+        let r = self.rng.gen_range(0, self.total);
+        self.total -= 1;
+        // check if we have already put something at this index
+        let x = self.map.get(&r).cloned().unwrap_or(r);
+        // swap - put total at index that we generated
+        self.map.insert(r, self.map.get(&self.total).cloned().unwrap_or(self.total));
+
+        return vec![x / self.cols, x % self.cols];
     }
 
     fn reset(&mut self) {
-        self.ij = Solution::init(self.m, self.n);
+        self.map.clear();
+        self.total = self.rows * self.cols;
     }
 }
 
