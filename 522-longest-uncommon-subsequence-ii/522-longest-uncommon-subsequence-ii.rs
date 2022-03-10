c401cbfdbb3aac3045ad6934f2969f22d5af453f
子風 <zpsyhapcst@gmail.com>
2022-03-10 16:38:20 +0800

    Time: 0 ms (100.00%), Space: 2.1 MB (100.00%) - LeetHub

--- 522-longest-uncommon-subsequence-ii/522-longest-uncommon-subsequence-ii.rs
+++ 522-longest-uncommon-subsequence-ii/522-longest-uncommon-subsequence-ii.rs
@@ -4,26 +4,31 @@ impl Solution {
     pub fn find_lu_slength(mut strs: Vec<String>) -> i32 {
         strs.sort_unstable_by_key(|s| -(s.len() as i32));
         // println!("{:?}", strs);
-        for (i, word1) in strs.iter().enumerate() {
-            if strs
-                .iter()
-                .enumerate()
-                .all(|(j, word2)| i == j || !is_subseq(word1, word2))
-            {
-                return word1.len() as i32;
-            }
-        }
-        return -1;
+
+        strs.iter()
+            .enumerate()
+            .find(|(i, word1)| {
+                strs.iter()
+                    .enumerate()
+                    .all(|(j, word2)| *i == j || !is_subseq(word1, word2))
+            })
+            .map(|(_, s)| s.len() as i32)
+            .unwrap_or(-1)
     }
 }
 
 fn is_subseq(w1: &str, w2: &str) -> bool {
     // True iff word1 is a subsequence of word2.
-    let mut i = 0;
-    for c in w2.chars() {
-        if i < w1.len() && w1[i..i + 1] == c.to_string() {
-            i += 1
+    let mut w1 = w1.chars();
+    let mut w2 = w2.chars();
+
+    'outer: while let Some(c1) = w1.next() {
+        while let Some(c2) = w2.next() {
+            if c1 == c2 {
+                continue 'outer;
+            }
         }
+        return false;
     }
-    i == w1.len()
+    true
 }
