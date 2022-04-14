5740e657bfcb53076dfac05b3e260cda7f8112cf
子風 <zpsyhapcst@gmail.com>
2022-04-14 14:08:23 +0800

    Time: 30 ms (76.47%), Space: 2.8 MB (88.24%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.rs
+++ 525-contiguous-array/525-contiguous-array.rs
@@ -1,28 +1,29 @@
+// https://leetcode.com/problems/contiguous-array/discuss/1743637/C%2B%2B-with-Explanation-oror-Easy-to-Understand-oror-Unordered_Map
+
+use std::collections::HashMap;
+
 impl Solution {
     pub fn find_max_length(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let mut counts = vec![(0,0);n+1];
-        
-        let mut ans = 0;
-        for i in 1..=n {
-            counts[i] = counts[i-1];
-            match nums[i-1] {
-                0 => counts[i].0 += 1,
-                1 => counts[i].1 += 1,
-                _ => (),
+        let mut mp: HashMap<i32, i32> = HashMap::new();
+        mp.insert(0, -1);
+
+        let mut sum = 0;
+        let mut longest_subarray = 0;
+        for i in 0..nums.len() as i32 {
+            match nums[i as usize] {
+                0 => sum -= 1,
+                1 => sum += 1,
+                x => panic!("{} is not zero or one", x),
             }
-            // println!("{}:{:?}", i, counts[i]);
-            
-            let max_w = 2 * counts[i].0.min(counts[i].1);
-            let mut j = i - ans - 2;
-            while i - j <= max_w {
-                if counts[i].0 - counts[j].0 == counts[i].1 - counts[j].1 {
-                    ans = i - j;
+            if let Some(idx) = mp.get(&sum) {
+                if longest_subarray < i - idx {
+                    longest_subarray = i - idx;
                 }
-                j -= 2;
+            } else {
+                mp.insert(sum, i);
             }
         }
         
-        ans as i32
+        longest_subarray as i32
     }
 }
