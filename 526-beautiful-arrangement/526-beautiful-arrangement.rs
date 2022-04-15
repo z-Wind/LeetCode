5225b3748b311fc8af6a7335e8aa8c073bef689e
子風 <zpsyhapcst@gmail.com>
2022-04-15 09:56:34 +0800

    Time: 59 ms (77.78%), Space: 2.1 MB (77.78%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.rs
+++ 526-beautiful-arrangement/526-beautiful-arrangement.rs
@@ -1,22 +1,24 @@
 impl Solution {
     pub fn count_arrangement(n: i32) -> i32 {
-        match n {
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            4 => 8,
-            5 => 10,
-            6 => 36,
-            7 => 41,
-            8 => 132,
-            9 => 250,
-            10 => 700,
-            11 => 750,
-            12 => 4010,
-            13 => 4237,
-            14 => 10680,
-            15 => 24679,
-            _ => panic!(),
+        let mut nums = (1..=n).collect();
+        count_arrangement(nums, 1)
+    }
+}
+
+fn count_arrangement(mut nums: Vec<i32>, idx: i32) -> i32 {
+    if nums.is_empty() {
+        return 1;
+    }
+    
+    // println!("{}:{:?}", idx, nums);
+    let mut counts = 0;
+    for i in (0..nums.len()).rev() {
+        if nums[i] % idx == 0 || idx % nums[i] == 0 {
+            let num = nums.swap_remove(i);
+            counts += count_arrangement(nums.clone(), idx+1);
+            nums.push(num);
         }
     }
+    // println!("{}:{:?} => {}", idx, nums, counts);
+    counts
 }
