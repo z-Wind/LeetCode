f36e80238acb42f235c703898bfe638268465896
子風 <zpsyhapcst@gmail.com>
2022-04-15 10:32:16 +0800

    Time: 70 ms (9.09%), Space: 4.5 MB (27.27%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.rs
+++ 528-random-pick-with-weight/528-random-pick-with-weight.rs
@@ -1,29 +1,25 @@
-use rand::{thread_rng, Rng, rngs::ThreadRng};
+use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 struct Solution {
     sums: Vec<i32>,
 }
 
-
-/** 
+/**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
 impl Solution {
-
     fn new(w: Vec<i32>) -> Self {
-        let mut sums = vec![0; w.len()+1];
+        let mut sums = vec![0; w.len() + 1];
         for (i, num) in w.iter().enumerate() {
-            sums[i+1] = sums[i] + num;
+            sums[i + 1] = sums[i] + num;
         }
-        Self {
-            sums,
-        }
+        Self { sums }
     }
-    
+
     fn pick_index(&self) -> i32 {
         let mut rng = rand::thread_rng();
-        let stop = rng.gen_range(0, self.sums.last().unwrap());
+        let stop = rng.gen_range(0, self.sums[self.sums.len() - 1]);
         for (i, bound) in self.sums.windows(2).enumerate() {
             // [0, sum1), [sum1, sum2), .... [sum_n, sum_total)
             if bound[0] <= stop && stop < bound[1] {
@@ -33,6 +29,7 @@ impl Solution {
         unreachable!("The loop should always return");
     }
 }
+
 
 /**
  * Your Solution object will be instantiated and called as such:
