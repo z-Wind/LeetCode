bfb36f6324929db06f1ad73803d5c2f24b4d6f00
子風 <zpsyhapcst@gmail.com>
2022-08-01 15:18:52 +0800

    Time: 13 ms (11.11%), Space: 2.4 MB (74.07%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.rs
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.rs
@@ -1,25 +1,26 @@
+use std::collections::HashSet;
+
 impl Solution {
-    pub fn find_pairs(mut nums: Vec<i32>, k: i32) -> i32 {
-        nums.sort_unstable();
+    pub fn find_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let mut set = HashSet::new();
+        let mut record = HashSet::new();
 
         let mut count = 0;
-        let mut cur_num = None;
-        for i in 0..nums.len() {
-            if let Some(x) = cur_num {
-                if x == nums[i] {
-                    continue;
-                }
-            }
-            for j in i + 1..nums.len() {
-                let diff = nums[j] - nums[i];
-                if diff == k {
+        for num in nums {
+            if let Some(x) = num.checked_add(k) {
+                if set.contains(&x) && !record.contains(&(num, x)){
+                    record.insert((num, x));
                     count += 1;
-                    break;
-                } else if diff > k {
-                    break;
                 }
             }
-            cur_num = Some(nums[i]);
+            if let Some(x) = num.checked_sub(k) {
+                if set.contains(&x) && !record.contains(&(x, num)){
+                    record.insert((x, num));
+                    count += 1;
+                }
+            }
+            
+            set.insert(num);
         }
 
         return count;
