59a41bf8cd5a34f7aebf0c6a8c9ee26345c992dc
子風 <zpsyhapcst@gmail.com>
2022-08-01 15:30:46 +0800

    Time: 9 ms (11.11%), Space: 2.5 MB (37.04%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.rs
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.rs
@@ -7,19 +7,18 @@ impl Solution {
 
         let mut count = 0;
         for num in nums {
-            if let Some(x) = num.checked_add(k) {
-                if set.contains(&x) && !record.contains(&(num, x)){
-                    record.insert((num, x));
-                    count += 1;
-                }
-            }
-            if let Some(x) = num.checked_sub(k) {
-                if set.contains(&x) && !record.contains(&(x, num)){
-                    record.insert((x, num));
-                    count += 1;
-                }
+            let x = num + k;
+            if set.contains(&x) && !record.contains(&(num, x)){
+                record.insert((num, x));
+                count += 1;
             }
             
+            let x = num - k;
+            if set.contains(&x) && !record.contains(&(x, num)){
+                record.insert((x, num));
+                count += 1;
+            }
+
             set.insert(num);
         }
 
