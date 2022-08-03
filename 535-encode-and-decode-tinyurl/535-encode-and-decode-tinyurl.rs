46a59c9abddd1eeb8634c16a7ab53cb93bcc4afe
子風 <zpsyhapcst@gmail.com>
2022-08-03 13:58:20 +0800

    Time: 0 ms (100.00%), Space: 2.3 MB (80.00%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.rs
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.rs
@@ -1,5 +1,10 @@
+use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+#[derive(Default)]
 struct Codec {
-	
+	map: HashMap<u64, String>,
 }
 
 /** 
@@ -8,18 +13,36 @@ struct Codec {
  */
 impl Codec {
     fn new() -> Self {
-        Self{}
+        Default::default()
     }
 	
     // Encodes a URL to a shortened URL.
-    fn encode(&self, longURL: String) -> String {
-        longURL
+    fn encode(&mut self, longURL: String) -> String {
+        let hex_digest = calculate_hash(&longURL);
+        self.map.insert(hex_digest, longURL);
+        let url = format!("http://tinyurl.com/{:x}", hex_digest);
+        println!("{}", url);
+        url
     }
 	
     // Decodes a shortened URL to its original URL.
     fn decode(&self, shortURL: String) -> String {
-        shortURL
+        let hex_digest = shortURL.replace("http://tinyurl.com/", "");
+        let hex_digest = match u64::from_str_radix(&hex_digest, 16) {
+            Ok(num) => num,
+            Err(e) => return String::new(),
+        };
+        match self.map.get(&hex_digest) {
+            None => String::new(),
+            Some(url) => url.to_string(),
+        }
     }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
 
 /**
