5cdb11930fddae46da043f462cac1b252737e81b
子風 <zpsyhapcst@gmail.com>
2022-08-04 13:46:53 +0800

    Time: 9 ms (28.57%), Space: 3.2 MB (71.43%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.rs
+++ 539-minimum-time-difference/539-minimum-time-difference.rs
@@ -1,37 +1,19 @@
-// https://leetcode.com/problems/minimum-time-difference/discuss/100640/Verbose-Java-Solution-Bucket/208661
-
 impl Solution {
     pub fn find_min_difference(time_points: Vec<String>) -> i32 {
-        let mut mark = vec![false; 24 * 60];
-        let mut start = usize::MAX;
-        let mut end = usize::MIN;
-        for num in time_points.into_iter().map(time_to_num) {
-            let num = num as usize;
-            mark[num] = match mark[num] {
-                true => return 0,
-                false => {
-                    start = start.min(num);
-                    end = end.max(num);
-                    true
-                }
-            }
-        }
+        let mut time_points: Vec<i32> = time_points
+            .into_iter()
+            .map(|s| {
+                s.split(":").enumerate().fold(0, |acc, (i, t)| {
+                    acc + t.parse::<i32>().expect("Number") * (-59 * i as i32 + 60)
+                })
+            })
+            .collect();
+        time_points.sort_unstable();
+        time_points.push(time_points[0] + 60 * 24);
 
-        let mut ans = start + 60 * 24 - end;
-        let mut prev = start;
-        for i in start + 1..=end {
-            if mark[i] {
-                ans = ans.min(i - prev);
-                prev = i;
-            }
-        }
-
-        ans as i32
+        (1..time_points.len())
+            .map(|i| time_points[i] - time_points[i - 1])
+            .min()
+            .expect("Number") as i32
     }
-}
-
-fn time_to_num(time: String) -> i32 {
-    time.split(":").enumerate().fold(0, |acc, (i, t)| {
-        acc + t.parse::<i32>().unwrap() * (-59 * i as i32 + 60)
-    })
 }
