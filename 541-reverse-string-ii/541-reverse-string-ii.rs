acce5886f574d023951c5f50ed481441d338f134
子風 <zpsyhapcst@gmail.com>
2022-08-09 11:17:03 +0800

    Time: 2 ms (52.94%), Space: 2.1 MB (35.29%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.rs
+++ 541-reverse-string-ii/541-reverse-string-ii.rs
@@ -3,12 +3,12 @@ impl Solution {
         let k = k as usize;
         let s: Vec<char> = s.chars().collect();
 
-        let mut ans:Vec<&char> = Vec::with_capacity(s.len());
+        let mut ans = Vec::with_capacity(s.len());
         for (i, c) in s.chunks(k).enumerate() {
             if i % 2 == 0 {
-                ans.append(&mut c.into_iter().rev().collect());
+                ans.append(&mut c.iter().rev().collect());
             } else {
-                ans.append(&mut c.into_iter().collect());
+                ans.append(&mut c.iter().collect());
             }
         }
 
