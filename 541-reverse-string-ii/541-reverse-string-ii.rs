ed254b57be18c2cb9a587881c08238b9b69be2b7
子風 <zpsyhapcst@gmail.com>
2022-08-09 11:16:41 +0800

    Time: 3 ms (29.41%), Space: 2.4 MB (17.65%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.rs
+++ 541-reverse-string-ii/541-reverse-string-ii.rs
@@ -1,18 +1,17 @@
-// https://leetcode.com/problems/reverse-string-ii/discuss/848141/Rust-cheapest-and-best
-
 impl Solution {
     pub fn reverse_str(s: String, k: i32) -> String {
-        s.chars()
-            .collect::<Vec<char>>()
-            .chunks(k as usize)
-            .enumerate()
-            .map(|(i, chunk)| {
-                if i % 2 == 0 {
-                    chunk.iter().rev().collect::<String>()
-                } else {
-                    chunk.iter().collect()
-                }
-            })
-            .collect()
+        let k = k as usize;
+        let s: Vec<char> = s.chars().collect();
+
+        let mut ans:Vec<&char> = Vec::with_capacity(s.len());
+        for (i, c) in s.chunks(k).enumerate() {
+            if i % 2 == 0 {
+                ans.append(&mut c.into_iter().rev().collect());
+            } else {
+                ans.append(&mut c.into_iter().collect());
+            }
+        }
+
+        ans.into_iter().collect()
     }
 }
