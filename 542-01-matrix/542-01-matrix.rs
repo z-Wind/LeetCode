f593f5230698446b2b7d01a2f784cb02637442ae
子風 <zpsyhapcst@gmail.com>
2022-08-10 14:19:45 +0800

    Time: 20 ms (98.04%), Space: 2.7 MB (88.24%) - LeetHub

--- 542-01-matrix/542-01-matrix.rs
+++ 542-01-matrix/542-01-matrix.rs
@@ -1,41 +1,34 @@
-impl Solution {
-    pub fn update_matrix(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let m = mat.len();
-        let n = mat[0].len();
-        let mut dis_mat = vec![vec![-1; n]; m];
+// https://leetcode.com/problems/01-matrix/discuss/1369741/C%2B%2BJavaPython-BFS-DP-solutions-with-Picture-Clean-and-Concise-O(1)-Space
+// from zero cells
 
-        for i in 0..m {
-            for j in 0..n {
-                let mut dis = i32::MAX;
-                dis_nearest_zero(i, j, &mut dis_mat, &mat, 0, &mut dis);
-                dis_mat[i][j] = dis;
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn update_matrix(mut mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let (m, n) = (mat.len(), mat[0].len());
+
+        let mut deq = VecDeque::new();
+        for r in 0..m {
+            for c in 0..n {
+                if mat[r][c] == 0 {
+                    deq.push_back((r, c));
+                } else {
+                    mat[r][c] = -1; // Marked as not processed yet!
+                }
             }
         }
-
-        dis_mat
+        
+        let DIR = vec![0, 1, 0, -1, 0];
+        while let Some((r, c)) = deq.pop_front() {
+            for i in 0..4 {
+                let (nr, nc) = ((r as i32 + DIR[i]) as usize, (c as i32 + DIR[i + 1]) as usize);
+                if nr >= m || nc >= n || mat[nr][nc] != -1 {
+                    continue;
+                }
+                mat[nr][nc] = mat[r][c] + 1;
+                deq.push_back((nr, nc));
+            }
+        }
+        return mat;
     }
-}
-
-fn dis_nearest_zero(i: usize, j: usize, dis_mat: &mut Vec<Vec<i32>>, mat: &Vec<Vec<i32>>, tmp:i32, dis: &mut i32) {
-    let m = mat.len();
-    let n = mat[0].len();
-    
-    if tmp >= *dis || i >= m || j >= n || dis_mat[i][j] == -2 {
-        return;
-    } else if mat[i][j] == 0 {
-        *dis = (*dis).min(tmp);
-        return;
-    } else if dis_mat[i][j] >= 0 {
-        *dis = (*dis).min(tmp + dis_mat[i][j]);
-        return;
-    }
-    // tag visited
-    dis_mat[i][j] = -2;
-
-    dis_nearest_zero(i - 1, j, dis_mat, mat, tmp + mat[i][j], dis);
-    dis_nearest_zero(i, j - 1, dis_mat, mat, tmp + mat[i][j], dis);
-    dis_nearest_zero(i + 1, j, dis_mat, mat, tmp + mat[i][j], dis);
-    dis_nearest_zero(i, j + 1, dis_mat, mat, tmp + mat[i][j], dis);
-    
-    dis_mat[i][j] = -1;
 }
