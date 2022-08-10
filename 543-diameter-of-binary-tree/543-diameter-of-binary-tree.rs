de7f9759c49ac654338ebc0f898eabb8bd0c5d75
子風 <zpsyhapcst@gmail.com>
2022-08-10 15:35:57 +0800

    Time: 0 ms (100.00%), Space: 2.8 MB (10.64%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.rs
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.rs
@@ -16,34 +16,30 @@
 //     }
 //   }
 // }
+
+// https://leetcode.com/problems/diameter-of-binary-tree/discuss/101132/Java-Solution-MaxDepth
+
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         let mut diameter = 0;
-        length_node(root.as_ref(), &mut diameter);
+        max_depth(root.as_ref(), &mut diameter);
         diameter
     }
 }
 
-fn length_node(root: Option<&Rc<RefCell<TreeNode>>>, diameter: &mut i32) -> (i32, i32) {
+fn max_depth(root: Option<&Rc<RefCell<TreeNode>>>, diameter: &mut i32) -> i32 {
+    if root.is_none() {
+        return 0;
+    }
     let root = root.unwrap().borrow();
     let left = root.left.as_ref();
     let right = root.right.as_ref();
 
-    let left_len = if left.is_some() {
-        let (l, r) = length_node(left, diameter);
-        l.max(r) + 1
-    } else {
-        0
-    };
-    let right_len = if right.is_some() {
-        let (l, r) = length_node(right, diameter);
-        l.max(r) + 1
-    } else {
-        0
-    };
+    let left_len = max_depth(left, diameter);
+    let right_len = max_depth(right, diameter);
 
     *diameter = (*diameter).max(left_len + right_len);
-    (left_len, right_len)
+    left_len.max(right_len) + 1
 }
