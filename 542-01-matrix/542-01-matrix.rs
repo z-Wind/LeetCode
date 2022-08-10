472012d7accd448c773ad4cbc0058a1439c2772e
子風 <zpsyhapcst@gmail.com>
2022-08-10 14:39:04 +0800

    Time: 33 ms (62.74%), Space: 2.8 MB (70.59%) - LeetHub

--- 542-01-matrix/542-01-matrix.rs
+++ 542-01-matrix/542-01-matrix.rs
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/01-matrix/discuss/1369741/C%2B%2BJavaPython-BFS-DP-solutions-with-Picture-Clean-and-Concise-O(1)-Space
-// from zero cells
 
 use std::collections::VecDeque;
 
@@ -7,28 +6,26 @@ impl Solution {
     pub fn update_matrix(mut mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let (m, n) = (mat.len(), mat[0].len());
 
-        let mut deq = VecDeque::new();
         for r in 0..m {
             for c in 0..n {
-                if mat[r][c] == 0 {
-                    deq.push_back((r, c));
-                } else {
-                    mat[r][c] = -1; // Marked as not processed yet!
+                if mat[r][c] > 0 {
+                    let top = if r > 0 { mat[r - 1][c] } else { i32::MAX };
+                    let left = if c > 0 { mat[r][c - 1] } else { i32::MAX };
+                    mat[r][c] = top.min(left).saturating_add(1);
                 }
             }
         }
-        
-        let DIR = vec![0, 1, 0, -1, 0];
-        while let Some((r, c)) = deq.pop_front() {
-            for i in 0..4 {
-                let (nr, nc) = ((r as i32 + DIR[i]) as usize, (c as i32 + DIR[i + 1]) as usize);
-                if nr >= m || nc >= n || mat[nr][nc] != -1 {
-                    continue;
+
+        for r in (0..m).rev() {
+            for c in (0..n).rev() {
+                if mat[r][c] > 0 {
+                    let bottom = if r < m - 1 { mat[r + 1][c] } else { i32::MAX };
+                    let right = if c < n - 1 { mat[r][c + 1] } else { i32::MAX };
+                    mat[r][c] = mat[r][c].min(bottom.min(right).saturating_add(1));
                 }
-                mat[nr][nc] = mat[r][c] + 1;
-                deq.push_back((nr, nc));
             }
         }
+
         return mat;
     }
 }
