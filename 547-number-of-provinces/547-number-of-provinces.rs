88cf9342a4452d45245002a69fcc9d89757cecba
子風 <zpsyhapcst@gmail.com>
2022-08-29 10:51:59 +0800

    Time: 4 ms (78.08%), Space: 2.1 MB (82.19%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.rs
+++ 547-number-of-provinces/547-number-of-provinces.rs
@@ -1,36 +1,66 @@
-use std::collections::VecDeque;
+// https://leetcode.com/problems/number-of-provinces/discuss/101336/Java-solution-Union-Find/266043
+// Union Find
 
-impl Solution {
-    pub fn find_circle_num(mut is_connected: Vec<Vec<i32>>) -> i32 {
-        let n = is_connected.len();
-        let mut deq = VecDeque::new();
+struct UnionFind {
+    parent: Vec<usize>,
+    size: Vec<i32>,
+    count: i32,
+}
 
-        let mut ans = 0;
-        for i in 0..n {
-            // println!("start: {}", i);
-            if is_connected[i][i] == 0 {
-                continue;
-            }
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            size: vec![1; n],
+            count: n as i32,
+        }
+    }
 
-            is_connected[i][i] = 0;
-            deq.push_back(i);
-            while let Some(city) = deq.pop_front() {
-                // println!("connect: {}", city);
-                for j in i + 1..n {
-                    if is_connected[city][j] == 0 {
-                        continue;
-                    }
+    fn find(&mut self, mut p: usize) -> usize {
+        // path compression
+        while p != self.parent[p] {
+            self.parent[p] = self.parent[self.parent[p]];
+            p = self.parent[p];
+        }
+        return p;
+    }
 
-                    is_connected[city][j] = 0;
-                    is_connected[j][city] = 0;
-                    is_connected[j][j] = 0;
-                    deq.push_back(j);
-                }
-            }
-
-            ans += 1;
+    fn union(&mut self, p: usize, q: usize) {
+        let rootP = self.find(p);
+        let rootQ = self.find(q);
+        if rootP == rootQ {
+            return;
         }
 
-        ans
+        // union by size
+        if self.size[rootP] > self.size[rootQ] {
+            self.parent[rootQ] = rootP;
+            self.size[rootP] += self.size[rootQ];
+        } else {
+            self.parent[rootP] = rootQ;
+            self.size[rootQ] += self.size[rootP];
+        }
+        self.count -= 1;
+    }
+
+    fn count(&self) -> i32 {
+        self.count
+    }
+}
+
+impl Solution {
+    pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+        let n = is_connected.len();
+        let mut uf = UnionFind::new(n);
+
+        for i in 0..n {
+            for j in i + 1..n {
+                if is_connected[i][j] == 1 {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        uf.count
     }
 }
