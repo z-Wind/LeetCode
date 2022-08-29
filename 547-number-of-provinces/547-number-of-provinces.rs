42b4d6b3d6000d51f97c95ae854d5d0c8f671ed1
子風 <zpsyhapcst@gmail.com>
2022-08-29 10:04:05 +0800

    Time: 6 ms (46.58%), Space: 2.2 MB (82.19%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.rs
@@ -0,0 +1,36 @@
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn find_circle_num(mut is_connected: Vec<Vec<i32>>) -> i32 {
+        let n = is_connected.len();
+        let mut deq = VecDeque::new();
+        
+        let mut ans = 0;
+        for i in 0..n {
+            // println!("start: {}", i);
+            if is_connected[i][i] == 0 {
+                continue;
+            }
+            
+            is_connected[i][i] = 0;
+            deq.push_back(i);            
+            while let Some(city) = deq.pop_front() {
+                // println!("connect: {}", city);
+                for j in 0..n {
+                    if is_connected[city][j] == 0 {
+                        continue;
+                    }
+                    
+                    is_connected[city][j] = 0;
+                    is_connected[j][city] = 0;
+                    is_connected[j][j] = 0;
+                    deq.push_back(j);
+                }
+            }
+            
+            ans += 1;
+        }
+        
+        ans
+    }
+}
