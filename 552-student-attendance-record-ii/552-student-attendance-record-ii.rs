5d998acd01f35453f485ef54342705f0f23ce322
子風 <zpsyhapcst@gmail.com>
2022-08-30 10:42:39 +0800

    Time: 658 ms (20.00%), Space: 29.5 MB (20.00%) - LeetHub

--- 552-student-attendance-record-ii/552-student-attendance-record-ii.rs
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.rs
@@ -6,11 +6,11 @@ impl Solution {
     pub fn check_record(n: i32) -> i32 {
         let n = n as usize;
         let mut dp = vec![vec![vec![0; 2]; 3]; n + 1];
-        nums_possible_records(&mut dp, n, 0, 0) as i32
+        nums_possible_records(&mut dp, n, 0, 0)
     }
 }
 
-fn nums_possible_records(dp: &mut Vec<Vec<Vec<i64>>>, n: usize, L: usize, A: usize) -> i64 {
+fn nums_possible_records(dp: &mut Vec<Vec<Vec<i32>>>, n: usize, L: usize, A: usize) -> i32 {
     if n == 0 || L >= 3 || A >= 2 {
         return 0;
     } else if dp[n][L][A] > 0 {
@@ -21,14 +21,11 @@ fn nums_possible_records(dp: &mut Vec<Vec<Vec<i64>>>, n: usize, L: usize, A: usi
         let Lc = if L < 2 { 1 } else { 0 };
         let Ac = if A < 1 { 1 } else { 0 };
         1 + Lc + Ac
-    } else if L == 2 && A == 1 {
-        nums_possible_records(dp, n-1, 0, A)    // P
     } else {
-        nums_possible_records(dp, n-1, 0, A)    // P
-      + nums_possible_records(dp, n-1, L+1, A)  // L
-      + nums_possible_records(dp, n-1, 0, A+1)  // A
+        (((nums_possible_records(dp, n-1, 0, A) % 1_000_000_007    // P
+      + nums_possible_records(dp, n-1, L+1, A) % 1_000_000_007) % 1_000_000_007)  // L
+      + nums_possible_records(dp, n-1, 0, A+1)) % 1_000_000_007  // A
     };
 
-    dp[n][L][A] = dp[n][L][A] % 1_000_000_007;
     dp[n][L][A]
 }
