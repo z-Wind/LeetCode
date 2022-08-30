9fcf3bb8ee392920cbaf7d93d40661c50268c8d6
子風 <zpsyhapcst@gmail.com>
2022-08-30 10:03:53 +0800

    Time: 834 ms (20.00%), Space: 29.4 MB (20.00%) - LeetHub

---
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.rs
@@ -0,0 +1,32 @@
+// f(n, L, A) = P: f(n-1, 0, A) +
+//              L: f(n-1, L+1, A) +
+//              A: f(n-1, 0, A+1)
+
+impl Solution {
+    pub fn check_record(n: i32) -> i32 {
+        let n = n as usize;
+        let mut dp = vec![vec![vec![0; 2]; 3]; n + 1];
+        nums_possible_records(&mut dp, n, 0, 0) as i32
+    }
+}
+
+fn nums_possible_records(dp: &mut Vec<Vec<Vec<i64>>>, n: usize, L: usize, A: usize) -> i64 {
+    if n == 0 || L >= 3 || A >= 2 {
+        return 0;
+    } else if dp[n][L][A] > 0 {
+        return dp[n][L][A];
+    }
+
+    dp[n][L][A] = if n == 1 {
+        let Lc = if L < 2 { 1 } else { 0 };
+        let Ac = if A < 1 { 1 } else { 0 };
+        1 + Lc + Ac
+    } else {
+        nums_possible_records(dp, n-1, 0, A)    // P
+      + nums_possible_records(dp, n-1, L+1, A)  // L
+      + nums_possible_records(dp, n-1, 0, A+1)  // A
+    };
+
+    dp[n][L][A] = dp[n][L][A] % 1_000_000_007;
+    dp[n][L][A]
+}
