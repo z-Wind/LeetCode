29f546e6e7507969e85a19860f02bfac040ae844
子風 <zpsyhapcst@gmail.com>
2022-09-01 12:15:25 +0800

    Time: 11 ms (66.67%), Space: 3.2 MB (16.67%) - LeetHub

--- 554-brick-wall/554-brick-wall.rs
+++ 554-brick-wall/554-brick-wall.rs
@@ -4,7 +4,7 @@ impl Solution {
     pub fn least_bricks(wall: Vec<Vec<i32>>) -> i32 {
         let n = wall.len();
         let mut crosses = HashMap::new();
-        for row in &wall {
+        for row in wall {
             let mut edge = 0;
             for j in 0..row.len() - 1 {
                 edge += row[j];
@@ -15,6 +15,6 @@ impl Solution {
             }
         }
 
-        *crosses.values().min().unwrap_or(&(n as i32))
+        crosses.into_values().min().unwrap_or(n as i32)
     }
 }
