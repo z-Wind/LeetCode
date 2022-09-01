0b16b72b72523cc5bdec10e5ff53b258913b1f37
子風 <zpsyhapcst@gmail.com>
2022-09-01 16:01:51 +0800

    Time: 4 ms (63.83%), Space: 2.4 MB (44.68%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rs
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rs
@@ -1,8 +1,15 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/discuss/1640310/Rust-0ms-2.2MB
+// reverse the entire string: abc def ghi => ihg fed cba
+// reverse the order of the words: cba fed ihg
+
 impl Solution {
     pub fn reverse_words(s: String) -> String {
-        s.split_ascii_whitespace()
-            .map(|sub| sub.chars().rev().collect::<String>())
-            .collect::<Vec<String>>()
+        s.chars()
+            .rev()
+            .collect::<String>()
+            .split_whitespace()
+            .rev()
+            .collect::<Vec<_>>()
             .join(" ")
     }
 }
