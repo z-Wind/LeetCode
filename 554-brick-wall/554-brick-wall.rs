e85d7a7310024893ee112de5f4e4a95edef1dbe9
子風 <zpsyhapcst@gmail.com>
2022-09-01 10:44:24 +0800

    Time: 8 ms (83.33%), Space: 2.9 MB (100.00%) - LeetHub

--- 554-brick-wall/554-brick-wall.rs
+++ 554-brick-wall/554-brick-wall.rs
@@ -1,32 +1,21 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn least_bricks(wall: Vec<Vec<i32>>) -> i32 {
         let n = wall.len();
-        let mut lines = HashSet::new();
-        let mut edges: Vec<HashSet<i32>> = vec![HashSet::new(); n];
+        let width: i32 = wall[0].iter().sum();
+        let mut lines = HashMap::new();
         for i in 0..n {
             let mut edge = 0;
             for j in 0..wall[i].len() - 1 {
                 edge += wall[i][j];
-                edges[i].insert(edge);
-                lines.insert(edge);
+                lines.entry(edge).and_modify(|counter| *counter -= 1).or_insert(n as i32 - 1);
             }
-        }
-        // println!("{:?}", edges);
-
-        let mut ans = n as i32;
-        let width: i32 = wall[0].iter().sum();
-        for pos in lines {
-            let mut count = n as i32;
-            for i in 0..n {
-                if edges[i].contains(&pos) {
-                    count -= 1;
-                }
-            }
-            ans = ans.min(count);
         }
         
-        ans
+        if lines.is_empty() {
+            return n as i32;
+        }
+        lines.into_values().min().unwrap()
     }
 }
