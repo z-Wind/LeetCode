5cb410e0b8234e42c7fd6b94c341c3d482e783be
子風 <zpsyhapcst@gmail.com>
2022-09-01 15:52:45 +0800

    Time: 4 ms (63.83%), Space: 2.6 MB (5.32%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rs
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rs
@@ -1,12 +1,8 @@
 impl Solution {
     pub fn reverse_words(s: String) -> String {
-        let mut ans = String::new();
-        for sub in s.split_ascii_whitespace() {
-            ans.push_str(&sub.chars().rev().collect::<String>());
-            ans.push(' ');
-        }
-        ans.pop();
-        
-        ans
+        s.split_ascii_whitespace()
+            .map(|sub| sub.chars().rev().collect::<String>())
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 }
