34c1f9ad8fa42ec059ced0eb13293593bf3e28df
子風 <zpsyhapcst@gmail.com>
2022-09-01 10:59:16 +0800

    Time: 12 ms (50.00%), Space: 3.1 MB (16.67%) - LeetHub

--- 554-brick-wall/554-brick-wall.rs
+++ 554-brick-wall/554-brick-wall.rs
@@ -5,14 +5,14 @@ impl Solution {
         let n = wall.len();
         let mut lines = HashMap::new();
         for row in wall {
-            row[0..row.len() - 1].iter().fold(0, |mut edge, w| {
-                edge += w;
+            let mut edge = 0;
+            for j in 0..row.len() - 1 {
+                edge += row[j];
                 lines
                     .entry(edge)
                     .and_modify(|counter| *counter -= 1)
                     .or_insert(n as i32 - 1);
-                edge
-            });
+            }
         }
 
         if lines.is_empty() {
