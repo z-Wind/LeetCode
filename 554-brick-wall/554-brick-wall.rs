4aa097edc706b04f8dae93194edc397ad59639d9
子風 <zpsyhapcst@gmail.com>
2022-09-01 12:12:57 +0800

    Time: 9 ms (66.67%), Space: 3 MB (16.67%) - LeetHub

--- 554-brick-wall/554-brick-wall.rs
+++ 554-brick-wall/554-brick-wall.rs
@@ -3,20 +3,18 @@ use std::collections::HashMap;
 impl Solution {
     pub fn least_bricks(wall: Vec<Vec<i32>>) -> i32 {
         let n = wall.len();
-        let mut lines = HashMap::new();
-        let mut ans = n as i32;
-        for row in wall {
+        let mut crosses = HashMap::new();
+        for row in &wall {
             let mut edge = 0;
             for j in 0..row.len() - 1 {
                 edge += row[j];
-                lines
+                crosses
                     .entry(edge)
                     .and_modify(|counter| *counter -= 1)
                     .or_insert(n as i32 - 1);
-                ans = ans.min(lines[&edge]);
             }
         }
 
-        ans
+        crosses.into_values().min().unwrap_or(n as i32)
     }
 }
