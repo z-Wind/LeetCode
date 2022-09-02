816da298e9d72508a5831ea0279b7e0fc0b41788
子風 <zpsyhapcst@gmail.com>
2022-09-02 16:00:26 +0800

    Time: 7 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- 561-array-partition/561-array-partition.rs
+++ 561-array-partition/561-array-partition.rs
@@ -1,6 +1,31 @@
+// https://leetcode.com/problems/array-partition/discuss/102180/Java-O(n)-beats-100
+// Counting Sort
+
+const K: usize = 10000;
+
 impl Solution {
-    pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
-        nums.sort_unstable();
-        nums.iter().step_by(2).sum()
+    pub fn array_pair_sum(nums: Vec<i32>) -> i32 {
+        // Store the frequency of each element
+        let mut elementToCount = vec![0; 2 * K + 1];
+        for element in nums {
+            // Add K to element to offset negative values
+            elementToCount[element as usize + K] += 1;
+        }
+
+        // Initialize sum to zero
+        let mut maxSum: i32 = 0;
+        let mut isEvenIndex = true;
+        for element in 0..=2 * K {
+            while elementToCount[element] > 0 {
+                // Add element if it is at even position
+                maxSum += if isEvenIndex { (element - K) as i32 } else { 0 };
+                // Flip the value (one to zero or zero to one)
+                isEvenIndex = !isEvenIndex;
+                // Decrement the frequency count
+                elementToCount[element] -= 1;
+            }
+        }
+
+        maxSum
     }
 }
