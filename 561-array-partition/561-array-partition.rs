f709a80b58f7a27b7b9965c6db8fb623a89155fb
子風 <zpsyhapcst@gmail.com>
2022-09-02 15:51:59 +0800

    Time: 15 ms (78.26%), Space: 2.4 MB (63.04%) - LeetHub

--- 561-array-partition/561-array-partition.rs
+++ 561-array-partition/561-array-partition.rs
@@ -1,12 +1,6 @@
 impl Solution {
     pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
         nums.sort_unstable();
-        
-        let mut ans = 0;
-        for w in nums.chunks_exact(2) {
-            ans += w[0].min(w[1]);
-        }
-
-        ans
+        nums.iter().step_by(2).sum()
     }
 }
