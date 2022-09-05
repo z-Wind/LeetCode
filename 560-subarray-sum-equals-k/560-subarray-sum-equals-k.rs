2fec283ce098052db79608d42b3299aac1bb1e62
子風 <zpsyhapcst@gmail.com>
2022-09-05 15:22:00 +0800

    Time: 25 ms (45.24%), Space: 2.8 MB (57.14%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.rs
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.rs
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/discuss/1760146/C%2B%2B-Easy-solution-prefix-sum-map-easy-understanding-hashmap
 // sum[i..j] = sum[0..j] - sum[0..i]
 
 use std::collections::HashMap;
@@ -5,26 +6,18 @@ use std::collections::HashMap;
 impl Solution {
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
         let n = nums.len();
-        let mut sum = vec![0; n + 1];
-        let mut map: HashMap<i32, Vec<usize>> = HashMap::new();
-
-        for i in 1..=n {
-            sum[i] = sum[i - 1] + nums[i - 1];
-            map.entry(sum[i])
-                .and_modify(|x| x.push(i))
-                .or_insert(vec![i]);
-        }
-
         let mut ans = 0;
-        for i in 0..=n {
-            if let Some(v) = map.get(&(sum[i] + k)) {
-                match v.binary_search(&i) {
-                    Ok(n) => ans += (v.len() - 1 - n) as i32,
-                    Err(n) => ans += (v.len() - n) as i32,
-                }
-            }
-        }
+        let mut sum = 0;
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        map.insert(0, 1);
 
+        for num in nums {
+            sum += num;
+            if let Some(&freq) = map.get(&(sum - k)) {
+                ans += freq;
+            }
+            map.entry(sum).and_modify(|x| *x += 1).or_insert(1);
+        }
         ans
     }
 }
