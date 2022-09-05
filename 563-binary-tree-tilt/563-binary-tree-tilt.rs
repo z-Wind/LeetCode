7669966643e4b8ca45fc7e4a443ee2689541be40
子風 <zpsyhapcst@gmail.com>
2022-09-05 15:42:52 +0800

    Time: 4 ms (50.00%), Space: 2.8 MB (50.00%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.rs
+++ 563-binary-tree-tilt/563-binary-tree-tilt.rs
@@ -20,25 +20,22 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn find_tilt(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let (_, tilt) = find_tilt(root);
+        let (_, tilt) = find_tilt(&root);
         tilt
     }
 }
 
-fn find_tilt(root: Option<Rc<RefCell<TreeNode>>>) -> (i32, i32) {
-    if root.is_none() {
-        return (0, 0);
-    }
+fn find_tilt(node: &Option<Rc<RefCell<TreeNode>>>) -> (i32, i32) {
+    let node = match node {
+		Some(node) => node.borrow(),
+		None => return (0, 0),
+	};
 
-    let left = root.as_ref().unwrap().borrow().left.clone();
-    let (sum_left, tilt_left) = find_tilt(left);
+    let (sum_left, tilt_left) = find_tilt(&node.left);
+    let (sum_right, tilt_right) = find_tilt(&node.right);
 
-    let right = root.as_ref().unwrap().borrow().right.clone();
-    let (sum_right, tilt_right) = find_tilt(right);
-
-    let val = root.as_ref().unwrap().borrow().val;
     return (
-        val + sum_left + sum_right,
+        node.val + sum_left + sum_right,
         tilt_left + tilt_right + (sum_left - sum_right).abs(),
     );
 }
