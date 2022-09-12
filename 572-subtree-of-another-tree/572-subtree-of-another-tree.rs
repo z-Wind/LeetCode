02bbf914814fd442266e03c0dba1ab8ab521fe29
子風 <zpsyhapcst@gmail.com>
2022-09-12 13:19:53 +0800

    Time: 13 ms (20.90%), Space: 2.2 MB (71.64%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.rs
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.rs
@@ -26,15 +26,21 @@ impl Solution {
         root: Option<Rc<RefCell<TreeNode>>>,
         sub_root: Option<Rc<RefCell<TreeNode>>>,
     ) -> bool {
-        if root == sub_root {
-            return true;
-        }
-        if let Some(node) = root {
-            let node = node.borrow();
-            Self::is_subtree(node.left.clone(), sub_root.clone())
-                || Self::is_subtree(node.right.clone(), sub_root.clone())
-        } else {
-            return false;
-        }
+        is_subtree(&root, &sub_root)
+    }
+}
+
+fn is_subtree(
+    root: &Option<Rc<RefCell<TreeNode>>>,
+    sub_root: &Option<Rc<RefCell<TreeNode>>>,
+) -> bool {
+    if root == sub_root {
+        return true;
+    }
+    if let Some(node) = root {
+        let node = node.borrow();
+        is_subtree(&node.left, sub_root) || is_subtree(&node.right, sub_root)
+    } else {
+        return false;
     }
 }
