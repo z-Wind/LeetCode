d2d0ca3359af8e5ad9a09eb96c414705fa0ecbe9
子風 <zpsyhapcst@gmail.com>
2023-08-30 14:11:51 +0800

    Time: 23 ms (94.12%), Space: 2.4 MB (11.76%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.rs
+++ 0575-distribute-candies/0575-distribute-candies.rs
@@ -1,10 +1,9 @@
-use std::iter::FromIterator;
 use std::collections::HashSet;
 
 impl Solution {
     pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
         let n = candy_type.len();
-        let set:HashSet<i32> = HashSet::from_iter(candy_type);
+        let set:HashSet<i32> = candy_type.into_iter().collect();
         set.len().min(n/2) as i32
     }
 }
