38ec54b2244ba68578d94e1bf1528d7725d82bc1
子風 <zpsyhapcst@gmail.com>
2023-08-31 09:33:47 +0800

    Time: 0 ms (100%), Space: 2.7 MB (50%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.rs
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.rs
@@ -1,58 +1,37 @@
 impl Solution {
     pub fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32 {
-        if max_move == 0 {
-            return 0;
-        }
-
         let m = m as usize;
         let n = n as usize;
-        let o = max_move as usize + 1;
-
-        let mut dp = vec![vec![vec![0; n]; m]; o];
-        for i in 0..m {
-            dp[1][i][0] += 1;
-        }
-        for j in 0..n {
-            dp[1][0][j] += 1;
-        }
-        for i in 0..m {
-            dp[1][i][n - 1] += 1;
-        }
-        for j in 0..n {
-            dp[1][m - 1][j] += 1;
-        }
-
-        for k in 2..o {
-            for i in 0..m {
-                for j in 0..n {
-                    if i - 1 < m {
-                        dp[k][i][j] += dp[k - 1][i - 1][j];
-                        dp[k][i][j] %= 1_000_000_000 + 7;
-                    }
-                    if i + 1 < m {
-                        dp[k][i][j] += dp[k - 1][i + 1][j];
-                        dp[k][i][j] %= 1_000_000_000 + 7;
-                    }
-                    if j - 1 < n {
-                        dp[k][i][j] += dp[k - 1][i][j - 1];
-                        dp[k][i][j] %= 1_000_000_000 + 7;
-                    }
-                    if j + 1 < n {
-                        dp[k][i][j] += dp[k - 1][i][j + 1];
-                        dp[k][i][j] %= 1_000_000_000 + 7;
-                    }
-                }
-            }
-        }
-
-        //println!("{:?}", dp);
-        let mut sum = 0;
+        let k = max_move as usize;
         let i = start_row as usize;
         let j = start_column as usize;
-        for k in 0..o {
-            sum += dp[k][i][j];
-            sum %= 1_000_000_000 + 7;
-        }
-        sum
+
+        let mut dp = vec![vec![vec![-1; k + 1]; n]; m];
+        solve(&mut dp, m, n, i, j, k)
     }
+}
+
+fn solve(dp: &mut Vec<Vec<Vec<i32>>>, m: usize, n: usize, i: usize, j: usize, k: usize) -> i32 {
+    if i >= m || j >= n {
+        return 1;
+    }
+    if k == 0 {
+        return 0;
+    }
+
+    if dp[i][j][k] >= 0 {
+        return dp[i][j][k];
+    }
+
+    // mod 很重要，減少計算量，不然會 Time Limit Exceeded
+    let mut sum = solve(dp, m, n, i - 1, j, k - 1);
+    sum += solve(dp, m, n, i + 1, j, k - 1);
+    sum %= (1_000_000_000 + 7);
+    sum += solve(dp, m, n, i, j - 1, k - 1);
+    sum %= (1_000_000_000 + 7);
+    sum += solve(dp, m, n, i, j + 1, k - 1);
+    sum %= (1_000_000_000 + 7);
+
+    dp[i][j][k] = sum;
+    return sum;
 }
