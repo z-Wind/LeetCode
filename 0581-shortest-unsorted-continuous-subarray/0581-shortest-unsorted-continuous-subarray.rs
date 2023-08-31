7fcdb70d7fd39793bfef80c1214ddab1cafb27e8
子風 <zpsyhapcst@gmail.com>
2023-08-31 16:05:27 +0800

    Time: 3 ms (100%), Space: 2.1 MB (100%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.rs
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.rs
@@ -1,38 +1,40 @@
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray/solutions/1188866/rust-two-pass-time-o-n-space-o-1/
+
 impl Solution {
     pub fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
         let n = nums.len();
 
-        let mut left = nums
-            .windows(2)
-            .enumerate()
-            .find(|(_, w)| w[0] > w[1])
-            .map(|(i, _)| i as i32)
-            .unwrap_or(-1);
-        if left == -1 {
+        let (left, _) = nums.iter().enumerate().rev().fold(
+            (n - 1, i32::MAX),
+            |(mut left, mut min), (i, &num)| {
+                if num <= min {
+                    min = num;
+                } else {
+                    left = i;
+                }
+
+                (left, min)
+            },
+        );
+
+        if left == n - 1 {
             return 0;
         }
-        let minimum = nums[left as usize + 1..].iter().min().cloned().unwrap();
 
-        left = (0..left + 1)
-            .rev()
-            .find(|&i| nums[i as usize] <= minimum)
-            .unwrap_or(-1);
+        let (right, _) =
+            nums.iter()
+                .enumerate()
+                .fold((0, i32::MIN), |(mut right, mut max), (i, &num)| {
+                    if num >= max {
+                        max = num;
+                    } else {
+                        right = i;
+                    }
 
-        let mut right = nums
-            .windows(2)
-            .enumerate()
-            .rev()
-            .find(|(_, w)| w[0] > w[1])
-            .map(|(i, _)| i as i32)
-            .unwrap_or(-1);
-
-        let maxmum = nums[..=right as usize].iter().max().cloned().unwrap();
-
-        right = (right + 1..n as i32)
-            .find(|&i| nums[i as usize] >= maxmum)
-            .unwrap_or(n as i32);
+                    (right, max)
+                });
 
         // println!("{},{}", left, right);
-        right - left - 1
+        (right - left + 1) as i32
     }
 }
