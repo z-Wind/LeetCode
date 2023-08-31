9f475a6e16731d2a20a06c62a0be38ea0b232d0b
子風 <zpsyhapcst@gmail.com>
2023-08-31 15:02:56 +0800

    Time: 3 ms (100%), Space: 2.2 MB (100%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.rs
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.rs
@@ -1,28 +1,38 @@
 impl Solution {
     pub fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
-        let mut sorted = nums.clone();
-        sorted.sort_unstable();
+        let n = nums.len();
 
-        let mut left: i32 = -1;
-        let mut right: i32 = -1;
-
-        for (i, (x, y)) in nums.iter().zip(sorted.iter()).enumerate() {
-            if x != y {
-                left = i as i32;
-                break;
-            }
-        }
+        let mut left = nums
+            .windows(2)
+            .enumerate()
+            .find(|(_, w)| w[0] > w[1])
+            .map(|(i, _)| i as i32)
+            .unwrap_or(-1);
         if left == -1 {
             return 0;
         }
+        let minimum = nums[left as usize + 1..].iter().min().cloned().unwrap();
 
-        for (i, (x, y)) in nums.iter().zip(sorted.iter()).enumerate().rev() {
-            if x != y {
-                right = i as i32;
-                break;
-            }
-        }
+        left = (0..left + 1)
+            .rev()
+            .find(|&i| nums[i as usize] <= minimum)
+            .unwrap_or(-1);
 
-        right - left + 1
+        let mut right = nums
+            .windows(2)
+            .enumerate()
+            .rev()
+            .find(|(_, w)| w[0] > w[1])
+            .map(|(i, _)| i as i32)
+            .unwrap_or(-1);
+
+        let maxmum = nums[..=right as usize].iter().max().cloned().unwrap();
+
+        right = (right + 1..n as i32)
+            .find(|&i| nums[i as usize] >= maxmum)
+            .unwrap_or(n as i32);
+
+        // println!("{},{}", left, right);
+        right - left - 1
     }
 }
