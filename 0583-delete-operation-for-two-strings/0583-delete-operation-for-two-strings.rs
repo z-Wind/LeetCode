3e81f575f3a89a38f471654f0d6296e834134323
子風 <zpsyhapcst@gmail.com>
2023-09-01 14:04:55 +0800

    Time: 3 ms (100%), Space: 3 MB (14.29%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.rs
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.rs
@@ -1,32 +1,29 @@
+// https://leetcode.com/problems/delete-operation-for-two-strings/solutions/1195726/c-python-java-short-easy-solutions-w-explanation-optimization-from-brute-force-to-dp/
+
+// dp[i][j] will denote the number of steps required to equalize word1[:i] (characters from index 0 till i) and word2[:j] (characters from index 0 till j).
+
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
         let word1 = word1.as_bytes();
         let word2 = word2.as_bytes();
-        let mut dp = vec![vec![-1; word2.len()]; word1.len()];
+        let n = word1.len();
+        let m = word2.len();
+        let mut dp = vec![vec![0_i32; m + 1]; n + 1];
 
-        solve(&mut dp, 0, 0, word1, word2)
+        for i in 0..=n {
+            for j in 0..=m {
+                if i == 0 || j == 0 {
+                    dp[i][j] = (i + j) as i32;
+                } else {
+                    dp[i][j] = if word1[i - 1] == word2[j - 1] {
+                        dp[i - 1][j - 1]
+                    } else {
+                        (1 + dp[i - 1][j]).min(1 + dp[i][j - 1])
+                    };
+                }
+            }
+        }
+
+        dp[n][m]
     }
-}
-
-fn solve(dp: &mut Vec<Vec<i32>>, i: usize, j: usize, word1: &[u8], word2: &[u8]) -> i32 {
-    if i >= word1.len() && j >= word2.len() {
-        return 0;
-    } else if i >= word1.len() {
-        return (word2.len() - j) as i32;
-    } else if j >= word2.len() {
-        return (word1.len() - i) as i32;
-    } else if dp[i][j] != -1 {
-        return dp[i][j];
-    }
-
-    let step = if word1[i] == word2[j] {
-        solve(dp, i + 1, j + 1, word1, word2)
-    } else {
-        (1 + solve(dp, i, j + 1, word1, word2))
-            .min((1 + solve(dp, i + 1, j, word1, word2)))
-            .min((2 + solve(dp, i + 1, j + 1, word1, word2)))
-    };
-
-    dp[i][j] = step;
-    dp[i][j]
 }
