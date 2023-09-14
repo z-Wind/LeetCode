9d347125b8dd102a344c96a05a2f5716d8b7a469
子風 <zpsyhapcst@gmail.com>
2023-09-14 13:26:09 +0800

    Time: 1 ms (100%), Space: 2.1 MB (100%) - LeetHub

--- 0593-valid-square/0593-valid-square.rs
+++ 0593-valid-square/0593-valid-square.rs
@@ -1,17 +1,15 @@
 impl Solution {
     pub fn valid_square(p1: Vec<i32>, p2: Vec<i32>, p3: Vec<i32>, p4: Vec<i32>) -> bool {
-        let mut points = vec![
+        let mut points = [
             (&p2, len(&p1, &p2)),
             (&p3, len(&p1, &p3)),
             (&p4, len(&p1, &p4)),
         ];
         points.sort_unstable_by_key(|x| x.1);
-        points.dedup();
-        if points.len() < 3 {
-            return false;
-        }
 
-        points[0].1 == points[1].1
+        points[0].1 != 0
+            && points[0].1 == points[1].1
+            && points[1].1 != points[2].1
             && inner_product(&p1, points[0].0, points[1].0) == 0
             && inner_product(points[2].0, points[0].0, points[1].0) == 0
     }
