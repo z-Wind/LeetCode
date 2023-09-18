4f3433aa9b214e87a95273f23424cbfcc84b4220
子風 <zpsyhapcst@gmail.com>
2023-09-18 10:35:45 +0800

    Time: 9 ms (80%), Space: 2.4 MB (55%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.rs
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.rs
@@ -3,12 +3,14 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn find_restaurant(list1: Vec<String>, list2: Vec<String>) -> Vec<String> {
-        let map1: HashMap<String, usize> =
-            list1.into_iter().enumerate().map(|(i, s)| (s, i)).collect();
+        let map1: HashMap<String, usize> = list1.into_iter().zip(0..).collect();
 
         let mut result = Vec::new();
         let mut min_sum = usize::MAX;
         for (i, s) in list2.into_iter().enumerate() {
+            if i > min_sum {
+                break;
+            }
             if let Some(j) = map1.get(&s) {
                 let sum = i + j;
                 match sum.cmp(&min_sum) {
