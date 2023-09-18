2e37810331c4317054cf6f725140bb854268dd93
子風 <zpsyhapcst@gmail.com>
2023-09-18 09:27:25 +0800

    Time: 2 ms (25%), Space: 2.7 MB (37.5%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.rs
+++ 0598-range-addition-ii/0598-range-addition-ii.rs
@@ -1,9 +1,9 @@
 impl Solution {
     pub fn max_count(m: i32, n: i32, ops: Vec<Vec<i32>>) -> i32 {
-        let range = ops.iter().fold(vec![m, n], |acc, x| {
-            vec![acc[0].min(x[0]), acc[1].min(x[1])]
-        });
+        let range = ops
+            .iter()
+            .fold((m, n), |acc, x| (acc.0.min(x[0]), acc.1.min(x[1])));
 
-        range[0] * range[1]
+        range.0 * range.1
     }
 }
